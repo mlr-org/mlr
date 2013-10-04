c0ad7432a81066578e77bd76675e8d1c5bec163e
Jakob Bossek <info@jakobbossek.de>
2013-10-04 13:50:33 +0200

    uncomment some stuff in myex.R

--- mlrMBO/myex.R
+++ mlrMBO/myex.R
@@ -14,24 +14,24 @@ load_all("skel", reset=TRUE)
 configureMlr(show.learner.output=FALSE)
 
 # NUMERIC FUNCTION
-objfun = function(x) {
-  sin(x$x)
-}
+# objfun = function(x) {
+#   sin(x$x)
+# }
 
-ps = makeNumericParamSet(lower=3, upper=13, len=1)
+# ps = makeNumericParamSet(lower=3, upper=13, len=1)
 
-ctrl = makeMBOControl(init.design.points=6, iters=10, propose.points=1, 
-  infill.crit="ei", infill.opt="random", infill.opt.random.points=500)
+# ctrl = makeMBOControl(init.design.points=6, iters=10, propose.points=1, 
+#   infill.crit="ei", infill.opt="random", infill.opt.random.points=500)
 
-lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
+# lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
 
-run = exampleRun(objfun, ps, global.opt=-1, learner=lrn, 
-  control=ctrl, points.per.dim=100)
+# run = exampleRun(objfun, ps, global.opt=-1, learner=lrn, 
+#   control=ctrl, points.per.dim=100)
 
-print(run)
+# print(run)
 
-autoplot.MBOexampleRun(run, iters=3, densregion=TRUE)
-stop()
+# autoplot.MBOexampleRun(run, iters=3, densregion=TRUE)
+# stop()
 
 # NOISY DISCRETE FUNCTION
 # objfun = function(x) {
@@ -58,15 +58,15 @@ stop()
 
 # 2d NUMERIC FUNCTION
 
-# objfun = branin_function()
+objfun = branin_function()
 
-# ctrl = makeMBOControl(init.design.points=10, iters=10, propose.points=1, 
-#   infill.crit="ei", infill.opt="random", infill.opt.random.points=2000)
+ctrl = makeMBOControl(init.design.points=10, iters=10, propose.points=1, 
+  infill.crit="ei", infill.opt="random", infill.opt.random.points=2000)
 
-# lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
+lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
 
-# run = exampleRun(objfun, learner=lrn, control=ctrl, points.per.dim=50)
+run = exampleRun(objfun, learner=lrn, control=ctrl, points.per.dim=50)
 
-# print(run)
+print(run)
 
-#autoplot.MBOexampleRun(run, iters=3, pause=TRUE)
+pl = autoplot(run, iters=3, pause=TRUE)
