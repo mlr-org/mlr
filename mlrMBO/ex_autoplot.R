02de014f771b04cf9c763090a273796739274017
Jakob Bossek <info@jakobbossek.de>
2013-10-11 14:58:18 +0200

    examples for autoplot

--- mlrMBO/ex_autoplot.R
+++ mlrMBO/ex_autoplot.R
@@ -30,43 +30,44 @@ configureMlr(show.learner.output=FALSE)
 
 # print(run)
 
-# autoplot.MBOexampleRun(run, iters=3, densregion=TRUE)
+# autoplot(run, iters=8, densregion=TRUE)
 # stop()
 
 # NOISY DISCRETE FUNCTION
-# objfun = function(x) {
-# 	if (x$foo == "a") {
-# 		return(5 + rnorm(1))
-# 	} else if (x$foo == "b") {
-# 		return(4 + rnorm(1, sd=0.5))
-# 	} else {
-# 		return(3 + rnorm(1, sd=1))
-# 	}
-# }
+objfun = function(x) {
+	if (x$foo == "a") {
+		return(5 + rnorm(1))
+	} else if (x$foo == "b") {
+		return(4 + rnorm(1, sd=0.5))
+	} else {
+		return(3 + rnorm(1, sd=1))
+	}
+}
 
-# ps = makeParamSet(
-# 	makeDiscreteParam("foo", values = c("a", "b", "c"))
-# )
+ps = makeParamSet(
+	makeDiscreteParam("foo", values = c("a", "b", "c"))
+)
 
-# ctrl = makeMBOControl(init.design.points=20, iters=5, infill.opt.random.points=100, noisy=TRUE)
+ctrl = makeMBOControl(init.design.points=20, iters=5, infill.opt.random.points=100, noisy=TRUE)
 
-# run = exampleRun(objfun, par.set = ps, control=ctrl, points.per.dim=50)
+run = exampleRun(objfun, par.set = ps, control=ctrl, points.per.dim=50)
 
-# print(run)
+print(run)
 
-# autoplot.MBOexampleRun(run, iters=3)
+autoplot(run, iters=3)
+stop()
 
 # 2d NUMERIC FUNCTION
 
-objfun = branin_function()
+# objfun = generate_branin_function()
 
-ctrl = makeMBOControl(init.design.points=10, iters=10, propose.points=1, 
-  infill.crit="ei", infill.opt="random", infill.opt.random.points=2000)
+# ctrl = makeMBOControl(init.design.points=10, iters=10, propose.points=1, 
+#   infill.crit="ei", infill.opt="random", infill.opt.random.points=2000)
 
-lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
+# lrn = makeLearner("regr.km", predict.type="se", covtype="matern3_2")
 
-run = exampleRun(objfun, learner=lrn, control=ctrl, points.per.dim=50)
+# run = exampleRun(objfun, learner=lrn, control=ctrl, points.per.dim=50)
 
-print(run)
+# print(run)
 
 pl = autoplot(run, iters=3, pause=TRUE)
