e3c47223cdd81fb448f685773364d541f5750a36
Bernd Bischl <bernd_bischl@gmx.net>
2013-10-23 04:49:07 +0200

    restructure package

--- test_all.R
+++ test_all.R
@@ -11,13 +11,13 @@ library(MASS)
 library(ROCR)
 
 if (interactive()) {
-  load_all("skel")
+  load_all(".")
 } else {
   library(mlr)  
 }
 
-source("skel/inst/tests/helpers.R")
-source("skel/inst/tests/objects.R")
+source("inst/tests/helpers.R")
+source("inst/tests/objects.R")
 options(mlr.debug.seed=123L)
 configureMlr(show.learner.output=FALSE)
-test_dir("skel/inst/tests")
+test_dir("inst/tests")
