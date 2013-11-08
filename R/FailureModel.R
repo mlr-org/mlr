2ff9139ceb7fe468b19c73d5634ff3c19d9e0733
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-11-08 12:43:37 +0100

    clean up test

--- R/FailureModel.R
+++ R/FailureModel.R
@@ -1,14 +1,14 @@
 predict_nas = function(model, newdata) {
-	if (model$learner$type == "classif") {
+  if (model$learner$type == "classif") {
     levs = model$task.desc$class.levels
     if (model$learner$predict.type == "response")
       p = factor(rep(NA, nrow(newdata)), levels=levs)
     else
       p = matrix(as.numeric(NA), nrow=nrow(newdata), ncol=length(levs), dimnames=list(NULL, levs))
-	} else {
-		p = as.numeric(rep(NA, nrow(newdata)))
-	}
-	return(p)
+  } else {
+    p = as.numeric(rep(NA, nrow(newdata)))
+  }
+  return(p)
 }
 
 #' @S3method print FailureModel
--- inst/tests/test_FailureModel.R
+++ inst/tests/test_FailureModel.R
@@ -1,19 +1,19 @@
 context("FailureModel")
 
 test_that("FailureModel", {
-  configureMlr(on.learner.error="quiet")
-	m = train(makeLearner("classif.qda"), multiclass.task, subset=c(1,51,101))	
+  configureMlr(on.learner.error="quiet", show.learner.output=FALSE)
+  m = train(makeLearner("classif.qda"), multiclass.task, subset=c(1,51,101))	
   expect_true(inherits(m, "FailureModel"))
-	expect_true(!is.null(m$learner.model))
-	p = predict(m, newdata=iris)
-	expect_true(all(is.na(p$data$response)))
-	
-	
-	wl = makeLearner("regr.ksvm", epsilon=10)
-	m = train(wl, regr.task)	
+  expect_true(!is.null(m$learner.model))
+  p = predict(m, newdata=iris)
+  expect_true(all(is.na(p$data$response)))
+
+
+  wl = makeLearner("regr.ksvm", epsilon=10)
+  m = train(wl, regr.task)	
   expect_true(inherits(m, "FailureModel"))
-	expect_true(!is.null(m$learner.model))
-	p = predict(m, newdata=regr.df)
-	expect_true(all(is.na(p$data$response)))
-  configureMlr(on.learner.error="stop")
+  expect_true(!is.null(m$learner.model))
+  p = predict(m, newdata=regr.df)
+  expect_true(all(is.na(p$data$response)))
+  configureMlr(on.learner.error="stop", show.learner.output=FALSE)
 })
