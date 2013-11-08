4a2a9bdd3c2b781be6c669b224f215b12c5af507
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2013-11-08 14:46:24 +0100

    clean up test

--- inst/tests/test_learners.R
+++ inst/tests/test_learners.R
@@ -4,21 +4,21 @@ if (isExpensiveExampleOk()) {
 
 test_that("listLearners", {
   x1 = listLearners()
-  x2 = listLearners(type="classif") 
-  x3 = listLearners(type="regr") 
-	expect_true(length(x1) > 40)
-	expect_true(length(x2) > 10)
-	expect_true(length(x2) > 10)
+  x2 = listLearners(type="classif")
+  x3 = listLearners(type="regr")
+  expect_true(length(x1) > 40)
+  expect_true(length(x2) > 10)
+  expect_true(length(x2) > 10)
   expect_true(setequal(x1, union(x2, x3)))
-  
-  x4 = listLearners(type="classif", multiclass=TRUE, factors=TRUE, prob=TRUE) 
+
+  x4 = listLearners(type="classif", multiclass=TRUE, factors=TRUE, prob=TRUE)
   expect_true(length(x4) > 10 && all(x4 %in% x2))
 })
 
 test_that("listLearnersForTask", {
-  x1 = listLearnersForTask(task=binaryclass.task) 
-  x2 = listLearnersForTask(task=multiclass.task) 
-  x3 = listLearnersForTask(task=regr.task) 
+  x1 = listLearnersForTask(task=binaryclass.task)
+  x2 = listLearnersForTask(task=multiclass.task)
+  x3 = listLearnersForTask(task=regr.task)
   expect_true(length(x1) > 10)
   expect_true(length(x2) > 10)
   expect_true(length(x3) > 10)
@@ -29,9 +29,9 @@ test_that("listLearnersForTask", {
 
 test_that("learners work", {
   # binary classif
-  task = subsetTask(binaryclass.task, subset=c(1:50, 150:208), 
+  task = subsetTask(binaryclass.task, subset=c(1:50, 150:208),
     features=getTaskFeatureNames(binaryclass.task)[1:2])
-  lrns = listLearnersForTask(task=task) 
+  lrns = listLearnersForTask(task=task)
   lrns = lapply(lrns, makeLearner)
   lapply(lrns, function(lrn) {
     m = train(lrn, task)
@@ -39,9 +39,9 @@ test_that("learners work", {
   })
 
   # binary classif with prob
-  task = subsetTask(binaryclass.task, subset=c(1:50, 150:208), 
+  task = subsetTask(binaryclass.task, subset=c(1:50, 150:208),
     features=getTaskFeatureNames(binaryclass.task)[1:2])
-  lrns = listLearnersForTask(task=task, prob=TRUE) 
+  lrns = listLearnersForTask(task=task, prob=TRUE)
   lrns = lapply(lrns, makeLearner, predict.type="prob")
   lapply(lrns, function(lrn) {
     m = train(lrn, task)
@@ -52,17 +52,17 @@ test_that("learners work", {
   # binary classif with weights
   task = makeClassifTask(data=binaryclass.df, target=binaryclass.target)
   task = subsetTask(task, subset=c(1:50, 150:208), features=getTaskFeatureNames(task)[1:2])
-  lrns = listLearnersForTask(task=task, weights=TRUE) 
+  lrns = listLearnersForTask(task=task, weights=TRUE)
   lrns = lapply(lrns, makeLearner)
   lapply(lrns, function(lrn) {
     m = train(lrn, task, weights=1:task$task.desc$size)
     p = predict(m, task)
   })
-  
+
   # normal regr
   task = subsetTask(regr.task, subset=c(1:70),
     features=getTaskFeatureNames(regr.task)[1:2])
-  lrns = listLearnersForTask(task=task) 
+  lrns = listLearnersForTask(task=task)
   lrns = lapply(lrns, makeLearner)
   lapply(lrns, function(lrn) {
     if (lrn$id == "regr.km")
@@ -70,11 +70,11 @@ test_that("learners work", {
     m = train(lrn, task)
     p = predict(m, task)
   })
-  
+
   # regr with se
   task = subsetTask(regr.task, subset=c(1:70),
   features = getTaskFeatureNames(regr.task)[1:2])
-  lrns = listLearnersForTask(task=task, se=TRUE) 
+  lrns = listLearnersForTask(task=task, se=TRUE)
   lrns = lapply(lrns, makeLearner, predict.type="se")
   lapply(lrns, function(lrn) {
     if (lrn$id == "regr.km")
@@ -83,10 +83,10 @@ test_that("learners work", {
     p = predict(m, task)
     expect_equal(length(p$data$se), 70)
   })
-  
+
   # regr with weights
-  task = subsetTask(regr.task, subset=c(1:70), features=getTaskFeatureNames(regr.task)[1:2])
-  lrns = listLearnersForTask(task=task, weights=TRUE) 
+  task = subsetTask(regr.task, subset=1:70, features=getTaskFeatureNames(regr.task)[1:2])
+  lrns = listLearnersForTask(task=task, weights=TRUE)
   lrns = lapply(lrns, makeLearner)
   lapply(lrns, function(lrn) {
     if (lrn$id == "regr.km")
