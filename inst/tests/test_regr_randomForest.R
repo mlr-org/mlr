558ad68ef753204a097c687f87b153f644411776
Daniel Horn <daniel.horn@tu-dortmund.de>
2013-11-13 17:27:26 +0100

    unit test for regr.randomForest prediction type response

---
+++ inst/tests/test_regr_randomForest.R
@@ -0,0 +1,33 @@
+context("regr_randomForest")
+
+test_that("regr_randomForest", {
+  library(randomForest)
+  parset.list = list(
+    list(),
+    list(ntree=50,  mtry=2),
+    list(ntree=50, mtry=4),
+    list(ntree=200, mtry=2),
+    list(ntree=2000, mtry=4)
+  )
+  
+  old.predicts.list = list()
+  
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    pars = list(formula=regr.formula, data=regr.train)
+    pars = c(pars, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = do.call(randomForest, pars)
+    set.seed(getOption("mlr.debug.seed"))
+    p = predict(m, newdata=regr.test, type="response")
+    old.predicts.list[[i]] = p
+  }
+  
+  testSimpleParsets("regr.randomForest", regr.df, regr.target,
+                    regr.train.inds, old.predicts.list, parset.list)
+  
+  tt = randomForest
+  
+  testCVParsets("regr.randomForest", regr.df, regr.target, tune.train=tt, parset.list=parset.list)
+  
+})
