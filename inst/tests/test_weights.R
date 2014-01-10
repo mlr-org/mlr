c0ccfe5515f38ed09f86be04a8a8695748b76727
Jakob Richter <code@jakob-r.de>
2014-01-10 15:20:30 +0100

    test also weights passed to task

--- inst/tests/test_weights.R
+++ inst/tests/test_weights.R
@@ -3,24 +3,25 @@ context("weights")
 test_that("weights", {
   lrn = makeLearner("regr.lm")
   ws = 1:nrow(regr.df)
-	rt = makeRegrTask(target=regr.target, data=regr.df)
-	m = train(lrn, task=rt, weights=ws)
-	p = predict(m, task=rt, subset=30:100)
-	df = as.data.frame(p)
-	cns = colnames(df)
-	expect_equal(cns, c("id", "truth", "response"))
+  rt = makeRegrTask(target=regr.target, data=regr.df)
+  m = train(lrn, task=rt, weights=ws)
+  p = predict(m, task=rt, subset=30:100)
+  df = as.data.frame(p)
+  cns = colnames(df)
+  expect_equal(cns, c("id", "truth", "response"))
   
   #with weights in task
   rt_w = makeRegrTask(target=regr.target, data=regr.df, weights=ws)
   m_w = train(lrn, task=rt_w)
   p_w = predict(m_w, task=rt_w, subset=30:100)
   df_w = as.data.frame(p_w)
+  #FIXME: Better stopping message
   stopifnot(all(df_w$response == df$response))
 
-	# glm bug, we need do.call
-	m2 = do.call(lm, list(regr.formula, data=regr.df, weights=ws))
-	p2 = predict(m2, newdata=regr.df[30:100,])
-	expect_equal(p2, p$data$response, check.attributes=FALSE)
-	
-	expect_error(train(lrn, rger.task, weights=1:2))
+  # glm bug, we need do.call
+  m2 = do.call(lm, list(regr.formula, data=regr.df, weights=ws))
+  p2 = predict(m2, newdata=regr.df[30:100,])
+  expect_equal(p2, p$data$response, check.attributes=FALSE)
+  
+  expect_error(train(lrn, rger.task, weights=1:2))
 })
