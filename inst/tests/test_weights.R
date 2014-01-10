c203956c48232efb50eda3077defc1f3461a622e
Jakob Richter <code@jakob-r.de>
2014-01-10 14:05:24 +0100

    Testing for weights in makeRegrTask

--- inst/tests/test_weights.R
+++ inst/tests/test_weights.R
@@ -9,6 +9,13 @@ test_that("weights", {
 	df = as.data.frame(p)
 	cns = colnames(df)
 	expect_equal(cns, c("id", "truth", "response"))
+  
+  #with weights in task
+  rt_w = makeRegrTask(target=regr.target, data=regr.df, weights=ws)
+  m_w = train(lrn, task=rt_w)
+  p_w = predict(m_w, task=rt_w, subset=30:100)
+  df_w = as.data.frame(p_w)
+  stopifnot(all(df_w$response == df$response))
 
 	# glm bug, we need do.call
 	m2 = do.call(lm, list(regr.formula, data=regr.df, weights=ws))
