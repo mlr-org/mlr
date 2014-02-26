42528fb5a13f6bc8098a4b3011b58d805533ca21
Bernd Bischl <bernd_bischl@gmx.net>
2014-02-26 05:30:30 +0100

    clean up

--- R/tuneGrid.R
+++ R/tuneGrid.R
@@ -1,13 +1,13 @@
 tuneGrid = function(learner, task, resampling, measures, par.set, control, opt.path, show.info) {
   # drop names from par.set
-  vals = getValues(par.set) 
+  vals = getValues(par.set)
   inds = lapply(vals, seq_along)
   grid = expand.grid(inds)
   vals = lapply(seq_len(nrow(grid)), function(i) {
     val.inds = as.numeric(grid[i,])
     Map(function(v, j) v[[j]], vals, val.inds)
-  })  
-  evalOptimizationStatesTune(learner, task, resampling, measures, par.set, control, opt.path, 
+  })
+  evalOptimizationStatesTune(learner, task, resampling, measures, par.set, control, opt.path,
     show.info, vals, dobs=1L, eols=1L, remove.nas=FALSE)
 
   i = getOptPathBestIndex(opt.path, measureAggrName(measures[[1]]), ties="random")
