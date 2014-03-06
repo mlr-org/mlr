d8c9973d4bbb8e552e086cb9bc72883763e6ca6e
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-06 22:16:20 +0100

    clean up removeConstantFeature

--- R/removeConstantFeatures.R
+++ R/removeConstantFeatures.R
@@ -1,38 +1,46 @@
 #' Remove constant features from a data set.
-#' 
-#' Constant features can lead to errors in some models. Given a \code{data.frame}, this function
-#' removes all constant features. With the argument 'perc', there is a possibility to also remove
-#' features for whom less than 'perc' percent of the observations differ from the mode value.
-#' 
-#' @param x [\code{\link{data.frame}}] or [\code{\link{SupervisedTask}}] \cr
-#'   The data set or a task inlcuding it.
-#' @param perc [\code{numeric}]\cr
-#'   The percentage of a features' values (in [0, 1)) that must differ from the mode value. 
-#'   Default is 0, which means only constant features with exactly one observed level are removed. 
-#' @param safeCols [\code{character()}]\cr
-#'   Name(s) of the columns which must not be deleted. (E.g. the target column - automaticaly added when provided a \code{task})
+#'
+#' Constant features can lead to errors in some models and obviously provide
+#' no information in the training set that can be learned from.
+#' With the argument \dQuote{perc}, there is a possibility to also remove
+#' features for which less than \dQuote{perc} percent of the observations
+#' differ from the mode value.
+#'
+#' @param x [\code{\link{data.frame}} | \code{\link{SupervisedTask}}]\cr
+#'   The data set or task.
+#' @param perc [\code{numeric(1)}]\cr
+#'   The percentage of a feature values in [0, 1) that must differ from the mode value.
+#'   Default is 0, which means only constant features with exactly one observed level are removed.
+#' @param dont.rm [\code{character()}]\cr
+#'   Names of the columns which must not be deleted.
+#'   E.g. the target column - automatically added when provided a \code{task}.
 #' @param na.mode [\code{character}]\cr
 #' \itemize{
 #'   \item \code{ignore}: \code{NA}s will be ignored (lower value of differation)\cr
 #'   \item \code{factor}: \code{NA}s will be seen as own value (higher value of differation) \cr
 #'   \item \code{modus}: like factor but \code{NA}s can also be a modal value
-#' } 
-#' @return [\code{\link{data.frame}}] or [\code{\link{SupervisedTask}}]. The input \code{data.frame} or the task containing it without the columns containing the constant features.
+#' }
+#' @return [\code{\link{data.frame}} | \code{\link{SupervisedTask}}].
 #' @export
-removeConstantFeatures = function(x, perc = 0, safeCols = NULL, na.mode = "ignore", show.info = TRUE, printed.features = 10L) {
+removeConstantFeatures = function(x, perc = 0, dont.rm = character(0), na.mode = "ignore",
+  show.info = TRUE, printed.features = 10L) {
+
+  checkArg(perc, "numeric", lower=0, upper=1, na.ok=FALSE)
+  checkArg(dont.rm, "character", na.ok=FALSE)
+  checkArg(na.mode, choices=c("ignore", "factor", "modus"))
+  checkArg(show.info, "logical", len=1L, na.ok=FALSE)
   UseMethod("removeConstantFeatures")
-} 
+}
 
 #' @method removeConstantFeatures data.frame
 #' @S3method removeConstantFeatures data.frame
-removeConstantFeatures.data.frame = function(x, perc = 0, safeCols = NULL, na.mode = "ignore", show.info = TRUE, printed.features = 10L) {
-  checkArg(x, "data.frame")
-  checkArg(perc, "numeric", lower=0, upper=1)
-  stopifnot(perc!=1)
-  
-  if (ncol(x) == 0) 
+removeConstantFeatures.data.frame = function(x, perc = 0, dont.rm = character(0), na.mode = "ignore",
+  show.info = TRUE, printed.features = 10L) {
+
+
+  if (ncol(x) == 0)
     return(x)
-  
+
   myMode <- function(x) {
     #thanks to http://stackoverflow.com/questions/2547402/
     if(na.mode!="modus")
@@ -40,8 +48,8 @@ removeConstantFeatures.data.frame = function(x, perc = 0, safeCols = NULL, na.mo
     ux = unique(x)
     ux[which.max(tabulate(match(x, ux)))]
   }
-  
-  modi = lapply(x[, colnames(x) %nin% safeCols], myMode) #saves some calculations
+
+  modi = lapply(x[, colnames(x) %nin% dont.rm], myMode) #saves some calculations
   differ.ratio = sapply(names(modi), function(colName) {
     if(na.mode!="ignore")
       mean(!modi[[colName]] %equals% x[, colName])
@@ -50,11 +58,11 @@ removeConstantFeatures.data.frame = function(x, perc = 0, safeCols = NULL, na.mo
     }
   })
   select = differ.ratio > perc
-  select = colnames(x) %in% c(safeCols, names(select[select])) #preserves order
+  select = colnames(x) %in% c(dont.rm, names(select[select])) #preserves order
   if(show.info){
     delcols = head(sort(differ.ratio[!select]), printed.features)
-    messagef("Removing %i columns: %s", 
-             sum(!select), 
+    messagef("Removing %i columns: %s",
+             sum(!select),
              paste(names(delcols), round(delcols,2), collapse=", ", sep=": "))
   }
   x[, select, drop=FALSE]
@@ -62,10 +70,12 @@ removeConstantFeatures.data.frame = function(x, perc = 0, safeCols = NULL, na.mo
 
 #' @method removeConstantFeatures SupervisedTask
 #' @S3method removeConstantFeatures SupervisedTask
-removeConstantFeatures.SupervisedTask = function(x, perc = 0, safeCols = NULL, na.mode = "ignore", show.info = TRUE, printed.features = 10L) {
-  checkArg(x, "SupervisedTask")
-  safeCols = c(getTargetNames(x), safeCols)
-  res = removeConstantFeatures(x = getTaskData(x), perc = perc, safeCols = safeCols, na.mode = na.mode, show.info, printed.features)
+removeConstantFeatures.SupervisedTask = function(x, perc = 0, dont.rm = NULL, na.mode = "ignore",
+  show.info = TRUE, printed.features = 10L) {
+
+  dont.rm = c(getTargetNames(x), dont.rm)
+  res = removeConstantFeatures(x = getTaskData(x), perc = perc, dont.rm = dont.rm, na.mode = na.mode,
+    show.info, printed.features)
   changeData(task=x, data=res)
 }
 
--- inst/tests/test_removeConstantFeatures.R
+++ inst/tests/test_removeConstantFeatures.R
@@ -1,7 +1,8 @@
 context("removeConstantFeatures")
 
 test_that("removeConstantFeatures", {
-	testData = data.frame(
+
+  testData = data.frame(
     a = c(1L,2L),
     b = as.factor(1:2),
     c = c("a","b"),
@@ -9,26 +10,27 @@ test_that("removeConstantFeatures", {
     e = c(NA,1),
     f = c(1,NA),
     g = c(1,1),
-    target = as.factor(1:2))
+    target = as.factor(1:2)
+  )
   testData = testData[c(rep(1,9),2),]
   testData$safe = seq_row(testData)
-  
+
   testData_task = testData
   testData_task$d = as.factor(testData_task$d)
   task = makeClassifTask("test", data=testData_task, target="target")
-  
-  task_res = getTaskData(removeConstantFeatures(task, perc=0.1, safeCols="g"))
-  testData_res = removeConstantFeatures(testData, perc=0.1, safeCols=c("g","target"))
-	expect_equal(testData_res, task_res)
+
+  task_res = getTaskData(removeConstantFeatures(task, perc=0.1, dont.rm="g"))
+  testData_res = removeConstantFeatures(testData, perc=0.1, dont.rm=c("g","target"))
+  expect_equal(testData_res, task_res)
   expect_equal(colnames(testData_res), c("g", "target", "safe"))
-  
+
   testData_res = removeConstantFeatures(testData)
-	expect_equal(colnames(testData_res), c("a","b","c","d","target", "safe"))
-  
-	testData_res = removeConstantFeatures(testData, na.mode="factor", perc=0.2)
-	expect_equal(colnames(testData_res), c("e", "safe"))
-  
-	testData_res = removeConstantFeatures(testData, na.mode="modus", perc=0.2)
-	expect_equal(colnames(testData_res), "safe")
+  expect_equal(colnames(testData_res), c("a","b","c","d","target", "safe"))
+
+  testData_res = removeConstantFeatures(testData, na.mode="factor", perc=0.2)
+  expect_equal(colnames(testData_res), c("e", "safe"))
+
+  testData_res = removeConstantFeatures(testData, na.mode="modus", perc=0.2)
+  expect_equal(colnames(testData_res), "safe")
 })
 
