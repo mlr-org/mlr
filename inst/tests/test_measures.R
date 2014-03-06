a29793b69a6995095caf5c2a8d58461178d572c9
Bernd Bischl <bernd_bischl@gmx.net>
2014-03-06 15:49:24 +0100

    clean up

--- inst/tests/test_measures.R
+++ inst/tests/test_measures.R
@@ -1,30 +1,31 @@
 context("measures")
 
 test_that("measures", {
-	ct = binaryclass.task
-  
+  ct = binaryclass.task
+
   mymeasure = makeMeasure(id="foo", minimize=TRUE, classif=TRUE, regr=TRUE, allowed.pred.types=c("response", "prob"),
     fun=function(task, model, pred, extra.args) {
       tt = pred
       1
     }
   )
-	ms = list(mmce, acc, tp, fp, tn, fn, tpr, fpr, tnr, fnr, ppv, npv, mcc, f1, mymeasure)
-	
+  ms = list(mmce, acc, tp, fp, tn, fn, tpr, fpr, tnr, fnr, ppv, npv, mcc, f1, mymeasure)
+
   res = makeResampleDesc("CV", iters=3)
-	lrn = makeLearner("classif.rpart")
+  lrn = makeLearner("classif.rpart")
   mod = train(lrn, task=ct, subset=binaryclass.train.inds)
-	pred = predict(mod, task=ct, subset=binaryclass.test.inds)
+  pred = predict(mod, task=ct, subset=binaryclass.test.inds)
   perf = performance(pred, measures=ms)
-	
+
   r = resample(lrn, ct, res, measures=ms)
-	expect_equal(names(r$measures.test), c("iter", "mmce", "acc", "tp", "fp", "tn", "fn", "tpr", "fpr", "tnr", "fnr", "ppv", "npv", "mcc", "f1", "foo"))
-  
+  expect_equal(names(r$measures.test),
+    c("iter", "mmce", "acc", "tp", "fp", "tn", "fn", "tpr", "fpr", "tnr", "fnr", "ppv", "npv", "mcc", "f1", "foo"))
+
   #Test multiclass auc
   lrn = makeLearner("classif.randomForest",predict.type="prob")
   mod = train(lrn, task=multiclass.task, subset=multiclass.train.inds)
   pred = predict(mod, task=multiclass.task, subset=multiclass.test.inds)
-	perf = performance(pred, measures=multiclass.auc)
+  perf = performance(pred, measures=multiclass.auc)
   expect_true(is.numeric(perf))
-  
+
 })
