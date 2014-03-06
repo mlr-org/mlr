62b0d7c7e0409be62f397f9043662ec4020f4dc3
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-06 22:55:12 +0100

    clean up

--- inst/tests/test_helpers.R
+++ inst/tests/test_helpers.R
@@ -1,12 +1,18 @@
 context("helpers")
 
+if (interactive()) {
+
 test_that("makeOptPathDFFromMeasures", {
-  measures = list(mae,auc)
-  par.set = makeParamSet(makeIntegerParam("a"),
-                         makeIntegerParam("b"))
+  measures = list(mae, auc)
+  par.set = makeParamSet(
+    makeIntegerParam("a"),
+    makeIntegerParam("b")
+  )
   opt.path = makeOptPathDFFromMeasures(par.set, measures)
-  addOptPathEl(opt.path, x=list(1L,1L), y=c(0.5, 0.5))
-  res = getOptPathEl(opt.path,1L)
-  supposed.names = sapply(measures, function(m) paste(m$id,m$aggr$id,sep="."))
+  addOptPathEl(opt.path, x=list(1L, 1L), y=c(0.5, 0.5))
+  res = getOptPathEl(opt.path, 1L)
+  supposed.names = sapply(measures, function(m) paste(m$id, m$aggr$id, sep="."))
   expect_equal(names(res$y), supposed.names)
 })
+
+}
