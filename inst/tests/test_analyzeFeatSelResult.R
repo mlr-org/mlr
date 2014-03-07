45d065c8efe7b3cf1b14519b5e893202bb4d79e2
Jakob Richter <code@jakob-r.de>
2014-03-07 14:29:34 +0100

    Test for analyzeFeatSelResult

---
+++ inst/tests/test_analyzeFeatSelResult.R
@@ -0,0 +1,9 @@
+context("analyzeFeatSelResult")
+
+test_that("analyzeFeatSelResult", {
+  lrn = makeLearner("classif.rpart")
+  rdesc = makeResampleDesc("Holdout")
+  ctrlSeq = makeFeatSelControlSequential(method = "sfs")
+  sfSeq = selectFeatures(learner = lrn, task = multiclass.task, resampling = rdesc, control = ctrlSeq, show.info=FALSE)
+  expect_output(analyzeFeatSelResult(sfSeq, reduce = TRUE), "Petal.Width")
+})
