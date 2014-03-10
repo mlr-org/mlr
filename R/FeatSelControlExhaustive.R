d2ec84a9f96ecc2a7570145416772a6facbb388c
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-03-10 21:07:31 +0100

    clean up

--- R/FeatSelControlExhaustive.R
+++ R/FeatSelControlExhaustive.R
@@ -2,6 +2,7 @@
 #' @rdname FeatSelControl
 makeFeatSelControlExhaustive = function(same.resampling.instance=TRUE,
   maxit=NA_integer_, max.features=NA_integer_) {
+
   makeFeatSelControl(same.resampling.instance=same.resampling.instance,
     maxit=maxit, max.features=max.features, cl="FeatSelControlExhaustive")
 }
