5b0c06ceea5a256934d2a0ecac785b86f214bec1
Bernd Bischl <you@example.com>
2014-04-16 15:49:00 +0200

    clean up

--- inst/tests/test_classif_ctree.R
+++ inst/tests/test_classif_ctree.R
@@ -2,41 +2,41 @@ context("classif_ctree")
 
 test_that("classif_ctree", {
   library(party)
-	parset.list = list(
-			list(),
-			list(minsplit=10, mincriterion= 0.005),
-			list(minsplit=50, mincriterion=0.05),
-			list(minsplit=50, mincriterion=0.999),
-			list(minsplit=1, mincriterion=0.0005)
-	)
-	
-	old.predicts.list = list()
-	old.probs.list = list()
-	
-	for (i in 1:length(parset.list)) {
-		parset = parset.list[[i]]
-		ctrl = do.call(ctree_control, parset)
-		set.seed(getOption("mlr.debug.seed"))
-		m = ctree(formula=multiclass.formula, data=multiclass.train, control=ctrl)
-		p  = predict(m, newdata=multiclass.test, type="response")
-		p2 = Reduce(rbind, treeresponse(m, newdata=multiclass.test, type="prob"))
-		rownames(p2) = NULL
-		colnames(p2) = levels(multiclass.df[,multiclass.target])
-		old.predicts.list[[i]] = p
-		old.probs.list[[i]] = p2
-	}
-	
-	testSimpleParsets("classif.ctree", multiclass.df, multiclass.target, multiclass.train.inds, old.predicts.list, parset.list)
-	testProbParsets  ("classif.ctree", multiclass.df, multiclass.target, multiclass.train.inds, old.probs.list, parset.list)
-	
-	df = iris
-	df[,1] = 1:150
-	df1 = df[seq(1,150,2), ]
-	df2 = df[seq(2,150,2), ]
-	ct = makeClassifTask(target="Species", data=df1)
-	m = train(makeLearner("classif.ctree"), ct)
-	predict(m, newdata=df2)
-	
+  parset.list = list(
+    list(),
+    list(minsplit=10, mincriterion= 0.005),
+    list(minsplit=50, mincriterion=0.05),
+    list(minsplit=50, mincriterion=0.999),
+    list(minsplit=1, mincriterion=0.0005)
+    )
+
+  old.predicts.list = list()
+  old.probs.list = list()
+
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    ctrl = do.call(ctree_control, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = ctree(formula=multiclass.formula, data=multiclass.train, control=ctrl)
+    p  = predict(m, newdata=multiclass.test, type="response")
+    p2 = Reduce(rbind, treeresponse(m, newdata=multiclass.test, type="prob"))
+    rownames(p2) = NULL
+    colnames(p2) = levels(multiclass.df[,multiclass.target])
+    old.predicts.list[[i]] = p
+    old.probs.list[[i]] = p2
+  }
+
+  testSimpleParsets("classif.ctree", multiclass.df, multiclass.target, multiclass.train.inds, old.predicts.list, parset.list)
+  testProbParsets  ("classif.ctree", multiclass.df, multiclass.target, multiclass.train.inds, old.probs.list, parset.list)
+
+  df = iris
+  df[,1] = 1:150
+  df1 = df[seq(1,150,2), ]
+  df2 = df[seq(2,150,2), ]
+  ct = makeClassifTask(target="Species", data=df1)
+  m = train(makeLearner("classif.ctree"), ct)
+  predict(m, newdata=df2)
+
 })
 
 
