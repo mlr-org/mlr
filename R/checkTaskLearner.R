9d459f36d7fd754b77dda084f5f9e53d0c58200d
Bernd Bischl <you@example.com>
2014-04-16 18:11:40 +0200

    refactoring tasks and taskdescs and check functions

--- R/checkTaskLearner.R
+++ R/checkTaskLearner.R
@@ -10,9 +10,8 @@ checkTaskLearner = function(task, learner, weights) {
     stopf("Data set has factor inputs, but learner %s does not support that!", td$id, learner$id)
   if (td$type == "classif" && length(td$class.levels)> 2L && !learner$multiclass)
     stopf("Task %s is a multiclass-problem, but learner %s does not support that!", td$id, learner$id)
-  if (!(missing(weights) || is.null(weights)) && !learner$weights) {
+  if (!(missing(weights) || is.null(weights)) && !learner$weights)
     stopf("Weights vector passed to train, but learner %s does not support that!", learner$id)
-  }
   if (td$has.weights && !learner$weights)
     warning("Task contains weights but these are not used by learner %s !", learner$id)
 }
