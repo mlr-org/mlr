fd553bb9b5593315fe9c04545a00c17e055c1629
Bernd Bischl <bernd_bischl@gmx.net>
2014-04-29 01:38:59 +0200

    clean up

--- inst/tests/test_analyzeFeatSelResult.R
+++ inst/tests/test_analyzeFeatSelResult.R
@@ -4,6 +4,7 @@ test_that("analyzeFeatSelResult", {
   lrn = makeLearner("classif.rpart")
   rdesc = makeResampleDesc("Holdout")
   ctrlSeq = makeFeatSelControlSequential(method = "sfs")
-  sfSeq = selectFeatures(learner = lrn, task = multiclass.task, resampling = rdesc, control = ctrlSeq, show.info=FALSE)
+  sfSeq = selectFeatures(learner = lrn, task = multiclass.task, resampling = rdesc,
+    control = ctrlSeq, show.info = FALSE)
   expect_output(analyzeFeatSelResult(sfSeq, reduce = TRUE), "Petal.Width")
 })
