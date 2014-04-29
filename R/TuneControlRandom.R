09d3d653afa382b39cb82352ccf3e2e3faa9c40c
Bernd Bischl <bernd_bischl@gmx.net>
2014-04-29 03:43:44 +0200

    clean up

--- R/TuneControlRandom.R
+++ R/TuneControlRandom.R
@@ -1,8 +1,8 @@
 #' @export
 #' @rdname TuneControl
-makeTuneControlRandom = function(same.resampling.instance=TRUE, maxit=100L) {
+makeTuneControlRandom = function(same.resampling.instance = TRUE, maxit = 100L) {
   maxit = convertInteger(maxit)
-  checkArg(maxit, "integer", len=1L, na.ok=FALSE)
-  makeTuneControl(same.resampling.instance=same.resampling.instance, maxit=maxit,
-                  start=list(), cl="TuneControlRandom")
+  checkArg(maxit, "integer", len = 1L, na.ok = FALSE)
+  makeTuneControl(same.resampling.instance = same.resampling.instance, maxit = maxit,
+    start = list(), cl = "TuneControlRandom")
 }
