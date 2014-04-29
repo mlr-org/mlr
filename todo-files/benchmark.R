b97014d5aa7af68c28aad21e8ecc212b3500131f
Bernd Bischl <you@example.com>
2014-04-29 14:30:29 +0200

    clean up

--- todo-files/benchmark.R
+++ todo-files/benchmark.R
@@ -20,8 +20,8 @@
 #'   Default is \code{TRUE}.
 #' @return [\code{list}].
 #' @export
-benchmark = function(learners, tasks, resamplings, measures, same.resampling.instance=TRUE) {
-  
+benchmark = function(learners, tasks, resamplings, measures, same.resampling.instance = TRUE) {
+
   # check learners
   if (inherits(learners, "Learner"))
     learners = list(learners)
@@ -41,7 +41,7 @@ benchmark = function(learners, tasks, resamplings, measures, same.resampling.ins
   checkListElementClass(tasks, "SupervisedTask")
   if (!length(tasks))
     stop("No tasks were passed!")
-  
+
   task.ids = extractSubList(tasks,"task.desc")["id",]
   if (any(duplicated(task.ids)))
     stop("Tasks need unique ids!")
@@ -49,9 +49,9 @@ benchmark = function(learners, tasks, resamplings, measures, same.resampling.ins
 
   # check resamplings
   if (missing(resamplings))
-     resamplings = makeResampleDesc("CV", iters=10L)
+     resamplings = makeResampleDesc("CV", iters = 10L)
   if (inherits(resamplings, "ResampleInstance") || inherits(resamplings, "ResampleDesc"))
-    resamplings = replicate(length(tasks), resamplings, simplify=FALSE)
+    resamplings = replicate(length(tasks), resamplings, simplify = FALSE)
   if (length(resamplings) != length(tasks))
     stop("Number of resampling strategies and number of tasks differ!")
   names(resamplings) = task.ids
@@ -65,34 +65,34 @@ benchmark = function(learners, tasks, resamplings, measures, same.resampling.ins
   }
 
   # check rest
-  checkArg(same.resampling.instance, "logical", len=1L, na.ok=FALSE)
-  
+  checkArg(same.resampling.instance, "logical", len = 1L, na.ok = FALSE)
+
   measure.ids = extractSubList(measures, "id")
-  
+
   # instantiate resampling
   if (same.resampling.instance) {
     resamplings = Map(function(res, tt) {
       if (is(res, "ResampleInstance"))
         res
       else
-        makeResampleInstance(res,task=tt)
+        makeResampleInstance(res,task = tt)
     }, resamplings, tasks)
   }
 
-  inds.mat = as.matrix(expand.grid(learner=learner.ids, task=task.ids))
-  inds = split(inds.mat, f=seq_row(inds.mat))
-  more.args = list(learners=learners, tasks=tasks, resamplings=resamplings, measures=measures)
-  results = parallelMap(benchmarkParallel, inds, more.args=more.args)
-  res.measures = extractSubList(results, "aggr", simplify=FALSE)
+  inds.mat = as.matrix(expand.grid(learner = learner.ids, task = task.ids))
+  inds = split(inds.mat, f = seq_row(inds.mat))
+  more.args = list(learners = learners, tasks = tasks, resamplings = resamplings, measures = measures)
+  results = parallelMap(benchmarkParallel, inds, more.args = more.args)
+  res.measures = extractSubList(results, "aggr", simplify = FALSE)
   res.measures = do.call(rbind,res.measures)
   res.df = cbind.data.frame(inds.mat, res.measures)
-  
-  list(result.df = res.df, results=results)
+
+  list(result.df = res.df, results = results)
 }
 
 benchmarkParallel = function(index, learners, tasks, resamplings, measures) {
   ind.learner = index[1L][[1]]
   ind.task = index[2L][[1]]
   messagef("Task: %s, Learner: %s", ind.task, ind.learner)
-  resample(learners[[ind.learner]], tasks[[ind.task]], resamplings[[ind.task]], measures=measures)
+  resample(learners[[ind.learner]], tasks[[ind.task]], resamplings[[ind.task]], measures = measures)
 }
