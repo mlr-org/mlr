60c2f88085f7c55b8743e2a66e50ee6025f89970
Bernd Bischl <you@example.com>
2014-04-29 14:59:56 +0200

    clean up

--- R/RLearner_classif_plr.R
+++ R/RLearner_classif_plr.R
@@ -4,8 +4,8 @@ makeRLearner.classif.plr = function() {
     cl = "classif.plr",
     package = "stepPlr",
     par.set = makeParamSet(
-      makeNumericLearnerParam(id="lambda", default=1e-4, lower=0),
-      makeNumericLearnerParam(id="cp", default=2)
+      makeNumericLearnerParam(id = "lambda", default = 1e-4, lower = 0),
+      makeNumericLearnerParam(id = "cp", default = 2)
     ),
     twoclass = TRUE,
     multiclass = FALSE,
@@ -19,20 +19,20 @@ makeRLearner.classif.plr = function() {
 
 #' @S3method trainLearner classif.plr
 trainLearner.classif.plr = function(.learner, .task, .subset, .weights,  ...) {
-  d = getTaskData(.task, .subset, target.extra=TRUE, recode.target="01")
+  d = getTaskData(.task, .subset, target.extra = TRUE, recode.target = "01")
   if (missing(.weights)) {
-    plr(x=d$data, y=d$target, ...)
+    plr(x = d$data, y = d$target, ...)
   } else {
-    plr(x=d$data, y=d$target, weights=.weights, ...)
+    plr(x = d$data, y = d$target, weights = .weights, ...)
   }
 }
 
 #' @S3method predictLearner classif.plr
 predictLearner.classif.plr = function(.learner, .model, .newdata, ...) {
-  p = predict(.model$learner.model, newx=.newdata, type="response", ...)
+  p = predict(.model$learner.model, newx = .newdata, type = "response", ...)
   levs = c(.model$task.desc$negative, .model$task.desc$positive)
   if(.learner$predict.type == "prob"){
-    y = matrix(0, ncol=2, nrow=nrow(.newdata))
+    y = matrix(0, ncol = 2, nrow = nrow(.newdata))
     colnames(y) = levs
     y[,1L] = 1-p
     y[,2L] = p
