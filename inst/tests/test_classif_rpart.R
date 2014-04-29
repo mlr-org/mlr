23e5f95afd61ed4de51c65b943afb8cc74ee90a2
Bernd Bischl <you@example.com>
2014-04-29 15:07:31 +0200

    clean up

--- inst/tests/test_classif_rpart.R
+++ inst/tests/test_classif_rpart.R
@@ -4,34 +4,34 @@ test_that("classif_rpart", {
   library(rpart)
   parset.list = list(
     list(),
-    list(minsplit=10, cp= 0.005),
-    list(minsplit=50, cp=0.05),
-    list(minsplit=50, cp=0.999),
-    list(minsplit=1, cp=0.0005)
+    list(minsplit = 10, cp= 0.005),
+    list(minsplit = 50, cp = 0.05),
+    list(minsplit = 50, cp = 0.999),
+    list(minsplit = 1, cp = 0.0005)
   )
 
   old.predicts.list = list()
   old.probs.list = list()
-  
+
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    pars = list(formula=multiclass.formula, data=multiclass.train)
+    pars = list(formula = multiclass.formula, data = multiclass.train)
     pars = c(pars, parset)
     set.seed(getOption("mlr.debug.seed"))
     m = do.call(rpart, pars)
-    p  = predict(m, newdata=multiclass.test, type="class")
-	p2 = predict(m, newdata=multiclass.test, type="prob")
+    p  = predict(m, newdata = multiclass.test, type = "class")
+	p2 = predict(m, newdata = multiclass.test, type = "prob")
 	old.predicts.list[[i]] = p
 	old.probs.list[[i]] = p2
-}
+  }
 
   testSimpleParsets("classif.rpart", multiclass.df, multiclass.target, multiclass.train.inds, old.predicts.list, parset.list)
   testProbParsets  ("classif.rpart", multiclass.df, multiclass.target, multiclass.train.inds, old.probs.list, parset.list)
 
   tt = "rpart"
-  tp = function(model, newdata) predict(model, newdata, type="class")
+  tp = function(model, newdata) predict(model, newdata, type = "class")
 
-  testCVParsets("classif.rpart", multiclass.df, multiclass.target, tune.train=tt, tune.predict=tp, parset.list=parset.list)
+  testCVParsets("classif.rpart", multiclass.df, multiclass.target, tune.train = tt, tune.predict = tp, parset.list = parset.list)
 
 })
 
