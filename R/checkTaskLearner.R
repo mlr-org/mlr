3a7c9201dbe00aa51d34b5e8c21ae77bd0a21e6c
Jakob Richter <code@jakob-r.de>
2014-05-10 15:10:30 +0200

    checkTaskLearner is now more informative

--- R/checkTaskLearner.R
+++ R/checkTaskLearner.R
@@ -2,12 +2,18 @@ checkTaskLearner = function(task, learner, weights) {
   td = task$task.desc
   if (td$type != learner$type)
     stopf("Task %s is %s, but learner %s is for %s!", td$id, td$type, learner$id, learner$type)
-  if (td$has.missings && !learner$missings)
-    stopf("Task %s has missing values, but learner %s does not support that!", td$id, learner$id)
-  if (td$n.feat["numerics"] > 0L && !learner$numerics)
-    stopf("Task %s has numeric inputs, but learner %s does not support that!", td$id, learner$id)
-  if (td$n.feat["factors"] > 0L && !learner$factors)
-    stopf("Data set has factor inputs, but learner %s does not support that!", td$id, learner$id)
+  if (td$has.missings && !learner$missings) {
+    wrong.cols = columNamesOf(task, is.na)
+    stopf("Task %s has missing values in %s, but learner %s does not support that!", td$id, wrong.cols, learner$id)
+  }
+  if (td$n.feat["numerics"] > 0L && !learner$numerics) {
+    wrong.cols = columNamesOf(task, is.numeric)
+    stopf("Task %s has numeric inputs in %s, but learner %s does not support that!", td$id, wrong.cols, learner$id)
+  } 
+  if (td$n.feat["factors"] > 0L && !learner$factors) {
+    wrong.cols = columNamesOf(task, is.factor)
+    stopf("Data set has factor inputs in %s, but learner %s does not support that!", td$id, wrong.cols, learner$id)
+  }
   if (td$type == "classif" && length(td$class.levels)> 2L && !learner$multiclass)
     stopf("Task %s is a multiclass-problem, but learner %s does not support that!", td$id, learner$id)
   if (!(missing(weights) || is.null(weights)) && !learner$weights)
@@ -16,3 +22,8 @@ checkTaskLearner = function(task, learner, weights) {
     warning("Task contains weights but these are not used by learner %s !", learner$id)
 }
 
+columNamesOf =  function(task, findFunction){
+  .data = getTaskData(task)
+  has.it = apply(.data, 2, function(x) any(findFunction(x)))
+  paste(colnames(.data)[has.it], collapse=", ")
+}
