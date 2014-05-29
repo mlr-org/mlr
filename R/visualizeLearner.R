66fa64dac2456fbf606d0fe02ae67682bf7cd13a
Bernd Bischl <bernd_bischl@gmx.net>
2014-05-29 13:29:08 +0200

    improve visualizeLearner

--- R/visualizeLearner.R
+++ R/visualizeLearner.R
@@ -1,6 +1,18 @@
-#' Visualizes a learning algorithm on a 1D or 2D data set.
+#' @title Visualizes a learning algorithm on a 1D or 2D data set.
 #'
+#' @description
 #' Trains the model for 1 or 2 selected features, then displays it via \code{\link[ggplot2]{ggplot}}.
+#' Good for teaching or exploring models.
+#'
+#' For classification, only 2D plots are supported. The data points, the classification and
+#' potentially through color level the posterior probabilities are shown.
+#'
+#' For regression, 1D and 2D plots are supported. 1D sows the data, the estimated mean and potentially
+#' the estimated standard error. 2D does not show estimated standard error, but only the estimated mean
+#' via background color.
+#'
+#' The plot title displays the model id, its parameters, the test training performance
+#' and the cross-validation performance.
 #'
 #'
 #' @template arg_learner
@@ -21,11 +33,23 @@
 #' @param pointsize [\code{integer(1)}]\cr
 #'   Pointsize for ggplot2 geom_point for data points.
 #'   Default is 2.
+#' @param prob.alpha [\code{logical(1)}]\cr
+#'   For classification: Set alpha value of background to probability for
+#'   predicted class? Allows visualization of \dQuote{confidence} for prediction.
+#'   If not, only a constant color is displayed in the background for the predicted label.
+#'   Default is \code{TRUE}.
+#' @param err.col [\code{character(1)}]\cr
+#'   For classification: Color of misclassified data points.
+#'   Default is \dQuote{orange}
+#' @param cv [\code{integer(1)}]\cr
+#'   Do cross-validation and display in plot title?
+#'   Number of folds.
+#'   Default is 10.
 #' @return [\code{\link{WrappedModel}}].
 #' @export
 
 visualizeLearner = function(learner, task, features = NULL, measures,  ..., par.vals = list(),
-  gridsize = 50L, pointsize = 2L) {
+  gridsize = 50L, pointsize = 2L, prob.alpha = TRUE, err.col = "orange") {
 
   learner = checkLearner(learner)
   checkArg(task, "SupervisedTask")
@@ -46,6 +70,8 @@ visualizeLearner = function(learner, task, features = NULL, measures,  ..., par.
   checkArg(gridsize, "integer", len = 1L, na.ok = FALSE)
   pointsize = convertInteger(pointsize)
   checkArg(pointsize, "integer", len = 1L, na.ok = FALSE)
+  checkArg(prob.alpha, "logical", len = 1L, na.ok = FALSE)
+
   requirePackages("ggplot2", why = "visualizeLearner")
 
   # subset to features, set hyperpars
@@ -61,14 +87,15 @@ visualizeLearner = function(learner, task, features = NULL, measures,  ..., par.
   x1 = data[, x1n]
 
   # predictions
-  # if learner supports se, enable it
-  if (td$type == "regr" && taskdim == 1L && learner$se) {
+  # if learner supports prob or se, enable it
+  if (td$type == "regr" && taskdim == 1L && learner$se)
     learner = setPredictType(learner, "se")
-  }
+  if (td$type == "classif" && learner$prob)
+    learner = setPredictType(learner, "prob")
   mod = train(learner, task)
-  pred = predict(mod, task)
-  perf.train = performance(pred, measures = measures)
-  data$.response = pred$data$response
+  pred.train = predict(mod, task)
+  yhat = pred.train$data$response
+  perf.train = performance(pred.train, measures = measures)
   cv = crossval(learner, task, iters = 10L, measures = measures, show.info = FALSE)
   perf.cv = cv$aggr
 
@@ -82,24 +109,30 @@ visualizeLearner = function(learner, task, features = NULL, measures,  ..., par.
       seq(min(x2), max(x2), length.out = gridsize)
     )
     colnames(grid) = features
-    grid$.response = predict(mod, newdata = grid)$data$response
+    pred.grid = predict(mod, newdata = grid)
+    grid[, target] = pred.grid$data$response
   }
-   print(taskdim)
-
   if (td$type == "classif") {
-    data$.err = data$.response != y
+    data$.err = (y != yhat)
     if (taskdim == 2L) {
-      p = ggplot(grid, aes_string(x = x1n, y = x2n, z = ".response", fill = ".response"))
-      p = p + geom_tile()
-      # p = p + scale_fill_gradient2()
-      # p = p + geom_contour(breaks = c(-1, 0, 1))
+      p = ggplot(grid, aes_string(x = x1n, y = x2n))
+      if (learner$prob && prob.alpha) {
+        # max of rows is prob for selected class
+        grid$.prob.pred.class = apply(getProbabilities(pred.grid), 1, max)
+        p = p + geom_tile(data = grid, mapping = aes_string(fill = target, alpha = ".prob.pred.class"),
+          show_guide = TRUE)
+        p = p + scale_alpha(range = range(grid$.prob.pred.class))
+      } else {
+        p = p + geom_tile(mapping = aes_string(fill = target))
+      }
       p = p + geom_point(data = data, mapping = aes_string(x = x1n, y = x2n, shape = target),
         size = pointsize)
       if (any(data$.err)) {
         p = p + geom_point(data = subset(data, data$.err),
           mapping = aes_string(x = x1n, y = x2n, shape = target),
-          size = pointsize + .5, col = "orange")
+          size = pointsize + 1, col = err.col, show_guide = FALSE)
       }
+      p  = p + guides(alpha = FALSE)
     }
   } else if (td$type == "regr") {
     if (taskdim == 1L) {
