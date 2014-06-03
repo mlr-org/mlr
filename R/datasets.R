1af028e7f55d3bc83294d0ea1cb9f1c5a5651759
Michel <michellang@gmail.com>
2014-06-03 12:32:56 +0200

    update datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,15 +1,68 @@
-#' Exemplary iris classification task
+#' Iris classification task
 #'
 #' Contains the following objects:
 #' \describe{
 #'  \item{iris.task}{Classification task.}
-#'  \item{iris.lrn}{Classification learner using \code{classif.rpart}.}
+#'  \item{iris.lrn}{Classification learner using \code{classif.lda}.}
 #'  \item{iris.train}{Training subset of \code{iris.task}.}
 #'  \item{iris.test}{Test subset of \code{iris.task}.}
 #' }
 #'
-#' @name iris.mlr
+#' @name mlr.iris
+#' @aliases iris.task iris.lrn iris.train iris.test
 #' @references See \code{\link[datasets]{iris}}.
+#' @keywords data
+#' @docType data
+NULL
+
+#' Sonar classification task
+#'
+#' List with following named elements:
+#' \describe{
+#'  \item{sonar.task}{Classification task.}
+#'  \item{sonar.lrn}{Classification learner using \code{classif.rpart}.}
+#'  \item{sonar.train}{Training subset of \code{sonar.task}.}
+#'  \item{sonar.test}{Test subset of \code{sonar.task}.}
+#' }
+#'
+#' @name mlr.sonar
+#' @aliases sonar.task sonar.lrn sonar.train sonar.test
+#' @references See \code{\link[mlbench]{Sonar}}.
+#' @keywords data
+#' @docType data
+NULL
+
+#' BostonHousing regression task
+#'
+#' Contains the following objects:
+#' \describe{
+#'  \item{bh.task}{Regression task.}
+#'  \item{bh.lrn}{Classification learner using \code{regr.lm}.}
+#'  \item{bh.train}{Training subset of \code{bh.task}.}
+#'  \item{bh.test}{Test subset of \code{bh.task}.}
+#' }
+#'
+#' @name mlr.bh
+#' @aliases bh.task bh.lrn bh.train bh.test
+#' @references See \code{\link[mlbench]{BostonHousing}}.
+#' @keywords data
+#' @docType data
+NULL
+
+#' Wisonsin Prognostic Breast Cancer (WPBC) survival task
+#'
+#' Contains the following objects:
+#' \describe{
+#'  \item{wpbc.task}{Regression task.}
+#'  \item{wpbc.lrn}{Classification learner using \code{regr.lm}.}
+#'  \item{wpbc.train}{Training subset of \code{wpbc.task}.}
+#'  \item{wpbc.test}{Test subset of \code{wpbc.task}.}
+#' }
+#'
+#' @name mlr.wpbc
+#' @aliases wpbc.task wpbc.lrn wpbc.train wpbc.test
+#' @references See \code{\link[mboost]{wpbc}}. Four incomplete cases have been
+#'   removed from the task.
 #' @keywords data
 #' @docType data
 NULL
--- inst/makeData.R
+++ inst/makeData.R
@@ -6,43 +6,36 @@ dn = "../data"
 stopifnot(isDirectory(dn))
 
 data(iris, package = "datasets")
+train = (seq_row(iris) %% 3L) > 0
 iris.task = makeClassifTask("iris-example", data = iris, target = "Species")
-train = (seq_row(iris) %% 3L) > 0L
+iris.lrn = makeLearner("classif.lda")
 iris.train = subsetTask(iris.task, subset = train)
 iris.test = subsetTask(iris.task, subset = !train)
-iris.lrn = makeLearner("classif.lda")
-save(iris.task, iris.lrn, iris.train, iris.test,
-  file = file.path(dn, "iris.mlr.RData"))
-
+save(iris.task, iris.lrn, iris.train, iris.test, file = file.path(dn, "mlr.iris.RData"))
 
 data(BostonHousing, package = "mlbench")
-bh.task = makeRegrTask("BostonHousing-example", data = BostonHousing, target = "medv")
 train = (seq_row(BostonHousing) %% 3L) > 0L
+bh.task = makeRegrTask("BostonHousing-example", data = BostonHousing, target = "medv")
+bh.lrn = makeLearner("regr.lm")
 bh.train = subsetTask(bh.task, subset = train)
 bh.test = subsetTask(bh.task, subset = !train)
-bh.lrn = makeLearner("regr.lm")
-save(bh.task, bh.lrn, bh.train, bh.test,
-  file = file.path(dn, "bh.mlr.RData"))
+save(bh.task, bh.lrn, bh.train, bh.test, file = file.path(dn, "mlr.bh.RData"), compress = "xz")
 
 
 data(Sonar, package = "mlbench")
-sonar.task = makeClassifTask("Sonar-example", data = Sonar, target = "Class")
 train = (seq_row(Sonar) %% 3L) > 0L
+sonar.task = makeClassifTask("Sonar-example", data = Sonar, target = "Class")
+sonar.lrn = makeLearner("classif.rpart")
 sonar.train = subsetTask(sonar.task, subset = train)
 sonar.test = subsetTask(sonar.task, subset = !train)
-sonar.lrn = makeLearner("classif.rpart")
-save(sonar.task, sonar.lrn, sonar.train, sonar.test,
-  file = file.path(dn, "sonar.mlr.RData"))
-
+save(sonar.task, sonar.lrn, sonar.train, sonar.test, file = file.path(dn, "mlr.sonar.RData"), compress = "xz")
 
 data(wpbc, package = "mboost")
-data = wpbc
-data$status = ifelse(data$status == "R", 1L, 0L)
-data = data[complete.cases(data), ]
-wpbc.task = makeSurvTask("wpbc-example", data = data, target = c("time", "status"))
-train = (seq_row(data) %% 3L) > 0L
+wpbc$status = ifelse(wpbc$status == "R", 1L, 0L)
+wpbc = wpbc[complete.cases(wpbc), ]
+train = (seq_row(wpbc) %% 3L) > 0L
+wpbc.task = makeSurvTask("wpbc-example", data = wpbc, target = c("time", "status"))
+wpbc.lrn = makeLearner("classif.rpart")
 wpbc.train = subsetTask(wpbc.task, subset = train)
 wpbc.test = subsetTask(wpbc.task, subset = !train)
-wpbc.lrn = makeLearner("classif.rpart")
-save(wpbc.task, wpbc.lrn, wpbc.train, wpbc.test,
-  file = file.path(dn, "wpbc.mlr.RData"))
+save(wpbc.task, wpbc.lrn, wpbc.train, wpbc.test, file = file.path(dn, "mlr.wpbc.RData"), compress = "xz")
