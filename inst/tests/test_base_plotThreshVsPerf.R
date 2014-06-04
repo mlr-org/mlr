bde396ecf2e59e95a71ee48123f197f0c8a853e6
Bernd Bischl <you@example.com>
2014-06-04 15:56:11 +0200

    new method: plotThresVsPerf

---
+++ inst/tests/test_base_plotThreshVsPerf.R
@@ -0,0 +1,8 @@
+context("plotThreshVsPerf")
+
+test_that("plotThreshVsPerf", {
+  gs = 10
+  lrn = makeLearner("classif.rpart", predict.type = "prob")
+  r = holdout(lrn, binaryclass.task)
+  plotThreshVsPerf(r$pred, gridsize = gs)
+})
