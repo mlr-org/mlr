1ee7f1f47b9fad4d4ee8e94314523868f18b65a6
Jakob Richter <code@jakob-r.de>
2014-06-06 16:24:35 +0200

    add IBk also for regression

---
+++ R/RLearner_regr_IBk.R
@@ -0,0 +1,29 @@
+#' @export
+makeRLearner.regr.IBk = function() {
+  makeRLearnerRegr(
+    cl = "regr.IBk",
+    package = "RWeka",
+    par.set = makeParamSet(
+      makeLogicalLearnerParam(id = "I"),
+      makeLogicalLearnerParam(id = "F"),
+      makeIntegerLearnerParam(id = "K", lower = 1L, default = 1L),
+      makeLogicalLearnerParam(id = "E"),
+      makeIntegerLearnerParam(id = "W", lower = 0L),
+      makeLogicalLearnerParam(id = "X"),
+      makeUntypedLearnerParam(id = "A", default = "weka.core.neighboursearch.LinearNNSearch")
+    ),
+    properties = c("twoclass", "multiclass", "numerics", "factors", "prob")
+  )
+}
+
+#' @export
+trainLearner.regr.IBk = function(.learner, .task, .subset, .weights = NULL,  ...) {
+  ctrl = Weka_control(...)
+  IBk(getTaskFormula(.task), data = getTaskData(.task, .subset), control = ctrl, na.action = na.pass)
+}
+
+#' @export
+predictLearner.regr.IBk = function(.learner, .model, .newdata, ...) {
+  type = switch(.learner$predict.type, prob = "prob", "class")
+  predict(.model$learner.model, newdata = .newdata, type = type, ...)
+}
