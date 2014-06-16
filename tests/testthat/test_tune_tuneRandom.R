d4c8afece50a3375c388f8d725778aa8329acf07
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-16 03:08:08 +0200

    clean up

--- tests/testthat/test_tune_tuneRandom.R
+++ tests/testthat/test_tune_tuneRandom.R
@@ -4,12 +4,12 @@ test_that("tuneRandom", {
   lrn = makeLearner("classif.ksvm")
   rdesc = makeResampleDesc("Holdout")
   ps = makeParamSet(
-    makeNumericParam("C", lower=0.001, upper=1), 
-    makeDiscreteParam("kernel", values=c("rbfdot", "vanilladot"))
+    makeNumericParam("C", lower = 0.001, upper = 1),
+    makeDiscreteParam("kernel", values = c("rbfdot", "vanilladot"))
   )
-  
-  ctrl = makeTuneControlRandom(maxit=5)
-  tr = tuneParams(lrn, multiclass.task, rdesc, par.set=ps, control=ctrl)
+
+  ctrl = makeTuneControlRandom(maxit = 5)
+  tr = tuneParams(lrn, multiclass.task, rdesc, par.set = ps, control = ctrl)
   expect_equal(getOptPathLength(tr$opt.path), 5)
   expect_true(!is.na(tr$y))
 })
@@ -19,12 +19,12 @@ test_that("tuneRandom works with dependent params", {
   rdesc = makeResampleDesc("Holdout")
   lrn = makeLearner("classif.ksvm")
   ps = makeParamSet(
-    makeDiscreteParam("kernel", values=c("vanilladot", "rbfdot")), 
-    makeNumericParam("C", lower=1, upper=2),
-    makeNumericParam("sigma", lower=1, upper=2, requires=quote(kernel == "rbfdot"))
+    makeDiscreteParam("kernel", values = c("vanilladot", "rbfdot")),
+    makeNumericParam("C", lower = 1, upper = 2),
+    makeNumericParam("sigma", lower = 1, upper = 2, requires = quote(kernel == "rbfdot"))
   )
-  ctrl = makeTuneControlRandom(maxit=5)
-  tr = tuneParams(lrn, multiclass.task, rdesc, par.set=ps, control=ctrl)
+  ctrl = makeTuneControlRandom(maxit = 5)
+  tr = tuneParams(lrn, multiclass.task, rdesc, par.set = ps, control = ctrl)
   expect_equal(getOptPathLength(tr$opt.path), 5)
   expect_true(!is.na(tr$y))
 })
