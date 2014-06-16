5ed7337e56ce665a8d1ea6f5e0d978216192f45f
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-16 04:50:06 +0200

    fix tuneRandom with trafo

--- R/tuneRandom.R
+++ R/tuneRandom.R
@@ -1,5 +1,5 @@
 tuneRandom = function(learner, task, resampling, measures, par.set, control, opt.path, show.info) {
-  vals = sampleValues(n = control$extra.args$maxit, par = par.set)
+  vals = sampleValues(n = control$extra.args$maxit, par = par.set, trafo = TRUE)
   evalOptimizationStatesTune(learner, task, resampling, measures, par.set, control, opt.path,
     show.info, vals, dobs = seq_along(vals), eols = NA_integer_, remove.nas = TRUE)
   i = getOptPathBestIndex(opt.path, measureAggrName(measures[[1]]), ties = "random")
--- tests/testthat/test_tune_tuneRandom.R
+++ tests/testthat/test_tune_tuneRandom.R
@@ -28,3 +28,14 @@ test_that("tuneRandom works with dependent params", {
   expect_equal(getOptPathLength(tr$opt.path), 5)
   expect_true(!is.na(tr$y))
 })
+
+test_that("tuneRandom works with trafo", {
+  lrn = makeLearner("classif.ksvm")
+  ps = makeParamSet(makeNumericParam("sigma", lower=-10, upper = -1, trafo = function(x) 2^x))
+  rdesc = makeResampleDesc("Holdout")
+  ctrl = makeTuneControlRandom(maxit = 3)
+  tr = tuneParams(lrn, iris.task, rdesc, par.set = ps, control = ctrl)
+  expect_equal(getOptPathLength(tr$opt.path), 3)
+  expect_true(!is.na(tr$y) && is.finite(tr$y))
+})
+
