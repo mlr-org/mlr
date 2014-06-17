386d47402ff7183d9e0c2615212ac40dccfac48f
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-17 22:55:46 +0200

    clean up

--- tests/testthat/test_base_configureMlr.R
+++ tests/testthat/test_base_configureMlr.R
@@ -2,11 +2,11 @@ context("configureMlr")
 
 test_that("getOptions and configureMlr", {
   mlr.options = getMlrOptions()
-  expect_equal(length(mlr.options), 4L)
-  
+  expect_equal(length(mlr.options), 5L)
+
   configureMlr(on.learner.error="quiet")
   expect_equal(getMlrOptions()$'on.learner.error', "quiet")
-  
+
   do.call(configureMlr, mlr.options)
   expect_equal(getMlrOptions(), mlr.options)
 })
