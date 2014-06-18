854c5dd76b3d0ed7141a84d158d3bd1a59f7168b
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-18 02:02:01 +0200

    clean up downsample

--- R/DownsampleWrapper.R
+++ R/DownsampleWrapper.R
@@ -5,36 +5,30 @@
 #' It will only be trained on a subset of the original data to save computational time.
 #'
 #' @template arg_learner
-#' @param dw.select [\code{character(1)}]\cr
-#'   How to select the size of the downsampled dataset.
-#'   \dQuote{perc} = select observations by percentage, \dQuote{abs} = select absolute number
-#'   of observations.
-#'   Default is \dQuote{perc}.
-#' @param dw.val [\code{numeric(1)}]\cr
-#'   Depends on \code{select}:
-#'   Either a percentage from [0, 1] or a number of observations.
-#' @param dw.stratify [\code{boolean(1)}]\cr
-#'   Should the downsampled data be stratified according to the target classes? Default is \code{FALSE}.
+#' @param dw.perc [\code{numeric(1)}]\cr
+#'   See \code{\link{downsample}}.
+#' @param dw.stratify [\code{logical(1)}]\cr
+#'   See \code{\link{downsample}}.
 #' @template ret_learner
 #' @family downsample
 #' @export
-makeDownsampleWrapper = function(learner, dw.select = "perc", dw.val = 1, dw.stratify = FALSE) {
-  checkArg(learner, "Learner")
-  checkDownsampleArguments(select = dw.select, val = dw.val, stratify = dw.stratify)
+makeDownsampleWrapper = function(learner, dw.perc = 1, dw.stratify = FALSE) {
+  learner = checkLearner(learner)
+  checkArg(dw.perc, "numeric", len = 1L, na.ok = FALSE, lower = 0, upper = 1)
+  checkArg(dw.stratify, "logical", len = 1L, na.ok = FALSE)
   id = paste(learner$id, "downsampled", sep = ".")
   ps = makeParamSet(
-    makeDiscreteLearnerParam(id = "dw.select", values = c("perc","abs")),
-    makeNumericLearnerParam(id = "dw.val", lower = 0),
-    makeLogicalLearnerParam(id = "dw.stratify")
+    makeNumericLearnerParam(id = "dw.perc", lower = 0, upper = 1, default = 1),
+    makeLogicalLearnerParam(id = "dw.stratify", default = FALSE)
   )
-  pv = list(dw.select = dw.select, dw.val = dw.val, dw.stratify = dw.stratify)
+  pv = list(dw.perc = dw.perc, dw.stratify = dw.stratify)
   makeBaseWrapper(id, learner, package = "mlr", par.set = ps, par.vals = pv, cl = "DownsampleWrapper")
 }
 
 #' @export
-trainLearner.DownsampleWrapper = function(.learner, .task, .subset, .weights = NULL, dw.select, dw.val, dw.stratify, ...) {
+trainLearner.DownsampleWrapper = function(.learner, .task, .subset, .weights = NULL, dw.perc, dw.stratify, ...) {
   .task = subsetTask(.task, .subset)
-  .task = downsample(.task, select = dw.select, val = dw.val, stratify = dw.stratify)
+  .task = downsample(.task, perc = dw.perc, stratify = dw.stratify)
   m = train(.learner$next.learner, .task, weights = .weights)
   makeChainModel(next.model = m, cl = "DownsampleModel")
 }
--- R/downsample.R
+++ R/downsample.R
@@ -1,66 +1,40 @@
 #' @title Downsample (subsample) a task or a data.frame.
 #'
 #' @description
-#'   Decrease the observations in a \code{task} or a \code{ResampleInstance}
-#'   to a given percentage or absolute number.
+#' Decrease the observations in a \code{task} or a \code{ResampleInstance}
+#' to a given percentage of observations.
 #'
 #' @param obj [\code{\link{SupervisedTask}} | \code{\link{ResampleInstance}}]\cr
 #'   Input data or a \code{ResampleInstance}.
-#' @param select [\code{character(1)}]\cr
-#'   How to select the size of the downsampled dataset.
-#'   \dQuote{perc} = select observations by percentage, \dQuote{abs} = select absolute number
-#'   of observations.
-#'   Default is \dQuote{perc}.
-#' @param val [\code{numeric(1)}]\cr
-#'   Depends on \code{select}:
-#'   Either a percentage from [0, 1] or a number of observations.
-#' @param stratify [\code{boolean(1)}]\cr
-#'   Should the downsampled data be stratified according to the target classes? Default is \code{FALSE}.
+#' @param perc [\code{numeric(1)}]\cr
+#'   Percentage from [0, 1].
+#'   Default is 1.
+#' @param stratify [\code{logical(1)}]\cr
+#'   Only for classification:
+#'   Should the downsampled data be stratified according to the target classes?
+#'   Default is \code{FALSE}.
 #' @seealso \code{\link{makeResampleInstance}}
 #' @return [\code{data.frame} | \code{\link{SupervisedTask}} | \code{\link{ResampleInstance}}]. Same type as \code{obj}.
 #' @family downsample
 #' @export
-downsample = function(obj, select = "perc", val = 1, stratify = FALSE) {
-  checkDownsampleArguments(select, val, stratify)
+downsample = function(obj, perc = 1, stratify = FALSE) {
+  checkArg(obj, c("SupervisedTask", "ResampleInstance"))
+  checkArg(perc, "numeric", len = 1L, na.ok = FALSE, lower = 0, upper = 1)
+  checkArg(stratify, "logical", len = 1L, na.ok = FALSE)
   UseMethod("downsample")
 }
 
 #' @export
-downsample.SupervisedTask = function(obj, select = "perc", val = 1, stratify = FALSE) {
-  if(select == "abs") {
-    perc = val / obj$task.desc$size
-  } else {
-    perc = val
-  }
-  holdoutDesc = makeResampleDesc(method = "Holdout", stratify = stratify, split = perc)
-  holdoutInst = makeResampleInstance(desc = holdoutDesc, task = obj)
-  subsetTask(task = obj, subset = holdoutInst$train.inds[[1]])
+downsample.SupervisedTask = function(obj, perc = 1, stratify = FALSE) {
+  rin = makeResampleInstance("Holdout", stratify = stratify, split = perc, task = obj)
+  subsetTask(task = obj, subset = rin$train.inds[[1L]])
 }
 
 #' @export
-downsample.ResampleInstance = function(obj, select = "perc", val = 1, stratify = FALSE) {
-  if(stratify) {
+downsample.ResampleInstance = function(obj, perc = 1, stratify = FALSE) {
+  if (stratify)
     stop("Stratifying is not supported for a ResampleInstance!")
-  }
-  obj$train.inds = lapply(obj$train.inds, function(x) {
-    if(select == "perc") {
-      n = round(val * length(x))
-    } else {
-      n = val
-    }
-    if(n > length(x)) {
-      stopf("The given val = %i is bigger than the observations in the sample: %i.", n, length(x))
-    }
-    sample(x, size = n)
-  })
+  obj$train.inds = lapply(obj$train.inds, function(x) sample(x, size = length(x) * perc))
+  return(obj)
 }
 
-checkDownsampleArguments = function(select, val, stratify) {
-  switch(select,
-    perc = checkArg(val, "numeric", len = 1L, na.ok = FALSE, lower = 0, upper = 1),
-    abs = {
-      n = convertInteger(val)
-      checkArg(n, "integer", len = 1L, na.ok = FALSE, lower = 1L)
-      })
-  checkArg(stratify, "logical")
-}
