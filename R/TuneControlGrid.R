321596fa03c4b2488b7c20d4360c4e5e8619637a
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-21 22:37:18 +0200

    tune: improve grid search a lot by using generateGridDesign

--- R/TuneControlGrid.R
+++ R/TuneControlGrid.R
@@ -1,6 +1,6 @@
 #' @export
 #' @rdname TuneControl
-makeTuneControlGrid = function(same.resampling.instance=TRUE) {
-  makeTuneControl(same.resampling.instance=same.resampling.instance,
-    start=list(), cl="TuneControlGrid")
+makeTuneControlGrid = function(same.resampling.instance = TRUE, resolution = 10L) {
+  makeTuneControl(same.resampling.instance = same.resampling.instance,
+    start = list(), resolution = resolution, cl = "TuneControlGrid")
 }
