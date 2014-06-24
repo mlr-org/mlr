0bd28d02b93c61ea08813810fe06a605dd6922a6
Bernd Bischl <you@example.com>
2014-06-24 16:22:33 +0200

    clean up

--- R/BaggingWrapper.R
+++ R/BaggingWrapper.R
@@ -1,5 +1,6 @@
-#' Fuse learner with the bagging technique.
+#' @title Fuse learner with the bagging technique.
 #'
+#' @description
 #' Fuses a learner with the bagging method
 #' (i.e., similar to what a \code{randomForest} does).
 #' Creates a learner object, which can be
@@ -17,8 +18,7 @@
 #' probabilities are predicted by considering the proportions of all predicted labels.
 #' For regression the mean value and the standard deviations across predictions is computed.
 #'
-#' @param learner [\code{\link{Learner}}]\cr
-#'   The learner. Prediction type of the basic learner must be \dQuote{response}.
+#' @template arg_learner
 #' @param bag.iters [\code{integer(1)}]\cr
 #'   Iterations = number of fitted models in bagging.
 #'   Default is 10.
@@ -31,46 +31,37 @@
 #' @param bag.feats [\code{numeric(1)}]\cr
 #'   Percentage size of randomly selected features in bags.
 #'   Default is 1.
-#' @param predict.type [\code{character(1)}]\cr
-#'   Classification: \dQuote{response} (= labels) or \dQuote{prob}
-#'   (= probabilities and labels by selecting the ones with maximal probability).
-#'   Regression: \dQuote{response} (= mean response) or \dQuote{se} (= standard errors
-#'   and mean response).
-#'   Default is \dQuote{response}.
-#' @return [\code{\link{Learner}}].
+#' @template ret_learner
 #' @export
-makeBaggingWrapper = function(learner, bag.iters = 10L, bag.replace = TRUE, bag.size, bag.feats = 1,
-  predict.type = "response") {
+makeBaggingWrapper = function(learner, bag.iters = 10L, bag.replace = TRUE, bag.size, bag.feats = 1) {
 
-  checkArg(learner, "Learner")
+  learner = checkLearner(learner)
   bag.iters = convertInteger(bag.iters)
-  checkArg(bag.iters, "integer", len=1L, na.ok=FALSE, lower=1L)
-  checkArg(bag.replace, "logical", na.ok=FALSE)
+  checkArg(bag.iters, "integer", len = 1L, na.ok = FALSE, lower = 1L)
+  checkArg(bag.replace, "logical", na.ok = FALSE)
   if (missing(bag.size)) {
     bag.size = if (bag.replace) 1 else 0.632
   } else {
-    checkArg(bag.size, "numeric", len=1L, na.ok=FALSE, lower=0, upper=1)
+    checkArg(bag.size, "numeric", len = 1L, na.ok = FALSE, lower = 0, upper = 1)
   }
-  checkArg(bag.feats, "numeric", len=1L, na.ok=FALSE, lower=0, upper=1)
+  checkArg(bag.feats, "numeric", len = 1L, na.ok = FALSE, lower = 0, upper = 1)
   if (learner$predict.type != "response")
-    stop("Predict type of the basic learner must be response.")
-  id = paste(learner$id, "bagged", sep=".")
+    stop("Predict type of the basic learner must be 'response'.")
+  id = paste(learner$id, "bagged", sep = ".")
   packs = learner$packages
   ps = makeParamSet(
-    makeIntegerLearnerParam(id="bag.iters", lower=1L, default=10L),
-    makeLogicalLearnerParam(id="bag.replace", default=TRUE),
-    makeNumericLearnerParam(id="bag.size", lower=0, upper=1),
-    makeNumericLearnerParam(id="bag.feats", lower=0, upper=1, default=2/3)
+    makeIntegerLearnerParam(id = "bag.iters", lower = 1L, default = 10L),
+    makeLogicalLearnerParam(id = "bag.replace", default = TRUE),
+    makeNumericLearnerParam(id = "bag.size", lower = 0, upper = 1),
+    makeNumericLearnerParam(id = "bag.feats", lower = 0, upper = 1, default = 2/3)
   )
-  pv = list(bag.iters=bag.iters, bag.replace=bag.replace,
-    bag.size=bag.size, bag.feats=bag.feats)
-  x = makeBaseWrapper(id, learner, packs, par.set=ps, par.vals=pv, cl="BaggingWrapper")
+  pv = list(bag.iters = bag.iters, bag.replace = bag.replace,
+    bag.size = bag.size, bag.feats = bag.feats)
+  x = makeBaseWrapper(id, learner, packs, par.set = ps, par.vals = pv, cl = "BaggingWrapper")
   x = switch(x$type,
     "classif" = addProperties(x, "prob"),
     "regr" = addProperties(x, "se"))
-  if(!is.na(predict.type))
-    x = setPredictType(x, predict.type)
-  x
+  return(x)
 }
 
 #' @export
@@ -90,33 +81,33 @@ trainLearner.BaggingWrapper = function(.learner, .task, .subset, .weights = NULL
     w = .weights[bag]
     if (bag.feats < 1) {
       feats2 = sample(feats, k, replace = FALSE)
-      .task2 = subsetTask(.task, features=feats2)
-      train(.learner$next.learner, .task2, subset=bag, weights = w)
+      .task2 = subsetTask(.task, features = feats2)
+      train(.learner$next.learner, .task2, subset = bag, weights = w)
   } else {
-      train(.learner$next.learner, .task, subset=bag, weights = w)
+      train(.learner$next.learner, .task, subset = bag, weights = w)
     }
   })
-  makeChainModel(next.model=models, cl="BaggingModel")
+  makeChainModel(next.model = models, cl = "BaggingModel")
 }
 
 #' @export
 predictLearner.BaggingWrapper = function(.learner, .model, .newdata, ...) {
   models = getBaggingModels(.model)
-  g = if(.learner$type == "classif") as.character else identity
+  g = if (.learner$type == "classif") as.character else identity
   p = sapply(models, function(m) {
-    nd = .newdata[, m$features, drop=FALSE]
-    g(predict(m, newdata=nd, ...)$data$response)
+    nd = .newdata[, m$features, drop = FALSE]
+    g(predict(m, newdata = nd, ...)$data$response)
   })
   if (.learner$predict.type == "response") {
-    g = if(.learner$type == "classif")
+    g = if (.learner$type == "classif")
       as.factor(apply(p, 1L, computeMode))
     else
       rowMeans(p)
   } else {
-    if(.learner$type == 'classif') {
+    if (.learner$type == 'classif') {
       levs = .model$task.desc$class.levels
       p = apply(p, 1L, function(x) {
-        x = factor(x, levels = levs)
+        x = factor(x, levels = levs) # we need all level for the table and we need them in consitent order!
         as.numeric(prop.table(table(x)))
       })
       setColNames(t(p), levs)
@@ -130,8 +121,7 @@ predictLearner.BaggingWrapper = function(.learner, .model, .newdata, ...) {
 #' @export
 makeWrappedModel.BaggingWrapper = function(learner, learner.model, task.desc, subset, features, factor.levels, time) {
   x = NextMethod()
-  class(x) = c("BaggingModel", class(x))
-  return(x)
+  addClasses(x, "OverBaggingModel")
 }
 
 #' @export
--- R/OverBaggingWrapper.R
+++ R/OverBaggingWrapper.R
@@ -1,52 +1,47 @@
 #' @title Fuse learner with the bagging technique and oversampling for imbalancy correction.
 #'
-#' Fuses a learner with the bagging method
-#' (i.e., similar to what a \code{randomForest} does).
+#' @description
+#' Fuses a classification learner for binary classification with an over-bagging method
+#' for imbalancy correction when we have strongly unequal class sizes.
 #' Creates a learner object, which can be
 #' used like any other learner object.
 #' Models can easily be accessed via \code{\link{getBaggingModels}}.
 #'
 #' OverBagging is implemented as follows:
 #' For each iteration a random data subset is sampled. Minority class examples
-#' are oversampled and majority class examples are either sampled with replacement
-#' or all majority class examples take part in each bag. 
-#' Note that this is usually handled in a slightly different way
-#' in the random forest where features are sampled at each tree split).
+#' are oversampled with replacement with a given rate.
+#' Majority class examples are either simply copied into each bag, or bootstrapped with replacement
+#' until we have as many majority class examples as in the original training data.
+#' Features are currently not changed or sampled.
 #'
 #' Prediction works as follows:
 #' For classification we do majority voting to create a discrete label and
 #' probabilities are predicted by considering the proportions of all predicted labels.
 #'
-#' @param learner [\code{\link{Learner}}]\cr
-#'   The learner. Prediction type of the basic learner must be \dQuote{response}.
+#' @template arg_learner
 #' @param obw.iters [\code{integer(1)}]\cr
-#'   Iterations = number of fitted models in bagging.
+#'   Number of fitted models in bagging.
 #'   Default is 10.
 #' @param obw.rate [\code{numeric(1)}]\cr
 #'   Factor to upsample the smaller class in each bag.
 #'   Must be between 1 and \code{Inf},
 #'   where 1 means no oversampling and 2 would mean doubling the class size.
-#' @param obw.maxcl [\code{numeric(1)}]\cr
+#' @param obw.maxcl [\code{character(1)}]\cr
 #'   character value that controls how to sample majority class.
-#'   all means every instance of the majority class gets in each bag,
-#'   boot means the majority class instances are bootstrapped in each iteration.   
-#' @param predict.type [\code{character(1)}]\cr
-#'   Classification: \dQuote{response} (= labels) or \dQuote{prob}
-#'   (= probabilities and labels by selecting the ones with maximal probability).
-#'   Regression: \dQuote{response} (= mean response) or \dQuote{se} (= standard errors
-#'   and mean response).
-#'   Default is \dQuote{response}.
-#' @return [\code{\link{Learner}}].
+#'   \dQuote{all} means every instance of the majority class gets in each bag,
+#'   \dQuote{boot} means the majority class instances are bootstrapped in each iteration.
+#'   Default is \dQuote{boot}.
+#' @template ret_learner
+#' @family imbalancy
 #' @export
-makeOverBaggingWrapper = function(learner, obw.iters = 10L, obw.rate = 2,
-  obw.maxcl = "boot", predict.type = "response") {
-  
+makeOverBaggingWrapper = function(learner, obw.iters = 10L, obw.rate, obw.maxcl = "boot") {
+
   learner = checkLearner(learner, "classif")
   obw.iters = convertInteger(obw.iters)
   checkArg(obw.iters, "integer", len = 1L, na.ok = FALSE, lower = 1L)
   checkArg(obw.rate, "numeric", len = 1L, na.ok = FALSE, lower = 1)
-  checkArg(obw.maxcl, "character", choices=c("boot", "all"))  
-  
+  checkArg(obw.maxcl, choices = c("boot", "all"))
+
   if (learner$predict.type != "response")
     stop("Predict type of the basic learner must be response.")
   id = paste(learner$id, "overbagged", sep = ".")
@@ -54,30 +49,24 @@ makeOverBaggingWrapper = function(learner, obw.iters = 10L, obw.rate = 2,
   ps = makeParamSet(
     makeIntegerLearnerParam(id = "obw.iters", lower = 1L, default = 10L),
     makeNumericLearnerParam(id = "obw.rate", lower = 1),
-    makeDiscreteLearnerParam(id = "obw.maxcl", c("boot", "all"))    
+    makeDiscreteLearnerParam(id = "obw.maxcl", c("boot", "all"))
   )
   pv = list(obw.iters = obw.iters, obw.rate = obw.rate, obw.maxcl = obw.maxcl)
   x = makeBaseWrapper(id, learner, packs, par.set = ps, par.vals = pv, cl = "OverBaggingWrapper")
   x = addProperties(x, "prob")
-   
-  if(!is.na(predict.type))
-    x = setPredictType(x, predict.type)
-  x
+
+  return(x)
 }
 
 #' @export
-trainLearner.OverBaggingWrapper = function(.learner, .task, .subset, .weights = NULL, 
+trainLearner.OverBaggingWrapper = function(.learner, .task, .subset, .weights = NULL,
    obw.iters, obw.rate, obw.maxcl, ...) {
 
   .task = subsetTask(.task, subset = .subset)
-  
-  models = lapply(seq_len(obw.iters), function(i) { 
-    if (obw.maxcl == "boot") {
-      bag = sampleBinaryClass(getTaskTargets(.task), obw.rate, cl = "min", minreplace = TRUE, maxreplace = TRUE)
-    } else {
-      bag = sampleBinaryClass(getTaskTargets(.task), obw.rate, cl = "min", minreplace = TRUE, maxreplace = FALSE)
-    }
-    train(.learner$next.learner, .task, subset = bag, weights = NULL)
+  y = getTaskTargets(.task)
+  models = lapply(seq_len(obw.iters), function(i) {
+    bag = sampleBinaryClass(y, obw.rate, cl = "min", minreplace = TRUE, maxreplace = (obw.maxcl == "boot"))
+    train(.learner$next.learner, .task, subset = bag, weights = .weights)
   })
   makeChainModel(next.model = models, cl = "OverBaggingModel")
 }
@@ -86,16 +75,15 @@ trainLearner.OverBaggingWrapper = function(.learner, .task, .subset, .weights = 
 predictLearner.OverBaggingWrapper = function(.learner, .model, .newdata, ...) {
   models = getBaggingModels(.model)
   p = sapply(models, function(m) {
-    nd = .newdata[, m$features, drop=FALSE]
-    as.character(predict(m, newdata=nd, ...)$data$response)
+    as.character(predict(m, newdata = .newdata, ...)$data$response)
   })
   if (.learner$predict.type == "response") {
     g = as.factor(apply(p, 1L, computeMode))
   } else {
     levs = .model$task.desc$class.levels
     p = apply(p, 1L, function(x) {
-    x = factor(x, levels = levs)
-    as.numeric(prop.table(table(x)))
+      x = factor(x, levels = levs) # we need all level for the table and we need them in consitent order!
+      as.numeric(prop.table(table(x)))
     })
     setColNames(t(p), levs)
   }
@@ -104,8 +92,7 @@ predictLearner.OverBaggingWrapper = function(.learner, .model, .newdata, ...) {
 #' @export
 makeWrappedModel.OverBaggingWrapper = function(learner, learner.model, task.desc, subset, features, factor.levels, time) {
   x = NextMethod()
-  class(x) = c("OverBaggingModel", class(x))
-  return(x)
+  addClasses(x, "OverBaggingModel")
 }
 
 #' @export
