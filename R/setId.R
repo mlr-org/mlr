c81d037b6f15419b4a76027d6fccc7e7b5920183
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-01 11:56:33 +0200

    new function: setId

---
+++ R/setId.R
@@ -0,0 +1,17 @@
+#' Set the id of a learner object.
+#'
+#' @template arg_learner
+#' @param id [\code{character(1)}]\cr
+#'    New id for learner.
+#' @template ret_learner
+#' @export
+#' @family learner
+setId = function(learner, id) {
+  learner = checkLearner(learner)
+  assertString(id)
+  learner$id = id
+  return(learner)
+}
+
+
+
