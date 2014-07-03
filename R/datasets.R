b5bdb732b02b0ca446ca711a4363c9791f7a5fb7
Michel <michellang@gmail.com>
2014-07-03 14:29:14 -0700

    Revert "datasets + docs"
    This reverts commit c715263d52095bc549596ee5b0a5e12ab8526dc9.

--- R/datasets.R
+++ R/datasets.R
@@ -1,33 +1,60 @@
+#' Iris classification task
+#'
+#' Contains the task (\code{iris.task}) and a resample instance
+#' (\code{iris.rin}).
+#'
+#' @name mlr.iris
+#' @aliases iris.task iris.rin
+#' @references See \code{\link[datasets]{iris}}.
+#' @keywords data
+#' @docType data
+NULL
+
 #' Sonar classification task
 #'
-#' Classification task for the Sonar data set.
+#' Contains the task (\code{sonar.task}) and a matching resample instance
+#' (\code{sonar.rin}).
 #'
 #' @name mlr.sonar
-#' @aliases sonar
-#' @references See \code{\link[mlbench]{Sonar}} in package mlbench.
+#' @aliases sonar.task sonar.rin
+#' @references See \code{\link[mlbench]{Sonar}}.
+#' @keywords data
+#' @docType data
+NULL
+
+#' Sonar classification task
+#'
+#' Contains the task (\code{sonar.task}) and a matching resample instance
+#' (\code{sonar.rin}).
+#'
+#' @name mlr.sonar
+#' @aliases sonar.task sonar.rin
+#' @references See \code{\link[mlbench]{Sonar}}.
 #' @keywords data
 #' @docType data
 NULL
 
 #' Boston Housing regression task
 #'
-#' Regression task for the BostonHousing data set.
+#' Contains the task (\code{bh.task}) and a matching resample instance
+#' (\code{bh.rin}).
 #'
 #' @name mlr.bh
-#' @aliases bh
-#' @references See \code{\link[mlbench]{BostonHousing}} in package mlbench.
+#' @aliases bh.task bh.rin
+#' @references See \code{\link[mlbench]{BostonHousing}}.
 #' @keywords data
 #' @docType data
 NULL
 
 #' Wisonsin Prognostic Breast Cancer (WPBC) survival task
 #'
-#' Survival task for the WPBC data set.
+#' Contains the task (\code{wpbc.task}) and a matching resample instance
+#' (\code{wpbc.rin}).
 #'
 #' @name mlr.wpbc
-#' @aliases wpbc
-#' @references See \code{\link[mboost]{wpbc}} in package mboost.
-#'   Four incomplete cases have been removed from the task.
+#' @aliases wpbc.task wpbc.rin
+#' @references See \code{\link[mboost]{wpbc}}. Four incomplete cases
+#'   have been removed from the task.
 #' @keywords data
 #' @docType data
 NULL
