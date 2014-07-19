957fb969f0e14cf02781a243c25262ba4232440e
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-20 01:46:39 +0200

    new: getFailureModelMsg

---
+++ R/getFailuremodelMsg.R
@@ -0,0 +1,34 @@
+#' @title Return error message of FailureModel.
+#'
+#' @description
+#' Such a model is created when one sets the corresponding option in \code{\link{configureMlr}}.
+#' If no failure occurred, \code{NA} is returned.
+#'
+#' For complex wrappers this getter returns the first error essage encountered in ANY model that failed.
+#'
+#' @template arg_wrappedmod
+#' @return [\code{character(1)}].
+#' @export
+getFailureModelMsg = function(model) {
+  UseMethod("getFailureModelMsg")
+}
+
+#' @export
+getFailureModelMsg.WrappedModel = function(model) {
+  if (inherits(model, "FailureModel"))
+    return(as.character(model$learner.model))
+  if (inherits(model, "BaseWrapperModel"))
+    return(getFailureModelMsg(model$learner.model$next.model))
+  return(NA_character_)
+}
+
+#' @export
+getFailureModelMsg.BaggingModel = function(model) {
+  mods = getBaggingModels(model, learner.models = FALSE)
+  msgs = sapply(mods, getFailureModelMsg)
+  j = which.first(!is.na(msgs))
+  ifelse(j == 0L, NA_character_ , msgs[j])
+}
+
+
+
