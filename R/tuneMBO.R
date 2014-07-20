ea2977f44b88a532c878cc53ca48a62adafe67bf
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-20 05:07:30 +0200

    fix tuneMBO

--- R/tuneMBO.R
+++ R/tuneMBO.R
@@ -1,4 +1,3 @@
-#FIXME: should we really define validation error like this?
 tuneMBO = function(learner, task, resampling, measures, par.set, control,
   opt.path, show.info) {
 
@@ -8,20 +7,20 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   mbo.control$final.evals = 0L
   cx = identity
 
-  # FIXME: use ... in mbo
   tff = function(x) tunerFitnFun(x, learner = learner, task = task, resampling = resampling, measures = measures,
     par.set = par.set, ctrl = control, opt.path = opt.path, show.info = show.info,
     convertx = cx, remove.nas = TRUE)
 
-  #FIXME remove this when mbo on cran
+  #FIXME: remove this when mbo on cran
   mbofun = get("mbo", envir = getNamespace("mlrMBO"))
 
-  or = mbofun(tff, par.set, design = NULL, learner = control$learner, control = mbo.control, show.info = FALSE)
+  # remove trafos for mbo, we do this in tunerFitnFun
+  ps2 = par.set
+  for (i in 1:length(ps2$pars))
+    ps2$pars[[i]]$trafo = NULL
 
-  # FIXME: check this this is really ok, that we dont trafo in mlrMBO
-  # x = trafoValue(par.set, or$x)
-  # FIXME: check how to select best point here
-  i = getOptPathBestIndex(opt.path, measureAggrName(measures[[1]]), ties = "random")
-  e = getOptPathEl(opt.path, i)
-  makeTuneResult(learner, control, removeMissingValues(e$x), e$y, opt.path)
+  or = mbofun(tff, ps2, design = NULL, learner = control$learner, control = mbo.control, show.info = FALSE)
+  x = trafoValue(par.set, or$x)
+  # we take the point that mbo proposes and its estimated y
+  makeTuneResult(learner, control, x, setNames(or$y, opt.path$y.names[1L]), opt.path)
 }
