b6bee8fe61a65c24312b4410f899d07a7f9f7694
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-21 14:20:18 +0200

    clean up

--- tests/testthat/test_surv_glmboost.R
+++ tests/testthat/test_surv_glmboost.R
@@ -2,15 +2,16 @@ context("surv_glmboost")
 
 test_that("surv_glmboost", {
   library(survival)
+  library(mboost)
   parset.list = list(
     list(mstop = 100L, nu = 0.1),
     list(mstop = 50L, nu = 1),
     list(mstop = 200L, nu = 0.5),
     list(mstop = 250L, nu = 0.05)
   )
-  
+
   old.predicts.list = list()
-  
+
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
     ctrl = boost_control(mstop = parset$mstop, nu = parset$nu)
@@ -18,10 +19,10 @@ test_that("surv_glmboost", {
     pars = list(f, data = surv.train, control = ctrl, family = CoxPH())
     set.seed(getOption("mlr.debug.seed"))
     m = do.call(glmboost, pars)
-    
+
     p  = predict(m, newdata = surv.test, type = "link")
     old.predicts.list[[i]] = drop(p)
   }
-  
+
   testSimpleParsets("surv.glmboost", surv.df, surv.target, surv.train.inds, old.predicts.list, parset.list)
 })
