8d6b39059035f8153820522009ab829c72ef18ba
studerus <erich.studerus@gmail.com>
2014-07-26 16:59:20 +0200

    New learner: classif.lqa

---
+++ R/RLearner_classif_lqa.R
@@ -0,0 +1,64 @@
+#' @export
+makeRLearner.classif.lqa = function() {
+  makeRLearnerClassif(
+    cl = "classif.lqa",
+    package = "lqa",
+    par.set = makeParamSet(
+      makeDiscreteLearnerParam(id = "penalty",
+        values = c("adaptive.lasso", "ao", "bridge", "enet", "fused.lasso", "genet", "icb", "lasso",
+                   "licb", "oscar", "penalreg", "ridge", "scad", "weighted.fusion")),
+      makeNumericLearnerParam(id = "lambda", lower = 0,
+        requires = expression(penalty %in% c("adaptive.lasso", "ao", "bridge", "genet", "lasso",
+                                             "oscar", "penalreg", "ridge", "scad"))),
+      makeNumericLearnerParam(id = "gamma", lower = 1 + .Machine$double.eps, 
+        requires = expression(penalty %in% c("ao", "bridge", "genet", "weighted.fusion"))),
+      makeNumericLearnerParam(id = "alpha", lower = 0, requires = expression(penalty == "genet")),
+      makeNumericLearnerParam(id = "c", lower = 0, requires = expression(penalty == "oscar")),
+      makeNumericLearnerParam(id = "a", lower = 2 + .Machine$double.eps,
+        requires = expression(penalty == "scad")),
+      makeNumericLearnerParam(id = "lambda1", lower = 0, 
+        requires = expression(penalty %in% c("enet", "fused.lasso", "icb", "licb", "weighted.fusion"))),
+      makeNumericLearnerParam(id = "lambda2", lower = 0,
+        requires = expression(penalty %in% c("enet", "fused.lasso", "icb", "licb", "weighted.fusion"))),
+      makeDiscreteLearnerParam(id = "method", default = "lqa.update2",
+        values = c("lqa.update2", "ForwardBoost", "GBlockBoost")),
+      makeNumericLearnerParam(id = "var.eps", default = .Machine$double.eps, lower = 0),
+      makeIntegerLearnerParam(id = "max.steps", lower = 1L, default = 5000L),
+      makeNumericLearnerParam(id = "conv.eps", default = 0.001, lower = 0),
+      makeLogicalLearnerParam(id = "conv.stop", default = TRUE),
+      makeNumericLearnerParam(id = "c1", default = 1e-08, lower = 0),
+      makeIntegerLearnerParam(id = "digits", default = 5L, lower = 1L)
+    ),
+    properties = c("numerics", "prob", "twoclass", "weights"),
+    par.vals = list()
+  )
+}
+
+#' @export
+trainLearner.classif.lqa = function(.learner, .task, .subset, .weights = NULL,
+                                    var.eps, max.steps, conv.eps, conv.stop, c1,
+                                    digits, ...) {
+  ctrl = learnerArgsToControl(lqa.control, var.eps, max.steps, conv.eps, conv.stop, c1, digits)
+  d = getTaskData(.task, .subset, target.extra = TRUE, recode.target = "01")
+  args = c(list(x = d$data, y = d$target, family = binomial(), control = ctrl), list(...))
+  rm(d)
+  is.tune.param = names(args) %in% c("lambda", "gamma", "alpha", "c", "a", "lambda1", "lambda2")
+  args$penalty = do.call(args$penalty, list(lambda = unlist(args[is.tune.param])))
+  args = args[!is.tune.param]
+  if (!is.null(.weights))
+    args$weights = .weights
+  
+  do.call(lqa, args)
+}
+
+#' @export
+predictLearner.classif.lqa = function(.learner, .model, .newdata, ...) {
+  p = predict(.model$learner.model, new.x = cbind(1, .newdata), ...)$mu.new
+  if(.learner$predict.type == "prob"){
+    p = cbind(1 - p, p)
+    colnames(p) = .model$task.desc$class.levels
+  } else {
+    p = factor(p > 0.5, c(FALSE, TRUE), .model$task.desc$class.levels)
+  }
+  return(p)
+}
---
+++ tests/testthat/test_classif_lqa.R
@@ -0,0 +1,31 @@
+context("classif_lqa")
+
+test_that("classif_lqa", {
+  library(lqa)
+  parset.list = list(
+    list(penalty = 'lasso', lambda = 0.01),
+    list(penalty = 'fused.lasso', lambda1 = 0.001, lambda2 = 0.01)
+  )
+
+  old.predicts.list = list()
+  old.probs.list = list()
+  
+  x = binaryclass.train
+  y = as.numeric(x[, binaryclass.class.col]) - 1
+  x[, binaryclass.class.col] = NULL
+  newx = cbind(1, binaryclass.test)
+  newx[, binaryclass.target] = NULL
+  
+  old.probs.list[[1]] = predict(lqa(x, y, family = binomial(), penalty = lasso(0.01)), newx)$mu.new
+  old.probs.list[[2]] = predict(lqa(x, y, family = binomial(), penalty = fused.lasso(c(0.001, 0.01))), newx)$mu.new
+  
+  old.predicts.list[[1]] =  factor(old.probs.list[[1]] > 0.5, c(TRUE, FALSE), binaryclass.class.levs)
+  old.predicts.list[[2]] =  factor(old.probs.list[[2]] > 0.5, c(TRUE, FALSE), binaryclass.class.levs)
+
+
+  testSimpleParsets("classif.lqa", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+    old.predicts.list, parset.list)
+  testProbParsets ("classif.lqa", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+    old.probs.list, parset.list)
+
+})
