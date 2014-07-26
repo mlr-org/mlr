560a2ecd405af322371b805bdb27024f0149c164
Bernd Bischl <bernd_bischl@gmx.net>
2014-07-26 17:22:27 +0200

    clean up lqa a little bit

--- tests/testthat/test_classif_lqa.R
+++ tests/testthat/test_classif_lqa.R
@@ -9,16 +9,16 @@ test_that("classif_lqa", {
 
   old.predicts.list = list()
   old.probs.list = list()
-  
+
   x = binaryclass.train
   y = as.numeric(x[, binaryclass.class.col]) - 1
   x[, binaryclass.class.col] = NULL
   newx = cbind(1, binaryclass.test)
   newx[, binaryclass.target] = NULL
-  
+
   old.probs.list[[1]] = predict(lqa(x, y, family = binomial(), penalty = lasso(0.01)), newx)$mu.new
   old.probs.list[[2]] = predict(lqa(x, y, family = binomial(), penalty = fused.lasso(c(0.001, 0.01))), newx)$mu.new
-  
+
   old.predicts.list[[1]] =  factor(old.probs.list[[1]] > 0.5, c(TRUE, FALSE), binaryclass.class.levs)
   old.predicts.list[[2]] =  factor(old.probs.list[[2]] > 0.5, c(TRUE, FALSE), binaryclass.class.levs)
 
