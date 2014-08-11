42299cce2cf1cb2f36907f3a49d20d2f19db23a5
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-11 19:46:13 +0200

    ClassifTask: set arg 'postive' in a better way in TaskDesc. We always need this property for binary tasks. When we create binary tasks via changeData, it was sometimes not set, leading to an invalid task

--- R/Task.R
+++ R/Task.R
@@ -46,7 +46,7 @@
 #'   during a resampling iteration.
 #'   Default is \code{NULL} which means no blocking.
 #' @param positive [\code{character(1)}]\cr
-#'   Positive class for binary classification.
+#'   Positive class for binary classification (otherwise ignored and set to NA).
 #'   Default is the first factor level of the target attribute.
 #' @param fixup.data [\code{character(1)}]\cr
 #'   Should some basic cleaning up of data be performed?
