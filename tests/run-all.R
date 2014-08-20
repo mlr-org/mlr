de5de11840566495bd10f89d44df0e3a4e440a50
Michel <michellang@gmail.com>
2014-08-20 11:54:12 +0200

    try to trick travis

--- tests/run-all.R
+++ tests/run-all.R
@@ -3,6 +3,15 @@ library(testthat)
 # make sure YOU run ALL!
 if (identical(Sys.getenv("TRAVIS"), "true")) {
   test_check("mlr")
+  categories = list.files("testthat", pattern = "^test_")
+  p = "^test(_[[:alnum:]]+_).+"
+  stopifnot(all(grepl(p, categories)))
+  categories = unique(sub("^test(_[[:alnum:]]+_).+", "\\1", categories))
+
+  for (category in categories) {
+    cat(sprintf("### Testing category '%s'", category), file = stdout)
+    test_check("mlr", filter = category)
+  }
 } else {
   test_check("mlr", filter = "base")
 }
