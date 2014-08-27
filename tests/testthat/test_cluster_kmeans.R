bb72f68f49686549bea79123bbd7a40ae8c869e8
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-27 21:34:43 +0200

    clean up

--- tests/testthat/test_cluster_kmeans.R
+++ tests/testthat/test_cluster_kmeans.R
@@ -1,22 +1,24 @@
 context("cluster_kmeans")
 
 test_that("cluster_kmeans", {
-    library(clue)
+  library(clue)
 
-    centers = matrix(c(1,2,3,4,2,3,4,5), ncol=4)
-	parset.list = list(
-        list(centers=centers)
-	)
-	
-	old.predicts.list = list()
-	
-	for (i in 1:length(parset.list)) {
-		parset = parset.list[[i]]
-		set.seed(getOption("mlr.debug.seed"))
-		m = kmeans(noclass.train, centers = centers)
-		p = as.integer(cl_predict(m, newdata = noclass.test, type = "class_ids"))
-		old.predicts.list[[i]] = p
-	}
-	
-	testSimpleParsets("cluster.kmeans", noclass.df, character(0L), noclass.train.inds, old.predicts.list, parset.list)
+  centers = matrix(c(1, 2, 3, 4, 2, 3, 4, 5), ncol = 4)
+  parset.list = list(
+    list(centers = centers),
+    list(centers = 2L)
+  )
+
+  old.predicts.list = list()
+
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    set.seed(getOption("mlr.debug.seed"))
+    m = kmeans(noclass.train, centers = centers)
+    p = as.integer(cl_predict(m, newdata = noclass.test, type = "class_ids"))
+    old.predicts.list[[i]] = p
+  }
+
+  testSimpleParsets("cluster.kmeans", noclass.df, character(0L), noclass.train.inds,
+    old.predicts.list, parset.list)
 })
