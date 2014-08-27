43956829094aa7d7fa81eac89b4ad6b192f7adcf
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-27 19:37:47 +0200

    save timing and other info at end of test_all on disk

--- test_all.R
+++ test_all.R
@@ -18,6 +18,8 @@ library(FNN)
 library(randomForestSRC)
 library(glmnet) # otherwise auc is shadowed
 library(caret) # otherwise train shadowed
+library(plyr)
+library(survival)
 
 if (interactive()) {
   load_all(".")
@@ -31,10 +33,13 @@ source("tests/testthat/helper_objects.R")
 args = commandArgs()
 file = args[which(args == "--args") + 1L]
 if (length(file) == 0 || is.na(file)) {
-  test.info = test_dir("tests/testthat", filter = "onv")
+  tests = test_dir("tests/testthat")
 } else {
   catf("Run test for file %s", file.path("tests", "testthat", file))
-  test.info = test_file(file.path("tests", "testthat", file))
+  tests = test_file(file.path("tests", "testthat", file))
 }
-save2(file = "testinfo.RData", test.info)
+tests$group = sapply(strsplit(tests$file, "_"), function(x) x[2])
+groups = ddply(tests, "group", summarise,
+  failed = sum(failed), error = sum(error), time = sum(real))
+save2(file = "testinfo.RData", tests, groups)
 
