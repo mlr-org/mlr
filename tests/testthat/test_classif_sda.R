5481ce505df6a3f3ae769a8334e0b063c300b702
Bernd Bischl bischl@Bischl <bernd_bischl@gmx.net>
2014-08-29 20:36:36 +0200

    suppress stupid sda output in unit test

--- tests/testthat/test_classif_sda.R
+++ tests/testthat/test_classif_sda.R
@@ -2,10 +2,12 @@ context("classif_sda")
 
 test_that("classif_sda", {
   library(sda)
-  set.seed(getOption("mlr.debug.seed"))
-  m = sda(as.matrix(dropNamed(multiclass.train, multiclass.target)), multiclass.train[, multiclass.target])
-  set.seed(getOption("mlr.debug.seed"))
-  p = predict(m, as.matrix(dropNamed(multiclass.test, multiclass.target)))
+  capture.output({
+    set.seed(getOption("mlr.debug.seed"))
+    m = sda(as.matrix(dropNamed(multiclass.train, multiclass.target)), multiclass.train[, multiclass.target])
+    set.seed(getOption("mlr.debug.seed"))
+    p = predict(m, as.matrix(dropNamed(multiclass.test, multiclass.target)))
+  })
 
   testSimple("classif.sda", multiclass.df, multiclass.target, multiclass.train.inds, p$class)
   testProb  ("classif.sda", multiclass.df, multiclass.target, multiclass.train.inds, p$posterior)
