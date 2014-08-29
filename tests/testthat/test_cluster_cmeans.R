04e9ecc1cead6527f8007436f40a23604b93b4f8
Lars Kotthoff <larsko@4c.ucc.ie>
2014-08-29 10:05:12 +0100

    test for fuzzy clustering

--- tests/testthat/test_cluster_cmeans.R
+++ tests/testthat/test_cluster_cmeans.R
@@ -19,4 +19,17 @@ test_that("cluster_cmeans", {
 	}
 	
 	testSimpleParsets("cluster.cmeans", noclass.df, character(0L), noclass.train.inds, old.predicts.list, parset.list)
+
+    # test fuzzy clustering memberships
+    set.seed(getOption("mlr.debug.seed"))
+    m = cmeans(noclass.train, centers = centers)
+    p = as.matrix(cl_predict(m, newdata = noclass.test, type = "memberships"))
+
+    lrn = makeLearner("cluster.cmeans", predict.type = "prob")
+    m = train(lrn, task = makeClusterTask(data = noclass.train))
+    pp = as.matrix(predict(m, newdata = noclass.test)$data)
+
+    print(p)
+    print(pp)
+    #expect_equal(p, pp, tol=1e-3)
 })
