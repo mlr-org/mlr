83f00dd7b1dc39dc565e259ead194118655c807c
Kassner <tobias.kassner@tu-dortmund.de>
2014-09-01 18:58:41 +0200

    added learner 'ctree for regression' and its test function

---
+++ R/RLearner_regr_ctree.R
@@ -0,0 +1,40 @@
+#' @export
+makeRLearner.regr.ctree = function() {
+  makeRLearnerRegr(
+    cl = "regr.ctree",
+    package = "party",
+    par.set = makeParamSet(
+      makeDiscreteLearnerParam(id = "teststat", default = "quad", values = c("quad", "max")),
+      makeDiscreteLearnerParam(id = "testtype", default = "Bonferroni", values = c("Bonferroni", "MonteCarlo", "Univariate", "Teststatistic")),
+      makeNumericLearnerParam(id = "mincriterion", default = 0.95, lower = 0, upper = 1),
+      makeIntegerLearnerParam(id = "minsplit", default = 20L, lower = 1L),
+      makeIntegerLearnerParam(id = "minbucket", default = 7L, lower = 1L),
+      makeLogicalLearnerParam(id = "stump", default = FALSE),
+      makeIntegerLearnerParam(id = "nresample", default = 9999L, lower = 1L, requires = expression(testtype=="MonteCarlo")),
+      makeIntegerLearnerParam(id = "maxsurrogate", default = 0L, lower = 0L),
+      makeIntegerLearnerParam(id = "mtry", default = 0L, lower = 0L),
+      makeLogicalLearnerParam(id = "savesplitstats", default = TRUE),
+      makeIntegerLearnerParam(id = "maxdepth", default = 0L, lower = 0L)
+    ),
+    properties = c("missings", "numerics", "factors", "weights"),
+    name = "Conditional Inference Trees",
+    short.name = "ctree",
+    note = ""
+  )
+}
+
+#' @export
+trainLearner.regr.ctree = function(.learner, .task, .subset, .weights = NULL, teststat, testtype,
+  mincriterion, minsplit, minbucket, stump, nresample, maxsurrogate, mtry,
+  savesplitstats, maxdepth, ...) {
+
+  ctrl = learnerArgsToControl(ctree_control, teststat, testtype, mincriterion, minsplit,
+    minbucket, stump, nresample, maxsurrogate, mtry, savesplitstats, maxdepth)
+  f = getTaskFormula(.task)
+  ctree(f, data = getTaskData(.task, .subset), controls = ctrl, ...)
+}
+
+#' @export
+predictLearner.regr.ctree = function(.learner, .model, .newdata, ...) {
+  predict(.model$learner.model, newdata = .newdata, ...)[, 1L]
+}
