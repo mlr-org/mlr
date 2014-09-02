969fc28df978e3fc2e63215a2cd0389af166e9c1
Michel <michellang@gmail.com>
2014-09-02 12:59:50 +0200

    cv.glmnet learner (work in progress)

---
+++ R/RLearner_surv_cv.glmnet.R
@@ -0,0 +1,66 @@
+#' @export
+makeRLearner.surv.cv.glmnet = function() {
+  makeRLearnerSurv(
+    cl = "surv.cv.glmnet",
+    package = "glmnet",
+    par.set = makeParamSet(
+      makeNumericLearnerParam(id = "alpha", default = 1, lower = 0, upper = 1),
+      makeIntegerLearnerParam(id = "nfolds", default = 10L, lower = 3L),
+      makeDiscreteLearnerParam(id = "type.measure", values = c("deviance"), default = "deviance"),
+      makeLogicalLearnerParam(id = "exact", default = FALSE, when = "predict"),
+      # FIXME bug?
+      # makeDiscreteLearnerParam(id = "s", values = c("lambda.1se", "lambda.min"), default = "lambda.1se", when = "predict"),
+      makeIntegerLearnerParam(id = "nlambda", default = 100L, lower = 1L),
+      makeNumericLearnerParam(id = "lambda.min.ratio", lower = 0, upper = 1),
+      makeLogicalLearnerParam(id = "standardize", default = TRUE),
+      makeLogicalLearnerParam(id = "intercept", default = TRUE),
+      makeNumericLearnerParam(id = "thresh", default = 1e-07, lower = 0),
+      makeIntegerLearnerParam(id = "dfmax", lower = 0L),
+      makeIntegerLearnerParam(id = "pmax", lower = 0L),
+      makeIntegerVectorLearnerParam(id = "exclude", lower = 1L), # this is like a subset?
+      makeNumericVectorLearnerParam(id = "penalty.factor", lower = 0, upper = 1),
+      makeNumericVectorLearnerParam(id = "lower.limits", upper = 0),
+      makeNumericVectorLearnerParam(id = "upper.limits", lower = 0),
+      makeIntegerLearnerParam(id = "maxit", default = 100000L, lower = 1L),
+      makeNumericLearnerParam(id = "fdev", default = 1.0e-5, lower = 0, upper = 1),
+      makeNumericLearnerParam(id = "devmax", default = 0.999, lower = 0, upper = 1),
+      makeNumericLearnerParam(id = "eps", default = 1.0e-6, lower = 0, upper = 1),
+      makeNumericLearnerParam(id = "big", default = 9.9e35),
+      makeIntegerLearnerParam(id = "mnlam", default = 5, lower = 1),
+      makeNumericLearnerParam(id = "pmin", default = 1.0e-9, lower = 0, upper = 1),
+      makeNumericLearnerParam(id = "exmx", default = 250.0),
+      makeNumericLearnerParam(id = "prec", default = 1e-10),
+      makeIntegerLearnerParam(id = "mxit", default = 100, lower = 1)
+    ),
+    properties = c("numerics", "weights", "rcens"),
+    name = "GLM with regularization (cross validated lambda)",
+    short.name = "cv.glmnet",
+    note = ""
+  )
+}
+
+#' @export
+trainLearner.surv.cv.glmnet = function(.learner, .task, .subset, .weights = NULL,  ...) {
+  d = getTaskData(.task, .subset, target.extra = TRUE, recode.target = "rcens")
+  args = c(list(x = as.matrix(d$data), y = d$target, family = "cox", parallel = FALSE), list(...))
+  rm(d)
+  if (!is.null(.weights))
+    args$weights = .weights
+
+  saved.ctrl = glmnet.control()
+  is.ctrl.arg = names(args) %in% names(saved.ctrl)
+  if (any(is.ctrl.arg)) {
+    on.exit(do.call(glmnet.control, saved.ctrl))
+    do.call(glmnet.control, args[is.ctrl.arg])
+    args = args[!is.ctrl.arg]
+  }
+
+  do.call(cv.glmnet, args)
+}
+
+#' @export
+predictLearner.surv.cv.glmnet = function(.learner, .model, .newdata, ...) {
+  if(.learner$predict.type == "response")
+    return(as.numeric(predict(.model$learner.model, newx = as.matrix(.newdata), type = "link", ...)))
+  stop("Unknown predict type")
+}
---
+++ tests/testthat/test_surv_cv.glmnet.R
@@ -0,0 +1,38 @@
+context("surv_cv.glmnet")
+
+# test_that("surv_cv.glmnet", {
+#   library(survival)
+#   library(glmnet)
+#   parset.list = list(
+#     list(),
+#     list(alpha = 0.3),
+#     list(alpha = 1, nlambda = 10),
+#     list(prec = 1e-3)
+#   )
+#
+#   old.predicts.list = list()
+#
+#   for (i in 1:length(parset.list)) {
+#     parset = parset.list[[i]]
+#     y = as.matrix(surv.train[, surv.target])
+#     colnames(y) = c("time", "status")
+#     pars = c(list(y = Surv(time=surv.train[, "time"], event=surv.train[, "event"]),
+#       x = as.matrix(surv.train[, -c(1,2,7)]), family = "cox"), parset)
+#     set.seed(getOption("mlr.debug.seed"))
+#     ctrl.args = names(formals(glmnet.control))
+#     if (any(names(pars) %in% ctrl.args)) {
+#       do.call(glmnet.control, pars[names(pars) %in% ctrl.args])
+#       m = do.call(cv.glmnet, pars[!names(pars) %in% ctrl.args])
+#       glmnet.control(factory = TRUE)
+#     } else {
+#       m = do.call(cv.glmnet, pars)
+#     }
+#     p  = predict(m, newx = as.matrix(surv.test[, -c(1,2,7)]), type = "link")
+#     old.predicts.list[[i]] = as.numeric(p)
+#   }
+#
+#   testSimpleParsets("surv.cv.glmnet", surv.df[, -7], surv.target, surv.train.inds, old.predicts.list, parset.list)
+#
+#   # check that we restored the factory default
+#   expect_true(glmnet.control()$prec < 1e-4) # should be ==1e-5
+# })
