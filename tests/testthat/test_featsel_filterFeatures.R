1f3d5bf5d270dcf3e644ed48e252a715be499c25
Michel <michellang@gmail.com>
2014-09-05 10:43:49 +0200

    added test for getFilterResults

--- tests/testthat/test_featsel_filterFeatures.R
+++ tests/testthat/test_featsel_filterFeatures.R
@@ -15,4 +15,22 @@ test_that("filterFeatures", {
   f1 = filterFeatures(multiclass.task, select = "abs", val = round(0.5 * ncol(multiclass.df)))
   f2 = filterFeatures(multiclass.task, val = 0.5)
   expect_equal(f1, f2)
+
+  lrn1 = makeFilterWrapper("classif.rpart", fw.val = 0.2)
+  m = train(lrn1, multiclass.task)
+  res = getFilterResult(m)
+  expect_is(res, "FilterResult")
+  expect_is(res, "character")
+  expect_equal(length(res), 1L)
+
+  lrn2 = makeFilterWrapper("classif.randomForest", fw.val = 0.5)
+  bench = benchmark(learners = list(lrn1, lrn2), tasks = multiclass.task, resampling = makeResampleDesc("CV", iters = 3))
+  res = getFilterResult(bench)
+  # FIXME: why is this a list?
+  expect_is(res, "list")
+  res = res[[1]]
+  expect_is(res, "list")
+  expect_true(setequal(names(res), c("classif.rpart.filtered", "classif.randomForest.filtered")))
+  res = res[[1]]
+  expect_is(res[[1]], "FilterResult")
 })
