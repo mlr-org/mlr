5067b045d2beb268aed49ea5b3fae838dbc9c529
studerus <erich.studerus@gmail.com>
2014-09-07 13:01:52 +0200

    New learner: classif.bartMachine

---
+++ R/RLearner_classif_bartMachine.R
@@ -0,0 +1,56 @@
+#' @export
+makeRLearner.classif.bartMachine = function() {
+  makeRLearnerClassif(
+    cl = "classif.bartMachine",
+    package = "bartMachine",
+    par.set = makeParamSet(
+      makeIntegerLearnerParam(id = "num_trees", default = 50L, lower = 1L),
+      makeIntegerLearnerParam(id = "num_burn_in", default = 250L, lower = 0L),
+      makeIntegerLearnerParam(id = "num_interations_after_burn_in", default = 1000L, lower = 0L),
+      makeNumericLearnerParam(id = "alpha", default = 0.95, lower = 0),
+      makeNumericLearnerParam(id = "beta", default = 2, lower = 0),
+      makeNumericLearnerParam(id = "k", default = 2, lower = 0),
+      makeNumericLearnerParam(id = "q", default = 0.9, lower = 0, upper = 1),
+      makeNumericLearnerParam(id = "prob_rule_class", default = 0.5, lower = 0, upper = 1),
+      makeNumericVectorLearnerParam(id = "mh_prob_steps", default = c(2.5, 2.5, 4)/9, len = 3L),
+      makeLogicalLearnerParam(id = "debug_log", default = FALSE),
+      makeLogicalLearnerParam(id = "run_in_sample", default = TRUE),
+      makeNumericVectorLearnerParam(id = "cov_prior_vec"),
+      makeLogicalLearnerParam(id = "use_missing_data", default = FALSE),
+      makeIntegerLearnerParam(id = "num_rand_samps_in_library", default = 10000, lower = 1),
+      makeLogicalLearnerParam(id = "use_missing_data_dummies_as_covars", default = FALSE),
+      makeLogicalLearnerParam(id = "replace_missing_data_with_x_j_bar", default = FALSE),
+      makeLogicalLearnerParam(id = "impute_missingness_with_rf_impute", default = FALSE),
+      makeLogicalLearnerParam(id = "impute_missingness_with_x_j_bar_for_lm", default = TRUE),
+      makeLogicalLearnerParam(id = "mem_cache_for_speed", default = TRUE),
+      makeLogicalLearnerParam(id = "verbose", default = TRUE)
+    ),
+    properties = c("numerics", "prob", "twoclass", "factors", "missings"),
+    name = "Bayesian Additive Regression Trees",
+    short.name = "bartMachine",
+    note = ""
+  )
+}
+
+#' @export
+trainLearner.classif.bartMachine = function(.learner, .task, .subset, .weights = NULL, ...) {
+  d = getTaskData(.task, .subset, target.extra = TRUE)
+  bartMachine(X = d$data, y = d$target, ...)
+}
+
+#' @export
+predictLearner.classif.bartMachine = function(.learner, .model, .newdata, ...) {
+  levs = c(.model$task.desc$negative, .model$task.desc$positive)
+  if(.learner$predict.type == "prob"){
+    p = predict(.model$learner.model, new_data = .newdata, type = "prob", ...)
+    y = matrix(0, ncol = 2, nrow = nrow(.newdata))
+    colnames(y) = levs
+    y[, 1L] = 1-p
+    y[, 2L] = p
+  } else {
+    y = predict(.model$learner.model, new_data = .newdata, type = "class", ...)
+    y = factor(y, levs)
+  }
+  destroy_bart_machine(.model$learner.model)
+  return(y)
+}
---
+++ tests/testthat/test_classif_bartMachine.R
@@ -0,0 +1,46 @@
+context("classif_bartMachine")
+
+test_that("classif_bartMachine", {
+  library(bartMachine)
+  parset.list = list(
+    list(),
+    list(alpha = 0.8, num_trees = 25L)
+  )
+
+  old.predicts.list = list()
+  old.probs.list = list()
+
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    x = binaryclass.train
+    y = x[, binaryclass.class.col]
+    x[, binaryclass.class.col] = NULL
+    pars = list(X = x, y = y, verbose = FALSE)
+    pars = c(pars, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = do.call(bartMachine, pars)
+    newx = binaryclass.test
+    newx[, binaryclass.class.col] = NULL
+    set.seed(getOption("mlr.debug.seed"))
+    p = predict(m, new_data = newx, type = "class")
+    set.seed(getOption("mlr.debug.seed"))
+    p2 = predict(m, new_data = newx, type = "prob")
+    destroy_bart_machine(m)
+    old.predicts.list[[i]] = p
+    old.probs.list[[i]] = p2
+  }
+
+  #Does not yet work because we can not yet set the seed for bartMachine, see
+  #https://github.com/kapelner/bartMachine/issues/2
+  #testSimpleParsets("classif.bartMachine", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+  #  old.predicts.list, parset.list)
+  #testProbParsets ("classif.bartMachine", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+  #  old.probs.list, parset.list)
+  
+  for(i in 1:length(parset.list)){
+    expect_true(length(old.predicts.list[[i]]) == nrow(binaryclass.test))
+    expect_true(length(old.probs.list[[i]]) == nrow(binaryclass.test))
+  }
+
+
+})
