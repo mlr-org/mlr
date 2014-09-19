c313274bfb05cb3f3a610b889ca5d9bc9cdc39d3
schiffner <schiffner@math.uni-duesseldorf.de>
2014-09-19 13:25:12 +0200

    added several data sets

--- R/datasets.R
+++ R/datasets.R
@@ -22,17 +22,19 @@ NULL
 #' @docType data
 NULL
 
-#' Sonar classification task
+#' Wisconsin Breast Cancer classification task
 #'
-#' Contains the task (\code{sonar.task}) and a matching resample instance
-#' (\code{sonar.rin}).
+#' Contains the task (\code{bc.task}) and a matching resample instance
+#' (\code{bc.rin}).
 #'
-#' @name mlr.sonar
-#' @aliases sonar.task sonar.rin
-#' @references See \code{\link[mlbench]{Sonar}}.
+#' @name mlr.bc
+#' @aliases bc.task bc.rin
+#' @references See \code{\link[mlbench]{BreastCancer}}.
+#'   The column \code{"Id"} and all incomplete cases have been removed from the task.
 #' @keywords data
 #' @docType data
 NULL
+
 
 #' Boston Housing regression task
 #'
@@ -46,6 +48,7 @@ NULL
 #' @docType data
 NULL
 
+
 #' Wisonsin Prognostic Breast Cancer (WPBC) survival task
 #'
 #' Contains the task (\code{wpbc.task}) and a matching resample instance
@@ -55,6 +58,61 @@ NULL
 #' @aliases wpbc.task wpbc.rin
 #' @references See \code{\link[TH.data]{wpbc}}.
 #'  Incomplete cases have been removed from the task.
+#' @keywords data
+#' @docType data
+NULL
+
+#' NCCTG Lung Cancer survival task
+#'
+#' Contains the task (\code{lung.task}) and a matching resample instance
+#' (\code{lung.rin}).
+#'
+#' @name mlr.lung
+#' @aliases lung.task lung.rin
+#' @references See \code{\link[survival]{lung}}.
+#'  Incomplete cases have been removed from the task.
+#' @keywords data
+#' @docType data
+NULL
+
+
+#' Motor Trend Car Road Tests clustering task
+#'
+#' Contains the task (\code{mtcars.task}) and a matching resample instance
+#' (\code{mtcars.rin}).
+#'
+#' @name mlr.mtcars
+#' @aliases mtcars.task mtcars.rin
+#' @references See \code{\link[datasets]{mtcars}}.
+#' @keywords data
+#' @docType data
+NULL
+
+#' European Union Agricultural Workforces clustering task
+#'
+#' Contains the task (\code{agri.task}) and a matching resample instance
+#' (\code{agri.rin}).
+#'
+#' @name mlr.agri
+#' @aliases agri.task agri.rin
+#' @references See \code{\link[cluster]{agriculture}}.
+#' @keywords data
+#' @docType data
+NULL
+
+
+#' Iris cost-sensitive classification task
+#'
+#' Contains the task (\code{costIris.task}) and a resample instance
+#' (\code{costIris.rin}).
+#'
+#' @name mlr.costIris
+#' @aliases costIris.task costIris.rin
+#' @references See \code{\link[datasets]{iris}}.
+#'   The cost matrix was generated artificially following
+#'
+#'   Tu, H.-H. and Lin, H.-T. (2010), One-sided support vector regression for multiclass cost-sensitive classification.
+#'   In ICML, J. Fürnkranz and T. Joachims, Eds., Omnipress, 1095--1102. 
 #' @keywords data
 #' @docType data
 NULL
--- inst/makeData.R
+++ inst/makeData.R
@@ -7,17 +7,12 @@ dn = "../data"
 stopifnot(isDirectory(dn))
 DATASEED = 7761
 
+# classification
 set.seed(DATASEED)
 data(iris, package = "datasets")
 iris.task = makeClassifTask("iris-example", data = iris, target = "Species")
 iris.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = iris.task)
 save(iris.task, iris.rin, file = file.path(dn, "mlr.iris.RData"))
-
-set.seed(DATASEED)
-data(BostonHousing, package = "mlbench")
-bh.task = makeRegrTask("BostonHousing-example", data = BostonHousing, target = "medv")
-bh.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = bh.task)
-save(bh.task, bh.rin, file = file.path(dn, "mlr.bh.RData"), compress = "xz")
 
 
 set.seed(DATASEED)
@@ -26,6 +21,25 @@ sonar.task = makeClassifTask("Sonar-example", data = Sonar, target = "Class")
 sonar.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = sonar.task)
 save(sonar.task, sonar.rin, file = file.path(dn, "mlr.sonar.RData"), compress = "xz")
 
+
+set.seed(DATASEED)
+data(BreastCancer, package = "mlbench")
+BreastCancer$Id = NULL
+BreastCancer = BreastCancer[complete.cases(BreastCancer), ]
+bc.task = makeClassifTask("BreastCancer-example", data = BreastCancer, target = "Class")
+bc.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = bc.task)
+save(bc.task, bc.rin, file = file.path(dn, "mlr.bc.RData"), compress = "xz")
+
+
+# regression
+set.seed(DATASEED)
+data(BostonHousing, package = "mlbench")
+bh.task = makeRegrTask("BostonHousing-example", data = BostonHousing, target = "medv")
+bh.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = bh.task)
+save(bh.task, bh.rin, file = file.path(dn, "mlr.bh.RData"), compress = "xz")
+
+
+# survival analysis
 set.seed(DATASEED)
 data(wpbc, package = "TH.data")
 wpbc$status = ifelse(wpbc$status == "R", 1L, 0L)
@@ -33,3 +47,37 @@ wpbc = wpbc[complete.cases(wpbc), ]
 wpbc.task = makeSurvTask("wpbc-example", data = wpbc, target = c("time", "status"))
 wpbc.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = wpbc.task)
 save(wpbc.task, wpbc.rin, file = file.path(dn, "mlr.wpbc.RData"), compress = "xz")
+
+
+set.seed(DATASEED)
+data(lung, package = "survival")
+lung$status = lung$status - 1
+lung = lung[complete.cases(lung), ]
+lung.task = makeSurvTask("lung-example", data = lung, target = c("time", "status"))
+lung.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = lung.task)
+save(lung.task, lung.rin, file = file.path(dn, "mlr.lung.RData"), compress = "xz")
+
+
+# cluster analysis
+set.seed(DATASEED)
+data(mtcars, package = "datasets")
+mtcars.task = makeClusterTask("mtcars-example", data = mtcars)
+mtcars.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = mtcars.task)
+save(mtcars.task, mtcars.rin, file = file.path(dn, "mlr.mtcars.RData"), compress = "xz")
+
+
+set.seed(DATASEED)
+data(agriculture, package = "cluster")
+agri.task = makeClusterTask("agriculture-example", data = agriculture)
+agri.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = agri.task)
+save(agri.task, agri.rin, file = file.path(dn, "mlr.agri.RData"), compress = "xz")
+
+
+# cost-sensitive classification
+set.seed(DATASEED)
+data(iris, package = "datasets")
+cost = matrix(runif(150*3,0,2000), 150) * (1 - diag(3))[iris$Species,]
+iris$Species = NULL
+costIris.task = makeCostSensTask("cost-sensitive iris-example", data = iris, cost = cost)
+costIris.rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), task = costIris.task)
+save(costIris.task, costIris.rin, file = file.path(dn, "mlr.costIris.RData"), compress = "xz")
