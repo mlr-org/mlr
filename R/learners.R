371145396555d6687634e32fed7afd9d3fa1f10f
Bernd Bischl <bernd_bischl@gmx.net>
2014-09-26 22:29:36 +0200

    clean up

--- R/learners.R
+++ R/learners.R
@@ -1,9 +1,9 @@
 #' @title List of supported learning algorithms.
 #'
 #' @description
-#'   All supported learners can be found by \link{\code{listLearners}} or as a handy table in the tutorial appendix: \url{http://berndbischl.github.io/mlr/tutorial/html/integrated_learners/}.
+#' All supported learners can be found by \link{\code{listLearners}} or as a table
+#' in the tutorial appendix: \url{http://berndbischl.github.io/mlr/tutorial/html/integrated_learners/}.
 #'
 #' @name learners
 #' @rdname learners
-#' @seealso listLearners
 NULL
