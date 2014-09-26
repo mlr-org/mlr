f5908b18d2fb4a49f85c2a896cf4f490e50e6051
Michel <michellang@gmail.com>
2014-09-26 11:45:55 +0200

    dropped data frame method for joinClassLevels

--- tests/testthat/test_base_joinClassLevels.R
+++ tests/testthat/test_base_joinClassLevels.R
@@ -1,17 +1,16 @@
 context("joinClassLevels")
 
 test_that("joinClassLevels", {
-  expect_error(joinClassLevels(iris, "foo"))
-  expect_error(joinClassLevels(iris, "Species", new.levels = list(c("setosa", "virginica"))))
-  expect_error(joinClassLevels(iris, "Species", new.levels = list(a = c("setosa", "virginica1"))))
-  expect_error(joinClassLevels(iris, "Species", new.levels =
-      list(a = c("setosa", "virginica"), b = "virginica")), "once")
+  expect_error(joinClassLevels(multiclass.task, "foo"), "list")
+  expect_error(joinClassLevels(multiclass.task, new.levels = list(c("setosa", "virginica"))), "named")
+  expect_error(joinClassLevels(multiclass.task, new.levels = list(a = c("setosa", "virginica1"))), "existing")
+  expect_error(joinClassLevels(multiclass.task, new.levels = list(a = c("setosa", "virginica"), b = "virginica")), "once")
 
-  d = joinClassLevels(iris, "Species", new.levels = list(a = c("setosa", "virginica")))
+  d = getTaskData(joinClassLevels(multiclass.task, new.levels = list(a = c("setosa", "virginica"))))
   expect_true(is.factor(d$Species))
   expect_equal(sum(d$Species == "versicolor"), 50L)
   expect_equal(sum(d$Species == "a"), 100L)
-  d = joinClassLevels(iris, "Species", new.levels = list(a = "setosa", b = "virginica"))
+  d = getTaskData(joinClassLevels(multiclass.task, new.levels = list(a = "setosa", b = "virginica")))
   expect_true(is.factor(d$Species))
   expect_equal(sum(d$Species == "versicolor"), 50L)
   expect_equal(sum(d$Species == "a"), 50L)
@@ -23,6 +22,3 @@ test_that("joinClassLevels", {
   expect_equal(sum(d$Species == "versicolor"), 50L)
   expect_equal(sum(d$Species == "a"), 100L)
 })
-
-
-
