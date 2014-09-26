2c635b7c0ac56b8f22d3ec957bf14aa5981896b0
Michel <michellang@gmail.com>
2014-09-26 12:14:09 +0200

    dropped data frame method in normalizeFeatures

--- tests/testthat/test_base_normalizeFeatures.R
+++ tests/testthat/test_base_normalizeFeatures.R
@@ -1,18 +1,17 @@
 context("normalizeFeatures")
 
 test_that("normalizeFeatures", {
-  df = data.frame(x = c(0,-1,4,2,3), y = letters[1:5])
-  expect_equal(df, normalizeFeatures(df, method="range", range=c(-1,4)))
-  expect_equal(df, normalizeFeatures(df, method = "center", exclude="x"))
-  x = normalizeFeatures(df, method = "standardize")$x
+  df = data.frame(x = c(0,-1,4,2,3), y = letters[1:5], target = letters[1:5])
+  task = makeClassifTask(data = df, target = "target")
+  expect_equal(df, getTaskData(normalizeFeatures(task, method="range", range=c(-1,4))))
+  expect_equal(df, getTaskData(normalizeFeatures(task, method = "center", exclude="x")))
+  x = getTaskData(normalizeFeatures(task, method = "standardize"))$x
   expect_equal(mean(x), 0)
   expect_equal(sd(x), 1)
-  x = normalizeFeatures(df, method = "center")$x
+  x = getTaskData(normalizeFeatures(task, method = "center"))$x
   expect_equal(mean(x), 0)
 
   norm.task = normalizeFeatures(multiclass.task, method="range")
   expect_equal(norm.task$task.desc, multiclass.task$task.desc)
   expect_equal(range(norm.task$env$data$Sepal.Width), c(0,1))
 })
-
-
