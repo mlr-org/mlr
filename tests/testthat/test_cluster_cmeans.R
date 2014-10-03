2e7726a827e1e3d9ac18c089ac582374b17da04a
Lars Kotthoff <larsko@4c.ucc.ie>
2014-10-03 21:48:52 +0100

    fix broken test

--- tests/testthat/test_cluster_cmeans.R
+++ tests/testthat/test_cluster_cmeans.R
@@ -30,5 +30,7 @@ test_that("cluster_cmeans", {
   m = train(lrn, task = makeClusterTask(data = noclass.train))
   pp = as.matrix(predict(m, newdata = noclass.test)$data)
 
-  expect_equal(p, pp, check.attributes = FALSE)
+  expect_equal(getMaxIndexOfRows(p), pp[,1], check.attributes = FALSE)
+  expect_equal(p[,1], pp[,2], check.attributes = FALSE)
+  expect_equal(p[,2], pp[,3], check.attributes = FALSE)
 })
