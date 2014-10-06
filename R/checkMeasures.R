b22d2fb6eed0e488c7cacb1c01d923338e29d7aa
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-06 21:24:58 +0200

    check that we were passed at least 1 measure

--- R/checkMeasures.R
+++ R/checkMeasures.R
@@ -3,7 +3,7 @@ checkMeasures = function(measures, obj, aggr = NULL) {
     measures = default.measures(obj)
   } else {
     measures = ensureVector(measures, n = 1L, cl = "Measure")
-    assertList(measures, types = "Measure")
+    assertList(measures, types = "Measure", min.len = 1L)
   }
   if (!is.null(aggr))
     measures = lapply(measures, setAggregation, aggr = aggr)
