957c90eb28eff45eb263d4d65e6c7f94f1ac4a61
Jakob Bossek <info@jakobbossek.de>
2014-10-08 12:20:21 +0200

    add forresters reinterpolation method

--- tests/testthat/test_regr_kmforrester.R
+++ tests/testthat/test_regr_kmforrester.R
@@ -1,14 +1,17 @@
-#context("regr_kmforrester")
-#
-#test_that("regr_kmforrester", {
-#  library(DiceKriging)
-#  parset.list = list(
-#   # list(covtype="gauss"),
-#    list(covtype="matern5_2")
-#  )
-#  dd = regr.df[1:50, c(1:3, 14)]
-#  rt = makeRegrTask(data=dd, target=regr.target)
-#  res = makeResampleDesc("Holdout")
-#  r = resample(makeLearner("regr.kmforrester"), rt, res)
-#  checkTrue(!is.na(r$aggr["mse.test.mean"]))
-#})
+context("regr_kmforrester")
+
+test_that("regr_kmforrester", {
+  library(DiceKriging)
+
+  x.seq = c(1, 2, 3)
+  dd = data.frame(x = x.seq, y = x.seq^2 + rnorm(length(x.seq)))
+
+  rt = makeRegrTask(data = dd, target = "y")
+
+  learner = makeLearner("regr.kmforrester", predict.type = "se")
+  mod = train(learner = learner, task = rt)
+
+  # check if standard error vanishes at the design points
+  preds = predict(mod, newdata = data.frame(x = x.seq))
+  expect_true(all(preds$se == 0))
+})
