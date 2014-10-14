147d928bfa8ebbfcee47652d36a1459ad05b7bc5
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-14 11:18:59 +0200

    make extra test group for parallel tests

---
+++ tests/run-parallel.R
@@ -0,0 +1,6 @@
+library(testthat)
+i# FIXME: I am not sure if enabling this leads to travis 'hanging'. we currently must test it locally
+# if (identical(Sys.getenv("TRAVIS"), "true")) {
+  # test_check("mlr", "_imbal_")
+# }
+
