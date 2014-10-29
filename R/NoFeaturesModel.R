edebb3e0682fec987f4cc0bc704c16825d59ecc6
Michel <michellang@gmail.com>
2014-10-29 12:55:48 +0100

    fixed nofeaturesmodel for wrapping

--- R/NoFeaturesModel.R
+++ R/NoFeaturesModel.R
@@ -5,8 +5,9 @@ makeNoFeaturesModel = function(targets, task.desc) {
 
 
 predict_nofeatures = function(model, newdata) {
-  y = getLearnerModel(model)$targets
-  type = model$learner$type
+  lrn = getLearnerModel(model)
+  y = lrn$targets
+  type = lrn$type
   # for regression return constant mean
   if (type == "regr") {
     return(rep(mean(y), nrow(newdata)))
@@ -14,14 +15,14 @@ predict_nofeatures = function(model, newdata) {
   if (type == "classif") {
     tab = prop.table(table(y))
     probs = as.numeric(tab)
-    if (model$learner$predict.type == "response")
+    if (lrn$predict.type == "response")
       return(sample(as.factor(names(tab)), nrow(newdata), prob = probs, replace = TRUE))
     probs = t(replicate(nrow(newdata), probs))
     colnames(probs) = names(tab)
     return(probs)
   }
   if (type == "surv") {
-    if (model$learner$predict.type == "response")
+    if (lrn$predict.type == "response")
       return(runif(nrow(y)))
     # FIXME: probs / brier for survival should use something like median survival time
   }
