e47ce222596d0162c72c931b3d1e44ad2de1d62d
Bernd Bischl <bernd_bischl@gmx.net>
2014-10-29 01:11:27 +0100

    clean up

--- tests/testthat/test_featsel_filterFeatures.R
+++ tests/testthat/test_featsel_filterFeatures.R
@@ -22,23 +22,11 @@ test_that("filterFeatures", {
 
   lrn1 = makeFilterWrapper("classif.rpart", fw.perc = 0.2)
   m = train(lrn1, multiclass.task)
-  res = getFilterResult(m)
-  expect_is(res, "FilterResult")
-  expect_is(res, "character")
-  expect_equal(length(res), 1L)
+  f = getFilteredFeatures(m)
+  expect_is(f, "character")
+  expect_equal(length(f), 1L)
 
-  lrn2 = makeFilterWrapper("classif.randomForest", fw.perc = 0.5)
-  bench = benchmark(learners = list(lrn1, lrn2), tasks = multiclass.task, resampling = makeResampleDesc("CV", iters = 3))
-  res = getFilterResult(bench)
-  # FIXME: why is this a list?
-  expect_is(res, "list")
-  res = res[[1]]
-  expect_is(res, "list")
-  expect_true(setequal(names(res), c("classif.rpart.filtered", "classif.randomForest.filtered")))
-  res = res[[1]]
-  expect_is(res[[1]], "FilterResult")
-
-  ## Loop through all filters
+  # Loop through all filters
   filter.list = listFilterMethods(desc = FALSE, tasks = TRUE, features = FALSE)
   filter.list.classif = as.character(filter.list$id)[filter.list$task.classif]
   filter.list.classif = setdiff(filter.list.classif, c("univariate")) #make extra test
@@ -50,6 +38,7 @@ test_that("filterFeatures", {
     filterFeatures(task = regr.num.task, method = filter, perc = 0.5)
   }
 
-  ## extra test of univariate filter
-  getFilterValues(task = multiclass.task, method = "univariate", perc = 0.5, perf.learner = makeLearner("classif.rpart"), measures = mmce)
+  # extra test of univariate filter
+  getFilterValues(task = multiclass.task, method = "univariate", perc = 0.5,
+    perf.learner = makeLearner("classif.rpart"), measures = mmce)
 })
