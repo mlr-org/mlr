f9b9768f10fdb9e381b3f377aedb924934701719
Kassner <tobias.kassner@tu-dortmund.de>
2014-10-30 21:48:49 +0100

    added argument checks

--- todo-files/generateLearningCurve.R
+++ todo-files/generateLearningCurve.R
@@ -3,26 +3,30 @@
 generateLearningCurve = function(lrn, task, test.size = 0.3, test.inds = NULL,
   n.seq = seq(0.1, 1, by = 0.1), measures, repls = 1L)  {
 
-  l = length(lrn)
-  for (i in 1:l) {
-    lrn[[i]] = checkLearner(lrn[[i]])
-  }
+  assertClass(task, "Task")
+  assertNumeric(test.size, lower = 0L, upper = 1L, len = 1)
+  assertNumeric(n.seq, lower = 0L, upper = 1L, min.len = 2L, any.missing = FALSE)
+  repls = asInt(repls, lower = 1L)
   
-  # assertClass(task, "Task")
-  n = task$task.desc$size
-  # assertNumeric(n.seq, min.len = 2L, any.missing = FALSE)
   if (is.null(test.inds)) {
     test.inds = makeResampleInstance("Holdout", task = task, split = test.size)$train[[1L]]
   } else {
     test.inds = asInteger(test.inds)
   }
+  
+  l = length(lrn)
+  for (i in 1:l) {
+    lrn[[i]] = checkLearner(lrn[[i]])
+  }
+  
+  m = length(measures)
   measures = checkMeasures(measures, task)
-  # repls = asInt(repls, lower = 1L)
+  
+  n = task$task.desc$size
   k = length(n.seq)
   inds.all = setdiff(1:n, test.inds)
   
-  perfs = replicate(l, array(NA, dim = c(repls, k, length(measures))),
-                    simplify = FALSE)
+  perfs = replicate(l, array(NA, dim = c(repls, k, m)), simplify = FALSE)
   lrn.names = unlist(lapply(lrn, function(x) x$short.name))
   measure.names = sapply(measures, measureAggrName)
   names(perfs) = lrn.names
@@ -30,39 +34,32 @@ generateLearningCurve = function(lrn, task, test.size = 0.3, test.inds = NULL,
     dimnames(perfs[[i]]) = list(1:repls, n.seq, measure.names)
   }
   
-  n.obs = numeric(length(n.seq))
+  more = diff(c(0, round(n.seq * (n - length(test.inds)))))
+  n.obs = cumsum(more)
   for (repl in 1:repls) {
-    # inds = sample(n.seq[1L])
-    m.last = 0
     rest = inds.all
     inds.last = integer(0L)
     for (j in 1:k) {
-      m = n.seq[j]
-      more = (m - m.last) * (n - length(test.inds))
-      inds.new = sample(rest, more)
+      inds.new = sample(rest, more[j])
       inds.cur = c(inds.last, inds.new)
-      if (repl == 1) {
-        n.obs[j] = length(inds.cur) 
-      }
       for (i in 1:l) {
         mod = train(lrn[[i]], task, subset = inds.cur)
         pred = predict(mod, task, subset = test.inds)
         perfs[[i]][repl, j, ] = performance(pred, task = task, measures = measures)
       }
-      m.last = m
       inds.last = inds.cur
     }
   }
   
   res = data.frame()
   for (j in 1:l) {
-    for (i in 1:length(measures)) {
+    for (i in 1:m) {
       new = data.frame(n.obs = n.obs, perfs = colMeans(perfs[[j]])[,i],
-                       measure = measure.names[i],
-                       learner = lrn.names[j])
+                       measure = measure.names[i], learner = lrn.names[j])
       res = rbind(res, new)
     }
   }
+  rownames(res) = NULL
   
   return(res)
 }
@@ -74,7 +71,7 @@ plotLearningCurve = function(res) {
 }
 
 
-# r1 = generateLearningCurve(lrn = list("classif.rpart", "classif.lda", "classif.knn"),
+# r1 = generateLearningCurve(lrn = list("classif.rpart", "classif.knn", "classif.svm"),
 #                           task = iris.task, measures = list(mmce, acc), repls = 12L)
 # plotLearningCurve(r1)
 
