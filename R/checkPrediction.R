b1804e7ead486c78f5cef6bceb264be82cc033df
Lars Kotthoff <larsko@4c.ucc.ie>
2014-11-12 12:22:18 +0000

    fix typo

--- R/checkPrediction.R
+++ R/checkPrediction.R
@@ -1,5 +1,5 @@
 checkPrediction = function(pred, task.type = NULL, predict.type = NULL, check.truth = FALSE) {
-  assertClass(p, "Prediction")
+  assertClass(pred, "Prediction")
   if (!is.null(task.type) && pred$task.desc$type %nin% task.type)
     stopf("Prediction must be one of '%s', but is: '%s'", collapse(task.type), pred$task.desc$type)
   if (!is.null(predict.type) && pred$predict.type %nin% predict.type)
