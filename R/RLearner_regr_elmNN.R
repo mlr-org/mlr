780c3e0ef5650808ef1717cc69a2e1d4723828cd
studerus <erich.studerus@gmail.com>
2014-11-16 14:19:04 +0100

    New learner: elmNN

---
+++ R/RLearner_regr_elmNN.R
@@ -0,0 +1,28 @@
+#' @export
+makeRLearner.regr.elmNN = function() {
+  makeRLearnerRegr(
+    cl = "regr.elmNN",
+    package = "elmNN",
+    par.set = makeParamSet(
+      makeIntegerLearnerParam(id = "nhid", default = 1L, lower = 1L),
+      makeDiscreteLearnerParam(id = "actfun", default = "sig", values = c("sig", "radbas", "hardlim",
+        "hardlims", "satlins", "tansig", "tribas", "poslin", "purelin"))
+    ),
+    par.vals = list(nhid = 1L, actfun = "sig"),
+    properties = c("numerics"),
+    name = "Extreme Learning Machine for Single Hidden Layer Feedforward Neural Networks",
+    short.name = "elmNN",
+    note = "nhid has been set to 1 and actfun has been set to \"sig\" by default"
+  )
+}
+
+#' @export
+trainLearner.regr.elmNN = function(.learner, .task, .subset, .weights = NULL, ...) {
+  d = getTaskData(.task, .subset, target.extra = TRUE)
+  elmNN::elmtrain(x = as.matrix(d$data), y = d$target, ...)
+}
+
+#' @export
+predictLearner.regr.elmNN = function(.learner, .model, .newdata, ...) {
+  elmNN::predict.elmNN(.model$learner.model, newdata = as.matrix(.newdata), ...)[,1L]
+}
