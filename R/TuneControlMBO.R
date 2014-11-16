3f359011dbf2567d341626a23fe662186a82968f
Michel <michellang@gmail.com>
2014-11-16 21:46:39 +0100

    added argument 'continue' to mbo

--- R/TuneControlMBO.R
+++ R/TuneControlMBO.R
@@ -2,21 +2,26 @@
 
 #FIXME: export when mbo is on cran
 
-# @param learner [\code{\link{Learner}}] \cr
+# @param learner [\code{\link{Learner}}]\cr
 #   Regression learner to model performance landscape.
+# @param continue [\code{logical(1)}]\cr
+#   Resume calculation from previous run using \code[mboContinue}?
+#   Requires \dQuote{save.file.path) to be set.
 # @param mbo.control [\code{\link[mlrMBO]{MBOControl}}] \cr
 #   Control object for model-based optimization tuning.
 # @export
 # @rdname TuneControl
 makeTuneControlMBO = function(same.resampling.instance = TRUE, impute.val = NULL, learner, mbo.control,
-  tune.threshold = FALSE, tune.threshold.args = list(), log.fun = NULL) {
+  tune.threshold = FALSE, tune.threshold.args = list(), continue = FALSE, log.fun = NULL) {
 
   assertClass(learner, classes = "Learner")
   assertClass(mbo.control, "MBOControl")
+  assertFlag(continue)
   x = makeTuneControl(same.resampling.instance = same.resampling.instance, impute.val = impute.val,
     start = NULL, tune.threshold = tune.threshold, tune.threshold.args = tune.threshold.args,
     cl = "TuneControlMBO", log.fun = log.fun)
   x$learner = learner
   x$mbo.control = mbo.control
+  x$continue = continue
   return(x)
 }
--- R/tuneMBO.R
+++ R/tuneMBO.R
@@ -15,15 +15,21 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
     par.set = par.set, ctrl = control, opt.path = opt.path, show.info = show.info,
     convertx = cx, remove.nas = TRUE)
 
-  #FIXME: remove this when mbo on cran
-  mbofun = get("mbo", envir = getNamespace("mlrMBO"))
-
   # remove trafos for mbo, we do this in tunerFitnFun
   ps2 = par.set
   for (i in seq_along(ps2$pars))
     ps2$pars[[i]]$trafo = NULL
 
-  or = mbofun(tff, ps2, design = NULL, learner = control$learner, control = mbo.control, show.info = FALSE)
+  state = mbo.control$save.file.path
+  if (control$continue && file.exists(state)) {
+    messagef("Resuming previous MBO run using state in '%s'...", state)
+    or = mboContinue(state)
+  } else {
+    # FIXME: remove this when mbo on cran
+    mbofun = get("mbo", envir = getNamespace("mlrMBO"))
+    or = mbofun(tff, ps2, design = NULL, learner = control$learner, control = mbo.control, show.info = FALSE)
+  }
+
   x = trafoValue(par.set, or$x)
   # we take the point that mbo proposes and its estimated y
   makeTuneResult(learner, control, x, setNames(or$y, opt.path$y.names[1L]), opt.path)
