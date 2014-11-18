33ffc216db028a7cb2a01c9fcc88f51641eb6925
unknown <richter@Hiwi-Weihs.fb05.statistik.uni-dortmund.de>
2014-11-18 12:20:40 +0100

    typo in propVectorToMatrix

--- R/helpers.R
+++ R/helpers.R
@@ -67,7 +67,7 @@ propVectorToMatrix = function(p, levs) {
   assertNumeric(p)
   y = matrix(0, ncol = 2L, nrow = length(p))
   colnames(y) = levs
-  y[, 1L] = x
-  y[, 2L] = 1-x
+  y[, 1L] = p
+  y[, 2L] = 1-p
   y
 }
