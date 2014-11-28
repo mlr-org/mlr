dcba9346c19e70d598e3b70bef5602a5b4dd76f4
Michel <michellang@gmail.com>
2014-11-28 10:59:40 +0100

    updated mlr->be connector

--- todo-files/batchmark.R
+++ todo-files/batchmark.R
@@ -1,43 +1,44 @@
-mlrBenchmark = function(reg, learners, tasks, resamplings, measures = NULL, repls = 1L, save.models = FALSE, overwrite = FALSE, pm.opts = list()) {
+batchmark = function(reg, learners, tasks, resamplings, measures = NULL, repls = 1L, save.models = FALSE, overwrite = FALSE, pm.opts = list()) {
+  # not needed for BE>=1.4, remove then
   fixID = function(x) gsub(".", "_", x, fixed = TRUE)
 
   BatchExperiments:::checkExperimentRegistry(reg)
   if ("mlr" %nin% names(reg$packages))
     stop("mlr is required on the slaves, please add mlr via 'addRegistryPackages'")
- 
+
   learners = ensureVector(learners, 1L, cl = "Learner")
   assertList(learners, types = "Learner", min.len = 1L)
   learner.ids = vcapply(learners, "[[", "id")
   if (anyDuplicated(learner.ids))
     stop("Duplicated learner ids found")
-  
+
   tasks = ensureVector(tasks, 1L, cl = "Task")
   assertList(tasks, types = "Task", min.len = 1L)
   task.ids = vcapply(tasks, getTaskId)
   if (anyDuplicated(task.ids))
     stop("Duplicated task ids found")
-  
+
   resamplings = ensureVector(resamplings, length(tasks), "ResampleDesc")
   assertList(resamplings, "ResampleDesc", len = length(tasks))
- 
+
   if (is.null(measures)) {
     measures = default.measures(tasks[[1L]])
   } else {
     measures = ensureVector(measures, 1L, "Measure")
     assertList(measures, types = "Measure", min.len = 1L)
   }
- 
+
   assertCount(repls)
   assertFlag(save.models)
   assertFlag(overwrite)
   assertList(pm.opts, names = "named")
 
   # generate problems
-  pdes = Map(function(id, task, rdesc) {
+  pdes = Map(function(id, task, rdesc, seed) {
     static = list(rdesc = rdesc, task = task)
-    addProblem(reg, id, static = static, dynamic = resample.fun, overwrite = overwrite)
+    addProblem(reg, id, static = static, dynamic = resample.fun, overwrite = overwrite, seed = seed)
     makeDesign(id, design = data.frame(i = seq_len(rdesc$iters)))
-  }, id = fixID(task.ids), task = tasks, rdesc = resamplings)
+  }, id = fixID(task.ids), task = tasks, rdesc = resamplings, seed = reg$seed + seq_along(tasks))
 
   # generate algos
   ades = Map(function(id, learner) {
