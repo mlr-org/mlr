05f8d7e6966d24d2b853449847e03ac2c81c65cb
Michel <michellang@gmail.com>
2014-12-03 00:43:32 +0100

    fixes for optimCBP

--- R/RLearner_surv_optimCoxBoostPenalty.R
+++ R/RLearner_surv_optimCoxBoostPenalty.R
@@ -11,9 +11,11 @@ makeRLearner.surv.optimCoxBoostPenalty = function() {
       makeDiscreteLearnerParam(id = "criterion", default = "pscore", values = c("pscore", "score", "hpscore", "hscore")),
       makeNumericLearnerParam(id = "stepsize.factor", default = 1, lower = 0),
       makeDiscreteLearnerParam(id = "sf.scheme", default = "sigmoid", values = c("sigmoid", "linear")),
-      makeIntegerLearnerParam(id = "iter.max", default = 10, lower = 1)
+      makeIntegerLearnerParam(id = "iter.max", default = 10, lower = 1),
+      makeLogicalLearnerParam(id = "return.score", default = TRUE)
       # FIXME: still missing some arguments
     ),
+    par.vals = list(return.score = FALSE),
     properties = c("numerics", "factors", "weights", "rcens"),
     name = "Cox Proportional Hazards Model with Componentwise Likelihood based Boosting, automatic tuning enabled",
     short.name = "optimCoxBoostPenalty",
@@ -22,31 +24,27 @@ makeRLearner.surv.optimCoxBoostPenalty = function() {
 }
 
 #' @export
-trainLearner.surv.optimCoxBoostPenalty = function(.learner, .task, .subset, .weights = NULL, ...) {
+trainLearner.surv.optimCoxBoostPenalty = function(.learner, .task, .subset, .weights = NULL, return.score, ...) {
   data = getTaskData(.task, subset = .subset, target.extra = TRUE, recode.target = "rcens")
   info = getFixDataInfo(data$data, factors.to.dummies = TRUE, ordered.to.int = TRUE)
-  data$data = as.matrix(fixDataForLearner(data$data, info))
 
-  cb = CoxBoost::optimCoxBoostPenalty(
+  pars = c(list(
     time = data$target[, 1L],
     status = data$target[, 2L],
-    x = data$data,
-    weights = .weights,
-    ...
-  )
+    x = as.matrix(fixDataForLearner(data$data, info)),
+    weights = .weights
+  ), list(...))
+  rm(data)
 
+  cb = do.call(CoxBoost::optimCoxBoostPenalty, pars)
   if (cb$cv.res$optimal.step == 0L)
     stop("Error modeling CoxBoost: Could not determine the optimal step number")
 
-  attachTrainingInfo(CoxBoost::CoxBoost(
-    time = data$target[, 1L],
-    status = data$target[, 2L],
-    x = data$data,
-    weights = .weights,
-    stepno = cb$cv.res$optimal.step,
-    penalty = cb$penalty,
-    ...
-  ), info)
+  # remove iter max which may not be passed down to CoxBoost directly
+  pars$iter.max = NULL
+
+  pars = insert(pars, list(stepno = cb$cv.res$optimal.step, penalty = cb$penalty, return.score = return.score))
+  attachTrainingInfo(do.call(CoxBoost::CoxBoost, pars), info)
 }
 
 #' @export
