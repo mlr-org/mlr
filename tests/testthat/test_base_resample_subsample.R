61560f64dac3b95cdcd70169554e680402e4aeb5
Bernd Bischl <bernd_bischl@gmx.net>
2014-12-02 18:19:14 +0100

    clean up

--- tests/testthat/test_base_resample_subsample.R
+++ tests/testthat/test_base_resample_subsample.R
@@ -36,6 +36,6 @@ test_that("subsampling instance is stochastic", {
   }
   rin1 = makeResampleInstance(makeResampleDesc("Subsample", iters = 3), size = 500)
   rin2 = makeResampleInstance(makeResampleDesc("Subsample", iters = 3), size = 500)
-  expect_true(!all(sort(rin1$test.inds[[1]]) = = sort(rin2$test.inds[[1]])))
+  expect_true(!all(sort(rin1$test.inds[[1]]) == sort(rin2$test.inds[[1]])))
 })
 
