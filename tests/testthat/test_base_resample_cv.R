da581689726fe2acb9910b8b0a52d2a8db87d0b7
Bernd Bischl <bernd_bischl@gmx.net>
2014-12-02 18:17:55 +0100

    merge test group resample with base

--- tests/testthat/test_base_resample_cv.R
+++ tests/testthat/test_base_resample_cv.R
@@ -28,3 +28,24 @@ test_that("cv resampling works", {
 
   testCV("classif.rpart", multiclass.df, multiclass.target, tune.train = tt, tune.predict = tp, parset = parset)
 })
+
+test_that("cv instance works is stochastic", {
+  rin = makeResampleInstance(makeResampleDesc("CV", iters = 3), size = 25)
+
+  folds = rin$desc$iters
+  expect_equal(folds, 3)
+
+  for (i in 1:folds) {
+    i1 = rin$train.inds[[i]]
+    i2 = rin$test.inds[[i]]
+    expect_true(min(i1) >= 1)
+    expect_true(max(i1) <= 25)
+    expect_true(min(i2) >= 1)
+    expect_true(max(i2) <= 25)
+    expect_equal(sort(c(unique(i1), i2)), 1:25)
+  }
+  rin1 = makeResampleInstance(makeResampleDesc("CV", iters = 2L), size = 500)
+  rin2 = makeResampleInstance(makeResampleDesc("CV", iters = 2L), size = 500)
+  expect_true(!all(sort(rin1$test.inds[[1]]) == sort(rin2$test.inds[[1]])))
+})
+
