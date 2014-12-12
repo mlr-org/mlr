9a73be3e1266737800e1d7a1ec07c99cc35785b5
studerus <erich.studerus@gmail.com>
2014-12-12 17:29:56 +0100

    Some work on plotROCCoords

--- todo-files/getROCCoords.R
+++ todo-files/getROCCoords.R
@@ -17,24 +17,14 @@
 #' mod = train(lrn, sonar.task)
 #' ps = predict(mod, sonar.task)
 #' coords = getROCCoords(ps)
-#' library(ggplot2)
-#' ggplot(coords, aes(x = fpr, y = tpr)) +
-#' geom_segment(aes(x = 0, y = 0, xend = 1, yend = 1), color = "grey", size = 0.5) +
-#' geom_path() +
-#' theme_bw() +
-#' coord_fixed(ratio = 1)
+#' plotROCCoords(coords)
 #'
 #' lrn1 = makeLearner("classif.lda", predict.type = "prob")
 #' lrn2 = makeLearner("classif.ksvm", predict.type = "prob")
 #' rdesc = makeResampleDesc("CV", iters = 10, predict = "both")
 #' res = benchmark(list(lrn1, lrn2), list(pid.task, sonar.task), rdesc)
 #' coords = getROCCoords(res)
-#' ggplot(coords, aes(x = fpr, y = tpr, group = learner, color = learner)) +
-#' geom_segment(aes(x = 0, y = 0, xend = 1, yend = 1), color = "grey", size = 0.5) +
-#' geom_path() +
-#' theme_bw() +
-#' coord_fixed(ratio = 1) +
-#' facet_grid(task ~ set)
+#' plotROCCoords(coords)
 #' }
 #' @export
 getROCCoords = function(obj, thresholds = 50L) {
@@ -54,7 +44,7 @@ getROCCoords.Prediction = function(obj, thresholds = 50L) {
   data = rbind(data.frame(threshold = 1, tpr = 0, fpr = 0), data, data.frame(threshold = 0, tpr = 1, fpr = 1))
   data = data[!duplicated(data$thres, fromLast = TRUE),]
   rownames(data) = NULL
-  makeS3Obj("ROCCoords", data = data)
+  makeS3Obj("ROCCoords", data = data, averaged = FALSE)
 }
 
 #' @export
@@ -74,7 +64,7 @@ getROCCoords.ResamplePrediction = function(obj, thresholds = 50L) {
     cbind(thres, tpr, fpr)
   })
   data = ddply(coords, ~ set + thres, summarize, tpr = mean(tpr), fpr = mean(fpr))
-  makeS3Obj("ROCCoords", data = data)
+  makeS3Obj("ROCCoords", data = data, averaged = TRUE)
 }
 
 #' @export
@@ -90,11 +80,12 @@ getROCCoords.BenchmarkResult = function(obj, thresholds = 50L) {
   for (i in seq_along(obj)) {
     x = obj[[i]]
     for (j in seq_along(x)) {
-      res[[k]] = data.frame(task = names(obj)[i], learner = names(x)[j], getROCCoords(x[[j]]$pred, thresholds))
+      res[[k]] = data.frame(task = names(obj)[i], learner = names(x)[j], getROCCoords(x[[j]]$pred, thresholds)$data)
       k = k + 1L
     }
   }
-  do.call(rbind, res)
+  data = do.call(rbind, res)
+  makeS3Obj("ROCCoords", data = data, averaged = TRUE)
 }
 
 #' @title Plot ROC coords with ggplot2.
@@ -104,10 +95,23 @@ getROCCoords.BenchmarkResult = function(obj, thresholds = 50L) {
 #'
 #' @family roc
 #' @export
-plotROCCords = function(coords, line.size = 0.5, coord.fixed = TRUE) {
-  pl = ggplot(coords, aes(x = fpr, y = tpr)) +
-  pl = pl + geom_segment(aes(x = 0, y = 0, xend = 1, yend = 1), color = "grey", size = line.size) +
-  pl = pl + geom_path(size = line.size)
+plotROCCoords = function(coords, line.size = 0.5, coord.fixed = TRUE, xlab = NULL, ylab = NULL) {
+  dat = coords$data
+  if (is.null (xlab))
+    xlab = paste0(ifelse(coords$averaged, "Average f", "F"), "alse positive rate")
+  if (is.null (ylab))
+    ylab = paste0(ifelse(coords$averaged, "Average t", "T"), "rue positive rate")
+  n = c(learner = nlevels(dat$learner), task = nlevels(dat$task), set = nlevels(dat$set)) 
+  vars = names(sort(n[n > 1L], decreasing = TRUE))
+  color.var = if(!length(vars)) NULL else vars[1]
+  pl = ggplot(dat, aes(x = fpr, y = tpr)) +
+    geom_segment(aes(x = 0, y = 0, xend = 1, yend = 1), color = "grey", size = line.size) +
+    labs(y = ylab, x = xlab) +
+    geom_path(size = line.size, aes_string(color = color.var, group = color.var))
+  if (length(vars) == 2)
+    pl = pl + facet_wrap(reformulate(vars[2L]))
+  if (length(vars) == 3)
+    pl = pl + facet_grid(reformulate(vars[2L], vars[3L]))
   if (coord.fixed)
     pl = pl + coord_fixed(ratio = 1)
   return(pl)
