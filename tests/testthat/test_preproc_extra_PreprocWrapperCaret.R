705b75104ea0f55409f631293ab0523bd67d8087
Jakob Richter <code@jakob-r.de>
2014-12-14 22:34:09 +0100

    Update test_preproc_extra_PreprocWrapperCaret.R

--- tests/testthat/test_preproc_extra_PreprocWrapperCaret.R
+++ tests/testthat/test_preproc_extra_PreprocWrapperCaret.R
@@ -3,7 +3,7 @@ context("PreprocWrapperCaret")
 test_that("basic PreprocWrapperCaret works", {
   lrn1 = makeLearner("classif.rpart")
   lrn2 = makePreprocWrapperCaret(lrn1, method = c("center"))
-  m = mlr::train(lrn2, multiclass.task)  
+  m = train(lrn2, multiclass.task)  
   p = predict(m, multiclass.task)
   perf = performance(p, mmce)
   expect_true(perf < 0.1)
