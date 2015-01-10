7f727a1a55a52fc515d616ce33b7ccde5c379531
Bernd Bischl <bernd_bischl@gmx.net>
2015-01-10 11:33:14 +0100

    clean up

--- tests/testthat/test_classif_binomial.R
+++ tests/testthat/test_classif_binomial.R
@@ -9,22 +9,22 @@ test_that("classif_binomial", {
 
   old.predicts.list = list()
   old.probs.list = list()
+  nof = 1:55 # remove feats
 
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    print(parset)
     set.seed(getOption("mlr.debug.seed"))
-    m = glm(formula = binaryclass.formula, data = binaryclass.train, family = binomial(link = parset$link))
-    p  = predict(m, newdata = binaryclass.test)
-    p.prob = 1-p
-    p.class = as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
+    m = glm(formula = binaryclass.formula, data = binaryclass.train[, -nof], family = binomial(link = parset$link))
+    p  = predict(m, newdata = binaryclass.test[,-nof], type = "response")
+    p = 1 - p
+    p.class = as.factor(binaryclass.class.levs[ifelse(p > 0.5, 1, 2)])
     old.predicts.list[[i]] = p.class
-    old.probs.list[[i]] = p.prob
+    old.probs.list[[i]] = p
   }
 
-  testSimpleParsets("classif.binomial", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+  testSimpleParsets("classif.binomial", binaryclass.df[,-nof], binaryclass.target, binaryclass.train.inds,
     old.predicts.list, parset.list)
-  testProbParsets  ("classif.binomial", binaryclass.df, binaryclass.target, binaryclass.train.inds,
+  testProbParsets  ("classif.binomial", binaryclass.df[,-nof], binaryclass.target, binaryclass.train.inds,
     old.probs.list, parset.list)
 })
 
