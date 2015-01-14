58d00d8b002fa05e8c7b6abd633575b10aaa0ac7
Michel <michellang@gmail.com>
2015-01-14 15:43:13 +0100

    cleanup

--- todo-files/test_cluster_DBScan.R
+++ todo-files/test_cluster_DBScan.R
@@ -1,22 +1,23 @@
 context("cluster_DBScan")
 
 test_that("cluster_DBScan", {
-  library(RWeka)
-	parset.list = list(
-			list(),
-			list(M=10)
-	)
-	
-	old.predicts.list = list()
-	
-	for (i in 1:length(parset.list)) {
-		parset = parset.list[[i]]
-		ctrl = do.call(Weka_control, parset)
-		set.seed(getOption("mlr.debug.seed"))
-		m = DBScan(noclass.train, control=ctrl)
-		p = predict(m, noclass.test) + 1
-		old.predicts.list[[i]] = p
-	}
-	
-	testSimpleParsets("cluster.DBScan", noclass.df, character(0L), noclass.train.inds, old.predicts.list, parset.list)
+  skip("issue #130")
+
+  requirePackages("RWeka")
+  parset.list = list(
+    list()
+  )
+
+  old.predicts.list = list()
+
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    ctrl = do.call(RWeka::Weka_control, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = RWeka::DBScan(noclass.train, control=ctrl)
+    p = predict(m, noclass.test) + 1
+    old.predicts.list[[i]] = p
+  }
+
+  testSimpleParsets("cluster.DBScan", noclass.df, character(0L), noclass.train.inds, old.predicts.list, parset.list)
 })
