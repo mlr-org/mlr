8c3c0422f5ab0dd34ef704421022dc0c360adecb
Bernd Bischl <bernd_bischl@gmx.net>
2015-01-23 18:55:18 +0100

    clean up

--- tests/testthat/test_learners_classiflabelswitch.R
+++ tests/testthat/test_learners_classiflabelswitch.R
@@ -1,38 +1,67 @@
 
-# n = 50L
-# p = 2L
-# mydata1 = matrix(runif(2*n*p), nrow = 2*n, ncol = p)
-# mydata1 = as.data.frame(mydata1)
-# mydata1[1:n,]  = mydata1[1:n,] + 10L
-# mydata1[51:100,]  = mydata1[51:100,] - 10L
-# mydata1$y = factor(rep(c("a", "b"), each = c(n)))
+n = 50L
+p = 2L
+mydata1 = matrix(runif(2*n*p), nrow = 2*n, ncol = p)
+mydata1 = as.data.frame(mydata1)
+mydata1[1:n,]  = mydata1[1:n,] + 10L
+mydata1[(n+1):(2*n),]  = mydata1[(n+1):(2*n),] - 10L
+mydata1$y = factor(rep(c("a", "b"), each = c(n)))
+mydata2 = mydata1
+mydata2$y = factor(rep(c("a", "b"), each = c(n)), levels = c("b", "a"))
 
-# hpars = list(
-#   classif.lssvm = list(kernel = "vanilladot")
-#   # classif.LiblineaRLogReg = list(type = 7),
-#   # classif.LiblineaRBinary = list(type = 1),
-#   # classif.LiblineaRMultiClass = list(type = 1)
-# )
+mydata3 = matrix(runif(3*n*p), nrow = 3*n, ncol = p)
+mydata3 = as.data.frame(mydata3)
+mydata3[1:n,]  = mydata3[1:n,] + 10L
+mydata3[(n+1):(2*n),]  = mydata3[(n+1):(2*n),] - 10L
+mydata3$y = factor(rep(c("a", "b", "c"), each = c(n)))
+mydata4 = mydata3
+mydata4$y = factor(rep(c("a", "b", "c"), each = c(n)), levels = c("c", "b", "a"))
+
+hpars = list(
+  classif.lssvm = list(kernel = "vanilladot"),
+  classif.LiblineaRLogReg = list(type = 7),
+  classif.LiblineaRBinary = list(type = 1),
+  classif.LiblineaRMultiClass = list(type = 1)
+)
 
 
-# test_that("two class works with predtype response", {
-#   configureMlr(on.learner.error = "warn", show.learner.output = FALSE)
-#   mytask1 = makeClassifTask(data = mydata1, target = "y", positive = "a")
-#   lrns = listLearners(mytask1, create = TRUE)
-#   lids = extractSubList(lrns, "id")
-#   names(lrns) = lids
-#   toremove = grepl("classif.mock", lids)
-#   toremove = toremove | grepl("classif.LiblineaRMultiClass", lids)
-#   lrns = lrns[!toremove]
-#   errs = vnapply(lrns, function(lrn) {
-#     id = lrn$id
-#     hps = hpars[[id]]
-#     if (!is.null(hps))
-#       lrn = setHyperPars(lrn, par.vals = hps)
-#     holdout(lrn, mytask1, split = 0.5, stratify = TRUE)$aggr[[1L]]
-#   })
-#   print(lids)
-#   print(sort(errs, na.last = TRUE))
+test_that("no labels are switched", {
+  configureMlr(on.learner.error = "warn", show.learner.output = FALSE)
+  mytask1a = makeClassifTask(data = mydata1, target = "y", positive = "a")
+  mytask1b = makeClassifTask(data = mydata1, target = "y", positive = "b")
+  mytask2a = makeClassifTask(data = mydata2, target = "y", positive = "a")
+  mytask2b = makeClassifTask(data = mydata2, target = "y", positive = "b")
+  mytask3 = makeClassifTask(data = mydata3, target = "y")
+  mytask4 = makeClassifTask(data = mydata4, target = "y")
 
-# })
+
+  checkErrsForTask = function(task, predtype) {
+    props = if (predtype == "response") character(0L) else "prob"
+    lrns = listLearners(task, create = TRUE, properties = props)
+    lids = extractSubList(lrns, "id")
+    names(lrns) = lids
+    toremove = grepl("classif.mock", lids)
+    toremove = toremove | grepl("classif.LiblineaRMultiClass", lids)
+    lrns = lrns[!toremove]
+
+    errs = vnapply(lrns, function(lrn) {
+      lrn = setPredictType(lrn, predtype)
+      id = lrn$id
+      hps = hpars[[id]]
+      if (!is.null(hps))
+        lrn = setHyperPars(lrn, par.vals = hps)
+      holdout(lrn, task, split = 0.5, stratify = TRUE)$aggr[[1L]]
+    })
+    expect_true(all(!is.na(errs) & errs <= 0.3))
+    print(sort(errs, na.last = TRUE))
+  }
+  for (predtype in c("response", "prob")) {
+    checkErrsForTask(mytask1a, predtype)
+    checkErrsForTask(mytask1b, predtype)
+    checkErrsForTask(mytask2a, predtype)
+    checkErrsForTask(mytask2b, predtype)
+    checkErrsForTask(mytask3, predtype)
+    checkErrsForTask(mytask4, predtype)
+  }
+})
 
