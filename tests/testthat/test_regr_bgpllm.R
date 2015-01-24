beb4b370a0830a7a21c043cc30083fcf2cec4a6e
Karin Schork <schork.karin@gmail.com>
2015-01-24 17:08:47 +0100

    add new learner regr.bgpllm (from package tgp)

---
+++ tests/testthat/test_regr_bgpllm.R
@@ -0,0 +1,20 @@
+context("regr_bgpllm")
+
+test_that("regr_bgpllm", {
+  requirePackages("tgp", default.method = "load")
+  parset.list = list(
+    list(meanfn = "linear", bprior = "bflat", corr = "expsep")
+  )
+  y = regr.train[, regr.target]
+  old.predicts.list = list()
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    pars = list(X = regr.train[, 1:3], Z = y, verb = 0, pred.n = FALSE)
+    pars = c(pars, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m <- do.call(tgp::bgpllm, pars)
+    
+    old.predicts.list[[i]] = predict(m, XX = regr.test[, 1:3], pred.n = FALSE)$ZZ.km
+  }
+  testSimpleParsets("regr.bgpllm", regr.df[, c(1:3, 14)], regr.target, regr.train.inds, old.predicts.list, parset.list)
+})
