c2633a5ea4d7861929dc2408602d102470e45c58
Bernd Bischl <bernd_bischl@gmx.net>
2015-01-30 18:40:16 +0100

    clean up

--- tests/testthat/test_classif_blackboost.R
+++ tests/testthat/test_classif_blackboost.R
@@ -28,7 +28,7 @@ test_that("classif_blackboost", {
     set.seed(getOption("mlr.debug.seed"))
     old.predicts.list[[i]] = predict(m, newdata = binaryclass.test, type = "class")
     set.seed(getOption("mlr.debug.seed"))
-    old.probs.list[[i]] = predict(m, newdata = binaryclass.test, type = "response")[,1]
+    old.probs.list[[i]] = 1 - predict(m, newdata = binaryclass.test, type = "response")[,1]
   }
 
   testSimpleParsets("classif.blackboost", binaryclass.df, binaryclass.target,
