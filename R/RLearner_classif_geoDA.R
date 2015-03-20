bed1b6f0f72b81df713be04c850972fe937d557d
Jakob Richter <code@jakob-r.de>
2015-03-20 12:52:18 +0100

    added recode.target = "drop.levels" where necessary (Fixes Issue #169)

--- R/RLearner_classif_geoDA.R
+++ R/RLearner_classif_geoDA.R
@@ -13,7 +13,7 @@ makeRLearner.classif.geoDA = function() {
 
 #' @export
 trainLearner.classif.geoDA = function(.learner, .task, .subset, .weights = NULL,  ...) {
-  d = getTaskData(.task, .subset, target.extra = TRUE)
+  d = getTaskData(.task, .subset, target.extra = TRUE, recode.target = "drop.levels")
   DiscriMiner::geoDA(variables = d$data, group = d$target, ...)
 }
 
