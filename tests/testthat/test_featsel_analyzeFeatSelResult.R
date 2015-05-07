df2fbddcb2dace036ebb72cc428e9aaeed952d49
Pascal Kerschke <pascal.kerschke@wi.uni-muenster.de>
2015-05-07 17:37:03 +0200

    fixed bug in selectFeaturesSequential (cf. issues 245 and 246)

--- tests/testthat/test_featsel_analyzeFeatSelResult.R
+++ tests/testthat/test_featsel_analyzeFeatSelResult.R
@@ -9,3 +9,14 @@ test_that("analyzeFeatSelResult", {
     control = ctrlSeq, show.info = FALSE)
   expect_output(analyzeFeatSelResult(sfSeq, reduce = TRUE), "Petal.Width")
 })
+
+test_that("analyzeFeatSelResult with tune threshold (cf. issue #245)", {
+  set.seed(1909)
+  ctrl = makeFeatSelControlSequential(method = "sfs", alpha = 0.1, tune.threshold = TRUE)
+  rdesc = makeResampleDesc("Holdout")
+  lrn = makeLearner("classif.lda", predict.type = "prob")
+  task = subsetTask(sonar.task, features = paste("V", 11:16, sep = ""))
+  sfeats = selectFeatures(learner = lrn, task = task, resampling = rdesc, control = ctrl,
+    show.info = FALSE)
+  expect_output(analyzeFeatSelResult(sfeats, reduce = TRUE), "V12")
+})
