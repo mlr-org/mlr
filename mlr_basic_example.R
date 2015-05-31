20374dbdfa14d751270bf3b493375920cfe4cc13
IcedragonP <philipp_probst@gmx.de>
2015-05-31 09:56:26 +0200

    measures for classif added

--- mlr_basic_example.R
+++ mlr_basic_example.R
@@ -1,8 +1,11 @@
+library(devtools)
+load_all("/media/philipp/DATA/Studium/12.Semester/Masterarbeit/mlr_multilabel/mlr")
+
 library(mlr)
 
 # Beispiel
 data(iris)
-iris <- data.frame(iris,as.factor(sample(1:3,150,replace=T)))
+iris <- data.frame(iris,as.factor(sample(c("TRUE","FALSE"),150,replace=T)))
 colnames(iris)[6] <- "neu"
 ## Define the task:
 task = makeClassifTask(id = "tutorial", data = iris[-c(1:10),], target = "Species")
@@ -22,11 +25,31 @@ pr2 = predict(tr2,newdata=iris[1:10,])
 rdesc = makeResampleDesc(method = "CV", stratify = FALSE)
 ## Do the resampling:
 r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE)
-r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE, measures = list(mmce, ber))
-r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE)
+r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE, measures = list(timetrain,mmce))
+
+r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,timetrain))
+r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(mmce))
+r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,mmce,acc,timetrain))
 a <- traceback()
 
 r2$pred$data$V1
 ## Get the mean misclassification error:
 r2$aggr
 
+# With probabilities
+lrn = makeLearner("classif.randomForest",predict.type="prob")
+task = makeClassifTask(id = "tutorial", data = iris[-c(1:10),], target = "neu")
+tr = train(lrn,task)
+pr = predict(tr,newdata=iris)
+plotROCRCurves(pr,diagonal=T)
+
+task = makeMultilabelTask(id = "tutorial", data = iris[-c(1:10),], target = c("neu","Species"))
+tr = train(lrn,task)
+pr = predict(tr,newdata=iris[1:10,])
+
+
+
+lrn1 = makeLearner("classif.lda", predict.type = "prob")
+mod1 = train(lrn1, sonar.task)
+pred1 = predict(mod1, task = sonar.task)
+plotROCRCurves(pred1)
