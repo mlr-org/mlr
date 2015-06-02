9888b742d73a4162f7d683f706d804fb1fc06dcf
IcedragonP <philipp_probst@gmx.de>
2015-06-02 10:15:32 +0200

    example updated

--- mlr_basic_example.R
+++ mlr_basic_example.R
@@ -13,12 +13,19 @@ task2 = makeMultilabelTask(id="multi",data = iris[-c(1:10),], target= c("Species
 
 ## Define the learner:
 lrn = makeLearner("classif.randomForest")
+lrn2 = makeMultilabelWrapper("classif.rpart")
+lrn2 = makeLearner("classif.randomForest", par.vals=list(ntree=30))
+lrn2 = makeMultilabelWrapper(lrn2)
 ## Train 
 tr = train(lrn,task)
-tr2 = train(lrn,task2)
+tr2 = train(lrn,task2) # geht nicht, da class des learners nicht MultilabelWrapper
+tr2 = train(lrn2,task2)
+
+tr = train(lrn, task, subset = 50:100)
+tr2 = train(lrn2,task2, subset = 50:100) # schlechte Vorhersage mit dem Subset (test ob Subset funktioniert)
 
 ## Predict
-pr = predict(tr,newdata=iris[1:10])
+pr = predict(tr,newdata=iris[1:10,])
 pr2 = predict(tr2,newdata=iris[1:10,])
 pr2 = predict(tr2,newdata=iris[1:10,-c(5,6)])
 
@@ -28,9 +35,9 @@ rdesc = makeResampleDesc(method = "CV", stratify = FALSE)
 r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE)
 r = resample(learner = lrn, task = task, resampling = rdesc, show.info = FALSE, measures = list(timetrain,mmce))
 
-r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,timetrain))
-r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(mmce))
-r2 = resample(learner = lrn, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,mmce,acc,timetrain))
+r2 = resample(learner = lrn2, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,timetrain))
+r2 = resample(learner = lrn2, task = task2, resampling = rdesc, show.info = FALSE, measures = list(mmce))
+r2 = resample(learner = lrn2, task = task2, resampling = rdesc, show.info = FALSE, measures = list(hamloss,mmce,acc,timetrain))
 
 r2$pred$data$V1
 ## Get the mean misclassification error:
@@ -44,7 +51,7 @@ pr1 = predict(tr,newdata=iris[1:10,])
 plotROCRCurves(pr1,diagonal=T)
 
 task = makeMultilabelTask(id = "tutorial", data = iris[-c(1:10),], target = c("neu","Species"))
-tr = train(lrn,task)
+tr = train(makeMultilabelWrapper(lrn),task)
 pr2 = predict(tr,newdata=iris[1:10,]) 
 
 # ROCR Curves
@@ -53,11 +60,11 @@ iris <- data.frame(iris,as.factor(sample(c("TRUE","FALSE"),150,replace=T)),
                    as.factor(sample(c("TRUE","FALSE"),150,replace=T)))
 colnames(iris)[6:7] <- c("neu1","neu2")
 task = makeMultilabelTask(id = "tutorial", data = iris[-c(1:10),], target = c("neu1","neu2"))
-tr = train(lrn,task)
+tr = train(makeMultilabelWrapper(lrn),task)
 pr2 = predict(tr,newdata=iris[1:10,]) 
 
 plotROCRCurves(pr2)
-
+# wie soll positive und negative bestimmt werden?
 
 
 
