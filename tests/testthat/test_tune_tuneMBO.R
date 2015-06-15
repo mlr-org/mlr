0ec1635a6763659c16a91c167e235938047bf221
Jakob Richter <code@jakob-r.de>
2015-06-15 12:22:41 +0200

    add mfMBO test

--- tests/testthat/test_tune_tuneMBO.R
+++ tests/testthat/test_tune_tuneMBO.R
@@ -1,10 +1,9 @@
 context("tuneMBO")
 
 # FIXME remove if mbo is on cran
-
 if (interactive()) {
-
 test_that("tuneMBO", {
+  skip_if_not_installed("mlrMBO")
   requirePackages("!mlrMBO")
   res = makeResampleDesc("Holdout")
   ps = makeParamSet(
@@ -37,6 +36,26 @@ test_that("tuneMBO", {
   )
   tr = tuneParams("classif.randomForest", multiclass.task, res, par.set = ps, control = ctrl)
   expect_equal(getOptPathLength(tr$opt.path), n1 + n2)
+})
+
+test_that("tuneMBO works with mfMBO", {
+  skip_if_not_installed("mlrMBO")
+  requirePackages("!mlrMBO")
+  res = makeResampleDesc("Holdout")
+  ps = makeParamSet(
+    makeNumericParam("cost", lower = -15, upper = 15, trafo = function(x) 2^x),
+    makeNumericParam("gamma", lower = -15, upper = 15, trafo = function(x) 2^x)
+  )
+
+  n1 = 10; n2 = 2;
+  mbo.ctrl = makeMBOControl(init.design.points = n1, iters = n2, save.on.disk.at = integer(0L))
+  mbo.ctrl = setMBOControlMultiFid(control = mbo.ctrl, param = "dw.perc", lvls = c(0.2, 1), costs = c(0.2, 1))
+  ctrl = makeTuneControlMBO(learner = makeLearner("regr.lm"), mbo.control = mbo.ctrl)
+  tune.lrn = makeLearner("classif.svm")
+  tune.lrn = makeDownsampleWrapper(tune.lrn)
+  tr = tuneParams(tune.lrn, iris.task, res, par.set = ps, control = ctrl)
+  expect_equal(getOptPathLength(tr$opt.path), n1+n2)
+  expect_equal(dim(as.data.frame(tr$opt.path)), c(n1 + n2, 2 + 1 + 4 + 1))
 })
 
 }
