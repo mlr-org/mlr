a165f594843c88ab8aed7f9f06edf029a12ad134
Bernd Bischl <bernd_bischl@gmx.net>
2015-06-16 22:30:16 +0200

    new fun convertMLBenchObjToTask, fixes #332

---
+++ tests/testthat/test_base_convertMLBenchObjToTask.R
@@ -0,0 +1,25 @@
+context("convertMLBenchObjToTask")
+
+test_that("convertMLbenchObjToTask", {
+  library(mlbench)
+  # get all mlbench.* functions, 1spiral does not work
+  fs = ls("package:mlbench", pattern = "mlbench")
+  n = 77L
+  for (f in setdiff(fs, c("mlbench.1spiral"))) {
+    task = convertMLBenchObjToTask(f, n = n)
+    expect_is(task, "Task")
+    # for some, n is not properly respected in mlbench
+    if (f %nin% c("mlbench.corners", "mlbench.hypercube", "mlbench.simplex"))
+      expect_equal(task$task.desc$size, n)
+  }
+
+  # get all mlbench datasets, HouseVotes84 and Ozone have NAs in target col
+  ds = data(package = "mlbench")
+  ds = ds$results[, "Item"]
+  for (d in setdiff(ds, c("HouseVotes84", "Ozone"))) {
+    task = convertMLBenchObjToTask(d, n = n)
+    expect_is(task, "Task")
+  }
+})
+
+
