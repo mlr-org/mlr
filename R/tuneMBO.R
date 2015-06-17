0907790fde7fe999d96cb7df45970f0c8cac3f4e
Jakob Richter <code@jakob-r.de>
2015-06-17 10:34:22 +0200

    use (the) force in tuneMBO

--- R/tuneMBO.R
+++ R/tuneMBO.R
@@ -11,6 +11,8 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   mbo.control$final.evals = 0L
   cx = identity
 
+  # put all required info into the function env
+  force(learner); force(task); force(resampling); force(measures); force(par.set); force(control); force(opt.path); force(show.info)
   tff = function(x) tunerFitnFun(x, learner = learner, task = task, resampling = resampling, measures = measures,
     par.set = par.set, ctrl = control, opt.path = opt.path, show.info = show.info,
     convertx = cx, remove.nas = TRUE)
@@ -35,5 +37,8 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   x = trafoValue(par.set, or$x)
   # we take the point that mbo proposes and its estimated y
   # FIXME: threshold
-  makeTuneResult(learner, control, x, setNames(or$y, opt.path$y.names[1L]), NULL, opt.path)
+  res = makeTuneResult(learner, control, x, setNames(or$y, opt.path$y.names[1L]), NULL, opt.path)
+  if (control$mbo.save)
+    res$mbo.result = or
+  res
 }
