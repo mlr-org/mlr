9456e74bc3b7f29fbd495fbd29d6e3e7f7064d74
Zachary M. Jones <zmj@zmjones.com>
2015-06-22 13:53:46 -0400

    compute partial predictions for features w/o conditioning
     - adds interaction flag to generatePartialPredictionData,
       when FALSE and length(features) > 1, partial predictions
       for each feature are computed
     - plotPartialPrediction and plotPartialPredictionGGVIS can
       use facetting/shiny to visualize the above type of output

--- tests/testthat/test_base_generatePartialPrediction.R
+++ tests/testthat/test_base_generatePartialPrediction.R
@@ -2,7 +2,7 @@ context("generatePartialPrediction")
 
 test_that("generatePartialPredictionData", {
   fr = train("regr.rpart", regr.task)
-  dr = generatePartialPredictionData(fr, getTaskData(regr.task), c("lstat", "chas"),
+  dr = generatePartialPredictionData(fr, getTaskData(regr.task), c("lstat", "chas"), TRUE,
                                      fmin = c(1, NA), fmax = c(40, NA), gridsize = 5L)
   expect_that(max(dr$data$lstat), equals(40.))
   expect_that(min(dr$data$lstat), equals(1.))
@@ -12,25 +12,31 @@ test_that("generatePartialPredictionData", {
 
   fc = train("classif.rpart", multiclass.task)
   dc = generatePartialPredictionData(fc, getTaskData(multiclass.task), c("Petal.Width", "Petal.Length"),
-                                     function(x) table(x) / length(x), gridsize = 5L)
-  plotPartialPrediction(dc, facet = "Petal.Length")
-  ## plotPartialPredictionGGVIS(dc, interaction = "Petal.Length")
+                                     fun = function(x) table(x) / length(x), gridsize = 5L)
+  plotPartialPrediction(dc)
+  ## plotPartialPredictionGGVIS(dc)
   fcp = train(makeLearner("classif.rpart", predict.type = "prob"), multiclass.task)
   expect_error(generatePartialPrediction(fcp, getTaskData(multiclass.task), "Petal.Width",
                                          function(x) quantile(x, c(.025, .5, .975))), gridsize = 5L)
-  dcp = generatePartialPredictionData(fcp, getTaskData(iris.task), c("Petal.Width", "Petal.Length"), gridsize = 5L)
+  dcp = generatePartialPredictionData(fcp, getTaskData(iris.task), c("Petal.Width", "Petal.Length"),
+                                      interaction = TRUE, gridsize = 5L)
   plotPartialPrediction(dcp, facet = "Petal.Length")
   ## plotPartialPredictionGGVIS(dcp, interaction = "Petal.Length")
   fs = train("surv.coxph", surv.task)
   ds = generatePartialPredictionData(fs, getTaskData(surv.task), c("Petal.Width", "Petal.Length"), gridsize = 5L)
-  plotPartialPrediction(ds, facet = "Petal.Length")
-  ## plotPartialPredictionGGVIS(ds, interaction = "Petal.Length")
+  plotPartialPrediction(ds)
+  ## plotPartialPredictionGGVIS(ds)
 
-  db = generatePartialPredictionData(fr, getTaskData(regr.task), c("lstat", "chas"),
-                                     function(x) quantile(x, c(.25, .5, .75)), gridsize = 5L)
+  db = generatePartialPredictionData(fr, getTaskData(regr.task), c("lstat", "chas"), TRUE,
+                                     fun = function(x) quantile(x, c(.25, .5, .75)), gridsize = 5L)
   expect_that(colnames(db$data), equals(c("lower", "medv", "upper", "lstat", "chas")))
   plotPartialPrediction(db, facet = "chas")
   ## plotPartialPredictionGGVIS(db, interaction = "chas")
+
+  db2 = generatePartialPredictionData(fr, getTaskData(regr.task), c("lstat", "crim"), FALSE,
+                                      fun = function(x) quantile(x, c(.25, .5, .75)), gridsize = 5L)
+  plotPartialPrediction(db2)
+  ## plotPartialPredictionGGVIS(db2)
 })
 
 test_that("generateFeatureGrid", {
