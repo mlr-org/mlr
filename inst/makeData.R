b044883ec969d8ab08140f3c7ac9ecc8eccfe751
Lars Kotthoff <larsko@cs.ubc.ca>
2015-07-10 11:06:35 -0700

    fix typo in data generation script

--- inst/makeData.R
+++ inst/makeData.R
@@ -80,7 +80,7 @@ save(costiris.task, file = file.path(dn, "costiris.task.RData"), compress = "xz"
 # multilabel
 set.seed(DATASEED)
 d = load2("../thirdparty/yeast.RData")
-mtcars.task = makeMultilabelTask("yeast-example", data = d, target = paste0("label", 1:14))
+yeast.task = makeMultilabelTask("yeast-example", data = d, target = paste0("label", 1:14))
 save(yeast.task, file = file.path(dn, "yeast.task.RData"), compress = "xz")
 
 
