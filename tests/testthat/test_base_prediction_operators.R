fd884d5c28d0768442f7f4b7018ca7401e35c5b0
IcedragonP <philipp_probst@gmx.de>
2015-07-12 11:56:08 +0200

    unit test für getPredictionTruth getPredictionResponse getPredictionProbabilities

--- tests/testthat/test_base_prediction_operators.R
+++ tests/testthat/test_base_prediction_operators.R
@@ -2,12 +2,18 @@ context("prediction operators")
 
 mypredict = function(task) predict(train(sprintf("%s.rpart", getTaskType(task)), task), task)
 classif.pred = mypredict(binaryclass.task)
+multilabel.pred = predict(train("multilabel.rFerns", task), task)
+lrn1 = makeLearner("classif.rpart")
+lrn2 = makeMultilabelBinaryRelevanceWrapper(lrn1)
+multilabel.pred2 = predict(train(lrn2, task), task)
 regr.pred = mypredict(regr.task)
 surv.pred = mypredict(surv.task)
 cluster.pred = predict(train("cluster.kmeans", agri.task), agri.task)
 
 test_that("getPredictionResponse", {
   expect_true(is.factor(getPredictionResponse(classif.pred)))
+  expect_true(is.logical(getPredictionResponse(multilabel.pred)))
+  expect_true(is.logical(getPredictionResponse(multilabel.pred2)))
   expect_true(is.numeric(getPredictionResponse(regr.pred)))
   expect_true(is.numeric(getPredictionResponse(surv.pred)))
   expect_true(is.integer(getPredictionResponse(cluster.pred)))
@@ -15,6 +21,8 @@ test_that("getPredictionResponse", {
 
 test_that("getPredictionTruth", {
   expect_true(is.factor(getPredictionTruth(classif.pred)))
+  expect_true(is.logical(getPredictionTruth(multilabel.pred)))
+  expect_true(is.logical(getPredictionTruth(multilabel.pred2)))
   expect_true(is.numeric(getPredictionTruth(regr.pred)))
   expect_true(is.Surv(getPredictionTruth(surv.pred)))
   expect_error(getPredictionTruth(cluster.pred), "no truth")
@@ -22,8 +30,12 @@ test_that("getPredictionTruth", {
 
 test_that("getPredictionProbabilities", {
   expect_error(getPredictionProbabilities(classif.pred, "R"), "Probabilities")
+  expect_error(getPredictionProbabilities(multilabel.pred2, "y2"), "Probabilities")
   expect_error(getPredictionProbabilities(regr.pred, "xxx"), "ClassifTask")
   lrn = makeLearner("classif.rpart", predict.type = "prob")
-  pred = predict(train(lrn, binaryclass.task), binaryclass.task)
-  expect_true(is.numeric(getPredictionProbabilities(pred, "R")))
+  classif.pred = predict(train(lrn, binaryclass.task), binaryclass.task)
+  expect_true(is.numeric(getPredictionProbabilities(classif.pred, "R")))
+  lrn2 = makeMultilabelBinaryRelevanceWrapper(lrn)
+  multilabel.pred2 = predict(train(lrn2, task), task)
+  expect_true(is.numeric(getPredictionProbabilities(multilabel.pred2, "y2")))
 })
