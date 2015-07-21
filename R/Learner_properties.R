c9526c6a3e9fa1a38ad373c0e9314b0c2e9c5592
Jakob Richter <code@jakob-r.de>
2015-07-21 22:46:18 +0200

    remove LearnerProperties Modificatory consequently

--- R/Learner_properties.R
+++ R/Learner_properties.R
@@ -27,59 +27,8 @@ getLearnerProperties.Learner = function(learner) {
 
 #' @rdname LearnerProperties
 #' @export
-setLearnerProperties = function(learner, props) {
-  learner = checkLearner(learner)
-  assertSubset(props, getSupportedLearnerProperties(learner$type))
-  learner$properties = unique(props)
-  learner
-}
-
-#' @export
-setProperties = function(learner, props) {
-  .Deprecated("setLearnerProperties")
-  setLearnerProperties(learner, props)
-}
-
-#' @rdname LearnerProperties
-#' @export
-addLearnerProperties = function(learner, props) {
-  learner = checkLearner(learner)
-  assertSubset(props, getSupportedLearnerProperties(learner$type))
-  learner$properties = union(getLearnerProperties(learner), props)
-  learner
-}
-
-#' @export
-addProperties = function(learner, props) {
-  .Deprecated("addLearnerProperties")
-  addLearnerProperties(learner, props)
-}
-
-#' @rdname LearnerProperties
-#' @export
-removeLearnerProperties = function(learner, props) {
-  learner = checkLearner(learner)
-  assertSubset(props, getSupportedLearnerProperties(learner$type))
-  learner$properties = setdiff(getLearnerProperties(learner), props)
-  learner
-}
-
-#' @export
-removeProperties = function(learner, props) {
-  .Deprecated("removeLearnerProperties")
-  removeLearnerProperties(learner, props)
-}
-
-#' @rdname LearnerProperties
-#' @export
 hasLearnerProperties = function(learner, props) {
   learner = checkLearner(learner)
   assertSubset(props, getSupportedLearnerProperties())
   props %in% getLearnerProperties(learner)
-}
-
-#' @export
-hasProperties = function(learner, props) {
-  .Deprecated("hasLearnerProperties")
-  hasLearnerProperties(learner, props)
 }
