25578ed09809c40f9389fe3054ede976fe440c8a
Jakob Richter <code@jakob-r.de>
2015-07-29 13:12:49 +0200

    fix documentation

--- R/getResampleExtract.R
+++ R/getResampleExtract.R
@@ -1,8 +1,13 @@
+#In the following we have the functions
+# getReampleExtract2: Returns a feasable function we can use as extract in resample() for each specific learner class. All is handled recursively because we have wrapped learners. So getResampleExtract2 builds a list of all appliable functions
+#
+# getResampleExtract converts the list of functions obtained by getResampleExtract2 to a single function which returns a list of each function result.
+
 getResampleExtract2 = function(learner) {
   UseMethod("getResampleExtract2")
 }
 
-getResampleExtract2.NULL = function(learner) {
+getResampleExtract2.default = function(learner) {
   stopf("Wrapper without underlying Learner.")
 }
 
@@ -28,7 +33,6 @@ getResampleExtract2.FilterWrapper = function(learner) {
 
 getResampleExtract = function(learner){
   functions = getResampleExtract2(learner)
-  force(functions)
   function(x) {
     lapply(functions, function(fun) fun(x))
   }
