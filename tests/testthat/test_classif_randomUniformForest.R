eb66128263118d59afe05cccd0dd6dc8d0453c81
Florian Fendt <Flo@Florian-Fendts-MacBook-Pro.local>
2015-09-06 18:17:20 +0200

    even shorter test

--- tests/testthat/test_classif_randomUniformForest.R
+++ tests/testthat/test_classif_randomUniformForest.R
@@ -7,19 +7,15 @@ test_that("classif_randomUniformForest", {
     list(),
     list(ntree=50, mtry=2)
     )
-  type = c("response", "prob")
 
   #randomUniformForest ist not reproducible with set.seed, so we just check for createability
   for(i in 1:length(parset.list)){
-    for(j in 1:length(type)){
-          parset = parset.list[[i]]
-    ruf.classif.lrn = try(makeLearner("classif.randomUniformForest", par.vals = parset, predict.type = type[j]))
+    parset = parset.list[[i]]
+    ruf.classif.lrn = try(makeLearner("classif.randomUniformForest", par.vals = parset, predict.type = "response"))
     expect_is(ruf.classif.lrn, "classif.randomUniformForest")
     ruf.classif.m = try(train(ruf.classif.lrn, multiclass.task))
     expect_is(ruf.classif.m, "WrappedModel")
     ruf.classif.p = try(predict(ruf.classif.m, newdata = multiclass.test))
     expect_is(ruf.classif.p, c("PredictionClassif", "Prediction"))
-
-    }
   }
 })
