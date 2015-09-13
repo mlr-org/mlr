b6617eac8f1ec7c506416212aaab17a4ac11d1c3
Florian Fendt <Flo@FlorianendtsMBP.fritz.box>
2015-09-13 13:19:27 +0200

    new learners randomForestSRCSyn

---
+++ tests/testthat/test_classif_randomForestSRCSyn.R
@@ -0,0 +1,30 @@
+context("classif_randomForestSRCSyn")
+
+test_that("classif_randomForestSRCSyn", {
+  requirePackages("randomForestSRC", default.method = "load")
+
+  parset.list = list(
+    list(),
+    list(ntree = 10L),
+    list(ntree = 25L, mtry = 4L),
+    list(ntree = 25L, nodesize = 2L, na.action = "na.impute")
+  )
+  old.predicts.list = list()
+  old.probs.list = list()
+
+  for (i in 1L:length(parset.list)) {
+    parset = parset.list[[i]]
+    parset = c(parset, list(data = binaryclass.train, formula = binaryclass.formula, newdata = binaryclass.test,
+     importance = "none", proximity = FALSE, forest = TRUE, verbose = FALSE))
+    set.seed(getOption("mlr.debug.seed"))
+    mod = do.call(randomForestSRC::rfsrcSyn, parset)
+    max.id = apply(mod$predicted, MARGIN = 1L, which.max)
+    p = factor(colnames(mod$predicted)[max.id])
+    old.predicts.list[[i]] = p
+    probs = mod$predicted[, binaryclass.task$task.desc$positive]
+    old.probs.list[[i]] = probs
+  }
+
+  testSimpleParsets("classif.randomForestSRCSyn", binaryclass.df, binaryclass.target, binaryclass.train.inds, old.predicts.list, parset.list)
+  testProbParsets ("classif.randomForestSRCSyn", binaryclass.df, binaryclass.target, binaryclass.train.inds, old.probs.list, parset.list)
+})
---
+++ tests/testthat/test_regr_randomForestSRCSyn.R
@@ -0,0 +1,24 @@
+context("regr_randomForestSRCSyn")
+
+test_that("regr_randomForestSRCSyn", {
+  requirePackages("randomForestSRC", default.method = "load")
+
+  parset.list = list(
+    list(),
+    list(ntree = 10L),
+    list(ntree = 5L, mtry = 4L),
+    list(ntree = 5L, nodesize = 2L, na.action = "na.impute")
+  )
+  old.predicts.list = list()
+
+  for (i in 1L:length(parset.list)) {
+    parset = parset.list[[i]]
+    parset = c(parset, list(data = regr.train, formula = regr.formula, newdata = regr.test,
+     importance = "none", proximity = FALSE, forest = TRUE, verbose = FALSE))
+    set.seed(getOption("mlr.debug.seed"))
+    p = do.call(randomForestSRC::rfsrcSyn, parset)$predicted
+    old.predicts.list[[i]] = p
+  }
+
+  testSimpleParsets("regr.randomForestSRCSyn", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list)
+})
