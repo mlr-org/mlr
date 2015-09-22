2d8b26a56e7dbcf732859e5a273c53a010d937fe
Michel <michellang@gmail.com>
2015-09-22 11:32:41 +0200

    set tunable = FALSE for some hyperpars of new ranger-based learners

--- R/RLearner_regr_ranger.R
+++ R/RLearner_regr_ranger.R
@@ -6,22 +6,21 @@ makeRLearner.regr.ranger = function() {
     package = "ranger",
     par.set = makeParamSet(
       makeIntegerLearnerParam(id = "num.trees", lower = 1L, default = 500L),
-      # FIXME: Add default value when data dependent defaults are implemented: mtry=floor(sqrt(#independent vars)) 
-      makeIntegerLearnerParam(id = "mtry", lower = 1L), 
-      makeIntegerLearnerParam(id = "min.node.size", lower = 1L, default = 5L), 
+      # FIXME: Add default value when data dependent defaults are implemented: mtry=floor(sqrt(#independent vars))
+      makeIntegerLearnerParam(id = "mtry", lower = 1L),
+      makeIntegerLearnerParam(id = "min.node.size", lower = 1L, default = 5L),
       makeLogicalLearnerParam(id = "replace", default = TRUE),
       makeNumericVectorLearnerParam(id = "split.select.weights", lower = 0, upper = 1),
       makeUntypedLearnerParam(id = "always.split.variables"),
       makeLogicalLearnerParam(id = "respect.unordered.factors", default = FALSE),
-      makeDiscreteLearnerParam(id = "importance", values = c("none", "impurity", "permutation"), 
-        default = "none"),
-      makeLogicalLearnerParam(id = "scale.permutation.importance", default = FALSE,
-        requires = expression(importance == "permutation")),
+      makeDiscreteLearnerParam(id = "importance", values = c("none", "impurity", "permutation"), default = "none", tunable = FALSE),
+      makeLogicalLearnerParam(id = "scale.permutation.importance", default = FALSE, requires = expression(importance == "permutation"), tunable = FALSE),
       makeIntegerLearnerParam(id = "num.threads", lower = 1L, when = "both", tunable = FALSE),
       makeLogicalLearnerParam(id = "save.memory", default = FALSE, tunable = FALSE),
       makeLogicalLearnerParam(id = "verbose", default = TRUE, when = "both", tunable = FALSE),
       makeIntegerLearnerParam(id = "seed", when = "both", tunable = FALSE)
     ),
+    par.vals = list(num.threads = 1L),
     properties = c("numerics", "factors"),
     name = "Random Forests",
     short.name = "ranger",
@@ -31,7 +30,7 @@ makeRLearner.regr.ranger = function() {
 
 #' @export
 trainLearner.regr.ranger = function(.learner, .task, .subset, .weights, ...) {
-  ranger::ranger(formula = getTaskFormula(.task), data = getTaskData(.task, .subset), 
+  ranger::ranger(formula = getTaskFormula(.task), data = getTaskData(.task, .subset),
     write.forest = TRUE, ...)
 }
 
