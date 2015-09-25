e6e6d090c29119069df0dfe1cb27935d4006e4bf
Lars Kotthoff <larsko@cs.ubc.ca>
2015-09-25 03:59:09 -0700

    clean up irace tests (related to #499)

--- tests/testthat/test_tune_tuneIrace.R
+++ tests/testthat/test_tune_tuneIrace.R
@@ -7,10 +7,10 @@ test_that("tuneIrace", {
     makeIntegerParam("minsplit", lower = 1, upper = 10)
   )
 
-  n = 40
-  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 2, minNbSurvival = 1)
+  n = 20
+  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 1L, minNbSurvival = 1)
   tr1 = tuneParams(makeLearner("classif.rpart"), multiclass.task, rdesc, par.set = ps1, control = ctrl)
-  expect_true(getOptPathLength(tr1$opt.path) >= 30 && getOptPathLength(tr1$opt.path) <= n)
+  expect_true(getOptPathLength(tr1$opt.path) >= 10 && getOptPathLength(tr1$opt.path) <= n)
   expect_true(!is.na(tr1$y))
 
   # with trafo
@@ -20,10 +20,10 @@ test_that("tuneIrace", {
     makeNumericParam("sigma", lower = -5, upper = 5, trafo = function(x) 2^x)
   )
 
-  n = 40
-  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 2, minNbSurvival = 1)
+  n = 20
+  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 1L, minNbSurvival = 1)
   tr2 = tuneParams(makeLearner("classif.ksvm"), multiclass.task, rdesc, par.set = ps2, control = ctrl)
-  expect_true(getOptPathLength(tr2$opt.path) >= 30 && getOptPathLength(tr2$opt.path) <= n)
+  expect_true(getOptPathLength(tr2$opt.path) >= 10 && getOptPathLength(tr2$opt.path) <= n)
   expect_true(!is.na(tr2$y))
 })
 
@@ -34,9 +34,9 @@ test_that("tuneIrace works with dependent params", {
   )
   lrn = makeLearner("classif.ksvm")
   rdesc = makeResampleDesc("Holdout")
-  ctrl = makeTuneControlIrace(maxExperiments = 40 ,nbIterations = 2, minNbSurvival = 1)
+  ctrl = makeTuneControlIrace(maxExperiments = 40 ,nbIterations = 2L, minNbSurvival = 1)
   tr = tuneParams(lrn, multiclass.task, rdesc, par.set = ps, control = ctrl)
-  expect_true(getOptPathLength(tr$opt.path) >= 30 && getOptPathLength(tr$opt.path) <= 100)
+  expect_true(getOptPathLength(tr$opt.path) >= 20 && getOptPathLength(tr$opt.path) <= 100)
   expect_true(!is.na(tr$y))
 
   # another complex example
@@ -77,87 +77,68 @@ test_that("tuneIrace works with tune.threshold", {
   rdesc = makeResampleDesc("Holdout", stratify = TRUE, split = 0.1)
   ps = makeParamSet(makeIntegerParam("minsplit", lower = 1, upper = 3))
 
-  n = 40
-  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 2, minNbSurvival = 1)
+  n = 20
+  ctrl = makeTuneControlIrace(maxExperiments = n, nbIterations = 1L, minNbSurvival = 1)
   tr = tuneParams("classif.rpart", multiclass.task, rdesc, par.set = ps, control = ctrl)
 })
 
-test_that("tuneIrace handles the number of digits", {
-  lrn = makeLearner("classif.gbm", predict.type = "prob")
-  rdesc = makeResampleDesc(method = "Holdout")
-  
-  ctrl = makeTuneControlIrace(maxExperiments = 60L)
-  ps = makeParamSet(makeNumericParam("shrinkage", lower = 1e-5, upper = 1e-4))
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  res = resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)
-  
-  ctrl = makeTuneControlIrace(maxExperiments = 120L)
-  ps = makeParamSet(
-    makeNumericParam("shrinkage", lower = pi * 1e-20, upper = 5.242e12),
-    makeIntegerParam("interaction.depth", lower = 1, upper = 10)
-  )
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  res = resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)  
-})
-
 test_that("tuneIrace uses digits", {
-  lrn = makeLearner("classif.gbm", predict.type = "prob")
   rdesc = makeResampleDesc(method = "Holdout")
+
+  ctrl = makeTuneControlIrace(maxExperiments = 30L, nbIterations = 1L)
+  ps = makeParamSet(makeNumericParam("shrinkage", lower = pi * 1e-20,
+    upper = 5.242e12))
+  lrn.tune = makeTuneWrapper("classif.gbm", resampling = rdesc, par.set = ps,
+    control = ctrl, show.info = FALSE)
+  res = resample(lrn.tune, task = multiclass.task, rdesc)
   
-  ctrl = makeTuneControlIrace(maxExperiments = 60L, digits = 5L)
-  ps = makeParamSet(makeNumericParam("shrinkage", lower = 1e-5, upper = 1e-4))
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  res = resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)    
+  lrn = makeLearner("classif.rpart")
+  ctrl = makeTuneControlIrace(maxExperiments = 30L, nbIterations = 1L,
+    digits = 5L)
+  ps = makeParamSet(makeNumericParam("cp", lower = 1e-5, upper = 1e-4))
+  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps,
+    control = ctrl, show.info = FALSE)
+  res = resample(lrn.tune, task = multiclass.task, rdesc)
   
   ctrl = makeTuneControlIrace(maxExperiments = 60L, digits = 4L)
-  ps = makeParamSet(makeNumericParam("shrinkage", lower = 1e-5, upper = 1e-4))
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)))
+  ps = makeParamSet(makeNumericParam("cp", lower = 1e-5, upper = 1e-4))
+  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps,
+    control = ctrl, show.info = FALSE)
+  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc)))
   
   ctrl = makeTuneControlIrace(maxExperiments = 60L, digits = "a")
-  ps = makeParamSet(makeNumericParam("shrinkage", lower = 1e-5, upper = 1e-4))
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)))
+  ps = makeParamSet(makeNumericParam("cp", lower = 1e-5, upper = 1e-4))
+  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps,
+    control = ctrl, show.info = FALSE)
+  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc)))
   
   ctrl = makeTuneControlIrace(maxExperiments = 60L, digits = c(6L, 7L))
-  ps = makeParamSet(makeNumericParam("shrinkage", lower = 1e-5, upper = 1e-4))
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, 
-    control = ctrl, measure = list(mmce), show.info = FALSE)
-  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc, 
-    measures = list(mmce), show.info = FALSE)))
+  ps = makeParamSet(makeNumericParam("cp", lower = 1e-5, upper = 1e-4))
+  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps,
+    control = ctrl, show.info = FALSE)
+  expect_error(suppressAll(resample(lrn.tune, task = multiclass.task, rdesc)))
 })
 
 test_that("makeTuneControlIrace handles budget parameter", {
   rdesc = makeResampleDesc("Holdout", stratify = TRUE, split = 0.1)
   ps = makeParamSet(makeIntegerParam("minsplit", lower = 1, upper = 3))
 
-  n = 40
-  expect_error(makeTuneControlIrace(budget = n, nbIterations = 2, minNbSurvival = 1, maxExperiments = n + 2L))
-  expect_error(makeTuneControlIrace(budget = n + 2L, nbIterations = 2, minNbSurvival = 1, maxExperiments = n))
+  n = 20
+  expect_error(makeTuneControlIrace(budget = n, nbIterations = 1L, minNbSurvival = 1, maxExperiments = n + 2L))
+  expect_error(makeTuneControlIrace(budget = n + 2L, nbIterations = 1L, minNbSurvival = 1, maxExperiments = n))
 
-  # check, whether it is ok to provide both arguments as long as they are the same
-  ctrl = makeTuneControlIrace(budget = n, nbIterations = 2, minNbSurvival = 1, maxExperiments = n)
+  # check whether it is ok to provide both arguments as long as they are the same
+  ctrl = makeTuneControlIrace(budget = n, nbIterations = 1L, minNbSurvival = 1, maxExperiments = n)
   tr1 = tuneParams(makeLearner("classif.rpart"), multiclass.task, rdesc, par.set = ps, control = ctrl)
   expect_true(getOptPathLength(tr1$opt.path) <= n)
 })
 
-test_that("issue 279", {
-  lrn = makeLearner("classif.gbm", predict.type = "prob")
+test_that("Error in hyperparameter tuning with scientific notation for lower/upper boundaries #279", {
   ps = makeParamSet(makeNumericParam("shrinkage", lower = 4e-5, upper = 1e-4))
-  ctrl = makeTuneControlIrace(maxExperiments = 60L)
+  ctrl = makeTuneControlIrace(maxExperiments = 30L, nbIterations = 1L)
   rdesc = makeResampleDesc(method = "Holdout")
-  lrn.tune = makeTuneWrapper(lrn, resampling = rdesc, par.set = ps, control = ctrl, measure = list(mmce))
+  lrn.tune = makeTuneWrapper("classif.gbm", resampling = rdesc, par.set = ps, control = ctrl)
 
   set.seed(123)
-  res = resample(lrn.tune, task = sonar.task, rdesc, measures = list(mmce))
+  res = resample(lrn.tune, task = sonar.task, rdesc)
 })
