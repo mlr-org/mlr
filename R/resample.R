548f7ac20773052531ad261d6f4f4aa1b3843911
Jakob Richter <code@jakob-r.de>
2015-09-25 17:13:40 +0200

    revert resample extract to 2.4 behaviour

--- R/resample.R
+++ R/resample.R
@@ -38,7 +38,11 @@
 #' @param models [\code{logical(1)}]\cr
 #'   Should all fitted models be returned?
 #'   Default is \code{FALSE}.
-#' @template arg_extract
+#' @param extract [\code{function}]\cr
+#'   Function used to extract information from a fitted model during resampling.
+#'   Is applied to every \code{\link{WrappedModel}} resulting from calls to \code{\link{train}}
+#'   during resampling.
+#'   Default is to extract nothing.
 #' @template arg_keep_pred
 #' @param ... [any]\cr
 #'   Further hyperparameters passed to \code{learner}.
@@ -54,7 +58,7 @@
 #' print(r$measures.test)
 #' print(r$pred)
 resample = function(learner, task, resampling, measures, weights = NULL, models = FALSE,
-  extract = NULL, keep.pred = TRUE, ..., show.info = getMlrOption("show.info")) {
+  extract, keep.pred = TRUE, ..., show.info = getMlrOption("show.info")) {
 
   learner = checkLearner(learner, ...)
   assertClass(task, classes = "Task")
@@ -68,8 +72,8 @@ resample = function(learner, task, resampling, measures, weights = NULL, models 
     assertNumeric(weights, len = n, any.missing = FALSE, lower = 0)
   }
   assertFlag(models)
-  if (is.null(extract))
-    extract = getResampleExtract(learner)
+  if (missing(extract))
+    extract = function(model) {}
   else
     assertFunction(extract)
   assertFlag(show.info)
