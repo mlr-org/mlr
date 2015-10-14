cb2e3d9524066fa312270b440cb999fa16d7f950
Florian Fendt <Flo@Florians-MacBook-Pro.local>
2015-10-14 14:12:24 +0200

    #447 ResampleDesc objects

--- R/zzz.R
+++ R/zzz.R
@@ -13,3 +13,28 @@
   configureMlr()
   parallelRegisterLevels(package = "mlr", levels = c("benchmark", "resample", "selectFeatures", "tuneParams"))
 }
+
+
+#' @rdname makeResampleDesc
+#' @aliases NULL 
+#' @docType NULL
+#' @usage NULL
+#' @format NULL
+#' @keywords NULL
+#' @section Standard ResampleDesc objects: 
+#' For common resampling strategies you can save some typing
+#' by using the following description objects:
+#' \describe{
+#' \item{hout}{holdout a.k.a. test sample estimation
+#' (two-thirds training set, one-third testing set)}
+#' \item{cv2}{2-fold cross-validation}
+#' \item{cv3}{3-fold cross-validation}
+#' \item{cv5}{5-fold cross-validation}
+#' \item{cv10}{10-fold cross-validation}
+#' }
+
+hout = makeResampleDesc("Holdout")
+cv2 = makeResampleDesc("CV", iters = 2L)
+cv3 = makeResampleDesc("CV", iters = 3L)
+cv5 = makeResampleDesc("CV", iters = 5L)
+cv10 = makeResampleDesc("CV", iters = 10L)
