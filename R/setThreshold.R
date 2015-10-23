b3fa965f0b3bb52ba7b43b5398f263c5cfb5d43a
Bernd Bischl <bernd_bischl@gmx.net>
2015-10-23 20:17:32 +0200

    mini cleanup, basically whitespace

--- R/setThreshold.R
+++ R/setThreshold.R
@@ -17,17 +17,17 @@
 #' @export
 #' @seealso \code{\link{predict.WrappedModel}}
 #' @examples
-#' ## create task and train learner (LDA)
+#' # create task and train learner (LDA)
 #' task = makeClassifTask(data = iris, target = "Species")
 #' lrn = makeLearner("classif.lda", predict.type = "prob")
 #' mod = train(lrn, task)
 #'
-#' ## predict probabilities and compute performance
+#' # predict probabilities and compute performance
 #' pred = predict(mod, newdata = iris)
 #' performance(pred, measures = mmce)
 #' head(as.data.frame(pred))
-
-#' ## adjust threshold and predict probabilities again
+#'
+#' # adjust threshold and predict probabilities again
 #' threshold = c(setosa = 0.4, versicolor = 0.3, virginica = 0.3)
 #' pred = setThreshold(pred, threshold = threshold)
 #' performance(pred, measures = mmce)
