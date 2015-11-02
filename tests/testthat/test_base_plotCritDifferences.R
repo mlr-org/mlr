805e183452b6888c2b4de840c81b0a8aed79ecb6
pfistfl <pfistererf@googlemail.com>
2015-11-02 09:50:59 +0100

    Fix Bug in critDiff plot, fix #544

--- tests/testthat/test_base_plotCritDifferences.R
+++ tests/testthat/test_base_plotCritDifferences.R
@@ -13,8 +13,12 @@ test_that("critDifferences", {
   expect_is(r1, "critDifferencesData")
   r2 = generateCritDifferencesData(res, ber, baseline = "classif.nnet", test = "nemenyi")
   expect_is(r2, "critDifferencesData")
+  # Test Issue #554
+  r3 = generateCritDifferencesData(res, ber, p.value = 0.1, test = "bd")
+  r3$cd.info$textvjust = 1L
+  expect_is(r3, "critDifferencesData")
+  r3$baseline = res$learner.id
   expect_message(generateCritDifferencesData(res, mmce, p.value = 10^(-10)))
-
   plotCritDifferences(r)
   ggsave(tempfile(fileext = ".png"))
   plotCritDifferences(r1)
