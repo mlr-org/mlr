32fb57d8a2e99f350d58c8bc477b376864834522
Zachary M. Jones <zmj@zmjones.com>
2015-11-09 11:36:20 -0500

    remove calibrate parameter

--- R/RLearner_regr_randomForest.R
+++ R/RLearner_regr_randomForest.R
@@ -168,4 +168,4 @@ jackknifeStandardError = function(.learner, .model, .newdata, ...) {
 
 # computes the bias corrected infintesimal jackknife using randomForestCI
 infinitesimalJackknifeStandardError = function(.learner, .model, .newdata, ...)
-  as.matrix(randomForestCI::randomForestInfJack(.model$learner.model, .newdata, ...))
+  as.matrix(randomForestCI::randomForestInfJack(.model$learner.model, .newdata, FALSE))
