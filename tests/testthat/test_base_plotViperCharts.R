351c890481b143eb499935c785d6d6df87f23100
Bernd Bischl <bernd_bischl@gmx.net>
2015-11-25 19:14:07 +0100

    vipercharts seems offline, diable test for now

--- tests/testthat/test_base_plotViperCharts.R
+++ tests/testthat/test_base_plotViperCharts.R
@@ -1,19 +1,22 @@
 context("plotViperCharts")
 
-test_that("plotViperCharts", {
-  lrn1 = makeLearner("classif.rpart", predict.type = "prob")
-  lrn2 = makeLearner("classif.lda", predict.type = "prob")
-  lrns = list(lrn1, lrn2)
-  m = train(lrn1, binaryclass.task)
-  p = predict(m, binaryclass.task)
-  plotViperCharts(p, browse = FALSE)
 
-  br = benchmark(lrn2, binaryclass.task, resampling = makeResampleDesc("Holdout"))
-  plotViperCharts(p, browse = FALSE)
+# FIXME: vipercharts URL seems offline, dont know why, see #571
 
-  rs = lapply(lrns, holdout, task = binaryclass.task)
-  names(rs) = c("a", "b")
-  plotViperCharts(rs, browse = FALSE)
-})
+# test_that("plotViperCharts", {
+#   lrn1 = makeLearner("classif.rpart", predict.type = "prob")
+#   lrn2 = makeLearner("classif.lda", predict.type = "prob")
+#   lrns = list(lrn1, lrn2)
+#   m = train(lrn1, binaryclass.task)
+#   p = predict(m, binaryclass.task)
+#   plotViperCharts(p, browse = FALSE)
+
+#   br = benchmark(lrn2, binaryclass.task, resampling = makeResampleDesc("Holdout"))
+#   plotViperCharts(p, browse = FALSE)
+
+#   rs = lapply(lrns, holdout, task = binaryclass.task)
+#   names(rs) = c("a", "b")
+#   plotViperCharts(rs, browse = FALSE)
+# })
 
 
