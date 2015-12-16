f487e42188e6234a70c22073513f2cedf0ed9393
Florian Fendt <Flo@Florians-MacBook-Pro.local>
2015-12-16 16:29:20 +0100

    #235 sample control

--- R/OverUnderSampling.R
+++ R/OverUnderSampling.R
@@ -2,35 +2,58 @@
 #'
 #' @description
 #'
-#' Oversampling: From the smaller class, observations are randomly drawn with repetitions.
+#' Oversampling: Observations are randomly drawn with repetitions.
 #'
-#' Undersampling: From the larger class, observations are randomly drawn without repetitions.
+#' Undersampling: Observations are randomly drawn without repetitions.
 #'
 #' @template arg_task
 #' @param rate [\code{numeric(1)}]\cr
-#'   Factor to upsample the smaller or downsample the bigger class.
+#'   Factor to upsample or downsample a class.
 #'   For undersampling: Must be between 0 and 1,
 #'   where 1 means no downsampling, 0.5 implies reduction to 50 percent
 #'   and 0 would imply reduction to 0 observations.
 #'   For oversampling: Must be between 1 and \code{Inf},
 #'   where 1 means no oversampling and 2 would mean doubling the class size.
+#' @param cl [\code{character(1)}]\cr
+#'   Which class should be over- or undersampled. If \code{NULL}, \code{oversample}
+#'   will take the smaller class and \code{undersample} the bigger one.
 #' @template ret_task
 #' @family imbalancy
 #' @export
-oversample = function(task, rate) {
+oversample = function(task, rate, cl = NULL) {
   checkTask(task, "ClassifTask", binary = TRUE)
   assertNumber(rate, lower = 1)
-  j = sampleBinaryClass(getTaskTargets(task), rate, cl = "min", clreplace = TRUE, 
-    othreplace = FALSE, bagging = FALSE)
+  cl.nullcheck = testNull(cl)
+  if (!cl.nullcheck) {
+    assertCharacter(cl, max.len = 1L)
+  }
+  y = getTaskTargets(task)
+  if (cl.nullcheck) {
+    cl = getMinMaxClass(y)$min.name
+  } else {
+    assertChoice(cl, levels(y))
+  }
+  j = sampleBinaryClass(y, rate = rate, cl = cl, clreplace = TRUE, 
+    othreplace = FALSE)
   subsetTask(task, j)
 }
 
 #' @rdname oversample
 #' @export
-undersample = function(task, rate) {
+undersample = function(task, rate, cl = NULL) {
   checkTask(task, "ClassifTask", binary = TRUE)
-  assertNumber(rate, lower = 0, upper = 1)
-  j = sampleBinaryClass(getTaskTargets(task), rate, cl = "max", clreplace = FALSE,
-    othreplace = FALSE, bagging = FALSE)
+  assertNumber(rate, upper = 1)
+  cl.nullcheck = testNull(cl)
+  if (!cl.nullcheck) {
+    assertCharacter(cl, max.len = 1L)
+  }
+  y = getTaskTargets(task)
+  if (cl.nullcheck) {
+    cl = getMinMaxClass(y)$max.name
+  } else {
+    assertChoice(cl, levels(y))
+  }
+  j = sampleBinaryClass(y, rate = rate, cl = cl, clreplace = FALSE,
+    othreplace = FALSE)
   subsetTask(task, j)
 }
