2535173e92cc09ed09bb42d0d2756ea23d6f4ea5
Zachary M. Jones <zmj@zmjones.com>
2016-01-29 22:42:58 -0500

    remove ROCR plot/generate functions

--- tests/testthat/test_base_rocr.R
+++ tests/testthat/test_base_rocr.R
@@ -9,29 +9,3 @@ test_that("ROCR", {
   a2 = ROCR::performance(p, "auc")@y.values[[1]]
   expect_true(a1 == a2)
 })
-
-test_that("generateROCRCurvesData", {
-  lrn1 = makeLearner("classif.rpart", predict.type = "prob")
-  lrn2 = makeLearner("classif.lda", predict.type = "prob")
-  lrns = list(lrn1, lrn2)
-  m1 = train(lrn1, binaryclass.task)
-  p1 = predict(m1, binaryclass.task)
-  d = generateROCRCurvesData(p1)
-  plotROCRCurves(d) # Prediction
-
-  m2 = train(lrn2, binaryclass.task)
-  p2 = predict(m2, binaryclass.task)
-  d2 = generateROCRCurvesData(list("lda" = p1, "rpart" = p2)) # list of Predictions
-  plotROCRCurves(d)
-  plotROCRCurvesGGVIS(d)
-
-  br = benchmark(lrn2, binaryclass.task, resampling = makeResampleDesc("Holdout"))
-  d3 = generateROCRCurvesData(br)
-  plotROCRCurves(d) # BenchmarkResult
-  plotROCRCurvesGGVIS(d)
-
-  rs = lapply(lrns, crossval, task = binaryclass.task)	# list of ResampleResult's
-  names(rs) = c("a", "b")
-  d = generateROCRCurvesData(rs)
-  plotROCRCurves(d)
-})
