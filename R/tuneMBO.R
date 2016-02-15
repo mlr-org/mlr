cb5603e4576293e49226aeef20f6b27eaec0cf68
Jakob Richter <code@jakob-r.de>
2016-02-15 17:56:51 +0100

    mlrMBO integration (Fixes Issue #718)

--- R/tuneMBO.R
+++ R/tuneMBO.R
@@ -4,16 +4,13 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   # requirePackages("mlrMBO", why = "tuneMBO", default.method = "load")
   mbo.control = control$mbo.control
 
-  if (any(!mbo.control$minimize))
-    stop("Maximization is handled by mlr, set all elements of 'minimize' to 'TRUE' in mbo.control.")
-
   # set final evals to 0 to save time. we dont really need final evals in this context.
   mbo.control$final.evals = 0L
   cx = identity
 
   # put all required info into the function env
   force(learner); force(task); force(resampling); force(measures); force(par.set); force(control); force(opt.path); force(show.info)
-  tff = function(x) tunerFitnFun(x, learner = learner, task = task, resampling = resampling, measures = measures,
+  tff = tunerSmoofFun(learner = learner, task = task, resampling = resampling, measures = measures,
     par.set = par.set, ctrl = control, opt.path = opt.path, show.info = show.info,
     convertx = cx, remove.nas = TRUE)
 
@@ -31,7 +28,7 @@ tuneMBO = function(learner, task, resampling, measures, par.set, control,
   } else {
     # FIXME: remove this when mbo on cran
     mbofun = get("mbo", envir = getNamespace("mlrMBO"))
-    or = mbofun(tff, ps2, design = control$mbo.design, learner = control$learner, control = mbo.control, show.info = FALSE)
+    or = mbofun(tff, design = control$mbo.design, learner = control$learner, control = mbo.control, show.info = FALSE)
   }
 
   x = trafoValue(par.set, or$x)
