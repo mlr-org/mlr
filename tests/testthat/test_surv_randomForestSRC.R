a8a98b6c1de9d886f6af3f7c078467ad7b39a4c1
Michel <michellang@gmail.com>
2016-03-02 15:28:27 +0100

    improved survival tests; added learner surv.cv.CoxBoost, removed learner surv.optimCoxBoostPenalty (deprecated)

--- tests/testthat/test_surv_randomForestSRC.R
+++ tests/testthat/test_surv_randomForestSRC.R
@@ -16,7 +16,7 @@ test_that("surv_randomForestSRC", {
     parset = c(parset, list(data = surv.train, formula = surv.formula, importance = "none", proximity = FALSE, forest = TRUE))
     set.seed(getOption("mlr.debug.seed"))
     # There are some crazy checks in RFSRC, we have to overwrite the formula here
-    parset$formula = Surv(time, event) ~ .
+    parset$formula = Surv(time, status) ~ .
     m = do.call(randomForestSRC::rfsrc, parset)
     p  = predict(m, newdata = surv.test, importance = "none", na.action = "na.impute")$predicted
     old.predicts.list[[i]] = drop(p)
