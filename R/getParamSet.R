73289f118452ada75915e0b80206fc988d8b16fb
Bernd Bischl <bernd_bischl@gmx.net>
2016-03-03 16:23:19 +0100

    ...

--- R/getParamSet.R
+++ R/getParamSet.R
@@ -1,10 +1,12 @@
 #' Get a description of all possible parameter settings for a learner.
 #'
 #' @template arg_learner
+#' @param ...\cr
+#'   Currently unused.
 #' @template ret_ps
 #' @family learner
 #' @export
-getParamSet = function(x, ...) {
+getParamSet = function(learner, ...) {
   checkLearner(learner)
   UseMethod("getParamSet")
 }
