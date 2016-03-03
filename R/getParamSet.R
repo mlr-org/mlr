dca78d845f7b7ddf46cfa365f5836ad3eb28dd3d
Bernd Bischl <bernd_bischl@gmx.net>
2016-03-03 11:07:15 +0100

    Revert "removed getParamSet generic (now in PH)"
    This reverts commit eed0eb96279a3b1a518c22e862444ff3ec00e746.

--- R/getParamSet.R
+++ R/getParamSet.R
@@ -1,3 +1,14 @@
+#' Get a description of all possible parameter settings for a learner.
+#'
+#' @template arg_learner
+#' @template ret_ps
+#' @family learner
+#' @export
+getParamSet = function(learner) {
+  checkLearner(learner)
+  UseMethod("getParamSet")
+}
+
 #'@export
 getParamSet.Learner = function(x, ...) {
   x$par.set
@@ -8,3 +19,5 @@ getParamSet.character = function(x, ...) {
   x = checkLearner(x)
   getParamSet(x, ...)
 }
+
+
