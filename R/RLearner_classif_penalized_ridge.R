49556328e05bbbac758efc4b0f1eefe0d98f8e7b
Bernd Bischl <bernd_bischl@gmx.net>
2016-04-25 19:22:16 +0200

    Local fix for penalized learners and restructuring them (#850)

--- R/RLearner_classif_penalized_ridge.R
+++ R/RLearner_classif_penalized_ridge.R
@@ -1,14 +1,12 @@
 #' @export
-makeRLearner.regr.penalized.ridge = function() {
-  makeRLearnerRegr(
-    cl = "regr.penalized.ridge",
+makeRLearner.classif.penalized.ridge = function() {
+  makeRLearnerClassif(
+    cl = "classif.penalized.ridge",
     package = "!penalized",
     par.set = makeParamSet(
       makeNumericLearnerParam(id = "lambda2", default = 0, lower = 0),
       makeUntypedLearnerParam(id = "unpenalized"),
       makeLogicalVectorLearnerParam(id = "positive", default = FALSE),
-      makeDiscreteLearnerParam(id = "model", default = "linear",
-        values = c("linear", "poisson")),
       makeNumericVectorLearnerParam(id = "startbeta"),
       makeNumericVectorLearnerParam(id = "startgamma"),
       makeNumericLearnerParam(id = "epsilon", lower = 0, default = 1e-10),
@@ -18,25 +16,31 @@ makeRLearner.regr.penalized.ridge = function() {
       makeLogicalLearnerParam(id = "standardize", default = FALSE),
       makeLogicalLearnerParam(id = "trace", default = FALSE, tunable = FALSE)
     ),
-    par.vals = list(trace = FALSE, model = "linear"),
-    properties = c("numerics", "factors"),
-    name = "Ridge Regression",
+    par.vals = list(trace = FALSE),
+    properties = c("twoclass", "numerics", "factors", "ordered", "prob"),
+    name = "Logistic Ridge Regression",
     short.name = "ridge",
     note = "trace=FALSE was set by default to disable logging output."
   )
 }
 
 #' @export
-trainLearner.regr.penalized.ridge = function(.learner, .task, .subset, .weights = NULL,  ...) {
+trainLearner.classif.penalized.ridge = function(.learner, .task, .subset, .weights = NULL,  ...) {
   f = getTaskFormula(.task)
-  penalized::penalized(f, data = getTaskData(.task, .subset), fusedl = FALSE, ...)
+  penalized::penalized(f, data = getTaskData(.task, .subset), fusedl = FALSE, model = "logistic", ...)
 }
 
 #' @export
-predictLearner.regr.penalized.ridge = function(.learner, .model, .newdata, ...) {
+predictLearner.classif.penalized.ridge = function(.learner, .model, .newdata, ...) {
   m = .model$learner.model
+  levs = .model$task.desc$class.levels
   # FIXME: should be removed, reported in issue 840
   m@formula$unpenalized[[2L]] = as.symbol(.model$task.desc$target)
   .newdata[,.model$task.desc$target] = 0
-  penalized::predict(m, data = .newdata,  ...)[, "mu"]
+  pred = penalized::predict(m, data = .newdata,  ...)
+  if (.learner$predict.type == "prob") {
+    propVectorToMatrix(pred, levs)
+  } else {
+    as.factor(ifelse(pred > 0.5, levs[2L], levs[1L]))
+  }
 }
