889736cb44028ee978eb067529a5ad750a453c78
Philipp Probst <philipp_probst@gmx.de>
2016-04-26 16:40:12 +0200

    Added Brier score for multiclass problems
    (maybe the model.matrix( ~ . -1, data = as.data.frame(truth)))^2) could be done better, needs reshape2, which is imported in mlr)

--- R/measures.R
+++ R/measures.R
@@ -344,6 +344,28 @@ multiclass.auc = makeMeasure(id = "multiclass.auc", minimize = FALSE, best = 1, 
   }
 )
 
+#' @export multiclass.brier
+#' @rdname measures
+#' @format none
+multiclass.brier = makeMeasure(id = "multiclass.brier", minimize = TRUE, best = 0, worst = Inf,
+  properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
+  name = "Multiclass Brier score",
+  fun = function(task, model, pred, feats, extra.args) {
+    if (!is.na(pred$task.desc$negative)) {
+      measureBrier(getPredictionProbabilities(pred), pred$data$truth, pred$task.desc$negative, pred$task.desc$positive)
+    } else {
+      measureMulticlassBrier(getPredictionProbabilities(pred), pred$data$truth)
+    }
+  }
+)
+
+#' @export measureMulticlassBrier
+#' @rdname measures
+#' @format none
+measureMulticlassBrier = function(probabilities, truth) {
+  mean(rowSums((probabilities - model.matrix( ~ . -1, data = as.data.frame(truth)))^2))
+}
+
 ###############################################################################
 ### classif binary ###
 ###############################################################################
