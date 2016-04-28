286a3e07d73d9767c7a28dc064ff1134c677504b
Philipp Probst <philipp_probst@gmx.de>
2016-04-28 11:26:29 +0200

    changed
    Changed the worst value to two. Could change the two class case to calculate it as in the more than two class case, but probably this will lead to too much confusion for the user, so I leave it like that. 
    That brier is binary can be seen in the tutorial page, but renaming it, would make it clearer.

--- R/measures.R
+++ R/measures.R
@@ -347,7 +347,7 @@ multiclass.auc = makeMeasure(id = "multiclass.auc", minimize = FALSE, best = 1, 
 #' @export multiclass.brier
 #' @rdname measures
 #' @format none
-multiclass.brier = makeMeasure(id = "multiclass.brier", minimize = TRUE, best = 0, worst = Inf,
+multiclass.brier = makeMeasure(id = "multiclass.brier", minimize = TRUE, best = 0, worst = 2,
   properties = c("classif", "classif.multi", "req.pred", "req.truth", "req.prob"),
   name = "Multiclass Brier score",
   fun = function(task, model, pred, feats, extra.args) {
