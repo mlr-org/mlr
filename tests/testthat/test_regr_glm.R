d34756cfd09811bc9638f32191570aaf94590261
MariaErdmann <maria.erdmann@gmail.com>
2016-04-30 18:04:18 +0200

    adds glm learner #837

---
+++ tests/testthat/test_regr_glm.R
@@ -0,0 +1,38 @@
+context("regr_glm")
+
+test_that("regr_glm", {
+  requirePackagesOrSkip("glm", default.method = "load")
+  
+  parset.list = list(
+    list(),
+    list(family = inverse.gaussian(link = "log"), start = rep(0.4,14)),
+    list(trace = TRUE, epsilon = 1e-10, maxit = 30),
+    list(x = TRUE, y = FALSE)
+  )
+  
+  old.predicts.list = list()
+  
+  for (i in 1:length(parset.list)) {
+    parset = parset.list[[i]]
+    pars = list(formula = regr.formula, data = regr.train)
+    pars = c(pars, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = do.call(stats::glm, pars)
+    set.seed(getOption("mlr.debug.seed"))
+    p = predict(m, newdata = regr.test, type = "response")
+    old.predicts.list[[i]] = p
+  }
+  
+  parset.list = list(
+    list(),
+    list(family = "inverse.gaussian", inverse.gaussian.link = "log", mustart = rep(0.4,14)),
+    list(trace = TRUE, epsilon = 1e-10, maxit = 30),
+    list(x = TRUE, y = FALSE)
+  )
+  
+  testSimpleParsets("regr.glm", regr.df, regr.target,
+    regr.train.inds, old.predicts.list, parset.list)
+  
+})
+
+
