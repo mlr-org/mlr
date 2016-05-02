4340553934545c25caffefa49ec0afe627b51263
Florian Fendt <Flo@MacBook-Pro.local>
2016-05-02 16:40:06 +0200

    #812 fix bug in convertBMRToRankMatrix

--- R/convertBMRToRankMatrix.R
+++ R/convertBMRToRankMatrix.R
@@ -47,8 +47,9 @@ convertBMRToRankMatrix = function(bmr, measure = NULL, ties.method = "average", 
   # convert into matrix, rows = leaner, cols = tasks
   df = reshape2::melt(df, c("task.id", "learner.id"), "alg.rank")
   df = reshape2::dcast(df, learner.id ~ task.id )
-  rownames(df) = df$learner.id
-  mat = as.matrix(df[,colnames(df) != "learner.id"])
-
+  task.id.names = setdiff(colnames(df), "learner.id")
+  mat = as.matrix(df[, task.id.names])
+  rownames(mat) = df$learner.id
+  colnames(mat) = task.id.names
   return(mat)
 }
