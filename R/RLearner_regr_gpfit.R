886969949e44153a88656cae5e3ef1bd95b99d3d
Dotterbart <dominik.lenk@tu-dortmund.de>
2016-05-17 15:02:15 +0200

    file added

---
+++ R/RLearner_regr_gpfit.R
@@ -0,0 +1,27 @@
+#' @export
+makeRLearner.regr.gpfit <- function(){
+  makeRLearnerRegr(
+    cl = "regr.gpfit",
+    package = "GPfit",
+    par.set = makeParamSet(
+      makeNumericVectorLearnerParam(id = "contro", len = 3),
+      makeNumericLearnerParam(id = "nug_thres", default = 20),
+      makeLogicalLearnerParam(id = "trace", default = FALSE),
+      makeIntegerLearnerParam(id = "maxit", default = 100),
+      makeNumericLearnerParam(id = "optim_start")
+    ),
+    properties = c("numerics"),
+    name = "Gaussian Process Model fitting",
+    short.name = "gpfit",
+    note = "The optimization routine assumes that the inputs are scaled to the unit hypercube [0,1]^d"
+  )
+}
+#' @export
+trainLearner.regr.gpfit = function(.learner, .task, .subset, ...) {
+  GPfit::GP_fit(getTaskData(.task, .subset, target.extra = TRUE)$data, getTaskTargets(.task, .subset), ...)
+}
+#' @export
+predictLearner.regr.gpfit = function(.learner, .model, .newdata, ...) {
+  predict(.model$learner.model, newdata = .newdata)$Y_hat
+}
+
