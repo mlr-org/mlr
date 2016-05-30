9685f132d7661e2c51ee0332c45f75a2bc12263f
Michel Lang <michellang@gmail.com>
2016-05-30 18:42:06 +0200

    remove upper limit for glmnet's parameter s (#915)

--- R/RLearner_surv_glmnet.R
+++ R/RLearner_surv_glmnet.R
@@ -5,7 +5,7 @@ makeRLearner.surv.glmnet = function() {
     package = "glmnet",
     par.set = makeParamSet(
       makeNumericLearnerParam(id = "alpha", default = 1, lower = 0, upper = 1),
-      makeNumericLearnerParam(id = "s", default = 0.01, lower = 0, upper = 1, when = "predict"),
+      makeNumericLearnerParam(id = "s", default = 0.01, lower = 0, when = "predict"),
       makeLogicalLearnerParam(id = "exact", default = FALSE, when = "predict"),
       makeIntegerLearnerParam(id = "nlambda", default = 100L, lower = 1L),
       makeNumericLearnerParam(id = "lambda.min.ratio", lower = 0, upper = 1),
