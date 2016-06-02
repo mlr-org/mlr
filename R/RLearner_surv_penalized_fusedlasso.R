50bceffc170ae52f4039951d84b07ae80bc6d9b1
Florian Fendt <Flo@MacBook-Pro.local>
2016-06-02 16:36:13 +0200

    fix surv lrns from penalize package

--- R/RLearner_surv_penalized_fusedlasso.R
+++ R/RLearner_surv_penalized_fusedlasso.R
@@ -17,7 +17,7 @@ makeRLearner.surv.penalized.fusedlasso = function() {
       makeLogicalLearnerParam(id = "standardize", default = FALSE),
       makeLogicalLearnerParam(id = "trace", default = FALSE, tunable = FALSE)
     ),
-    par.vals = list(lambda1 = 1, lambda2 = 1),
+    par.vals = list(lambda1 = 1, lambda2 = 1, trace = FALSE),
     properties = c("numerics", "factors", "ordered", "rcens"),
     name = "Fused Lasso Regression",
     short.name = "fusedlasso",
@@ -27,15 +27,24 @@ makeRLearner.surv.penalized.fusedlasso = function() {
 
 #' @export
 trainLearner.surv.penalized.fusedlasso = function(.learner, .task, .subset, .weights = NULL,  ...) {
-  f = getTaskFormula(.task)
-  penalized::penalized(f, data = getTaskData(.task, subset = .subset), model = "cox", fusedl = TRUE, ...)
+  data = getTaskData(.task, subset = .subset, target.extra = TRUE, recode.target = "rcens")
+  info = getFixDataInfo(data$data, factors.to.dummies = TRUE, ordered.to.int = TRUE)
+  data$data = as.matrix(fixDataForLearner(data$data, info))
+  attachTrainingInfo(
+    penalized::penalized(response = data$target, penalized = data$data, model = "cox",
+      fusedl = TRUE, ...),
+    info
+  )
 }
 
 #' @export
 predictLearner.surv.penalized.fusedlasso = function(.learner, .model, .newdata, ...) {
+  info = getTrainingInfo(.model)
+  .newdata = as.matrix(fixDataForLearner(.newdata, info))
   if(.learner$predict.type == "response") {
     # Note: this is a rather ugly hack but should work according to Jelle
-    penalized::survival(penalized::predict(.model$learner.model, data = .newdata), Inf)
+    penalized::survival(penalized::predict(.model$learner.model,
+      penalized = .newdata), Inf)
   } else {
     stop("Unknown predict type")
   }
