58db33c2598569f8524f3d4176cb16b3566a26f0
MariaErdmann <maria.erdmann@gmail.com>
2016-06-10 14:57:08 +0200

    Deleted param mtry.ratio, adapted note, training function, adapted test

--- tests/testthat/test_surv_randomForestSRC.R
+++ tests/testthat/test_surv_randomForestSRC.R
@@ -5,18 +5,14 @@ test_that("surv_randomForestSRC", {
 
   parset.list = list(
     list(),
-    list(ntree = 100),
-    list(ntree = 50, mtry.ratio = 0.9),
-    list(ntree = 50, nodesize = 2, na.action = "na.impute", splitrule = "logrank", importance = "permute", proximity = FALSE)
+    list(ntree = 100L),
+    list(ntree = 50L, mtry = 5L),
+    list(ntree = 50L, nodesize = 2L, na.action = "na.impute", splitrule = "logrank", importance = "permute", proximity = FALSE)
   )
   old.predicts.list = list()
 
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    if (!is.null(parset$mtry.ratio)) {
-      parset$mtry = parset$mtry.ratio * (ncol(surv.df) - 2)
-      parset$mtry.ratio = NULL
-    } 
     parset = c(parset, list(data = surv.train, formula = surv.formula, forest = TRUE))
     set.seed(getOption("mlr.debug.seed"))
     # There are some crazy checks in RFSRC, we have to overwrite the formula here
