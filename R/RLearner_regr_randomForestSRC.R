1d825da45fbb0e5213c0a5f77137673862599e41
schiffner <schiffner@math.uni-duesseldorf.de>
2016-06-13 16:42:23 +0200

    deleted param mtry.ratio also from classification and regression forests

--- R/RLearner_regr_randomForestSRC.R
+++ R/RLearner_regr_randomForestSRC.R
@@ -8,7 +8,6 @@ makeRLearner.regr.randomForestSRC = function() {
       makeDiscreteLearnerParam(id = "bootstrap", default = "by.root",
         values = c("by.root", "by.node", "none")),
       makeIntegerLearnerParam(id = "mtry", lower = 1L),
-      makeNumericLearnerParam(id = "mtry.ratio", lower = 0L, upper = 1L),
       makeIntegerLearnerParam(id = "nodesize", lower = 1L, default = 5L),
       makeIntegerLearnerParam(id = "nodedepth", default = -1L),
       makeDiscreteLearnerParam(id = "splitrule", default = "mse",
@@ -43,20 +42,14 @@ makeRLearner.regr.randomForestSRC = function() {
     properties = c("missings", "numerics", "factors", "ordered", "weights"),
     name = "Random Forest",
     short.name = "rfsrc",
-    note = '`na.action` has been set to `"na.impute"` by default to allow missing data support.
-      `mtry.ratio` indicates the proportion of variables randomly selected as candidates for a split.'
+    note = '`na.action` has been set to `"na.impute"` by default to allow missing data support.'
   )
 }
 
 #' @export
-trainLearner.regr.randomForestSRC = function(.learner, .task, .subset, .weights = NULL, mtry = NULL, mtry.ratio = NULL, ...) {
-  if (!is.null(mtry.ratio)) {
-    if (!is.null(mtry))
-      stop("You cannot set both 'mtry' and 'mtry.ratio'")
-    mtry = mtry.ratio * getTaskNFeats(.task)
-  }
+trainLearner.regr.randomForestSRC = function(.learner, .task, .subset, .weights = NULL, ...) {
   f = getTaskFormula(.task)
-  randomForestSRC::rfsrc(f, data = getTaskData(.task, .subset), forest = TRUE, mtry = mtry, case.wt = .weights, ...)
+  randomForestSRC::rfsrc(f, data = getTaskData(.task, .subset), forest = TRUE, case.wt = .weights, ...)
 }
 
 #' @export
