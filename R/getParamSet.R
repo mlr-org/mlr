fb00cd991d1676023ba6d2224c07ced5c0971eae
Michel Lang <michellang@gmail.com>
2016-06-14 18:23:53 +0200

    fixes for new PH

--- R/getParamSet.R
+++ R/getParamSet.R
@@ -1,23 +1,19 @@
-#' Get a description of all possible parameter settings for a learner.
+#' @title Get a description of all possible parameter settings for a learner.
 #'
 #' @template arg_learner
 #' @template ret_ps
 #' @family learner
+#' @name getParamSet
+#' @rdname getParamSet
+NULL
+
 #' @export
-getParamSet = function(learner) {
-  checkLearner(learner)
-  UseMethod("getParamSet")
+getParamSet.Learner = function(x) {
+  x$par.set
 }
 
-#'@export
-getParamSet.Learner = function(learner) {
-  learner$par.set
+#' @export
+getParamSet.character = function(x) {
+  x = checkLearner(x)
+  getParamSet(x)
 }
-
-#'@export
-getParamSet.character = function(learner) {
-  learner = checkLearner(learner)
-  getParamSet(learner)
-}
-
-
