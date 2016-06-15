64fdc2dbdfb148ba49f5c67d66fc1f21434621ef
jogi <jogi@dhcp-10-180-4-134.dynamic.eduroam.mwn.de>
2016-06-15 17:59:36 +0200

    In the case of no hyperpars, .val should be empty a but named list

--- R/setHyperPars.R
+++ R/setHyperPars.R
@@ -37,6 +37,9 @@ setHyperPars2 = function(learner, par.vals) {
 #' @export
 setHyperPars2.Learner = function(learner, par.vals) {
   ns = names(par.vals)
+  if(is.null(ns)) { 
+    names(learner$par.vals) = character(0)
+  }
   pars = learner$par.set$pars
   on.par.without.desc = coalesce(learner$config$on.par.without.desc, getMlrOptions()$on.par.without.desc)
   on.par.out.of.bounds = coalesce(learner$config$on.par.out.of.bounds, getMlrOptions()$on.par.out.of.bounds)
