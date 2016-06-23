6bc643af85e87bdc7e1edae23f9e2f386b597860
MariaErdmann <maria.erdmann@gmail.com>
2016-06-23 16:57:35 +0200

    Created helper functions in helper_learners_all and applied them in learners_all_surv

--- tests/testthat/test_learners_all_surv.R
+++ tests/testthat/test_learners_all_surv.R
@@ -7,110 +7,31 @@ test_that("learners work: surv ", {
     surv.cforest = list(mtry = 1L)
   )
   
-  fixHyperPars = function(lrn) {
-    if (lrn$id %in% names(hyperpars))
-      lrn = setHyperPars(lrn, par.vals = hyperpars[[lrn$id]])
-    return(lrn)
-  }
   
   # normal survival analysis
   sub.task = subsetTask(surv.task, subset = c(1:70),
     features = getTaskFeatureNames(surv.task)[c(3,4)])
-  checkLearnersAllOnTask = function(task){
-    lrns = mylist(task)
-    lrns = lapply(lrns$class, makeLearner)
-    for (lrn in lrns){
-      expect_output(print(lrn), lrn$id)
-      lrn = fixHyperPars(lrn)
-      m = train(lrn, task)
-      p = predict(m, task)
-      expect_true(!is.na(performance(p)))
-    }
-  }
+  lrns = mylist("surv", create = TRUE)
+  lapply(lrns, checkPerformance, task = sub.task, hyperpars = hyperpars)
   
-  checkLearnersAllOnTask(surv.task)
   
   # survival analysis with factors
-  checkLearnersHandleFactors = function(task) {
-    type = getTaskType(task) # to create new task
-    costs = getTaskCosts(task) # used to create new.task in case of costsens
-    target = getTaskTargetNames(task) # used to create new.task
-    d = getTaskData(task)
-    f = getTaskFeatureNames(task)[1] # get 1 feature to create factor variable
-    d[,f] = factor(sample(c("a", "b"), size = nrow(d), replace = TRUE)) # create factor variable
-    
-    new.task = switch(type,
-      classif = makeClassifTask(data = d, target = target),
-      cluster = makeClusterTask(data = d),
-      costsens = makeCostSensTask(data = d, costs = costs),
-      multilabel = makeMultilabelTask(data = d, target = target),
-      regr = makeRegrTask(data = d, target = target),
-      surv = makeSurvTask(data = d, target = target))
+  lrns = mylist("surv", properties = "factors", create = TRUE)
+  lapply(lrns, testThatLearnerHandlesFactors, task = surv.task, hyperpars = hyperpars)
   
-    lrns = mylist(task, properties = "factors", create = TRUE) # use task or new.task here?
-    
-    for (lrn in lrns) {
-      expect_output(print(lrn), lrn$id)
-      lrn = fixHyperPars(lrn)
-      m = train(lrn, task)
-      p = predict(m, task)
-      expect_true(!is.na(performance(p)))
-    }
-  }
-  
-  checkLearnersHandleFactors(surv.task)
-  
-  # mlr doesn't support prediction of probabilities yet
-  # binary classif with prob: only coxph and ranger
-  # task = subsetTask(surv.task, subset = c(1:70),
-  #   features = getTaskFeatureNames(surv.task)[c(3,4)])
-  # lrns = mylist(task, properties = "prob")
-  # lrns = lapply(lrns$class, makeLearner, predict.type = "prob")
-  # lapply(lrns, function(lrn) {
-  #   lrn = fixHyperPars(lrn)
-  #   m = train(lrn, task)
-  #   p = predict(m, task)
-  #   getPredictionProbabilities(p)
-  #   expect_true(!is.na(performance(p)))
-  # })
   
   # surv with weights
-  # bigger dataset necessary otherwise cvglmnet does not converge
-  task = surv.task
+  # normal size of surv.task necessary otherwise cvglmnet does not converge
   lrns = mylist("surv", properties = "weights", create = TRUE)
   lapply(lrns, testThatLearnerRespectsWeights, hyperpars = hyperpars,
-    task = task, train.inds = surv.train.inds,
+    task = surv.task, train.inds = surv.train.inds,
     test.inds = surv.test.inds,
     weights = rep(c(1L, 5L), length.out = length(surv.train.inds)),
     pred.type = "response", get.pred.fun = getPredictionResponse)
-  # surv.randomForestSRC is not working
   
 
-  # surv with missing.
-  checkLearnersHandleMissings = function(task) {
-    target = getTaskTargetNames(task)
-    type = getTaskType(task)
-    f = getTaskFeatureNames(task)[1] # get one feature where an NA will be placed
-    d = getTaskData(task)
-    costs = getTaskCosts(task)
-    d[1,f] = NA
-    new.task = switch(type,
-      classif = makeClassifTask(data = d, target = target),
-      cluster = makeClusterTask(data = d),
-      costsens = makeCostSensTask(data = d, costs = costs),
-      multilabel = makeMultilabelTask(data = d, target = target),
-      regr = makeRegrTask(data = d, target = target),
-      surv = makeSurvTask(data = d, target = target))
-    
-    lrns = mylist(new.task, properties = "missings", create = TRUE) # do this on task instead of new.task?
-    
-    for (lrn in lrns) {
-      lrn = fixHyperPars(lrn)
-      m = train(lrn, task)
-      p = predict(m, task)
-      expect_true(!is.na(performance(p)))
-    }
-  }
-  checkLearnersHandleMissings(surv.task)
+  # survival with missings
+  lrns = mylist("surv", properties = "missings", create = TRUE)
+  lapply(lrns, testThatLearnerHandlesMissings, task = surv.task, hyperpars = hyperpars) 
 
-  })
+})
