7a3bf7b570817c8f16f2727945e86ecae1429cc4
Michel Lang <michellang@gmail.com>
2016-06-28 23:28:27 +0200

    set respect.unordered.factors for ranger learners (#918)

--- tests/testthat/test_surv_ranger.R
+++ tests/testthat/test_surv_ranger.R
@@ -20,7 +20,7 @@ test_that("surv_ranger", {
   old.predicts.list = list()
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    pars = list(formula = surv.formula, data = surv.train, write.forest = TRUE)
+    pars = list(formula = surv.formula, data = surv.train, write.forest = TRUE, respect.unordered.factors = TRUE)
     pars = c(pars, parset)
     set.seed(getOption("mlr.debug.seed"))
     m = do.call(ranger::ranger, pars)
