b4c9a4e74aa72aa510c18acef2ca9735b2c864b5
MariaErdmann <maria.erdmann@gmail.com>
2016-06-29 17:36:49 +0200

    Just adjusted layout according to the other test_learners_all

--- tests/testthat/test_learners_all_surv.R
+++ tests/testthat/test_learners_all_surv.R
@@ -7,18 +7,15 @@ test_that("learners work: surv ", {
     surv.cforest = list(mtry = 1L)
   )
   
-  
   # normal survival analysis
   sub.task = subsetTask(surv.task, subset = c(1:70),
     features = getTaskFeatureNames(surv.task)[c(3,4)])
   lrns = mylist("surv", create = TRUE)
-  lapply(lrns, checkPerformance, task = sub.task, hyperpars = hyperpars)
-  
+  lapply(lrns, testThatLearnerCanTrainPredict, task = sub.task, hyperpars = hyperpars)
   
   # survival analysis with factors
   lrns = mylist("surv", properties = "factors", create = TRUE)
   lapply(lrns, testThatLearnerHandlesFactors, task = surv.task, hyperpars = hyperpars)
-  
   
   # surv with weights
   # normal size of surv.task necessary otherwise cvglmnet does not converge
@@ -29,7 +26,6 @@ test_that("learners work: surv ", {
     weights = rep(c(1L, 5L), length.out = length(surv.train.inds)),
     pred.type = "response", get.pred.fun = getPredictionResponse)
   
-
   # survival with missings
   lrns = mylist("surv", properties = "missings", create = TRUE)
   lapply(lrns, testThatLearnerHandlesMissings, task = surv.task, hyperpars = hyperpars) 
