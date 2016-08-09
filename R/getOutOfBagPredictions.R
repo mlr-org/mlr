169fa707b49d209e72157a212fab4d7a47f5cd50
root <root@localhost.localdomain>
2016-08-09 15:43:07 +0200

    changed the test further and the example

--- R/getOutOfBagPredictions.R
+++ R/getOutOfBagPredictions.R
@@ -28,13 +28,14 @@
 #' training.set = sample(1:150, 50)
 #' lrn = makeLearner("classif.ranger", predict.type = "prob", predict.threshold = 0.6)
 #' mod = train(lrn, sonar.task, subset = training.set)
-#' oob = getOutOfBag(mod, sonar.task)
+#' oob = getOutOfBagPredictions(mod, sonar.task)
 #' oob
 #' performance(oob, measures = list(auc, mmce))
 getOutOfBagPredictions = function(object, task) {
   assertClass(object, classes = "WrappedModel")
   assertClass(task, classes = "Task")
-  expect_identical(task$task.desc, object$task.desc)   # Does the task correspond to the model?
+  # Does the task correspond to the model?
+  assertSetEqual(identical(task$task.desc, object$task.desc), TRUE) 
 
   td = object$task.desc
   # extract truth column
