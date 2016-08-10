be5bfb4ab5594b304008155163f75ee11b1a12e2
Lars Kotthoff <larskotthoff@users.noreply.github.com>
2016-08-10 10:31:06 +0200

    Style

--- R/RLearner_classif_RRF.R
+++ R/RLearner_classif_RRF.R
@@ -37,14 +37,14 @@ makeRLearner.classif.RRF = function() {
 }
 
 #' @export
-trainLearner.classif.RRF <- function(.learner, .task, .subset, .weights, ...) {
+trainLearner.classif.RRF = function(.learner, .task, .subset, .weights, ...) {
   args = list(...)
   RRF::RRF(formula = getTaskFormula(.task), data = getTaskData(.task, .subset), 
            keep.forest= TRUE, ...)
 }
 
 #' @export
-predictLearner.classif.RRF <- function(.learner, .model, .newdata, ...) {
+predictLearner.classif.RRF = function(.learner, .model, .newdata, ...) {
   type = ifelse(.learner$predict.type=="response", "response", "prob")
   p = predict(object = .model$learner.model, newdata = .newdata, type = type, ...)
   return(p)
