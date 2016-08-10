59c78578960294cfeabf089c33fcace3f0aefe85
smilesun <smilesun.east@gmail.com>
2016-08-10 15:03:56 +0200

    add roxygen template

---
+++ man-roxygen/arg_labels.R
@@ -0,0 +1,4 @@
+#' @param labels [\code{character}|\code{logical}|\code{numerical}|\code{integer}]\cr
+#'   supply the subset for the labels in the multi-label classification problem, can be integer index
+#'   which is the column index in the dataframe, or the column name directly or logical vector
+
