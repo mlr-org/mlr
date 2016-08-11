172a92ad05a7536d139995efa000a0333983af22
Bernd Bischl <bernd_bischl@gmx.net>
2016-08-11 12:47:38 +0200

    ...

--- R/calculateConfusionMatrix.R
+++ R/calculateConfusionMatrix.R
@@ -2,17 +2,19 @@
 #'
 #' @description
 #' Calculates the confusion matrix for a (possibly resampled) prediction.
-#' Rows indicate true classes, columns predicted classes. The marginal elements count the number of 
+#' Rows indicate true classes, columns predicted classes. The marginal elements count the number of
 #' classification errors for the respective row or column, i.e., the number of errors
-#' when you condition on the corresponding true (rows) or predicted (columns) class. 
+#' when you condition on the corresponding true (rows) or predicted (columns) class.
 #' The last bottom right element displays the total amount of errors.
-#' 
+#'
 #' A list is returned that contains multiple matrices.
-#' If \code{relative = TRUE} we compute three
-#' matrices, one with absolute values and two with relative. The relative confusion matricies are 
-#' normalized based on rows and columns respectively. The \code{print} function returns the relative matricies in
-#' a compact way so that both row and column marginals can be seen in one matrix. For details see
-#' \code{\link{ConfMatrix}}.
+#' If \code{relative = TRUE} we compute three matrices, one with absolute values and two with relative.
+#' The relative confusion matrices are normalized based on rows and columns respectively,
+#' if {FALSE} we only compute the absolute value matrix.
+#'
+#' The \code{print} function returns the relative matrices in
+#' a compact way so that both row and column marginals can be seen in one matrix.
+#' For details see \code{\link{ConfMatrix}}.
 #'
 #' Note that for resampling no further aggregation is currently performed.
 #' All predictions on all test sets are joined to a vector yhat, as are all labels
@@ -21,15 +23,12 @@
 #'
 #' @template arg_pred
 #' @param relative [\code{logical(1)}]\cr
-#'   If \code{TRUE} two additional matricies are calculated. One is normalized by rows and one by
+#'   If \code{TRUE} two additional matrices are calculated. One is normalized by rows and one by
 #'   columns.
 #' @param sums {\code{logical(1)}}\cr
-#'   If \code{TRUE} add absolute number of observations in each group are added to the confusion matrix
-#'   of absolute values.
-#' @return [\code{\link{ConfMatrix}}]. A list containing one or three matricies 
-#' (one absolute and two relative) as well as additional information about the resampling.
+#'   If \code{TRUE} add absolute number of observations in each group.
+#' @return [\code{\link{ConfMatrix}}].
 #' @export
-#' @seealso \code{\link{predict.WrappedModel}}
 #' @examples
 #' # get confusion matrix after simple manual prediction
 #' allinds = 1:150
@@ -61,7 +60,7 @@ calculateConfusionMatrix = function(pred, relative = FALSE, sums = FALSE) {
   col.err = colSums(mt)
   result = rbind(cbind(tab, row.err), c(col.err, sum(col.err)))
   dimnames(result) = list(true = c(cls, "-err.-"), predicted = c(cls, "-err.-"))
-  
+
   if (sums) {
     rowsum = rowSums(tab)
     colsum = colSums(tab)
@@ -69,78 +68,78 @@ calculateConfusionMatrix = function(pred, relative = FALSE, sums = FALSE) {
     colnames(result)[k + 2] = "-n-"
     rownames(result)[k + 2] = "-n-"
   }
-  
+
   result = list(result = result, task.desc = getTaskDescription(pred), relative = relative, sums = sums)
-  
+
   js = 1:k # indexes for nonmargin cols
-  
+
   if (relative) {
-    
+
     normConfMatrix = function(r) {
       if (any(r[js] > 0))
         r / sum(r[js])
       else
         rep(0, k)
     }
-    
+
     #normalize by rows and add margins as a new column
     result.rel.row = t(apply(tab, 1, normConfMatrix))
     result.rel.row = cbind(result.rel.row, "-err-" = rowSums(result.rel.row) - diag(result.rel.row))
-    
+
     #normalize by columns and add margins as a new row
     result.rel.col = apply(tab, 2, normConfMatrix)
     result.rel.col = rbind(result.rel.col, "-err-" = colSums(result.rel.col) - diag(result.rel.col))
-    
+
     result$relative.row = result.rel.row
     result$relative.col = result.rel.col
     result$relative.error = sum(result$result[k+1, 1:(k+1)])/n
   }
-  
+
   addClasses(result, "ConfMatrix")
 }
 
 #' @export
 #' @describeIn calculateConfusionMatrix
-#' 
-#' @param  x [\code{confMatrix}]\cr
-#'  Result of \code{calculateConfusionMatrix}.
+#'
+#' @param x [\code{\link{ConfMatrix}}]\cr
+#'   Object to print.
 #' @param both [\code{logical(1)}]\cr
-#'  If \code{TRUE} both the absolute and relative confusion matricies are printed.
-#' @param digits [\code{numeric(1)}]\cr
-#'  How many numbers after the decimal point should be printed, only relevant for relative confusion matricies.
+#'   If \code{TRUE} both the absolute and relative confusion matrices are printed.
+#' @param digits [\code{integer(1)}]\cr
+#'   How many numbers after the decimal point should be printed, only relevant for relative confusion matrices.
 #' @param ... [any]\cr
 #'  Currently not used.
 print.ConfMatrix = function(x, both = TRUE, digits = 2, ...) {
-  
+
   assertFlag(both)
   assertInt(digits, lower = 1)
-  
+
   #formatting stuff, use digits after(!) the decimal point.
   nsmall = digits
   digits = nsmall - 1
-  
+
   cls = getTaskDescription(x$task.desc)$class.levels
   k = length(cls)
   n = getTaskDescription(x$task.desc)$size
-  
-  
+
+
   if (x$relative) {
     js = 1:k
-    res = paste(format(x$relative.row[js, js], digits = digits, nsmall = nsmall), 
+    res = paste(format(x$relative.row[js, js], digits = digits, nsmall = nsmall),
       format(x$relative.col[js, js], digits = digits, nsmall = nsmall), sep = "/")
     attributes(res) = attributes(x$relative.row[js, js])
-    
-    
+
+
     col.err = x$relative.col[k + 1,]
     row.err = x$relative.row[,k + 1]
-    full.err = stri_pad_right(format(x$relative.error, digits = digits, nsmall = nsmall), 
+    full.err = stri_pad_right(format(x$relative.error, digits = digits, nsmall = nsmall),
       width = nchar(res[1,1]))
-    
+
     #bind marginal errors correctly formatted to rows and columns
-    res = rbind(res, stri_pad_left(format(col.err, digits = digits, nsmall = nsmall), 
+    res = rbind(res, stri_pad_left(format(col.err, digits = digits, nsmall = nsmall),
       width = nchar(res[1,1])))
     res = cbind(res, c(format(row.err, digits = digits, nsmall = nsmall), full.err))
-    
+
     #also bind the marginal sums to the relative confusion matrix for printing
     if (x$sums) {
       res = rbind(cbind(res, c(x$result["-n-", 1:k], NA)), c(x$result[1:k, "-n-"], NA, n))
@@ -148,7 +147,7 @@ print.ConfMatrix = function(x, both = TRUE, digits = 2, ...) {
     } else {
       dimnames(res) = list(true = c(cls, "-err.-"), predicted = c(cls, "-err.-"))
     }
-    
+
     cat("Relative confusion matrix (normalized by row/column):\n")
     print(noquote(res))
     if (both) {
@@ -161,11 +160,11 @@ print.ConfMatrix = function(x, both = TRUE, digits = 2, ...) {
   }
 }
 
-#' @title ConfusionMatrix
+#' @title ConfMatrix
 #'
-#' @description 
+#' @description
 #' The result of \code{\link{calculateConfusionMatrix}}.
-#' 
+#'
 #' Object members:
 #' \describe{
 #' \item{result [\code{matrix}]}{Confusion matrix of absolute values and marginals. Can also contain
@@ -178,8 +177,7 @@ print.ConfMatrix = function(x, both = TRUE, digits = 2, ...) {
 #' \item{relative.row [\code{matrix}]}{Confusion matrix of relative values and marginals normalized by row.}
 #' \item{relative.col [\code{matrix}]}{Confusion matrix of relative values and marginals normalized by column.}
 #' \item{relative.error [\code{numeric(1)}]}{Relative error overall.}
-#' } 
-#' @name ConfusionMatrix
-#' @rdname ConfusionMatrix
-#' @aliases ConfMatrix
+#' }
+#' @name ConfMatrix
+#' @rdname ConfMatrix
 NULL
