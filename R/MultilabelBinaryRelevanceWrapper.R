583e877039d81b85413aaf3ea0c4ed8ab2e15f81
Jakob Richter <code@jakob-r.de>
2016-08-11 19:02:27 +0200

    cleanup passed variables for MultilabelBinaryRelevanceWrapper

--- R/MultilabelBinaryRelevanceWrapper.R
+++ R/MultilabelBinaryRelevanceWrapper.R
@@ -39,12 +39,12 @@ makeMultilabelBinaryRelevanceWrapper = function(learner) {
 trainLearner.MultilabelBinaryRelevanceWrapper = function(.learner, .task, .subset, .weights = NULL,...) {
   targets = getTaskTargetNames(.task)
   .task = subsetTask(.task, subset = .subset)
-  data = getTaskData(.task)
   parallelLibrary("mlr", master = FALSE, level = "mlr.ensemble", show.info = FALSE)
   exportMlrOptions(level = "mlr.ensemble")
-  models = parallelMap(doMultilabelBinaryRelevanceTrainIteration, tn = targets,
-                       more.args = list(weights = .weights, learner = .learner$next.learner, task = .task,
-                                        data = data), level = "mlr.ensemble")
+  models = parallelMap(
+    doMultilabelBinaryRelevanceTrainIteration, tn = targets,
+    more.args = list(weights = .weights, learner = .learner$next.learner, task = .task), 
+    level = "mlr.ensemble")
   makeHomChainModel(.learner, models)
 }
 
@@ -57,8 +57,9 @@ predictLearner.MultilabelBinaryRelevanceWrapper = function(.learner, .model, .ne
     function(m) getPredictionProbabilities(predict(m, newdata = .newdata, ...), cl = "TRUE")
   asMatrixCols(lapply(models, f))
 
-doMultilabelBinaryRelevanceTrainIteration = function(tn, learner, task, data, weights) {
+doMultilabelBinaryRelevanceTrainIteration = function(tn, learner, task, weights) {
   setSlaveOptions()
+  data = getTaskData(task)
   task = makeClassifTask(id = tn, data = dropNamed(data, setdiff(getTaskTargetNames(task), tn)), target = tn)
   train(learner, task, weights = weights)
 }
