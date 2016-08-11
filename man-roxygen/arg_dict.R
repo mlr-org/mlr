eaea99e1ec7c7efaf34e2ddefdf5cc1ae382532f
Pascal Kerschke <pascal.kerschke@uni-muenster.de>
2016-08-11 18:16:42 +0200

    rm dict_template

--- man-roxygen/arg_dict.R
+++
@@ -1,3 +0,0 @@
-#' @param dict [\code{environment} | \code{list} | \code{NULL}]\cr
-#'   Environment or list which will be used for evaluating the variables
-#'   of expressions within a parameter set. The default is \code{NULL}.
