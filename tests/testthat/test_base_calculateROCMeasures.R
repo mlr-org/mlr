dc222162b52ecb52a47e65ee5829d837d558e94b
janek <janek.thomas@web.de>
2016-08-11 12:25:58 +0200

    stupid bug

--- tests/testthat/test_base_calculateROCMeasures.R
+++ tests/testthat/test_base_calculateROCMeasures.R
@@ -7,7 +7,7 @@ test_that("calculateROCMeasures", {
   
   expect_list(r$measures, types = "double", any.missing = FALSE, len = 12)
   expect_matrix(r$confusion.matrix, mode = "double", any.missing = FALSE, nrows = 2, ncols = 2)
-  expect_true(all(rowSums(r$confusionMatrix) == 1))
+  expect_true(all(rowSums(r$confusion.matrix) == 1))
   
   response = getPredictionResponse(pred$pred)
   truth = getPredictionTruth(pred$pred)
