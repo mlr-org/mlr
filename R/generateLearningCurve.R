f62a112bc76a428047798493a80468f7d8bc8090
Bernd Bischl <bernd_bischl@gmx.net>
2016-08-11 14:21:54 +0200

    overhauled NEWS and mini code cleanup. NEWS are here ok now

--- R/generateLearningCurve.R
+++ R/generateLearningCurve.R
@@ -46,9 +46,7 @@
 generateLearningCurveData = function(learners, task, resampling = NULL,
   percs = seq(0.1, 1, by = 0.1), measures, stratify = FALSE, show.info = getMlrOption("show.info"))  {
 
-  # if single learner is passed, wrap it in list
-  if (inherits(learners, "Learner"))
-    learners = list(learners)
+  learners = ensureVector(learners, 1, "Learner")
   learners = lapply(learners, checkLearner)
   assertClass(task, "Task")
   assertNumeric(percs, lower = 0L, upper = 1L, min.len = 2L, any.missing = FALSE)
