c68f7aa71708e5a8d24c4874edfbeb65f60acce1
Lars Kotthoff <larsko@cs.ubc.ca>
2016-08-12 10:07:43 +0200

    fix families generation (hopefully)

--- thirdparty/gen_families.sh
+++ thirdparty/gen_families.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "\\name{mlrFamilies}"
-echo "\\title{mlr documentation families}"
-echo "\\description{List of all mlr documentation families with members.}"
-echo "\\arguments{"
+echo '\name{mlrFamilies}'
+echo '\title{mlr documentation families}'
+echo '\description{List of all mlr documentation families with members.}'
+echo '\arguments{'
 awk 'BEGIN { fam = 0 } /@family/ { fam = $3 } /^[a-z]/ { if(fam) { fams[fam] = fams[fam] $1 ", "; fam = 0 } } END { for(fam in fams) { sub(/, $/, "", fams[fam]); print "\\item{" fam "}{" fams[fam] "}" } }' R/*
-echo "}"
+echo '}'
