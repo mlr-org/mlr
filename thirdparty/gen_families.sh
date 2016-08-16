5118c0591fc82ee4c4d60dca4ffc30501feedd53
Lars Kotthoff <larsko@cs.ubc.ca>
2016-08-16 07:25:28 -0700

    sort families

--- thirdparty/gen_families.sh
+++ thirdparty/gen_families.sh
@@ -3,5 +3,5 @@ echo '\name{mlrFamilies}'
 echo '\title{mlr documentation families}'
 echo '\description{List of all mlr documentation families with members.}'
 echo '\arguments{'
-awk 'BEGIN { fam = 0 } /@family/ { fam = $3 } /^[a-z]/ { if(fam) { fams[fam] = fams[fam] $1 ", "; fam = 0 } } END { for(fam in fams) { sub(/, $/, "", fams[fam]); print "\\item{" fam "}{" fams[fam] "}" } }' R/*
+awk 'BEGIN { fam = 0 } /@family/ { fam = $3 } /^[a-z]/ { if(fam) { fams[fam] = fams[fam] $1 ", "; fam = 0 } } END { for(fam in fams) { sub(/, $/, "", fams[fam]); print "\\item{" fam "}{" fams[fam] "}" } }' R/* | sort
 echo '}'
