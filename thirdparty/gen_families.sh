2ac46ce1bd4d6744e17793951754d15a9dc89c07
Lars Kotthoff <larsko@cs.ubc.ca>
2016-08-15 22:50:46 -0700

    this should work

--- thirdparty/gen_families.sh
+++ thirdparty/gen_families.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
-echo -E '\name{mlrFamilies}'
-echo -E '\title{mlr documentation families}'
-echo -E '\description{List of all mlr documentation families with members.}'
-echo -E '\arguments{'
+#!/bin/bash
+echo '\name{mlrFamilies}'
+echo '\title{mlr documentation families}'
+echo '\description{List of all mlr documentation families with members.}'
+echo '\arguments{'
 awk 'BEGIN { fam = 0 } /@family/ { fam = $3 } /^[a-z]/ { if(fam) { fams[fam] = fams[fam] $1 ", "; fam = 0 } } END { for(fam in fams) { sub(/, $/, "", fams[fam]); print "\\item{" fam "}{" fams[fam] "}" } }' R/*
 echo '}'
