bd80dd0f44b5ff374b05ce09b9fbc450cabaff59
MariaErdmann <maria.erdmann@gmail.com>
2016-09-15 10:14:05 +0200

    Adapted families, added params ' stopintern' and 'treace' to stay consistent with the other learners, changed note, adapted tests.

--- tests/testthat/test_classif_blackboost.R
+++ tests/testthat/test_classif_blackboost.R
@@ -6,13 +6,13 @@ test_that("classif_blackboost", {
   parset.list1 = list(
     list(family = mboost::Binomial(), tree_control = party::ctree_control(maxdepth = 2),
       control = mboost::boost_control(mstop = 10L)),
-    list(family = mboost::Binomial(), tree_controls = party::ctree_control(maxdepth = 4),
+    list(family = mboost::Binomial(link = "probit"), tree_controls = party::ctree_control(maxdepth = 4),
       control = mboost::boost_control(mstop = 10L, nu = 0.03))
   )
 
   parset.list2 = list(
-    list(family = mboost::Binomial(), mstop = 10L, maxdepth = 2),
-    list(family = mboost::Binomial(), mstop = 10L, maxdepth = 4, nu = 0.03)
+    list(family = "Binomial", mstop = 10L, maxdepth = 2),
+    list(family = "Binomial", Binomial.link = "probit", mstop = 10L, maxdepth = 4, nu = 0.03)
   )
 
   old.predicts.list = list()
@@ -27,7 +27,7 @@ test_that("classif_blackboost", {
     set.seed(getOption("mlr.debug.seed"))
     old.predicts.list[[i]] = predict(m, newdata = binaryclass.test, type = "class")
     set.seed(getOption("mlr.debug.seed"))
-    old.probs.list[[i]] = 1 - predict(m, newdata = binaryclass.test, type = "response")[,1]
+    old.probs.list[[i]] = predict(m, newdata = binaryclass.test, type = "response")[,1]
   }
 
   testSimpleParsets("classif.blackboost", binaryclass.df, binaryclass.target,
