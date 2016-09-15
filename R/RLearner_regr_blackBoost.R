7f3cb60f203b2b6f22c55c211d38fb8a1e88b69c
MariaErdmann <maria.erdmann@gmail.com>
2016-09-15 10:17:03 +0200

    Git is not doing what I want

--- R/RLearner_regr_blackBoost.R
+++ R/RLearner_regr_blackBoost.R
@@ -4,21 +4,29 @@ makeRLearner.regr.blackboost = function() {
     cl = "regr.blackboost",
     package = c("mboost", "party"),
     par.set = makeParamSet(
-      makeDiscreteLearnerParam(id = "family", default = mboost::Gaussian(), values = list(Gaussian = mboost::Gaussian(), Huber = mboost::Huber(), Laplace = mboost::Laplace())),
+      makeDiscreteLearnerParam(id = "family", default = "Gaussian", values = c("Gaussian", "Laplace",
+        "Huber", "Poisson", "GammaReg", "NBinomial", "Hurdle", "custom.family")),
+      # families 'Poisson', 'NBinomial' and 'Hurdle' are for count data
+      makeUntypedLearnerParam(id = "custom.family.definition", requires = quote(family == "custom.family")),
+      makeNumericVectorLearnerParam(id = "nuirange", default = c(0,100), requires = quote(family %in% c("GammaReg", "NBinomial", "Hurdle"))),
+      makeNumericLearnerParam(id = "d", requires = quote(family == "Huber")),
       makeIntegerLearnerParam(id = "mstop", default = 100L, lower = 1L),
       makeNumericLearnerParam(id = "nu", default = 0.1, lower = 0, upper = 1),
       makeDiscreteLearnerParam(id = "risk", default = "inbag", values = c("inbag", "oobag", "none")),
-      makeDiscreteLearnerParam(id = "teststat", default = "quad", values = c("quad", "max")),
-      makeDiscreteLearnerParam(id = "testtype", default = "Bonferroni", values = c("Bonferroni", "MonteCarlo", "Univariate", "Teststatistic")),
-      makeNumericLearnerParam(id = "mincriterion", default = 0.95, lower = 0, upper = 1),
+      makeLogicalLearnerParam(id = "stopintern", default = FALSE),
+      # 'risk' and 'stopintern' will be kept for completeness sake
+      makeLogicalLearnerParam(id = "trace", default = FALSE, tunable = FALSE),
+      makeDiscreteLearnerParam(id = "teststat", default = "max", values = c("quad", "max")),
+      makeDiscreteLearnerParam(id = "testtype", default = "Teststatistic", values = c("Bonferroni", "MonteCarlo", "Univariate", "Teststatistic")),
+      makeNumericLearnerParam(id = "mincriterion", default = 0, lower = 0, upper = 1),
       makeIntegerLearnerParam(id = "minsplit", default = 20L, lower = 1L),
       makeIntegerLearnerParam(id = "minbucket", default = 7L, lower = 1L),
-      makeLogicalLearnerParam(id = "stump", default = FALSE),
+      makeLogicalLearnerParam(id = "stump", default = TRUE),
       makeIntegerLearnerParam(id = "nresample", default = 9999L, lower = 1L, requires = quote(testtype=="MonteCarlo")),
       makeIntegerLearnerParam(id = "maxsurrogate", default = 0L, lower = 0L),
       makeIntegerLearnerParam(id = "mtry", default = 0L, lower = 0L),
-      makeLogicalLearnerParam(id = "savesplitstats", default = TRUE, tunable = FALSE),
-      makeIntegerLearnerParam(id = "maxdepth", default = 0L, lower = 0L)
+      makeLogicalLearnerParam(id = "savesplitstats", default = FALSE, tunable = FALSE),
+      makeIntegerLearnerParam(id = "maxdepth", default = 2L, lower = 0L)
     ),
     properties = c("numerics", "factors", "weights", "missings"),
     name = "Gradient Boosting with Regression Trees",
@@ -27,14 +35,25 @@ makeRLearner.regr.blackboost = function() {
   )
 }
 
-trainLearner.regr.blackboost = function(.learner, .task, .subset, .weights = NULL, mstop, nu, risk, teststat, testtype, mincriterion, maxdepth, stump, ...) {
-  ctrl = learnerArgsToControl(mboost::boost_control, mstop, nu, risk)
-  tc = learnerArgsToControl(party::ctree_control, teststat, testtype, mincriterion, maxdepth, stump)
+trainLearner.regr.blackboost = function(.learner, .task, .subset, .weights = NULL, mstop, nu, risk, stopintern, trace, teststat, testtype, mincriterion, minsplit, minbucket, stump, nresample, maxsurrogate, mtry, savesplitstats, maxdepth, family = "Gaussian", custom.family.definition, nuirange = c(0,100), d = NULL, ...) {
+  ctrl = learnerArgsToControl(mboost::boost_control, mstop, nu, risk, stopintern, trace)
+  tc = learnerArgsToControl(party::ctree_control, teststat, testtype, mincriterion,
+    minsplit, minbucket, stump, nresample, maxsurrogate, mtry, savesplitstats, maxdepth)
+  family = switch(family,
+    Gaussian = mboost::Gaussian(),
+    Laplace = mboost::Laplace(),
+    Huber = mboost::Huber(d),
+    Poisson = mboost::Poisson(),
+    GammaReg = mboost::GammaReg(nuirange = nuirange),
+    NBinomial = mboost::NBinomial(nuirange = nuirange),
+    Hurdle = mboost::Hurdle(nuirange = nuirange),
+    custom.family = custom.family.definition
+  )
   f = getTaskFormula(.task)
   if (!is.null(.weights))
-    mboost::blackboost(f, data = getTaskData(.task, .subset), control = ctrl, tree_controls = tc, weights = .weights)
+    mboost::blackboost(f, data = getTaskData(.task, .subset), control = ctrl, tree_controls = tc, weights = .weights, family = family)
   else
-    mboost::blackboost(f, data = getTaskData(.task, .subset), control = ctrl, tree_controls = tc)
+    mboost::blackboost(f, data = getTaskData(.task, .subset), control = ctrl, tree_controls = tc, family = family)
 }
 
 predictLearner.regr.blackboost = function(.learner, .model, .newdata, ...) {
--- tests/testthat/test_regr_blackboost.R
+++ tests/testthat/test_regr_blackboost.R
@@ -4,13 +4,15 @@ test_that("regr_blackboost", {
   requirePackagesOrSkip(c("mboost","party"), default.method = "load")
 
   parset.list1 = list(
-    list(family=mboost::GaussReg(), tree_controls=party::ctree_control(maxdepth=2)),
-    list(family=mboost::GaussReg(), tree_controls=party::ctree_control(maxdepth=4), control=mboost::boost_control(nu=0.03))
+    list(tree_controls=party::ctree_control(maxdepth=2)),
+    list(family=mboost::Gaussian(), tree_controls=party::ctree_control(maxdepth=2)),
+    list(family=mboost::Huber(d = 0.3), tree_controls=party::ctree_control(maxdepth=4), control=mboost::boost_control(nu=0.03))
   )
 
   parset.list2 = list(
-    list(family=mboost::Gaussian(), maxdepth=2),
-    list(family=mboost::Gaussian(), maxdepth=4, nu=0.03)
+    list(maxdepth = 2),
+    list(family = "Gaussian", maxdepth=2),
+    list(family = "Huber", d = 0.3, maxdepth=4, nu=0.03)
   )
 
   old.predicts.list = list()
@@ -27,3 +29,36 @@ test_that("regr_blackboost", {
 
   testSimpleParsets("regr.blackboost", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list2)
 })
+
+
+test_that("regr_blackboost works with families for count data", {
+  # change target to count data
+  new.regr.df = regr.df
+  new.regr.df[, regr.target] = as.integer(floor(new.regr.df[,regr.target]))
+  new.regr.train = new.regr.df[regr.train.inds,]
+  new.regr.test = new.regr.df[regr.test.inds,]
+  
+  parset.list1 = list(
+    list(family = mboost::Poisson(), control = mboost::boost_control(nu = 0.02), tree_controls=party::ctree_control(maxdepth=2)),
+    list(family = mboost::NBinomial(), tree_controls=party::ctree_control(maxdepth=2)),
+    list(family = mboost::Hurdle(),tree_controls=party::ctree_control(maxdepth=2))
+  )
+  
+  parset.list2 = list(
+    list(family = "Poisson", nu = 0.02, maxdepth = 2),
+    list(family= "NBinomial", maxdepth = 2),
+    list(family = "Hurdle", maxdepth = 2)
+  )
+  old.predicts.list = list()
+  for (i in 1:length(parset.list1)) {
+    parset = parset.list1[[i]]
+    pars = list(regr.formula, data = new.regr.train)
+    pars = c(pars, parset)
+    set.seed(getOption("mlr.debug.seed"))
+    m = do.call(mboost::blackboost, pars)
+    set.seed(getOption("mlr.debug.seed"))
+    old.predicts.list[[i]] = predict(m, newdata = new.regr.test, type = "response")[,1]
+  }
+  testSimpleParsets("regr.blackboost", new.regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list2)
+})
+
