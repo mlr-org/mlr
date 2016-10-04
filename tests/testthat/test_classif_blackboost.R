c0d1991f4d765c737412b8016af9a00bb976198f
MariaErdmann <maria.erdmann@gmail.com>
2016-10-04 11:05:33 +0200

    Fix blackboost issue #1244 and adapt tests accordingly, add tests for family PropOdds

--- tests/testthat/test_classif_blackboost.R
+++ tests/testthat/test_classif_blackboost.R
@@ -4,17 +4,18 @@ test_that("classif_blackboost", {
   requirePackagesOrSkip(c("mboost", "party"), default.method = "load")
 
   parset.list1 = list(
-    #list(),
-    list(family = mboost::Binomial(), tree_control = party::ctree_control(maxdepth = 2),
-      control = mboost::boost_control(mstop = 10L)),
-    list(family = mboost::Binomial(link = "probit"), tree_controls = party::ctree_control(maxdepth = 4),
-      control = mboost::boost_control(mstop = 10L, nu = 0.03))
+    list(family = mboost::Binomial()),
+    list(family = mboost::Binomial(), 
+      control = mboost::boost_control(mstop = 10L),
+      tree_controls = party::ctree_control(teststat = "max", testtype = "Teststatistic",
+        mincriterion = 0, maxdepth = 4, savesplitstats = FALSE)),
+    list(family = mboost::Binomial(link = "probit"), control = mboost::boost_control(mstop = 10L, nu = 0.03))
   )
 
   parset.list2 = list(
-    #list(),
-    list(family = mboost::Binomial(), mstop = 10L, maxdepth = 2),
-    list(family = mboost::Binomial(), mstop = 10L, maxdepth = 4, nu = 0.03)
+    list(),
+    list(mstop = 10L, maxdepth = 4),
+    list(Binomial.link = "probit", mstop = 10L, nu = 0.03)
   )
 
   old.predicts.list = list()
@@ -37,3 +38,30 @@ test_that("classif_blackboost", {
   testProbParsets("classif.blackboost", binaryclass.df, binaryclass.target,
     binaryclass.train.inds, old.probs.list, parset.list2)
 })
+
+
+test_that("classif_blackboost works with family PropOdds", {
+  new.binary.df = binaryclass.df
+  new.binary.df[,binaryclass.target] = as.ordered(new.binary.df[,binaryclass.target])
+  new.classif.train = new.binary.df[binaryclass.train.inds,]
+  new.classif.test = new.binary.df[binaryclass.test.inds,]
+  parset.list1 = list(family = mboost::PropOdds())
+  parset.list2 = list(family = "PropOdds")
+  old.predicts.list = list()
+  old.probs.list = list()
+  parset = parset.list1#[[i]]
+  pars = list(binaryclass.formula, data = new.classif.train)
+  pars = c(pars, parset)
+  set.seed(getOption("mlr.debug.seed"))
+  m = do.call(mboost::blackboost, pars)
+  set.seed(getOption("mlr.debug.seed"))
+  old.predicts.list = predict(m, newdata = new.classif.test, type = "class")
+  set.seed(getOption("mlr.debug.seed"))
+  old.probs.list = predict(m, newdata = new.classif.test, type = "response")[,1]
+  
+  testSimple("classif.blackboost", new.binary.df, binaryclass.target, binaryclass.train.inds, old.predicts.list, parset.list2)
+  testProb("classif.blackboost", new.binary.df, binaryclass.target, binaryclass.train.inds, old.probs.list, parset.list2)
+  
+})
+
+
