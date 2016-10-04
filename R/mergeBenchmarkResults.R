091363d85cb164ed2e3651e8f8147bbb460951ff
giuseppec <giuseppe.casalicchio@gmail.com>
2016-10-04 13:39:14 +0200

    recompute missing measures and allow splitting of ResamplePrediction

--- R/mergeBenchmarkResults.R
+++ R/mergeBenchmarkResults.R
@@ -1,8 +1,8 @@
 #' @title Merge different BenchmarkResult objects.
-#' @description Combines the \code{\link{BenchmarkResult}} objects that were performed 
-#'   with different learners and tasks. 
+#' @description Combines the \code{\link{BenchmarkResult}} objects that were performed
+#'   with different learners and tasks.
 #'   This can be helpful if you, e.g. forgot to run one learner on the set of tasks you used.
-#' @param ... [\code{\link{BenchmarkResult}}]\cr 
+#' @param ... [\code{\link{BenchmarkResult}}]\cr
 #'   \code{BenchmarkResult} objects that should be merged.
 #' @return \code{\link{BenchmarkResult}}
 #' @details Note that if you want to merge several \code{\link{BenchmarkResult}}
@@ -15,15 +15,12 @@ mergeBenchmarkResults = function(...) {
   # check all objects have the class BenchmarkResult
   set = list(...)
   assertList(set, types = "BenchmarkResult")
-  
+
   # check for duplicated and missing task-learner combinations
   learner.names = unique(unlist(lapply(set, getBMRLearnerIds)))
   task.names = unique(unlist(lapply(set, getBMRTaskIds)))
   all.combos = expand.grid(task = task.names, learner = learner.names)
   all.combos = apply(all.combos, 1L, collapse, " - ")
-  # existing.combos = unlist(lapply(set, function(bmr) {
-  #   paste(getBMRTaskIds(bmr), getBMRLearnerIds(bmr), sep = " - ")
-  # }))
   existing.combos = rbindlist(lapply(set, function(bmr) {
     getBMRAggrPerformances(bmr, as.df = TRUE)[, c("task.id", "learner.id")]
   }))
@@ -32,28 +29,29 @@ mergeBenchmarkResults = function(...) {
     dupls = existing.combos[duplicated(existing.combos)]
     diff = setdiff(all.combos, existing.combos)
     msg = collapse(unique(c(dupls, diff)), "\n* ")
+    # FIXME: instead of exiting with error, we might allow this and add NA to the missing task-learner combos, check plot funs if we allow this.
     stopf("The following task - learner combination(s) occur either multiple times or are missing: \n* %s\n", msg)
   }
-  
+
   # check if all task.desc are equal for each task and stop if there are tasks with more than 1 unique td
   td = peelList(lapply(set, getBMRTaskDescriptions))
   inequal.td = vlapply(td, function(x) length(unique(x)) > 1)
-  if (any(inequal.td)) 
+  if (any(inequal.td))
     stopf("Task descriptions not equal for tasks: %s", collapse(names(unique.td)[!unique.td]))
-  
+
   # check if BMR use same measures
   # measures = lapply(set, function(x) unique(getBMRMeasureIds(x)))
   # intersect.measures = Reduce(intersect, measures)
-  # all.measures = unique(peelList(lapply(set, getBMRMeasures))) 
+  # all.measures = unique(peelList(lapply(set, getBMRMeasures)))
   # names(all.measures) = vcapply(all.measures, function(x) x$id)
   # measures.merged = unname(all.measures[intersect.measures])
   measures.merged = peelList(lapply(set, getBMRMeasures))
   measures.merged = measures.merged[!duplicated(measures.merged)]
-  
+
   # get all actual learners and merge
   lrns.merged = peelList(lapply(set, getBMRLearners))
   lrns.merged = lrns.merged[!duplicated(lrns.merged)]
-  
+
   # get BMR results, merge and set correct structure
   res.merged = peelList(extractSubList(set, "results", simplify = FALSE))
   res.merged = lapply(task.names, function(x) {
@@ -63,6 +61,12 @@ mergeBenchmarkResults = function(...) {
   })
   names(res.merged) = task.names
   res.merged = res.merged[order(names(res.merged))]
+  # recompute missing measures
+  for(i in 1:length(res.merged)) {
+    for(j in 1:length(res.merged[[i]])) {
+      res.merged[[i]][[j]] = addRRMeasure(res.merged[[i]][[j]], measures.merged)
+    }
+  }
 
   makeS3Obj("BenchmarkResult",
     results = res.merged,
--- tests/testthat/test_base_mergeBenchmarkResults.R
+++ tests/testthat/test_base_mergeBenchmarkResults.R
@@ -1,12 +1,12 @@
 context("mergeBenchmarkResults")
 
 test_that("mergeBenchmarkResults", {
-  
+
   learners = list(makeLearner("classif.rpart"),
     makeLearner("classif.ctree"),
     makeLearner("classif.randomForest"))
   tasks = list(iris.task, bc.task, sonar.task)
-  
+
   # checks if list of unmerged BenchmarkResults is equal to the merged BenchmarkResults
   checkBenchmarkResults = function(list, merged) {
     expect_is(merged, "BenchmarkResult")
@@ -16,29 +16,29 @@ test_that("mergeBenchmarkResults", {
     expect_true(all(c("mmce.x", "mmce.y")%in%colnames(res)))
     expect_true(identical(res$mmce.x, res$mmce.y))
   }
-  
+
   # Change order of leaners and see if merging works
   bench1 = benchmark(learners[1:2], tasks[[1L]], cv2)
   bench2 = benchmark(learners[2:1], tasks[[2L]], cv2)
   bench = mergeBenchmarkResults(bench1, bench2)
   checkBenchmarkResults(list(bench1, bench2), bench)
-  
+
   # Forgot to run another learner on both tasks
   bench3 = benchmark(learners[3L], tasks[1:2], cv2)
   merged = mergeBenchmarkResults(bench, bench3)
   checkBenchmarkResults(list(bench, bench3), merged)
-  
+
   # Forgot to run all learners on another task
   bench3 = benchmark(learners[1:2], tasks[[3L]], cv2)
   merged = mergeBenchmarkResults(bench, bench3)
   checkBenchmarkResults(list(bench, bench3), merged)
-  
+
   # Change order of tasks and see if merging works
   bench1 = benchmark(learners[[1L]], tasks[1:2], cv2)
   bench2 = benchmark(learners[[2L]], tasks[2:1], cv2)
   merged = mergeBenchmarkResults(bench1, bench2)
   checkBenchmarkResults(list(bench1, bench2), merged)
-  
+
   # Merge both learner and task
   bench1 = benchmark(learners[[1L]], tasks[1:2], cv2)
   # apply two additional lerners on same tasks
@@ -48,7 +48,7 @@ test_that("mergeBenchmarkResults", {
   # merge all learners
   result = mergeBenchmarkResults(bench1, bench2, bench3)
   checkBenchmarkResults(list(bench1, bench2, bench3), result)
-  
+
   # error message check
   l1t1 = benchmark(learners[[1L]], tasks[[1L]], cv2)
   l2t1 = benchmark(learners[[2L]], tasks[[1L]], cv2)
@@ -58,12 +58,15 @@ test_that("mergeBenchmarkResults", {
   # FIXME: do we want to merge BMR with different measures?
   #l1t1.acc = benchmark(learners[[1L]], tasks[[1L]], cv2, measures = acc)
   #expect_error(mergeBenchmarkResults(l2t1, l1t1.acc), "same measures")
-  
+
   # check measures
   bench1 = benchmark(learners[1:2], tasks[[1L]], cv2, measures = list(acc, mmce))
   bench2 = benchmark(learners[2:1], tasks[[2L]], cv2, measures = list(mmce, acc))
   bench2 = benchmark(learners[2:1], tasks[[2L]], cv2, measures = list(mmce, acc))
-  
-  
+
   # check inequal task descriptions
+
+  # check if recomputing of missing meausures works
+
+  # allow merging of different resamplings???
 })
