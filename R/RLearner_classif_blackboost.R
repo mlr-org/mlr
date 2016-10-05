2e49e7870d0d9aaf6a74b9a719e707177d343b84
MariaErdmann <maria.erdmann@gmail.com>
2016-10-05 15:37:37 +0200

    Addeed error message for prediction of probs for family AUC, AdaExp and custom.family

--- R/RLearner_classif_blackboost.R
+++ R/RLearner_classif_blackboost.R
@@ -66,11 +66,16 @@ trainLearner.classif.blackboost = function(.learner, .task, .subset, .weights = 
 predictLearner.classif.blackboost = function(.learner, .model, .newdata, ...) {
   type = ifelse(.learner$predict.type == "response", "class", "response")
   p = predict(.model$learner.model, newdata = .newdata, type = type, ...)
+  fam = getHyperPars(.learner)$family
   if (.learner$predict.type == "prob") {
-    td = .model$task.desc
-    p = p[, 1L]
-    levs = c(td$negative, td$positive)
-    return(propVectorToMatrix(p, levs))
+    if (fam %in% c("AdaExp", "AUC", "custom.family")){
+      stopf("Predictions of probabilities are not implemented for family %s", fam)
+    } else {
+      td = .model$task.desc
+      p = p[, 1L]
+      levs = c(td$negative, td$positive)
+      return(propVectorToMatrix(p, levs))
+    }  
   } else {
     return(p)
   }
