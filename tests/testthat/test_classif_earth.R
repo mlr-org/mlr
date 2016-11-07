2e85c3c4c9fb492808b8c9a73227d0c6af0a71cc
Richard Warnung <rwarnung@gmx.at>
2016-11-07 16:30:52 +0100

    Update test_classif_earth.R

--- tests/testthat/test_classif_earth.R
+++ tests/testthat/test_classif_earth.R
@@ -9,7 +9,7 @@ test_that("classif_earth", {
   p = predict(m, newdata = binaryclass.test, type = "response")[,1]
   p.class = as.factor(ifelse(p > 0.5, levs[1L], levs[2L]))
 
-  testSimple("classif.earth", binaryclass.df, binaryclass.target, binaryclass.train.inds, p.class,list(maxit = 50))
-  testProb("classif.earth",   binaryclass.df, binaryclass.target, binaryclass.train.inds, p,list(maxit = 50))
+  testSimple("classif.earth", binaryclass.df, binaryclass.target, binaryclass.train.inds, p.class, list(maxit = 50))
+  testProb("classif.earth",   binaryclass.df, binaryclass.target, binaryclass.train.inds, p, list(maxit = 50))
 
 })
