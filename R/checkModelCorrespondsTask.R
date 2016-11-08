e9806a56548f3eb5d7f8b18309c1a3743c0838cb
Jakob Richter <code@jakob-r.de>
2016-11-08 17:53:45 +0100

    no extra file for little helper function

--- R/checkModelCorrespondsTask.R
+++
@@ -1,6 +0,0 @@
-# checks if the model was trained on the corresponding task by comparing 
-# the descriptions
-checkModelCorrespondsTask = function(model, task) {
-  if(!identical(task$task.desc, model$task.desc))
-    stopf("Description of the model does not correspond to the task")
-}
--- R/getOOBPreds.R
+++ R/getOOBPreds.R
@@ -55,3 +55,10 @@ getOOBPreds = function(model, task) {
 getOOBPredsLearner = function(.learner, .model) {
   UseMethod("getOOBPredsLearner")
 }
+
+# checks if the model was trained on the corresponding task by comparing 
+# the descriptions
+checkModelCorrespondsTask = function(model, task) {
+  if(!identical(task$task.desc, model$task.desc))
+    stopf("Description of the model does not correspond to the task")
+}
