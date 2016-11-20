2b517862276ef4c029f38dadede125913180ad51
Florian Fendt <Flo@MacBook-Pro-4.local>
2016-11-20 17:29:34 +0100

    remove param maxstat from tests

--- tests/testthat/test_regr_ranger.R
+++ tests/testthat/test_regr_ranger.R
@@ -1,6 +1,5 @@
 context("regr_ranger")
-
-## Suppress 'experimental' warning for splirule = maxstat 
+ 
 test_that("regr_ranger", {
   requirePackagesOrSkip("ranger", default.method = "load")
 
@@ -8,9 +7,7 @@ test_that("regr_ranger", {
     list(),
     list(num.trees = 100),
     list(num.trees = 250, mtry = 4),
-    list(num.trees = 500, min.node.size = 2),
-    list(num.trees = 10L, splitrule = "maxstat", alpha = 0.25),
-    list(num.trees = 10L, splitrule = "maxstat", minprop = 0.25)
+    list(num.trees = 500, min.node.size = 2)
   )
   old.predicts.list = list()
 
@@ -18,10 +15,10 @@ test_that("regr_ranger", {
     parset = parset.list[[i]]
     parset = c(parset, list(data = regr.train, formula = regr.formula, write.forest = TRUE, respect.unordered.factors = TRUE))
     set.seed(getOption("mlr.debug.seed"))
-    m = suppressWarnings(do.call(ranger::ranger, parset))
+    m = do.call(ranger::ranger, parset)
     p  = predict(m, data = regr.test)
     old.predicts.list[[i]] = p$predictions
   }
 
-  suppressWarnings(testSimpleParsets("regr.ranger", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list))
+  testSimpleParsets("regr.ranger", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list)
 })
--- tests/testthat/test_surv_ranger.R
+++ tests/testthat/test_surv_ranger.R
@@ -1,7 +1,6 @@
 context("surv_ranger")
 
 ## TODO: Proper test required when predictions working
-## Suppress 'experimental' warning for splirule = maxstat 
 test_that("surv_ranger", {
   requirePackagesOrSkip(c("survival", "ranger"), default.method = "load")
 
@@ -9,16 +8,14 @@ test_that("surv_ranger", {
   task = makeSurvTask(data = surv.train, target = surv.target)
 
   set.seed(getOption("mlr.debug.seed"))
-  m = suppressWarnings(mlr::train(lrn, task))
+  m = mlr::train(lrn, task)
   expect_equal(m$learner.model$treetype, "Survival")
   p = predict(m, newdata = surv.test)
   expect_is(p, "PredictionSurv")
 
   parset.list = list(
     list(),
-    list(num.trees = 10L),
-    list(num.trees = 10L, splitrule = "maxstat", alpha = 0.25),
-    list(num.trees = 10L, splitrule = "maxstat", minprop = 0.25)
+    list(num.trees = 10L)
   )
 
   old.predicts.list = list()
@@ -27,10 +24,10 @@ test_that("surv_ranger", {
     pars = list(formula = surv.formula, data = surv.train, respect.unordered.factors = TRUE)
     pars = c(pars, parset)
     set.seed(getOption("mlr.debug.seed"))
-    m = suppressWarnings(do.call(ranger::ranger, pars))
+    m = do.call(ranger::ranger, pars)
     p = predict(m, data = surv.test)
     old.predicts.list[[i]] = rowMeans(p$chf)
   }
 
-  suppressWarnings(testSimpleParsets("surv.ranger", surv.df, surv.target, surv.train.inds, old.predicts.list, parset.list))
+  testSimpleParsets("surv.ranger", surv.df, surv.target, surv.train.inds, old.predicts.list, parset.list)
 })
