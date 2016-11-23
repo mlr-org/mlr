16d73b6922e6390aa78130f9a8d6f06f26d4f7e2
Richard Warnung <rwarnung@gmx.at>
2016-11-23 09:54:18 +0100

    I added paremeters -details follow below, tests are to be done.
    I did not add wp which corresponds to response weights as in my understanding 
    this does not make sense for a binary classifer.
    In later work one could expand the learner for muli-class classifcation or add another learner for this based on earh.
    Furthermore I did not add the paramter na.action as it is set to na.fail and only this is supported
    by earth - this is mentioned in the note.
    The lower value of nk is set to 1L as the documentation says that this number includes the intercept.
    linpreds is kept as makeUntypedLearnerParam as it can be logical vector or a character vector.
    maxit for the glm is set to tunable - I could imagine that some users wants to tune this.
    I did not add the arguments for variance models as I think that they do not apply for the classifier.
    Finally I added the parameters that are described in the documentation as "for internal or advanced use"
    to have them accessible. I did not add Object (an earth object) ab

--- R/RLearner_classif.earth.R
+++ R/RLearner_classif.earth.R
@@ -4,48 +4,56 @@ makeRLearner.classif.earth = function() {
     cl = "classif.earth",
     package = "earth",
     par.set = makeParamSet(
-      makeNumericLearnerParam(id = "trace", default = 0, lower = 0, upper = 5, tunable = FALSE),
+      makeNumericLearnerParam(id = "trace", default = 0, upper = 10, tunable = FALSE),
       makeLogicalLearnerParam(id = "keepxy",default = FALSE, tunable = FALSE),
       makeIntegerLearnerParam(id = "degree", default = 1L, lower = 1L),
       makeNumericLearnerParam(id = "penalty"),
-      makeIntegerLearnerParam(id = "nk", lower = 0L),
-      makeNumericLearnerParam(id = "thres", default = 0.001, lower = 0),
+      makeIntegerLearnerParam(id = "nk", lower = 1L),
+      makeNumericLearnerParam(id = "thres", default = 0.001),
       makeIntegerLearnerParam(id = "minspan", default = 0L),
       makeIntegerLearnerParam(id = "endspan", default = 0L),
-      makeNumericLearnerParam(id = "newvar.penalty", default = 0, lower = 0),
+      makeNumericLearnerParam(id = "newvar.penalty", default = 0),
       makeIntegerLearnerParam(id = "fast.k", default = 20L, lower = 0L),
-      makeNumericLearnerParam(id = "fast.beta", default = 1, lower = 0),
-      makeDiscreteLearnerParam(id = "pmethod", default = "backward",
+      makeNumericLearnerParam(id = "fast.beta", default = 1),
+      makeDiscreteLearnerParam(id = "pmethod", default = "cv",
                                values = c("backward", "none", "exhaustive", "forward", "seqrep", "cv")),
       makeIntegerLearnerParam(id = "nprune"),
-      makeIntegerLearnerParam(id = "ncross", default = 1L, lower = 1L),
-      makeIntegerLearnerParam(id = "nfold", default = 0L, lower = 0L),
-      makeLogicalLearnerParam(id = "stratify", default = TRUE),
-      makeUntypedLearnerParam(id = "linpreds", default = FALSE),
-      makeFunctionLearnerParam(id = "allowed")
+      makeIntegerLearnerParam(id = "ncross", default = 1L),
+      makeIntegerLearnerParam(id = "nfold", default = 0L),
+      makeLogicalLearnerParam(id = "stratify",default = TRUE),
+      makeUntypedLearnerParam(id = "linpreds",default = FALSE),
+      makeDiscreteLearnerParam("link", values = c("logit", "probit"),
+                               default = "logit"),
+      makeNumericLearnerParam(id = "maxit", default = 25L, tunable = TRUE),
+      makeFunctionLearnerParam(id = "allowed"),
+      makeNumericLearnerParam(id = "Adjust.endspan", default = 2, tunable = TRUE),
+      makeLogicalLearnerParam(id = "Force.weights", default = FALSE, tunable = TRUE),
+      makeLogicalLearnerParam(id = "Use.beta.cache", default = TRUE, tunable = FALSE),
+      makeLogicalLearnerParam(id = "Force.xtx.prune", default = FALSE, tunable = FALSE),
+      makeLogicalLearnerParam(id = "Get.leverages", default = TRUE, tunable = FALSE),
+      makeNumericLearnerParam(id = "Exhaustive.tol", default = 1e-10, tunable = FALSE)
     ),
-    properties = c("twoclass", "numerics", "factors", "prob", "weights"),
+    properties = c("twoclass", "numerics", "factors", "prob","weights"),
     name = "Flexible Discriminant Analysis",
     short.name = "fda",
-    note = "This learner performs flexible discriminant analsis using the earth algorithm."
+    note = "This learner performs flexible discriminant analysis using the earth algorithm. na.action is set to na.fail and only this is supported."
   )
 }
 
 #' @export
 trainLearner.classif.earth = function(.learner, .task, .subset, .weights = NULL, link = "logit", maxit = 25L, ...) {
   f = getTaskFormula(.task)
-  earth::earth(f, data = getTaskData(.task, .subset), weights = .weights, glm = list(family = binomial(link = link) , maxit = maxit), ...)
+  earth::earth(f, data = getTaskData(.task, .subset), glm = list(family = binomial(link = link) , maxit = maxit), ...)
 }
 
 #' @export
 predictLearner.classif.earth = function(.learner, .model, .newdata, ...) {
   p = predict(.model$learner.model, newdata = .newdata, type = "response", ...)
-  levs = .model$task.desc$class.levels
+  levs = .model$factor.levels[[1]]
   if (.learner$predict.type == "prob") {
-    p = setColNames(cbind(1 - p, p), levs)
+    p = setColNames(cbind(p, 1 - p), levs)
   } else {
-    p = as.factor(ifelse(p > 0.5, levs[2L], levs[1L]))
-    p = unname(p)
+    p = as.factor(ifelse(p > 0.5, levs[1L], levs[2L]))
   }
   return(p)
 }
