030222541899448ac3f3e1f504d87f8228865835
Laura <laura.beggel@de.bosch.com>
2016-11-28 16:02:19 +0100

    Feature extractor structure changed for preprocWrapper; first try in test_preProcWrapper_lb

--- R/TSfeatures_wavelets.R
+++ R/TSfeatures_wavelets.R
@@ -3,35 +3,51 @@
 #' @description The function extracts discrete wavelet transform coefficients
 #'   for the raw time series curve data.
 #'
-#' @param curves [\code{data.frame},\code{matrix}]\cr Time series curve data.
-#' @param filter, periodic \cr Optional. Which filter should be used. Default:
-#'   \code{filter} = \dQuote{la8}, \code{boundary} = \dQuote{periodic}. See
-#'   package \code{wavelets} for more information.
+#' @param data [\code{data.frame},\code{matrix}]\cr
+#'   Time series curve data.
+#' @param filter, periodic \cr
+#'   Optional, specifies which filter should be used.
+#'   Default: \code{filter} = \dQuote{la8}, \code{boundary} = \dQuote{periodic}.
+#'   See package \code{wavelets} for more information.
 #' @return Returns an \code{data.frame} object containing the wavelet
 #'   coefficients.
 #'
 #' @export
-getTSWaveletFeatures = function(curves, filter = NULL, boundary = NULL) {
+getTSWaveletFeatures = function(data, target, include.target = FALSE, filter = NULL, boundary = NULL) {
   requirePackages("wavelets", default.method = "load")
 
   assert(
-    checkClass(curves, "data.frame"),
-    checkClass(curves, "matrix")
+    checkClass(data, "data.frame"),
+    checkClass(data, "matrix")
   )
 
-  if (is.null(filter)) filter = "la8"
-  if (is.null(boundary)) boundary = "periodic"
+  if (is.null(filter))
+    filter = "la8"
+  if (is.null(boundary))
+    boundary = "periodic"
 
   # if matrix, transform to data.frame so that following code executes correctly
-  if(inherits(curves, "matrix"))
-    curves = as.data.frame(curves)
+  if(inherits(data, "matrix"))
+    data = as.data.frame(data)
+
+  cns = colnames(data)
+  # potentially extract y-col and remove it from data, we dont need it for fourier-trafo
+  if (target %in% cns) {
+    y = data[, target]
+    data[, target] = NULL
+  }
 
   wtdata = NULL
-  for (i in seq_row(curves)) {
-    a = t(curves[i,])
+  for (i in seq_row(data)) {
+    a = t(data[i,])
     wt = wavelets::dwt(a, filter = filter, boundary = boundary)
     wtdata = rbind(wtdata, unlist(c(wt@W,wt@V[[wt@level]])))
   }
   wtdata = as.data.frame(wtdata)
-  return(list(data = wtdata))
+
+  # potentially include target again
+  if (include.target)
+    wtdata[, target] = y
+
+  return(wtdata)
 }
