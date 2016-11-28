2b1300a5d29cca6ac1bf38906ac17b9861d189a7
Laura <laura.beggel@de.bosch.com>
2016-11-28 16:02:19 +0100

    Feature extractor structure changed for preprocWrapper; first try in test_preProcWrapper_lb

--- R/TSfeatures.R
+++ R/TSfeatures.R
@@ -7,14 +7,17 @@
 #'   returned task is a \code{ClassifTask}.
 #'
 #'
-#' @param task [\code{TimeSeriesClassifTask}]\cr Time series classification
-#'   task.
-#' @param method [\code{character(1)}]\cr Which method is used to create time
-#'   series features. Two methods available. Wavelet transformation:
-#'   \dQuote{wavelets} Fourier transformation: \dQuote{fourier}
-#' @param pars  \cr Further parameters passed as argument e.g., for feature
-#'   representation methods. For wavelets, \code{filter} and \code{boundary}.
-#'   For fourier,\code{fft.coeff}.
+#' @param task [\code{TimeSeriesClassifTask}]\cr
+#'   Time series classification task.
+#' @param method [\code{character(1)}]\cr
+#'   Which method is used to create time series features. Two methods available.
+#'   Wavelet transformation: \dQuote{wavelets}.
+#'   Fourier transformation: \dQuote{fourier}.
+#' @param pars  \cr
+#'   Further parameters passed as argument e.g., for feature representation
+#'   methods.
+#'   Wavelet transformation: \code{filter} and \code{boundary}.
+#'   Fourier transformation: \code{fft.coeff}.
 #' @return [\code{ClassifTask}].
 #' @export
 makeTSFeaturesClassifTask = function(task, method, pars = NULL) {
@@ -23,31 +26,30 @@ makeTSFeaturesClassifTask = function(task, method, pars = NULL) {
   assertClass(task, classes = "Task")
 
   #check for Time Series Classif Task
-  if ( !any(class(task) == "TimeSeriesClassifTask") )
+  if (!any(class(task) == "TimeSeriesClassifTask"))
     stop("Task is not a 'TimeSeriesClassifTask'. Please check task.")
   #check valid feature extraction method
-  if ( !(method %in%  c("wavelets", "fourier", "shapelets")) )
+  if (!(method %in%  c("wavelets", "fourier", "shapelets")))
     stop("Method for feature extraction must be one of 'wavelets' or 'fourier' or 'shapelets'. Please check method.")
   #check for valid pars
-  if ( !(all(names(pars) %in% c("filter", "boundary", "fft.coeff"))) )
+  if (!(all(names(pars) %in% c("filter", "boundary", "fft.coeff"))))
     stop("Pars includes non valid arguments. Must be filter or boundary (wavelets) or fft.coeff (fourier).")
 
-
-
+  target = task$task.desc$target
   z = getTaskData(task, target.extra = TRUE, recode.target = "-1+1")
+
   switch(method,
-         wavelets = {tsf = getTSWaveletFeatures(curves = z$data, filter = pars$filter, boundary = pars$boundary)},
-         fourier = {tsf = getTSFourierFeatures(curves = z$data, fft.coeff = pars$fft.coeff)}
+         wavelets = {tsf = getTSWaveletFeatures(data = z$data, target = target, include.target = FALSE, filter = pars$filter, boundary = pars$boundary)},
+         fourier = {tsf = getTSFourierFeatures(data = z$data, target = target, include.target = FALSE, fft.coeff = pars$fft.coeff)}
          )
 
-  if(method == "shapelets") {
+  if (method == "shapelets") {
     modelSh = getTSShapeletFeatures(curves = z$data, label.train = z$target, max.iter = 10)
     return(modelSh)
   }
 
-  newdata = cbind(as.factor(z$target), tsf$data)
-  colnames(newdata)[1] <- task$task.desc$target  # rename target column
-  newtask = makeClassifTask(data = newdata, target = task$task.desc$target, positive = task$task.desc$positive)
+  newdata = cbind(as.factor(z$target), tsf)
+  colnames(newdata)[1] <- target  # rename target column
+  newtask = makeClassifTask(data = newdata, target = target, positive = task$task.desc$positive)
   return(newtask)
-
 }
--- R/TSfeatures_fourier.R
+++ R/TSfeatures_fourier.R
@@ -3,57 +3,71 @@
 #' @description The functino creates time series features based on the fast
 #'   discrete fourier transform.
 #'
-#' @param curves \code{data.frame},\code{matrix}]\cr Time series curve data.
-#' @param fft.coeff \code{character}\cr Optional. Which 'transformation' of the
-#'   complex frequency domain representation should be calculated as feature
-#'   representation. Must be one of \dQuote{amplitude} or \dQuote{Phase}.
-#'   Default: \dQuote{amplitude}.
+#' @param data \code{data.frame},\code{matrix}]\cr
+#'   Time series curve data.
+#' @param fft.coeff \code{character}\cr
+#'   Optional, specifies which 'transformation' of the complex frequency domain
+#'   representation should be calculated as feature representation. Must be one
+#'   of \dQuote{amplitude} or \dQuote{Phase}. Default: \dQuote{amplitude}.
 #' @return Returns an \code{data.frame} object containing the fourier
 #'   coefficients.
 #'
 #' @export
-getTSFourierFeatures = function(curves, fft.coeff = NULL) {
+getTSFourierFeatures = function(data, target, include.target = FALSE, fft.coeff = NULL) {
   requirePackages("stats", default.method = "load")
 
   assert(
-    checkClass(curves, "data.frame"),
-    checkClass(curves, "matrix")
+    checkClass(data, "data.frame"),
+    checkClass(data, "matrix")
   )
 
-  if( is.null(fft.coeff) ) fft.coeff = "amplitude"
-  if ( !(fft.coeff %in%  c("amplitude", "phase")) )
+  if (is.null(fft.coeff))
+    fft.coeff = "amplitude"
+  if (!(fft.coeff %in%  c("amplitude", "phase")))
     stop("Transformation for complex frequency domain must be one of 'amplitude' or 'phase'. Please check method.")
 
+  # potentially extract y-col and remove it from data, we dont need it for fourier-trafo
+  cns = colnames(data)
+  if (target %in% cns) {
+    y = data[, target]
+    data[, target] = NULL
+  }
+
   # if data.frame, transform to matrix so that following code executes correctly
-  if(inherits(curves, "data.frame"))
-    curves = as.matrix(curves)
+  if (inherits(data, "data.frame"))
+    data = as.matrix(data)
 
   # calculate fourier coefficients -> complex numbers
-  ffttrafo = t(apply(curves,1, fft))
+  ffttrafo = t(apply(data,1, fft))
 
-  # extract amplitude or phase of fourier coefficients
+  # extract amplitude or phase of fourier coefficients which are real numbers
   switch(fft.coeff,
          amplitude = {fftPA = getFourierAmplitude(fourier.comp = ffttrafo)},
          phase = {fftPA = getFourierPhase(fourier.comp = ffttrafo)}
          )
-  print(fft.coeff)
 
-  if(!inherits(fftPA, "matrix"))
-    fftPA = as.data.frame(matrix(fftPA, nrow = 1)) else
-      fftPA = as.data.frame(fftPA)
+  if (!inherits(fftPA, "matrix")) {
+    fftPA = as.data.frame(matrix(fftPA, nrow = 1))
+  } else {
+    fftPA = as.data.frame(fftPA)
+  }
 
-  return(list(data = fftPA))
+  # potentially include target again
+  if (include.target)
+    fftPA[, target] = y
+
+  return(fftPA)
 }
 
 #' @export
-getFourierAmplitude = function(fourier.comp){
-  amp = apply(fourier.comp, 2, function(x) Re(x)^2 + Im(x)^2 ) # compute for each row
+getFourierAmplitude = function(fourier.comp) {
+  amp = apply(fourier.comp, 2, function(x) Re(x)^2 + Im(x)^2) # compute for each row
   amp = sqrt(amp)
   return(amp)
 }
 
 #' @export
-getFourierPhase = function(fourier.comp){
-  pha = apply(fourier.comp, 2, function(x) atan( Im(x) / Re(x) ) )
+getFourierPhase = function(fourier.comp) {
+  pha = apply(fourier.comp, 2, function(x) atan(Im(x) / Re(x)))
   return(pha)
 }
