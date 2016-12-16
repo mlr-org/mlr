e2d8a0b505f5f976981d07834060d35808a3b200
Laura <laura.beggel@de.bosch.com>
2016-12-16 16:49:41 +0100

    shapelet RLearner updated

--- R/TSfeatures_shapelets.R
+++ R/TSfeatures_shapelets.R
@@ -9,23 +9,23 @@
 #' @return Returns an \code{data.frame} object containing the shapelet feature transformation.
 #'
 #' @export
-getTSShapeletFeatures = function(curves, label.train, class.type = "binary", method = "hinge", max.iter = 100, K = 0.2, L = 0.1, C = 1, step = 0.01, init.method = "kmeans", auto.hinge = FALSE) {
+getTSShapeletFeatures = function(curves, label.train, method = "hinge", ...) {
 
   requirePackages("shapeletLib", default.method = "load")
 
-  assert(
-    checkClass(curves, "matrix"),
-    checkClass(curves, "data.frame"),
-    checkClass(method, "character"),
-    checkClass(max.iter, "numeric"),
-    checkClass(K, "numeric"),
-    checkClass(L, "numeric"),
-    checkClass(C, "numeric"),
-    checkClass(step, "numeric"),
-    checkClass(step, "character"),
-    checkClass(init.method, "character"),
-    checkClass(auto.hinge, "logical")
-    )
+  # assert(
+  #   checkClass(curves, "matrix"),
+  #   checkClass(curves, "data.frame"),
+  #   checkClass(method, "character"),
+  #   checkClass(max.iter, "numeric"),
+  #   checkClass(K, "numeric"),
+  #   checkClass(L, "numeric"),
+  #   checkClass(C, "numeric"),
+  #   checkClass(step, "numeric"),
+  #   checkClass(step, "character"),
+  #   checkClass(init.method, "character"),
+  #   checkClass(auto.hinge, "logical")
+  #   )
 
   #if curves is a data.frame, transform to matrix s.t. shapelet learning executes correctly
   if(inherits(curves, "data.frame"))
@@ -34,37 +34,10 @@ getTSShapeletFeatures = function(curves, label.train, class.type = "binary", met
   if( !(all(c(-1,1) %in% unique(label.train)) ) )
     stop("The labels must only contain -1 and 1 values.")
 
-  shapeletmodel = shapeletLib::shapelet_classification(method = method,
-                                                         class.type = class.type, K = K * dim(curves)[2],
-                                                         L = L * dim(curves)[2], C = C,
-                                                         data.train = curves,
-                                                         label.train =  label.train,
-                                                         step = step, max.iter = max.iter,
-                                                         init.method = init.method)
+  shapeletmodel = shapeletLib::learnShapelets(method = method, data.train = curves, label.train =  label.train, ...)
 
   return(list(model = shapeletmodel))
 
-#   requirePackages("wavelets", default.method = "load")
-#
-#   assert(
-#     checkClass(curves, "data.frame"),
-#     checkClass(curves, "matrix")
-#   )
-#
-#   if (is.null(filter)) filter = "la8"
-#   if (is.null(boundary)) boundary = "periodic"
-#
-#   # if matrix, transform to data.frame so that following code executes correctly
-#   if(inherits(curves, "matrix"))
-#     curves = as.data.frame(curves)
-#
-#   wtdata = NULL
-#   for (i in seq_row(curves)) {
-#     a = t(curves[i,])
-#     wt = wavelets::dwt(a, filter = filter, boundary = boundary)
-#     wtdata = rbind(wtdata, unlist(c(wt@W,wt@V[[wt@level]])))
-#   }
-#   wtdata = as.data.frame(wtdata)
-#   return(wtdata)
+
 
 }
