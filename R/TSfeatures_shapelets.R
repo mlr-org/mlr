7cd2caa3ef61fc7eb2ccb43aa9a51f2d3c443a83
Laura <laura.beggel@de.bosch.com>
2017-01-17 12:29:21 +0100

    functions adapted for new shapeletLib. Still in progress

--- R/TSfeatures_shapelets.R
+++ R/TSfeatures_shapelets.R
@@ -1,12 +1,27 @@
 #' @title Shapelet features transformation
 #'
-#' @description The function learns shapelet features transformation on the raw time series curve data for classification.
+#' @description The function learns shapelet features for classification. Based
+#'   on those shapelets (k x l), the raw time series curve data is tranformed.
 #'
-#' @param curves [\code{data.frame},\code{matrix}]\cr Time series curve data.
-#' @param filter, periodic \cr Optional. Which filter should be used. Default:
-#'   \code{filter} = \dQuote{la8}, \code{boundary} = \dQuote{periodic}. See
-#'   package \code{wavelets} for more information.
-#' @return Returns an \code{data.frame} object containing the shapelet feature transformation.
+#' @param curves [\code{data.frame},\code{matrix}]\cr
+#'   Time series curve data.
+#' @param label.train [\code{factor}]\cr
+#'   Corresponding labels of curves.
+#' @param ... [\code{any}]\cr
+#'   Addtional parameters passed to shapelet learning function.
+#'
+#' @return Returns an \code{list} object \code{model} containing the learned shapelet model.
+#'   I.e.
+#'   \itemize{
+#'     \item shapelets [\code{matrix}]: \code{k} learned shapelets.
+#'     \item w [\code{numeric}]: learned weights w_1,..., w_k.
+#'     \item w.0 [\code{numeric}]: learned w_0.
+#'     \item m [\code{matrix}]: shapelet-based data transformation .
+#'     \item f.val [\code{numeric}]: learning loss for each iteration.
+#'     \item method [\code{character}]: which loss was used.
+#'     \item max.iter [\code{integer}]: maximal number of iterations.
+#'     \item class [\code{character}]: binary or multi classification?
+#'   }
 #'
 #' @export
 getTSShapeletFeatures = function(curves, label.train, ...) {
@@ -17,33 +32,18 @@ getTSShapeletFeatures = function(curves, label.train, ...) {
     checkClass(curves, "matrix"),
     checkClass(curves, "data.frame")
   )
-  # assertCharacter(method)
-  # assertInt(max.iter)
-  # assertNumber(K)
-  # assertNumber(L)
-  # assertNumber(C)
-  #
-  # assert(
-  #   checkClass(step, "numeric"),
-  #   checkClass(step, "character")
-  # )
-  #
-  # assertCharacter(init.method)
-  # assertFlag(auto.hinge)
-
 
 
   #if curves is a data.frame, transform to matrix s.t. shapelet learning executes correctly
   if(inherits(curves, "data.frame"))
     curves = as.matrix(curves)
 
-  if( !(all(c(-1,1) %in% unique(label.train)) ) )
-    stop("The labels must only contain -1 and 1 values.")
+  if(!is.factor(label.train))
+    label.train = as.factor(label.train)
 
-  shapeletmodel = shapeletLib::learnShapelets(data.train = curves, label.train = label.train, ...)
+
+  shapeletmodel = shapeletLib::learnShapelets(data = curves, label = label.train, ...)
 
   return(list(model = shapeletmodel))
-
-
 
 }
