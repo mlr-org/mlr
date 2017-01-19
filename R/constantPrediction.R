b5992857fe92a7df8b3cc92934e3ee563093b61e
giuseppec <giuseppe.casalicchio@gmail.com>
2017-01-19 15:38:00 +0100

    bugfix for tp and f1 + add fixme for auc

--- R/constantPrediction.R
+++ R/constantPrediction.R
@@ -1,15 +1,15 @@
-#' @title Make a constant prediction 
+#' @title Make a constant prediction
 #'
-#' @description 
+#' @description
 #' This function is used to make the optimal constant prediction for a given task based on a measure.
-#' 
+#'
 #' In case of a regression task the measure is optimized with a line search between the min and max
 #' value of the response. For classification the measure is evaluated for every class
 #' of the response. If \code{predict.type = "prob"} the results over
 #' all classes are scaled to 1 and the probability for every class is returned, if \code{predict.type = "response"}
 #' only the best class label is returned.
-#' 
-#' Mostly for internal use. Featureleass learners that use this function can be created with 
+#'
+#' Mostly for internal use. Featureleass learners that use this function can be created with
 #' \code{makeLearner("classif.featurelesss")} or \code{makeLearner("regr.featureless")}.
 #'
 #' @template arg_task
@@ -17,42 +17,42 @@
 #' @param predict.type [\code{character(1)}]\cr
 #'  Only meaningful for classification tasks. Either \code{predict.type = "response"} (the default)
 #'  if the label of the best class is returned, or \code{predict.type = "prob"} for a vector of
-#'  probabilities. 
+#'  probabilities.
 #' @return [\code{factor(1)} | \code{numeric(1)} | \code{numeric}]\cr
 #'  For regression task the best constant numeric prediction is returned. For classification see \code{predict.type}.
 #' @export
 constantPrediction = function(task, measure, predict.type = "response") {
-  
+
   assert_choice(predict.type, c("response", "prob"))
   checkTask(task)
   measure = checkMeasures(measure)
-  
+
   UseMethod("constantPrediction")
 }
 
 #' @export
 constantPrediction.ClassifTask = function(task, measure, predict.type) {
-  
   if (task$type %nin% measure$properties)
     stopf("Measure %s does not support task type %s!", measure$id, task$type)
   if ("req.model" %in% measure$properties)
     stopf("Measure %s requires a fitted model and cannot be used to define a learner!", measure$id)
-  
+
   lvls = getTaskClassLevels(task)
-  
+
   if (length(lvls) > 2L && "classif.multi" %nin% measure$properties)
     stopf("Multiclass problems cannot be used for measure %s!", measure$id)
-  
+
   y = getTaskTargets(task)
   n = length(y)
   scores = vnapply(lvls, function(a) {
+    # FIXME: If we use always a factor here, we can't use a measure that needs req.prob like auc
     arep = factor(rep(a, n), levels = lvls)
     data = data.frame(truth = y, response = arep)
-    desc = makeS3Obj("TaskDesc", class.levels = lvls)
-    p = makeS3Obj("Prediction", data = data, task.desc = desc)
+    # we need to use task.desc from the task otherwise measures that need p$task.desc$positive such as tp or f1 wont work
+    p = makeS3Obj("Prediction", data = data, task.desc = task$task.desc)
     measure$fun(pred = p, extra.args = measure$extra.args)
   })
-  
+
   scores = scores / sum(scores)
 
   if (predict.type == "prob")
@@ -65,13 +65,13 @@ constantPrediction.ClassifTask = function(task, measure, predict.type) {
 
 #' @export
 constantPrediction.RegrTask = function(task, measure, ...) {
-  
+
   if (task$type %nin% measure$properties)
     stopf("Measure %s does not support task type %s!", measure$id, task$type)
   if ("req.model" %in% measure$properties)
     stopf("Measure %s requires a fitted model and cannot be used to define a learner!", measure$id)
-  
-  
+
+
   y = getTaskTargets(task)
   n = length(y)
   f = function (a) {
--- tests/testthat/test_classif_featureless.R
+++ tests/testthat/test_classif_featureless.R
@@ -4,7 +4,7 @@ test_that("classif_featureless", {
   costs = matrix(c(0, 10, 10, 1, 0, 1, 1, 1, 0), 3, 3, byrow = TRUE)
   rownames(costs) = colnames(costs) = getTaskClassLevels(iris.task)
   mymeas = makeCostMeasure(costs = costs)
-  
+
   getBestPrediction = function(task, measure) {
     levs = getTaskClassLevels(task)
     y = getTaskTargets(task)
@@ -12,8 +12,7 @@ test_that("classif_featureless", {
     scores = vnapply(levs, function(a) {
       arep = factor(rep(a, n), levels = levs)
       data = data.frame(truth = y, response = arep)
-      desc = makeS3Obj("TaskDesc", class.levels = levs)
-      p = makeS3Obj("Prediction", data = data, task.desc = desc)
+      p = makeS3Obj("Prediction", data = data, task.desc = task$task.desc)
       measure$fun(pred = p, extra.args = measure$extra.args)
     })
     if (measure$minimize)
@@ -21,16 +20,25 @@ test_that("classif_featureless", {
     else
       factor(names(scores)[which.max(scores)], levels = levs)
   }
-  
+
   levs = getTaskClassLevels(iris.task)
   lev1 = getBestPrediction(iris.task, mmce)
   lev2 = getBestPrediction(iris.task, mymeas)
   p1 = factor(rep(lev1, 60), levels = levs)  #dim(multiclass.df)[1] = 150
   p2 = factor(rep(lev2, 60), levels = levs)  #dim(multiclass.df)[1] = 150
-  
-  testSimple(t.name = "classif.featureless", df = multiclass.df, target = multiclass.target, train.inds = multiclass.train.inds, old.predicts = p1, parset = list(measure = mmce))
-  testSimple(t.name = "classif.featureless", df = multiclass.df, target = multiclass.target, train.inds = multiclass.train.inds, old.predicts = p2, parset = list(measure = mymeas))
-  
+
+  # Fixme: write a test that uses these measures:
+  #lrn = makeLearner("classif.featureless", measure = auc, predict.type = "prob")
+  #lrn = makeLearner("classif.featureless", measure = f1, predict.type = "prob")
+  #lrn = makeLearner("classif.featureless", measure = auc, predict.type = "prob")
+  #mod = train(lrn, bc.task)
+  #pred = predict(mod, bc.task)$data
+
+  testSimple(t.name = "classif.featureless", df = multiclass.df, target = multiclass.target,
+    train.inds = multiclass.train.inds, old.predicts = p1, parset = list(measure = mmce))
+  testSimple(t.name = "classif.featureless", df = multiclass.df, target = multiclass.target,
+    train.inds = multiclass.train.inds, old.predicts = p2, parset = list(measure = mymeas))
+
   # test that bad measures cannot be used
   expect_error(train(makeLearner("classif.featureless", measure = auc), iris.task),
     "Multiclass problems cannot be used for measure auc!")
@@ -38,7 +46,7 @@ test_that("classif_featureless", {
     "requires a fitted model")
   expect_error(train(makeLearner("classif.featureless", measure = mse), binaryclass.task),
     "Measure mse does not support task type classif!")
-  
+
   # test that printers work correctly and print the measure id and not <measure>
   lrn = makeLearner("classif.featureless", measure = auc)
   expect_output(print(lrn), "auc")
