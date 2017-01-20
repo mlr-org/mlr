1592f49eb8e9c6dd1d26094c553bad83cef01247
MariaErdmann <maria.erdmann@gmail.com>
2017-01-20 11:19:02 +0100

    mini fix

--- R/RLearner_multilabel_randomForestSRC.R
+++ R/RLearner_multilabel_randomForestSRC.R
@@ -14,14 +14,15 @@ makeRLearner.multilabel.randomForestSRC = function() {
       makeIntegerLearnerParam(id = "nodedepth", default = NULL,
         special.vals = list(NULL)),
       makeDiscreteLearnerParam(id = "splitrule", default = NULL,
-        values = c("gini", "gini.unwt", "gini.hvwt", "random"), special.vals = list(NULL)),
+        values = list("gini", "gini.unwt", "gini.hvwt", "random", NULL = NULL),
+        special.vals = list(NULL)),
       makeIntegerLearnerParam(id = "nsplit", lower = 0L, default = 0L,
         requires = quote(splitrule != "random")), # nsplit is ignored and internally set to 1L for splitrule = "random"
       makeLogicalLearnerParam(id = "split.null", default = FALSE),
       makeDiscreteLearnerParam(id = "importance", default = FALSE, tunable = FALSE,
         values = list(`FALSE` = FALSE, `TRUE` = TRUE, "none", "permute", "random", "anti",
           "permute.ensemble", "random.ensemble", "anti.ensemble")),
-      makeDiscreteLearnerParam(id = "na.action", default = "na.impute",
+      makeDiscreteLearnerParam(id = "na.action", default = "na.impute", 
         values = c("na.omit", "na.impute"), when = "both"),
       # FIXME the default in rfsrc() for na.action is na.omit
       makeIntegerLearnerParam(id = "nimpute", default = 1L, lower = 1L),
@@ -45,7 +46,7 @@ makeRLearner.multilabel.randomForestSRC = function() {
       makeLogicalLearnerParam(id = "membership", default = FALSE, tunable = FALSE),
       makeLogicalLearnerParam(id = "statistics", default = FALSE, tunable = FALSE),
       makeLogicalLearnerParam(id = "tree.err", default = FALSE, tunable = FALSE),
-      makeUntypedLearnerParam(id = "coerce.factor", default = NULL, special.vals = list())
+      makeUntypedLearnerParam(id = "coerce.factor", default = NULL, special.vals = list(NULL))
     ),
     par.vals = list(na.action = "na.impute"),
     properties = c("missings", "numerics", "factors", "prob", "weights"),
