47d97f491ee2d39404aadb64d6b2de57c9443149
giuseppec <giuseppe.casalicchio@gmail.com>
2017-01-21 17:13:18 +0100

    compare our measures with mldr

--- test_multilabel_mld.R
+++ test_multilabel_mld.R
@@ -1,60 +1,43 @@
-df = matrix(as.numeric(c(TRUE, FALSE, TRUE, TRUE, FALSE, TRUE)),
-  ncol = 2, byrow = TRUE)
-mymldr = mldr_from_dataframe(as.data.frame(df), labelIndices = c(1, 2), name = "testMLDR")
+library(mldr)
+bincombo = matrix(c(1, 0, 1, 1, 0, 1, 0, 0), ncol = 2, byrow = TRUE)
+response = bincombo[rep(1:4, each = 4),]
 
-p = !df
-storage.mode(p) = "numeric"
-
-meas = mldr_evaluate(mymldr, predictions = p)
-meas$Recall
-meas$HammingLoss
-meas$Recall
-meas$Precision
-
-bincombo = matrix(c(TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, FALSE, FALSE), ncol = 2, byrow = TRUE)
-multi.y1 = bincombo[rep(1:4, each = 4),]
-multi.p1 = bincombo[rep(1:4, times = 4),]
-
-df = multi.y1
-storage.mode(df) = "numeric"
-mldr = mldr_from_dataframe(as.data.frame(df), labelIndices = c(1, 2), name = "testMLDR")
-predictions = multi.p1
+mldr = mldr_from_dataframe(as.data.frame(response), labelIndices = c(1, 2), name = "testMLDR")
+predictions = bincombo[rep(1:4, times = 4),]
 storage.mode(predictions) = "numeric"
-#meas = mldr_evaluate(mymldr, predictions = p)
 
-trueLabels <- mldr$dataset[, mldr$labels$index]
-bipartition <- predictions
-active <- bipartition >= 0.5
-bipartition[active] <- 1
-bipartition[!active] <- 0
-counters <- data.frame(RealPositives = rowSums(trueLabels),
+# this is copy-paste from mldr_evaluate
+trueLabels = mldr$dataset[, mldr$labels$index]
+bipartition = predictions
+active = bipartition >= 0.5
+bipartition[active] = 1
+bipartition[!active] = 0
+counters = data.frame(RealPositives = rowSums(trueLabels),
   RealNegatives = rowSums(!trueLabels), PredictedPositives = rowSums(bipartition),
   PredictedNegatives = rowSums(!bipartition), TruePositives = rowSums(trueLabels &
       bipartition), TrueNegatives = rowSums(!trueLabels &
           !bipartition))
 
+#
 mldr:::mldr_Recall(counters)
-measureMultilabelTPR(multi.y1, multi.p1)
+measureMultilabelTPR(response, predictions)
 
 mldr:::mldr_HL(trueLabels, predictions)
-measureMultilabelHamloss(multi.y1, multi.p1)
+measureMultilabelHamloss(response, predictions)
 
 mldr:::mldr_Precision(counters)
-measureMultilabelPPV(multi.y1, multi.p1)
+measureMultilabelPPV(response, predictions)
 
-
-
-# FIXME: WTF?
 mldr:::mldr_SubsetAccuracy(trueLabels, predictions)
-measureMultilabelSubset01(multi.y1, multi.p1)
+1-measureMultilabelSubset01(response, predictions) # we have implemented the subset loss
 
 # FIXME: WTF?
 mldr:::mldr_Accuracy(counters)
-measureMultilabelACC(multi.y1, multi.p1)
+measureMultilabelACC(response, predictions)
 
 # FIXME: WTF?
 mldr:::mldr_FMeasure(counters)
 mldr:::mldr_MicroFMeasure(trueLabels, bipartition)
 mldr:::mldr_MacroFMeasure(trueLabels, bipartition)
-measureMultiLabelF1(multi.y1, multi.p1)
+measureMultiLabelF1(response, predictions)
 
