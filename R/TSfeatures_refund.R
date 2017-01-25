c8873424f3c065beaf8f1c80fcef4a44e21481a3
smilesun <smilesun.east@gmail.com>
2017-01-25 08:21:15 +0100

    add FPCA feature extraction and tests passed

--- R/TSfeatures_refund.R
+++ R/TSfeatures_refund.R
@@ -15,9 +15,9 @@
 #'   degree of piecewise regression function
 #' @return Returns an \code{data.frame} object containing the design matrix(converted to dataframe) according to the spline fit
 #' @export
-getTSFDboostFeatures = function(data, target, have.target = TRUE, include.target = FALSE, nknots = 10L, degree = 3L) {
+getTSFPCAFeatures = function(data, target, have.target = TRUE, include.target = FALSE) {
   requirePackages("mboost", default.method = "load")
-  requirePackages("FDboost", default.method = "load")
+  requirePackages("refund", default.method = "load")
   assert(
     checkClass(data, "data.frame"),
     checkClass(data, "matrix")
@@ -25,8 +25,6 @@ getTSFDboostFeatures = function(data, target, have.target = TRUE, include.target
   assertCharacter(target)
   assertFlag(include.target)
   assertFlag(have.target)
-  assertInteger(nknots)
-  assertInteger(degree)
   
   cns = colnames(data)
   if (target %in% cns && have.target == TRUE) {
@@ -38,10 +36,10 @@ getTSFDboostFeatures = function(data, target, have.target = TRUE, include.target
   if (inherits(data, "data.frame"))
     data = as.matrix(data)
   
-  blrn = FDboost::bsignal(x = as.matrix(data), s = 1:ncol(data), knots = nknots, degree = degree)
-  features_bsignal = mboost::extract(object = blrn, what = "design")  # get the design matrix from base learner
-  d_fdboost = as.data.frame(features_bsignal)
+  rst <- refund::fpca.sc(Y = data)
+  features_fpca <- rst$scores
+  d_fpca = as.data.frame(features_fpca)
   if (include.target)
-    d_fdboost[, target] = y
-  return(d_fdboost)
+    d_fpca[, target] = y
+  return(d_fpca)
 }
