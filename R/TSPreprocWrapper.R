d9551cd0f6b4187fe715c65499dfb53e5f985408
Laura <laura.beggel@de.bosch.com>
2017-02-03 10:49:12 +0100

    TSPreprocWrapper changed (#1475)

--- R/TSPreprocWrapper.R
+++ R/TSPreprocWrapper.R
@@ -3,23 +3,26 @@
 #' Extract time series features like wavelets, fourier,etc.
 #' @export
 makeTSPreprocWrapper = function(learner) {
-  learner = checkLearner(learner)
+  lrn = checkLearner(learner)
+
   trainfun = function(data, target, args) {
     taskTs = makeTimeSeriesClassifTask(data = data, target = target, positive = args$positive)
-    taskFa = convertTSTaskToNormalTask(task = taskTs, method = "fourier", pars = list(fft.coeff = "amplitude"))
+    taskFa = convertTSTaskToNormalTask(task = taskTs)
     A = getTaskData(taskFa)
     control = list()
     list(data = A, control = control) # Preprocessing train must return a list with elements data[data.frame] and control[list]!
   }
+
   predictfun = function(data, target, args, control) {
     return(trainfun(data, target, args)$data)
-    #y = intersect(target, colnames(data))  
+    #y = intersect(target, colnames(data))
     #data = do.call(control$fun, c(list(curves = data), args))
     #return(data)
   }
+
   require('stringi')
   lrn2 = makePreprocWrapper(lrn, train = trainfun, predict = predictfun, par.vals = list(positive = "1"))
-  lrn2$id = stri_replace(lrn$id, replacement = ".fourier", regex = "\\.preproc$")
+  lrn2$id = stri_replace(lrn2$id, replacement = ".fourier", regex = "\\.preproc$")
   lrn2 = addClasses(lrn2, "FourierFeaturesWrapper")
   return(lrn2)
 }
