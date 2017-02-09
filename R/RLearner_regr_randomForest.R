4c5a127fa7525212fa8d5d145cf11f5dd7e1c9ad
Bernd Bischl <bernd_bischl@gmx.net>
2017-02-10 00:49:16 +0100

    docs

--- R/RLearner_regr_randomForest.R
+++ R/RLearner_regr_randomForest.R
@@ -1,24 +1,38 @@
-#' @title RandomForest regression learner. 
+#' @title RandomForest regression learner.
 #'
 #' @description
 #' mlr learner for regression tasks using \code{\link[randomForest]{randomForest}}.
 #'
-#' This doc page exists, as we added additional uncertainty estimation functionality (\code{predict.type = "se"})
-#' for the randomForest, which is not provided by the underlying package. 
-#' Methods are
+#' This doc page exists, as we added additional uncertainty estimation functionality
+#' (\code{predict.type = "se"}) for the randomForest, which is not provided by the underlying package.
+#'
+#' Currently implemented methods are:
 #'
 #' \itemize{
-#' \item If \code{se.method = "bootstrap"} the standard error of a prediction is estimated by bootstrapping the random forest, where the number of bootstrap replicates and the number of trees in the ensemble are controlled by \code{se.boot} and \code{ntree.for.se} respectively, and then taking the standard deviation of the predictions.
-#' \item If \code{se.method = "jackknife"}, the default, the standard error of a prediction is estimated by computing the jackknife-after-bootstrap, the mean-squared difference between the prediction made by only using trees which did not contain said observation and the ensemble prediction.
-#' \item If \code{se.method = "sd"}, the standard deviation of the predictions across trees is returned as the variance estimate.
+#' \item If \code{se.method = "bootstrap"} the standard error of a prediction is estimated by
+#'   bootstrapping the random forest, where the number of bootstrap replicates and the number of
+#'   trees in the ensemble are controlled by \code{se.boot} and \code{ntree.for.se} respectively,
+#'   and then taking the standard deviation of the predictions.
+#' \item If \code{se.method = "jackknife"}, the default, the standard error of a prediction
+#'   is estimated by computing the jackknife-after-bootstrap, the mean-squared difference between
+#'   the prediction made by only using trees which did not contain said observation and
+#'   the ensemble prediction.
+#' \item If \code{se.method = "sd"}, the standard deviation of the predictions across trees is
+#'   returned as the variance estimate.
 #' }
 #'
-#' For both \dQuote{jackknife} and \dQuote{bootstrap}, a Monte-Carlo bias correction is applied and, in the case that this results in a negative variance estimate, the values are truncated at 0.
+#' For both \dQuote{jackknife} and \dQuote{bootstrap}, a Monte-Carlo bias correction is applied and,
+#' in the case that this results in a negative variance estimate, the values are truncated at 0.
 #'
-#' @references [Joseph Sexton] and [Petter Laake]; [Standard errors for bagged and random forest estimators], Computational Statistics and Data Analysis Volume 53, 2009, [801-811]. 
-#' Also see: [Stefan Wager], [Trevor Hastie], and [Bradley Efron]; [Confidence Intervals for Random Forests: The Jackknife and the Infinitesimal Jackknife], Journal of Machine Learning Research Volume 15, 2014, [1625-1651].
+#' @references [Joseph Sexton] and [Petter Laake];
+#' [Standard errors for bagged and random forest estimators],
+#' Computational Statistics and Data Analysis Volume 53, 2009, [801-811].
+#' Also see: [Stefan Wager], [Trevor Hastie], and [Bradley Efron];
+#' [Confidence Intervals for Random Forests: The Jackknife and the Infinitesimal Jackknife],
+#' Journal of Machine Learning Research Volume 15, 2014, [1625-1651].
 #'
 #' @name regr.randomForest
+#' @rdname regr.randomForest
 NULL
 
 #' @export
