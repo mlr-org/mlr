80f315acec7bc5567e313e269fea5fb48166354e
MHZ4munich <smilesun.east@gmail.com>
2017-02-14 19:22:33 +0100

    add forgotten file and now all tests passed

---
+++ R/FDA_feal_twoStepAIC.R
@@ -0,0 +1,68 @@
+MAGICNumber4TWOSTEPAIC = 163
+selectForward2Step = function(d, target, muffle = FALSE, secondStepIters = MAGICNumber4TWOSTEPAIC) {
+  feats = setdiff(colnames(d), target)
+  f1 = reformulate(termlabels = "1", response = target)
+  #print(f1)
+  f2 = sprintf("%s ~ %s", target, collapse(feats, "+"))
+  #print(f2)
+  f2 = as.formula(f2)
+  m0 = lm(f1, data = d)
+  n = nrow(d)
+  g = if (muffle) capture.output else identity
+  st = system.time({
+    g({
+      m1 = MASS::stepAIC(m0, direction = "forward", scope = list(lower = f1, upper = f2), trace = -1, k = log(n))
+    })
+  })
+  runtime = st[3L]
+  cc1 = coef(m1)
+  #print(cc1)
+  #messagef("size of model: %i; runtime[s]: %g", length(cc1) - 1L, runtime)
+  filename = paste("steplm1", "-",format(Sys.time(), "%b%d%H%M%S"), ".RData" , sep="")
+  #save2(model1 = m1, runtime = runtime, df = d, file = filename)
+  f3 = sprintf("%s ~ (%s)^2 - 1", target, collapse(names(cc1)[-1], "+"))
+  #print(f3)
+  system.time({
+    g({
+      m2 = MASS::stepAIC(m0, direction = "forward", scope = list(lower = f1, upper = f3), trace = -1, steps = secondStepIters, k = log(n))
+    })
+  })
+  cc2 = coef(m2)
+  #print(cc2)
+  runtime = st[3L]
+  #messagef("size of model: %i; runtime[s]: %g", length(cc2) - 1L, runtime)
+  #filename = paste("file_base", "-",format(Sys.time(), "%b%d%H%M%S"),rnorm(1), "-output.txt" , sep="")
+  filename = paste("steplm2", "-",format(Sys.time(), "%b%d%H%M%S"), ".RData" , sep="")
+  #save2(model2 = m2, runtime = runtime, df = d, file = filename)
+  return(m2)
+}
+
+#' @export
+makeRLearner.regr.twoStepAIC = function() {
+  makeRLearnerRegr(
+    cl = "regr.twoStepAIC",
+    package = "MASS",
+    par.set = makeParamSet(
+      #makeLogicalLearnerParam(id = "secondStepIters", default = MAGICNumber4TWOSTEPAIC, tunable = TRUE),
+      makeLogicalLearnerParam(id = "muffle", default = FALSE, tunable = FALSE)
+    ),
+    properties = c("numerics", "factors"),
+    name = "Simple two Step AIC for linear regression",
+    short.name = "tsAIC"
+  )
+}
+
+#' @export
+trainLearner.regr.twoStepAIC = function(.learner, .task, .subset, .weights = NULL,  ...) {
+  d = getTaskData(.task, .subset)
+  f = getTaskFormula(.task)
+  #Fixme: I don't know how to handle the default parameter here
+  mmodel = selectForward2Step(d = d, target = getTaskTargetNames(.task), muffle = FALSE, secondStepIters = MAGICNumber4TWOSTEPAIC)
+  return(mmodel)
+  #stats::lm(f, data = d, weights = .weights, ...)
+}
+
+#' @export
+predictLearner.regr.twoStepAIC = function(.learner, .model, .newdata, ...) {
+    predict(.model$learner.model, newdata = .newdata, ...)
+}
---
+++ tests/testthat/test_FDA_featsel_twoStepAIC.R
@@ -0,0 +1,67 @@
+context("regr_twoStepAIC")
+
+makeData_art_10 = function(n){
+  n = 100
+  l4c = list()
+  l4c$x1 = as.matrix(10 + 5 * rnorm(n))
+  l4c$x2 = exp( 3 * rnorm(n))
+  l4c$x3 = 5 + 10 * runif(n)
+  l4c$x4 = 100 + 50 * rnorm(n)
+  l4c$x5 = l4c$x1 + 3 * rnorm(n)
+  l4c$x6 = 2 * l4c$x2 + rexp(n)  # lognormal and exponential mixture
+  l4c$x7 = 0.5 * exp( 4 * rnorm(n)) # lognormal
+  l4c$x8 = 10 + 8 * runif(n)
+  l4c$x9 = l4c$x2 + l4c$x8 + 2 * rnorm(n)
+  l4c$x10 = 200 + 90 * rnorm(n)
+  l4c$y = 3 * l4c$x2 - 4 * l4c$x8 + 5 * l4c$x9 + 3 * rnorm(n) 
+  df = Reduce(cbind, x = l4c)
+  df = as.data.frame(df)
+  colnames(df)[length(l4c)] = "y"
+  return(df)
+}
+
+makeData_art_10_2 = function(n){
+  n = 100
+  l4c = list()
+  l4c$x1 = 10 + 5 * rnorm(n)
+  l4c$x2 = exp( 3 * rnorm(n))
+  l4c$x3 = 5 + 10 * runif(n)
+  l4c$x4 = 100 + 50 * rnorm(n)
+  l4c$x5 = l4c$x1 + 3 * rnorm(n)
+  l4c$x6 = 2 * l4c$x2 + rexp(n)  # lognormal and exponential mixture
+  l4c$x7 = 0.5 * exp( 4 * rnorm(n)) # lognormal
+  l4c$x8 = 10 + 8 * runif(n)
+  l4c$x9 = l4c$x2 + l4c$x8 + 2 * rnorm(n)
+  l4c$x10 = 200 + 90 * rnorm(n)
+  l4c$y = 3 * (l4c$x2 * l4c$x3 ) * - 4 * (l4c$x8 * l4c$x7) + 5 * (l4c$x9 * l4c$x1) + 3 * rnorm(n) 
+  df = Reduce(cbind, x = l4c)
+  df = as.data.frame(df)
+  colnames(df)[length(l4c)] = "y"
+  return(df)
+}
+
+
+test_that("regr_twoStepAIC", {
+  pars = list(regr.formula, data = regr.train)
+  set.seed(getOption("mlr.debug.seed"))
+  mmodel = selectForward2Step(d = regr.train, target = "medv", muffle = TRUE) 
+  p = predict(mmodel, newdata = regr.test)
+  testSimple("regr.twoStepAIC", regr.df, regr.target, regr.train.inds, p)
+})
+
+test_that("regr_twoStepAIC_art",{
+  df = makeData_art_10(1000)
+  regr.train = df[1:40,] # 4 times the feature number
+  regr.test = df[501:1000,]
+  mmodel = selectForward2Step(d = regr.train, target = "y", muffle = TRUE) 
+  p = predict(mmodel, newdata = regr.test)
+})
+
+test_that("regr_twoStepAIC_art_2",{
+  df = makeData_art_10_2(1000)
+  regr.train = df[1:40,]
+  regr.test = df[501:1000,]
+  mmodel = selectForward2Step(d = regr.train, target = "y", muffle = TRUE) 
+  p = predict(mmodel, newdata = regr.test)
+  
+})
