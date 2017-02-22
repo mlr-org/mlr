b40c6f883760e6b627b0d2b4ca930f316e6c378d
smilesun <smilesun.east@gmail.com>
2017-02-22 11:45:36 +0100

    add test that fda task also works with non-fda learner

--- tests/testthat/test_FDA_nonFDALrnOnFDATask.R
+++ tests/testthat/test_FDA_nonFDALrnOnFDATask.R
@@ -4,6 +4,6 @@ load_all()
 
 data = data.frame(x = rnorm(10), y = as.factor(sample(10, 2, replace = TRUE)))
 data[1,1] = NA
-task = makeClassifTask(data = data, target = "y")
+task = makeFDAClassifTask(data = data, target = "y")
 learner = makeLearner("classif.xgboost")
 model = train(learner, task)
