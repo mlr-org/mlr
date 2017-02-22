87b2b1f59378f0f77ce656605be817c1c6f7cdd8
smilesun <smilesun.east@gmail.com>
2017-02-22 17:55:12 +0100

    add fdaregr and fdaclassif to resample default measure so resample works

--- demo4FDA/test_curve.R
+++ demo4FDA/test_curve.R
@@ -2,27 +2,22 @@ load_all()
 library('BBmisc')
 configureMlr(show.info = TRUE)
 
-# load gunpoint data
-gp = load2("demo4TS/gunpoint.RData")
 
-
-# create our new tsclassif task (yuhu...)
-task = makeTimeSeriesClassifTask(data = gp, target = "X1", positive = "1")
 
 # # start with a simple CART tree (its simple and fast)
 lrn = makeLearner("classif.rpart")
 
 # # train, predict, measure perf, all manual
 # # (and use complete data, so insample eval....)
-model = train(lrn, task, subset = 1:50)
-pred = predict(model, task, subset = 51:200)
+model = train(lrn, gunpoint.task, subset = 1:50)
+pred = predict(model, gunpoint.task, subset = 51:200)
 p = performance(pred, measures = list(mmce, tpr))
 print(p)
 
 # create the sample GP splits as on UCR page, they used the first 50 for train, last 150 for test
 # then resample (this is holdout eval)
 rin = makeFixedHoldoutInstance(size = 200, train.inds = 1:50, test.inds = 51:200)
-r = resample(lrn, task, rin)
+r = resample(lrn, gunpoint.task, rin)
 print(r)
 
 # lets benchmark the tree vs an RBF SVM
