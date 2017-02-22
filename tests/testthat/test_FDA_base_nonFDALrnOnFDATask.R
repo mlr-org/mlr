ee5a8805719e9770da7ab8763f2ac1c0029d2c1c
smilesun <smilesun.east@gmail.com>
2017-02-22 18:27:42 +0100

    add tests for resample FDA and benchmark FDA

--- tests/testthat/test_FDA_base_nonFDALrnOnFDATask.R
+++ tests/testthat/test_FDA_base_nonFDALrnOnFDATask.R
@@ -1,9 +1,8 @@
-library(data.table)
-load_all()
-
-
+context("FDA_base_task")
+test_that("FDA_base_task", {
 data = data.frame(x = rnorm(10), y = as.factor(sample(10, 2, replace = TRUE)))
 data[1,1] = NA
 task = makeFDAClassifTask(data = data, target = "y")
 learner = makeLearner("classif.xgboost")
 model = train(learner, task)
+})
