cf8d235cf6b7388833071a80ca854caa86f976d2
MHZ4munich <smilesun.east@gmail.com>
2017-02-28 12:09:43 +0100

    fix shapelet test

--- tests/testthat/test_FDA_classif_shapelets.R
+++ tests/testthat/test_FDA_classif_shapelets.R
@@ -4,7 +4,8 @@ test_that("FDA_classif_shapelets", {
   requirePackagesOrSkip("shapeletLib", default.method = "load")
 
   set.seed(getOption("mlr.debug.seed"))
-  gp = load2("../../demo4FDA/gunpoint.RData")
+  gp = getTaskData(gunpoint.task)
+  #gp = load2("../../demo4FDA/gunpoint.RData")
   #df = as.data.frame(matrix(data = runif(1000), ncol = 100))
   #df[,"X1"] = as.factor(sample(x = c(1,-1), replace = TRUE, size = 10))
 
