67465813595c3b2ae1410639676d306f89facca2
MHZ4munich <smilesun.east@gmail.com>
2017-03-02 10:18:53 +0100

    add scope operator after Fabian fix the bug

--- R/RLearner_FDAregr_pfr.R
+++ R/RLearner_FDAregr_pfr.R
@@ -1,15 +1,25 @@
 #' @title Penalized functional regression.
 #'
 #' @description
-#' Learner for penalized functional regression.
+#' Learner for penalized functional regression from the pfr function of refund. 
+#' FIXME: currently I only have one parameter, but actually this function has 
+#' a million parameters and I need to find a way to specify them.
 #'
+#' @param s.k [code{\integer}] \cr
+#' the dimension of the basis used to represent the smooth term. 
+#' The default depends on the number of variables that the smooth is a function of.
+#' it should be chosen to be large enough that you are reasonably sure of having 
+#' enough degrees of freedom to represent the underlying ‘truth’ reasonably well, 
+#' but small enough to maintain reasonable computational efficiency. 
+#' k must be chosen: the defaults are essentially arbitrary??????????
+#' see mgcv::choose.k using mgcv::gam.check
 #' @export
 makeRLearner.fdaregr.pfr = function() {
   makeRLearnerRegr(
     cl = "fdaregr.pfr",
     package = "refund",
     par.set = makeParamSet(
-      makeIntegerLearnerParam(id = "af.k", default = -1L) # 50L comes from the refund documentation
+      makeIntegerLearnerParam(id = "s.k", default = -1L)  # see mgcv::s() documentation
     ),
     properties = c("numerics"),
     name = "penalized functional regression",
@@ -18,7 +28,7 @@ makeRLearner.fdaregr.pfr = function() {
 }
 
 #' @export
-trainLearner.fdaregr.pfr = function(.learner, .task, .subset, .weights = NULL, af.k = 1L, ...) {
+trainLearner.fdaregr.pfr = function(.learner, .task, .subset, .weights = NULL, s.k = 1L, ...) {
   d = getTaskData(.task, subset = .subset)
   tn = getTaskTargetNames(.task)
   tdesc = getTaskDescription(.task)
@@ -34,12 +44,21 @@ trainLearner.fdaregr.pfr = function(.learner, .task, .subset, .weights = NULL, a
   for (fdn in fdns) {
     gn = paste0(fdn, ".grid")
     mat.list[[fdn]]=  as.matrix(d[, tdesc$fd.features[[fdn]], drop = FALSE])
-    formula.terms[fdn] = sprintf("af(%s, basistype = 's', Qtransform = TRUE, k=%d)", fdn, af.k)
+    formula.terms[fdn] = genFDAFormula(fdn, s.k)
   }
   mat.list = c(mat.list, fdg)
   mat.list[[tn]] = d[, tn]
   form = as.formula(sprintf("%s ~ %s", tn, collapse(formula.terms, "+")))
-  pfr(formula = form, data = mat.list)
+  refund::pfr(formula = form, data = mat.list)
+}
+
+# FIXME: this function should return all possible smooth functions/surfaces supported by mgcv but 
+# for simplicity we only implement the af(FGAM) with s function
+genFDAFormula = function(fdn, s.k ){
+  # s{mgcv}, k is the dimension of the basis used to represent the smoooth term. The default depends 
+  # on the number of variables. k should not be less than the dimension of the null space of the 
+  # penalty for the term 
+  sprintf("af(%s, basistype = 's', Qtransform = TRUE, k=%d)", fdn, s.k)
 }
 
 reformat2list4mat2 = function(.data, tdesc){
@@ -66,4 +85,3 @@ predictLearner.fdaregr.pfr = function(.learner, .model, .newdata, ...) {
   pred = predict(.model$learner.model, newdata = list4mat, type = 'response')
   return(as.vector(pred))
 }
-
