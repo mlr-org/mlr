db94fe1297c155872c22890084d356355559123b
mb706 <mb706@uni>
2017-03-07 18:25:29 +0100

    Using the simplest linters from lintr to check for style. Some problems become apparent: 1. tests are also tested, byt don't adhere to style guidelines. 2. exponentiation is an infix-operator that shouldn't need spaces. 3. noncomliant library functions that we can't do anything give errors. 4. df slots with underscores exist

---
+++ tests/testthat/test_lint.R
@@ -0,0 +1,15 @@
+if (requireNamespace("lintr", quietly = TRUE)) {
+  context("lints")
+  test_that("lint check", {
+    linters = list(
+      commas = lintr::commas_linter,
+      infix.spaces = lintr::infix_spaces_linter,
+      open.curly = lintr::open_curly_linter(),
+      closed.curly = lintr::closed_curly_linter(),
+      spaces.left.parentheses = lintr::spaces_left_parentheses_linter,
+      snake.case = lintr::snake_case_linter,
+      absolute.paths = lintr::absolute_paths_linter)
+    lintr::expect_lint_free(linters = linters)
+  })
+}
+
