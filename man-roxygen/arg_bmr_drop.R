98590fbaed1ab131ed046b475fe8897e76a7323b
giuseppec <giuseppe.casalicchio@stat.uni-muenchen.de>
2017-03-07 11:42:10 +0100

    Update arg_bmr_drop.R
    add commas

--- man-roxygen/arg_bmr_drop.R
+++ man-roxygen/arg_bmr_drop.R
@@ -2,13 +2,13 @@
 #'   If drop is \code{FALSE} (the default), a nested list with
 #'   the following structure is returned:\cr
 #'   \code{res[task.ids][learner.ids]}.\cr
-#'   If drop is set to \code{TRUE} it is checked if the list
+#'   If drop is set to \code{TRUE}, it is checked if the list
 #'   structure can be simplified.\cr
-#'   If only one learner was passed a list with entries
+#'   If only one learner was passed, a list with entries
 #'   for each task is returned.\cr
-#'   If only one task was passed the entries are named after
+#'   If only one task was passed, the entries are named after
 #'   the corresponding learner.\cr
-#'   For an experiment with both one task and learner
+#'   For an experiment with both one task and learner,
 #'   the whole list structure is removed.\cr
 #'   Note that the name of the
 #'   task/learner will be dropped from the return object.
