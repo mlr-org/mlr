e1633950e30c14a26035cab761e0c386ad71f4a1
mb706 <mb706@uni>
2017-03-08 10:47:35 +0100

    checking for <- and -> now.

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -1,5 +1,29 @@
 if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
+
+  # prohibit <-
+  left_assign_linter = function(source_file) {
+    lapply(lintr:::ids_with_token(source_file, "LEFT_ASSIGN"), function(id) {
+        parsed <- lintr:::with_id(source_file, id)
+        Lint(filename = source_file$filename, line_number = parsed$line1, 
+          column_number = parsed$col1, type = "style", message = "Use <-, not =, for assignment.", 
+          line = source_file$lines[as.character(parsed$line1)], 
+          linter = "assignment_linter")
+    })
+  }
+
+  # prohibit ->
+  right_assign_linter = function(source_file) {
+    lapply(lintr:::ids_with_token(source_file, "RIGHT_ASSIGN"), function(id) {
+        parsed <- lintr:::with_id(source_file, id)
+        Lint(filename = source_file$filename, line_number = parsed$line1,
+          column_number = parsed$col1, type = "style", message = "Use <-, not =, for assignment.",  
+          line = source_file$lines[as.character(parsed$line1)],  
+          linter = "assignment_linter")
+    })
+  }
+ 
+  
   test_that("lint check", {
     # note that this must be a *named* list (bug in lintr)
     linters = list(
@@ -10,7 +34,9 @@ if (requireNamespace("lintr", quietly = TRUE)) {
       spaces.left.parentheses = lintr::spaces_left_parentheses_linter,
       snake.case = lintr::snake_case_linter,
       absolute.paths = lintr::absolute_paths_linter,
-      single.quotes = lintr::single_quotes_linter)
+      single.quotes = lintr::single_quotes_linter,
+      left.assign = left_assign_linter,
+      right.assign = right_assign_linter)
     lintr::expect_lint_free(linters = linters)
   })
 }
