eed437ce8f3a0e778346735e5196f3f11ba6fe19
Florian Fendt <flo_fendt@gmx.de>
2017-03-08 16:15:23 +0100

    mv check-function in seperate file

--- R/helpers.R
+++ R/helpers.R
@@ -86,14 +86,3 @@ replaceDupeMeasureNames = function(measures, x = "id") {
   unlist(meas.names)
 }
 
-# checks for duplicated entries in learner.names and stops
-# with error message containing the learner name that appeared more
-# than once
-checkDuplicatedLearnerNames = function(learner.names) {
-  dupl = duplicated(learner.names)
-  if (any(dupl)) {
-    dupl.learners = unique(learner.names[dupl])
-    stopf("Learner short names are not unique for: %s. \n  Set 'pretty.names = FALSE' to resolve this.",
-      collapse(dupl.learners))
-  }
-}
