0714a9d2b119bf09f258b2188480f00f12f5038d
Laura Beggel <laura.beggel@de.bosch.com>
2017-03-08 10:37:51 +0100

    example indention and naming in FDAClassif fixed

--- R/FDAClassifTask.R
+++ R/FDAClassifTask.R
@@ -10,14 +10,14 @@
 #' @return [\code{\link{FDAClassifTask}}].
 #' @export
 #' @example
-#'   dat = data.frame(matrix(rnorm(20), nrow = 2))
-#'   dat$target = as.factor(c(0,1))
-#'   # X1 to X5 is channel 1 and X6 to X10 channel 2
-#'   # grd specifies the time points the curves were sampled at.
-#'   grd = list(ch_1 = 1:5, ch_2 = 1:5)
-#'   # One row per Observation
-#'   tsk = makeFDAClassifTask(data = dat, fd.features = list(ch_1 = 1:5, ch_2 = 6:10),
-#'     target = "target", fd.grid = grd, positive = "1")
+#' dat = data.frame(matrix(rnorm(20), nrow = 2))
+#' dat$target = as.factor(c(0,1))
+#' # X1 to X5 is functional covariate 1 and X6 to X10 functional covariate 2
+#' # grd specifies the time points the curves were sampled at.
+#' grd = list(ch_1 = 1:5, ch_2 = 1:5)
+#' # One row per Observation
+#' tsk = makeFDAClassifTask(data = dat, fd.features = list(ch_1 = 1:5, ch_2 = 6:10),
+#'   target = "target", fd.grid = grd, positive = "1")
 #' @aliases FDAClassifTask
 makeFDAClassifTask = function(id = deparse(substitute(data)), data, target,
   weights = NULL, blocking = NULL, positive = NA_character_, fixup.data = "warn",
