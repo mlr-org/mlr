fe803ee90ede321f54f3facaeae0db1b22fd2236
mb706 <mb706@uni>
2017-03-08 10:31:52 +0100

    sinqle_quotes_linter does what we want.

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -1,6 +1,7 @@
 if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("lint check", {
+    # note that this must be a *named* list (bug in lintr)
     linters = list(
       commas = lintr::commas_linter,
       infix.spaces = lintr::infix_spaces_linter,
@@ -8,7 +9,8 @@ if (requireNamespace("lintr", quietly = TRUE)) {
       closed.curly = lintr::closed_curly_linter(),
       spaces.left.parentheses = lintr::spaces_left_parentheses_linter,
       snake.case = lintr::snake_case_linter,
-      absolute.paths = lintr::absolute_paths_linter)
+      absolute.paths = lintr::absolute_paths_linter,
+      single.quotes = lintr::single_quotes_linter)
     lintr::expect_lint_free(linters = linters)
   })
 }
