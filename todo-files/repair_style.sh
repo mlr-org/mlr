bc24b15934f9cab6abf03af54a7192f70d37f5a2
mb706 <mb706@uni>
2017-03-08 23:41:19 +0100

    embarrassing...

--- todo-files/repair_style.sh
+++ todo-files/repair_style.sh
@@ -4,14 +4,18 @@ if [ -z "$1" ] ; then
   echo "Argument: files to change."
   exit 1
 fi
+# this has a few false positives, especially within string constants.
 
-sed -i '/^ *[^#]/s/,\([^ ]\)/, \1/g' "$@"
-sed -i '/^ *[^#]/s/\([^,[#]\) \+,/\1,/g' "$@"
-sed -i '/^ *[^#]/s/ \+(/(/g' "$@"
-sed -i '/^ *[^#]/s/for(/for (/g' "$@"
-sed -i '/^ *[^#]/s/while(/while (/g' "$@"
-sed -i '/^ *[^#]/s/if(/if (/g' "$@"
-sed -i '/^ *[^#]/s/\([^ a-zA-Z0-9#]\)(/\1 (/g' "$@"
-sed -i "/^ *[^#\"][^\"]*$/s/'/\"/g" "$@"
-sed -i '/^ *[^#]/s/^\([^#(]*\)\([^#<]\)<-/\1\2=/g' "$@"
+sed -i '/^ *[^ #]/s/,\([^ ]\)/, \1/g' "$@"
+sed -i '/^ *[^ #]/s/,\([^ ]\)/, \1/g' "$@" # do this multiple times to catch [,,] etc
+sed -i '/^ *[^ #]/s/,\([^ ]\)/, \1/g' "$@"
+sed -i '/^ *[^ #]/s/\([^,[#]\) \+,/\1,/g' "$@"
+sed -i '/^ *[^ #]/s/ \+(/(/g' "$@"
+sed -i '/^ *[^ #]/s/\([^a-zA-Z._0-9]\)for(/\1for (/g' "$@"
+sed -i '/^ *[^ #]/s/\([^a-zA-Z._0-9]\)while(/\1while (/g' "$@"
+sed -i '/^ *[^ #]/s/\([^a-zA-Z._0-9]\)if(/\1if (/g' "$@"
+sed -i '/^ *[^ #]/s/\([^a-zA-Z._0-9]\)in(/\1in (/g' "$@"
+sed -i '/^ *[^ #]/s/\([^ a-zA-Z0-9#:([]\)(/\1 (/g' "$@"
+sed -i "/^ *[^ #\"][^\"]*$/s/'/\"/g" "$@"
+sed -i '/^ *[^ #]/s/^\([^#(]*\)\([^#<]\)<-/\1\2=/g' "$@"
 
