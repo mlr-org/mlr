9a45c96c3c563a8d3ba84d3a7d00a07433614998
Florian Fendt <flo_fendt@gmx.de>
2017-03-08 16:05:49 +0100

    new function simplifyMeasureNames

--- tests/testthat/test_base_helpers.R
+++ tests/testthat/test_base_helpers.R
@@ -31,3 +31,21 @@ test_that("listLearnerProperties", {
   expected = c("classif", "regr", "surv", "costsens", "cluster", "multilabel")
   expect_equal(expected, listTaskTypes())
 })
+
+test_that("simplifyMeasureNames", {
+  # setup some measures get ids and aggegated names
+  meas = list(mmce, acc, ber)
+  meas.aggr = vcapply(meas, measureAggrName)
+  meas.ids = extractSubList(meas, "id")
+  # some dummy-strings not representing measures
+  no.meas = c("abc", "def")
+  # join aggr.names and dummy entries together
+  xs = c(meas.aggr, no.meas)
+  # test that aggr names get clipped and dummies are unchanged
+  expected = c(meas.ids, no.meas)
+  expect_equal(expected, simplifyMeasureNames(xs))
+
+  # check measure ids are ignored too
+  xs = c("acc", "no measure")
+  expect_equal(xs, simplifyMeasureNames(xs))
+})
