072dfb75f0822211da487e4ab07aa0b0c91d14d2
pfistfl <pfistererf@googlemail.com>
2017-03-09 16:04:43 +0100

    updated unittests and docs

--- R/FDAFeatFourier.R
+++ R/FDAFeatFourier.R
@@ -5,23 +5,22 @@
 #' transform.
 #'
 #' @param data [\code{data.frame}]\cr
-#'   Functional data.
+#'   Data for a single functional covariate.
 #' @param target [\code{character}]\cr
 #'   Name of the target variable. Default: \dQuote{NULL}. The variable is only
 #'   set to be consistent with the API.
 #' @param trafo.coeff [\code{character}]\cr
 #'   Specifies which transformation of the complex frequency domain
-#'   representation should be calculated as feature representation. Must be one
+#'   representation should be calculated as a feature representation. Must be one
 #'   of \dQuote{amplitude} or \dQuote{phase}. Default: \dQuote{amplitude}.
-#' @return Returns an \code{data.frame} object containing the fourier
-#'   coefficients.
+#' @return [\code{data.frame}] containing the fourier coefficients.
 #' @export
 extractFDAFeatFourier = function(data, target = NULL, trafo.coeff = "phase") {
   assertClass(data, "data.frame")
   assertChoice(trafo.coeff, choices = c("amplitude", "phase"))
   # Transform data to matrix for stats::fft
   data = as.matrix(data)
-  
+
   # Calculate fourier coefficients (row wise) which are complex numbers
   fft.trafo = t(apply(data, 1, fft))
   # Extract amplitude or phase of fourier coefficients which are real numbers
@@ -29,7 +28,7 @@ extractFDAFeatFourier = function(data, target = NULL, trafo.coeff = "phase") {
     amplitude = sqrt(apply(fft.trafo, 2, function(x) Re(x)^2 + Im(x)^2)),
     phase = apply(fft.trafo, 2, function(x) atan(Im(x) / Re(x)))
   )
-  
+
   # If there is only one row in data, fft returns an array
   if (!inherits(fft.pa, "matrix")) {
     fft.pa = as.data.frame(matrix(fft.pa, nrow = 1))
