7cf095b3f8ff09a7a696c7e1cc2d4ddf20fbb32d
Jakob Richter <code@jakob-r.de>
2017-03-09 11:17:12 +0100

    use stringi and stuff

--- R/simplifyMeasureNames.R
+++ R/simplifyMeasureNames.R
@@ -3,20 +3,18 @@
 #' @description
 #' Clips aggregation names from character vector.
 #' E.g: 'mmce.test.mean' becomes 'mmce'.
-#' Elements that don't contain a measure name are ignored
-#' and returned unchanged.
+#' Elements that don't contain a measure name are ignored and returned unchanged.
 #' 
 #' @param xs [\code{character}]\cr
-#'   Character vector that (possibly)
-#'   contains aggregated measure names.
+#'   Character vector that (possibly) contains aggregated measure names.
 #' @return [\code{character}].
 #' @export
 simplifyMeasureNames = function(xs) {
-  assertCharacter(xs)
+  assertCharacter(xs, any.missing = FALSE)
   # get all measure names
   all.measure.names = listMeasures()
   # cut everything after and including the first '.'
-  xs.shortened = gsub("\\..*", "", xs)
+  xs.shortened = stri_replace_all_regex(xs, "\\..*", "")
   # check if this is a measure
   string.is.measure = (xs.shortened %in% all.measure.names) 
   # if yes: insert shortened name, else insert original input
