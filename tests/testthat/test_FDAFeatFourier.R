1957b3610fad5bdbe5a49c75aaeb9119f76939d4
Florian <pfistfl@users.noreply.github.com>
2017-03-09 16:15:16 +0100

    updated unittests and docs (#1642)

--- tests/testthat/test_FDAFeatFourier.R
+++ tests/testthat/test_FDAFeatFourier.R
@@ -1,16 +1,21 @@
 context("FDA_FeatFourier")
 
 test_that("FDA_FeatFourier", {
-  #gp = data.frame(v1  =  t(1:4))
-  gp = data.frame(v1 = 1:5, v2 = 2:6, v3 = 3:7, v4 = 4:8)
-  
-  fourier.gp = extractFDAFeatFourier(data = gp, trafo.coeff = "phase")
+
+  gp1 = data.frame(v1 = 1:5, v2 = 2:6, v3 = 3:7, v4 = 4:8)
+
+  fourier.gp = extractFDAFeatFourier(data = gp1, trafo.coeff = "phase")
   expect_equal(nrow(fourier.gp), nrow(gp))
   # Phase (arctan(...) in range(-pi/2, pi/2) )
   expect_true(all(fourier.gp < pi/2 & fourier.gp > -pi/2))
-  
+
   fourier.a.gp = extractFDAFeatFourier(data = gp, trafo.coeff = "amplitude")
   expect_equal(nrow(fourier.a.gp), nrow(gp))
   # Amplitude sqrt(Re^2 + Im^2) >= 0
   expect_true(all(fourier.a.gp >= 0))
+
+  # Can not have factors
+  gp2 = data.frame(v1  =  t(1:4), X1= as.factor(1))
+  expect_error(extractFDAFeatFourier(data = gp2, trafo.coeff = "amplitude"))
+
 })
