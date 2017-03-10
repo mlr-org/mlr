b9ee1f7518858ea5dcee48b8c590492194c2dda1
mb706 <mb706@users.noreply.github.com>
2017-03-10 11:24:24 +0100

    Adding debug traces to FailureModels and failed preds. (#851)
    fixes #750 
    
    * First draft: adding debug traces to FailureModels and failed preds.
    
    * Resampling saves error dumps if on.error.dump is TRUE.
    
    * Always need `extra` in opt.path when tuning.
    
    * Save error dumps in opt.path
    
    * makePrediction() argument dump is now optional.
    
    * Documentation files added
    
    * Small mistake when rebasing
    
    * Produce dumps only when asked by options.
    
    * Create .dump object even when empty, since opt.path complains otherwise.
    
    * bugfix: error when evalOptimizationState does not resample
    
    * Solved the last few warnings and notes raised by R test
    
    * Tests, some getter functions, printing.
    
    * There actually was a test checking the number of conf options m(
    
    * Nicer documentation.
    
    * Bugfixes
    
    * Roxygenise
    
    * Update predict.R
    
    * Update train.R

--- R/Prediction.R
+++ R/Prediction.R
@@ -32,12 +32,12 @@ NULL
 #' @description
 #' Internal, do not use!
 #' @export
-makePrediction = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   UseMethod("makePrediction")
 }
 
 #' @export
-makePrediction.RegrTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.RegrTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "truth", "response", "se"))
   data$id = id
   data$truth = truth
@@ -54,12 +54,13 @@ makePrediction.RegrTaskDesc = function(task.desc, row.names, id, truth, predict.
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
 }
 
 #' @export
-makePrediction.ClassifTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.ClassifTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "truth", "response", "prob"))
   data$id = id
   # truth can come from a simple "newdata" df. then there might not be all factor levels present
@@ -84,7 +85,8 @@ makePrediction.ClassifTaskDesc = function(task.desc, row.names, id, truth, predi
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
 
   if (predict.type == "prob") {
@@ -99,7 +101,7 @@ makePrediction.ClassifTaskDesc = function(task.desc, row.names, id, truth, predi
 }
 
 #' @export
-makePrediction.MultilabelTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.MultilabelTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "truth", "response", "prob"))
   data$id = id
   data$truth = truth
@@ -115,7 +117,8 @@ makePrediction.MultilabelTaskDesc = function(task.desc, row.names, id, truth, pr
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
   if (predict.type == "prob") {
     # set default threshold to 0.5
@@ -129,7 +132,7 @@ makePrediction.MultilabelTaskDesc = function(task.desc, row.names, id, truth, pr
 }
 
 #' @export
-makePrediction.SurvTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.SurvTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "truth.time", "truth.event", "response"))
   data$id = id
   # FIXME: recode times
@@ -143,12 +146,13 @@ makePrediction.SurvTaskDesc = function(task.desc, row.names, id, truth, predict.
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
 }
 
 #' @export
-makePrediction.ClusterTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.ClusterTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "response", "prob"))
   data$id = id
   if (predict.type == "response") {
@@ -167,13 +171,14 @@ makePrediction.ClusterTaskDesc = function(task.desc, row.names, id, truth, predi
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
   return(p)
 }
 
 #' @export
-makePrediction.CostSensTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_) {
+makePrediction.CostSensTaskDesc = function(task.desc, row.names, id, truth, predict.type, predict.threshold = NULL, y, time, error = NA_character_, dump = NULL) {
   data = namedList(c("id", "response"))
   data$id = id
   data$response = y
@@ -184,7 +189,8 @@ makePrediction.CostSensTaskDesc = function(task.desc, row.names, id, truth, pred
     threshold = NA_real_,
     task.desc = task.desc,
     time = time,
-    error = error
+    error = error,
+    dump = dump
   )
 }
 
