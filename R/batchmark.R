49c1eb828f9ff755dc6faef7e1a10f54367dadc2
Michel Lang <michellang@gmail.com>
2017-03-10 10:27:43 +0100

    cleanups

--- R/batchmark.R
+++ R/batchmark.R
@@ -1,7 +1,7 @@
 #' @title Run machine learning benchmarks as distributed experiments.
 #'
 #' @description
-#' This function is a very parallel version of \code{\link{benchmark}}.
+#' This function is a very parallel version of \code{\link{benchmark}} using \pkg{batchtools}.
 #' Experiments are created in the provided registry for each combination of
 #' learners, tasks and resamplings. The experiments are then stored in a registry and the
 #' runs can be started via \code{\link[batchtools]{submitJobs}]}. A job is one train/test split
@@ -13,15 +13,15 @@
 #'
 #' The general workflow with \code{batchmark} looks like that:
 #' \enumerate{
-#' \item{1.}{\code{batchtools::createExperimentRegistry()}}
-#' \item{2.}{\code{batchmark(...)}}
-#' \item{3.}{\code{batchtools:submitJobs()}}
-#' \item{4.}{wait until the jobs are finished...}
-#' \item{5.}{\code{reduceBatchtoolsResult()}}
+#' \item{Create an ExperimentRegistry using \code{\link[batchtools]{makeExperimentRegistry}}.}
+#' \item{Call \code{batchmark(...)} which defines jobs for all learners and tasks in an \code{\link[base]{expand.grid}} fashion.}
+#' \item{Submit jobs using \code{\link[batchtools]{submitJobs}}.}
+#' \item{Babysit the computation, wait for all jobs to finish using \code{\link[batchtools]{waitForJobs}}.}
+#' \item{Call \code{reduceBatchmarkResult()} to reduce results into a \code{\link{BenchmarkResult}}.}
 #' }
-#' 
-#' If you want to use this with \pkg{OpenML} datasets you can generate tasks from a vector 
-#' of dataset ids easily with 
+#'
+#' If you want to use this with \pkg{OpenML} datasets you can generate tasks from a vector
+#' of dataset ids easily with
 #' \code{tasks = lapply(data.ids, function(x) convertOMLDataSetToMlr(getOMLDataSet(x)))}.
 #' @inheritParams benchmark
 #' @param resamplings [(list of) \code{\link{ResampleDesc}}]\cr
@@ -35,9 +35,7 @@
 #' @export
 #' @family benchmark
 batchmark = function(learners, tasks, resamplings, measures, models = TRUE, reg = batchtools::getDefaultRegistry()) {
-
   requirePackages("batchtools", why = "batchmark", default.method = "load")
-
   assertFlag(models)
 
   learners = ensureBenchmarkLearners(learners)
@@ -82,9 +80,7 @@ getAlgoFun = function(lrn, measures, models) {
   force(measures)
   force(models)
   function(job, data, instance) {
-
     extract.this = getExtractor(lrn)
-
     calculateResampleIterationResult(learner = lrn, task = data$task, train.i = instance$train, test.i = instance$test,
       measures = measures, weights = instance$weights, rdesc = data$rdesc, model = models, extract = extract.this, show.info = FALSE)
   }
@@ -93,36 +89,34 @@ getAlgoFun = function(lrn, measures, models) {
 #' @title Reduce results of a batch-distributed benchmark.
 #'
 #' @description
-#' This creates a \code{\link{BenchmarkResult}} from a \code{\link[batchtools]{Registry}}.
+#' This creates a \code{\link{BenchmarkResult}} from a \code{\link[batchtools]{ExperimentRegistry}}.
 #' To setup the benchmark have a look at \code{\link{batchmark}}.
 #'
 #' @param ids [\code{data.frame} or \code{integer}]\cr
-#'   A \code{data.frame} (or \code{\link[data.table]{data.table}})
+#'   A \code{\link[base]{data.frame}} (or \code{\link[data.table]{data.table}})
 #'   with a column named \dQuote{job.id}.
 #'   Alternatively, you may also pass a vector of integerish job ids.
 #'   If not set, defaults to all jobs.
 #' @template arg_keep_pred
 #' @template arg_showinfo
-#' @param reg [\code{\link[batchtools]{Registry}}]\cr
+#' @param reg [\code{\link[batchtools]{ExperimentRegistry}}]\cr
 #'   Registry, created by \code{\link[batchtools]{makeExperimentRegistry}}. If not explicitly passed,
 #'   uses the last created registry.
 #' @return [\code{\link{BenchmarkResult}}].
 #' @export
 #' @family benchmark
 reduceBatchmarkResults = function(ids = NULL, keep.pred = TRUE, show.info = getMlrOption("show.info"), reg = batchtools::getDefaultRegistry()) {
-
-  #registry and ids are asserted later
+  # registry and ids are asserted later
   requirePackages("batchtools", why = "batchmark", default.method = "load")
   assertFlag(keep.pred)
 
   problems = batchtools::getProblemIds(reg)
   algorithms = batchtools::getAlgorithmIds(reg)
-  
+
   result = replicate(length(problems), namedList(algorithms), simplify = FALSE)
   names(result) = problems
 
   for (prob in problems) {
-
     exps = batchtools::findExperiments(prob.name = prob, ids = ids)
     job = batchtools::makeJob(id = exps[1, ])
     rin = job$instance$rin
@@ -131,9 +125,10 @@ reduceBatchmarkResults = function(ids = NULL, keep.pred = TRUE, show.info = getM
       problem = job$problem
 
       for (a in algorithms) {
-        res = batchtools::reduceResultsList(batchtools::findExperiments(prob.pattern = stri_paste("^", prob, "$"), 
+        # FIXME: Use prob.name + algo.name as soon as batchtools-0.9.3 hits CRAN
+        res = batchtools::reduceResultsList(batchtools::findExperiments(prob.pattern = stri_paste("^", prob, "$"),
           algo.pattern = stri_paste("^", a, "$"), ids = ids))
-        models = !is.null(res[[1]]$model)
+        models = !is.null(res[[1L]]$model)
         lrn = problem$data$learner[[a]]
         extract.this = getExtractor(lrn)
         rs = mergeResampleResult(learner.id = a, task = problem$data$task, iter.results = res, measures = problem$data$measures,
