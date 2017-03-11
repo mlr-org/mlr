e9457a492c2630f047e6b58be395e76dea9ffae4
mb706 <mb706@uni>
2017-03-11 16:02:06 +0100

    put learnerHelp in its own file.

---
+++ R/learnerHelp.R
@@ -0,0 +1,35 @@
+#' @title Access help page of learner functions.
+#'
+#' @description Interactive function that gives the user quick access to the
+#'   help pages associated with various functions involved in the given learner.
+#' @template arg_learner
+#' @export
+#' @family learner
+learnerHelp = function(learner) {
+  learner = checkLearner(learner)
+  if (!is.null(learner$callees) && learner$callees[1] != "") {
+    n = 1
+    if (length(learner$callees) > 1) {
+      repeat {
+        cat("Choose help page:\n")
+        cat(paste0(seq_along(learner$callees), " : ", learner$callees, "\n", collapse=""))
+        n <- readline("(Ctrl-C to cancel)\n...: ")
+        n <- ifelse(grepl("\\D",n),-1,as.integer(n))
+        if (is.finite(n) && n >= 1 && n <= length(learner$callees)) {
+          break
+        }
+        catf("Invalid input. Enter a number between 1 and %d", length(learner$callees))
+      }
+    }
+    for (pkg_ref in learner$package) {
+      h = utils::help(learner$callees[n], package = (pkg_ref))
+      if (length(h) > 0) {
+        return(h)
+      }
+    }
+  }
+  catf("No information about learner %s found.", coalesce(learner$name, learner$shortname, learner$id))
+  invisible(NULL)
+}
+
+
