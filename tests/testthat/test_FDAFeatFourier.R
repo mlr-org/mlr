9994cacb9d3b60b299ebe684f76ceaec44cb01be
pfistfl <pfistererf@googlemail.com>
2017-03-12 12:33:47 +0100

    fix unit test

--- tests/testthat/test_FDAFeatFourier.R
+++ tests/testthat/test_FDAFeatFourier.R
@@ -5,12 +5,12 @@ test_that("FDA_FeatFourier", {
   gp1 = data.frame(v1 = 1:5, v2 = 2:6, v3 = 3:7, v4 = 4:8)
 
   fourier.gp = extractFDAFeatFourier(data = gp1, trafo.coeff = "phase")
-  expect_equal(nrow(fourier.gp), nrow(gp))
+  expect_equal(nrow(fourier.gp), nrow(gp1))
   # Phase (arctan(...) in range(-pi/2, pi/2) )
   expect_true(all(fourier.gp < pi/2 & fourier.gp > -pi/2))
 
-  fourier.a.gp = extractFDAFeatFourier(data = gp, trafo.coeff = "amplitude")
-  expect_equal(nrow(fourier.a.gp), nrow(gp))
+  fourier.a.gp = extractFDAFeatFourier(data = gp1, trafo.coeff = "amplitude")
+  expect_equal(nrow(fourier.a.gp), nrow(gp1))
   # Amplitude sqrt(Re^2 + Im^2) >= 0
   expect_true(all(fourier.a.gp >= 0))
 
