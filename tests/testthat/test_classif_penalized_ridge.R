0b59ce95fab879cf1a44cd6e8b58f733acb4139d
Bernd Bischl <bernd_bischl@gmx.net>
2017-03-21 09:25:52 +0100

    Revert "classif.penalized (#1716)"
    This reverts commit e87320de39e0038569e0f4d3b64c7d3198798f8d.

--- tests/testthat/test_classif_penalized_ridge.R
+++ tests/testthat/test_classif_penalized_ridge.R
@@ -1,21 +1,18 @@
-context("classif_penalized")
+context("classif_penalized_ridge")
 
-test_that("classif_penalized", {
+test_that("classif_penalized_ridge", {
   requirePackages("!penalized", default.method = "load")
   parset.list = list(
-    list(maxiter = 100),
-    list(lambda1 = 2),
-    list(lambda1 = 2, lambda2 = 1),
-    list(lambda2 = 2),
-    list(fusedl = TRUE, lambda1 = 2, maxiter = 20L),
-    list(fusedl = TRUE, lambda1 = 2, lambda2 = 1, maxiter = 5L),
-    list(fusedl = TRUE, lambda2 = 2, maxiter = 20L)
+    list(),
+    list(lambda2 = 0.3),
+    list(lambda2 = 1),
+    list(lambda2 = 2)
   )
   old.probs.list = list()
 
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
-    pars = list(binaryclass.formula, data = binaryclass.train)
+    pars = list(binaryclass.formula, data = binaryclass.train, trace = FALSE)
     pars = c(pars, parset)
     set.seed(getOption("mlr.debug.seed"))
     capture.output(
@@ -25,18 +22,12 @@ test_that("classif_penalized", {
     m@formula$unpenalized[[2L]] = as.symbol(binaryclass.target)
     old.probs.list[[i]] = 1 - penalized::predict(m, data = binaryclass.test)
   }
-  testProbParsets("classif.penalized", binaryclass.df, binaryclass.target,
+  testProbParsets("classif.penalized.ridge", binaryclass.df, binaryclass.target,
     binaryclass.train.inds, old.probs.list, parset.list)
-  parset.list = list(
-    list(maxiter = 100),
-    list(lambda1 = 2, lambda2 = 1),
-    list(lambda1 = 1, lambda2 = 2),
-    list(lambda1 = 2, lambda2 = 1, maxiter = 2L, fusedl = TRUE),
-    list(lambda1 = 1, lambda2 = 2, maxiter = 4L, fusedl = TRUE)
-  )
 
   tt = function(formula, data, subset = 1:nrow(data), ...) {
-    penalized::penalized(formula, data = data[subset, ], ...)
+    penalized::penalized(formula, data = data[subset, ],
+      fusedl = FALSE, trace = FALSE, ...)
   }
 
   tp = function(model, newdata, ...) {
@@ -44,6 +35,6 @@ test_that("classif_penalized", {
     ifelse(pred > 0.5, binaryclass.class.levs[2L], binaryclass.class.levs[1L])
   }
 
-  testCVParsets("classif.penalized", binaryclass.df, binaryclass.target,
+  testCVParsets("classif.penalized.ridge", binaryclass.df, binaryclass.target,
     tune.train = tt, tune.predict = tp, parset.list = parset.list)
 })
