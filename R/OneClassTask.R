513ae5d40c20cca0fab29229527960999c976362
Minh Le <minh-anh.le@cdtm.de>
2017-03-23 15:07:47 +0100

    change from inheriting from unsupervised class to inheriting from supervised class

--- R/OneClassTask.R
+++ R/OneClassTask.R
@@ -1,30 +1,72 @@
 #' @export
 #' @rdname Task
 
-makeOneClassTask = function(id = deparse(substitute(data)), data,
-  weights = NULL, blocking = NULL, fixup.data = "warn", check.data = TRUE) {
+makeOneClassTask = function(id = deparse(substitute(data)), data, target,
+  weights = NULL, blocking = NULL, positive = NA_character_, fixup.data = "warn", check.data = TRUE) {
   assertString(id)
   assertDataFrame(data)
+  if (!missing(target)) {
+    assertString(target)
+  } else {
+    data$target = as.factor("normal")
+    target = "target"
+  }
   # some code on cran passed stuff like positive=1, we can live with the convert here
-  # for one class positive is always set to the one class
+  if (isScalarNumeric(positive))
+    positive = as.character(positive)
+  assertString(positive, na.ok = TRUE)
   assertChoice(fixup.data, choices = c("no", "quiet", "warn"))
   assertFlag(check.data)
 
-  task = makeUnsupervisedTask("oneclass", data, weights, blocking,
+  if (fixup.data != "no") {
+    x = data[[target]]
+    if (is.character(x) || is.logical(x) || is.integer(x)) {
+      data[[target]] = as.factor(x)
+    } else if (is.factor(x) && fixup.data == "warn" && hasEmptyLevels(x)) {
+      warningf("Target column '%s' contains empty factor levels", target)
+      data[[target]] = droplevels(x)
+    }
+  }
+
+  task = makeSupervisedTask("oneclass", data, target, weights, blocking,
     fixup.data = fixup.data, check.data = check.data)
 
-  task$task.desc = makeOneClassTaskDesc(id, data, weights, blocking)
-  addClasses(task, "OneClassTask")
+  if (check.data) {
+    assertFactor(data[[target]], any.missing = FALSE, empty.levels.ok = FALSE, .var.name = target)
+  }
 
+  task$task.desc = makeOneClassTaskDesc(id, data, target, weights, blocking, positive)
+  addClasses(task, "OneClassTask")
 }
 
-makeOneClassTaskDesc = function(id, data, weights, blocking) {
-  target = character(0L)
+makeOneClassTaskDesc = function(id, data, target, weights, blocking, positive) {
+  levs = levels(data[[target]])
+  m = length(levs)
+  if (is.na(positive)) {
+    if (m <= 2L)
+      positive = levs[1L]
+  } else {
+    if (m > 2L)
+      stop("Cannot set a positive class for a multiclass problem!")
+    assertChoice(positive, choices = levs)
+  }
   td = makeTaskDescInternal("oneclass", id, data, target, weights, blocking)
-  return(addClasses(td, c("OneClassTaskDesc", "UnsupervisedTaskDesc")))
+  td$class.levels = levs
+  td$positive = positive
+  td$negative = NA_character_
+  if (length(td$class.levels) == 1L)
+    td$negative = stri_paste("not_", positive)
+  else if (length(td$class.levels) == 2L)
+    td$negative = setdiff(td$class.levels, positive)
+  return(addClasses(td, c("OneClassTaskDesc", "SupervisedTaskDesc")))
 }
 
 #' @export
 print.OneClassTask = function(x, ...) {
-  print.UnsupervisedTask(x)
+  di = printToChar(table(getTaskTargets(x)), collapse = NULL)[-1L]
+  m = length(x$task.desc$class.levels)
+  print.SupervisedTask(x)
+  catf("Classes: %i", m)
+  catf(collapse(di, "\n"))
+  catf("Positive class: %s", x$task.desc$positive)
 }
