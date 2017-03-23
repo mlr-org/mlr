528d8d7b9f5d675e5d45f5b7237853bfd47c596e
Minh Le <minh-anh.le@cdtm.de>
2017-03-23 16:41:12 +0100

    one class training is unsupervised, therefore performance measurements of clustering methods can be used. “req.feats” is only required for performance measurements for clustering methods. oneclassTask can also have a target variable, clusterTask don’t therefore extracting features needs to be done differently for oneclass

--- R/performance.R
+++ R/performance.R
@@ -75,9 +75,13 @@ doPerformanceIteration = function(measure, pred = NULL, task = NULL, model = NUL
   if ("req.feats" %in% props) {
     if (is.null(task) && is.null(feats))
       stopf("You need to pass either task or features for measure %s!", m$id)
-    else if (is.null(feats))
-      feats = task$env$data[pred$data$id,, drop = FALSE]
-    else
+    else if (is.null(feats)) {
+      if(pred$task.desc$type == "oneclass") {
+        feats = task$env$data[pred$data$id, getTaskFeatureNames(task), drop = FALSE]
+      } else {
+        feats = task$env$data[pred$data$id,, drop = FALSE]
+      }
+    } else
       assertClass(feats, "data.frame")
   }
   # we need to find desc somewhere
