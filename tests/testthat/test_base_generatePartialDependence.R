7e77236a7d77d3a04ead9652f8b7ebfd2b74e025
vwunnava <btw.vinay@gmail.com>
2017-04-26 18:21:33 +0530

    Add unit test for rng parameter

--- tests/testthat/test_base_generatePartialDependence.R
+++ tests/testthat/test_base_generatePartialDependence.R
@@ -264,6 +264,9 @@ test_that("generatePartialDependenceData", {
   # issue 63 in the tutorial
   pd = generatePartialDependenceData(fcp, multiclass.task, "Petal.Width",
     individual = TRUE, derivative = TRUE, gridsize = gridsize)
+	
+  # test rng as paratmeter
+  pd = generatePartialDependenceData(fcp, multiclass.task, "Petal.Width", c(0.1,0.2,0.3,0.4,0.5,0.6,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.1,2.2,2.3,2.4,2.5))
 })
 
 test_that("generateFeatureGrid", {
