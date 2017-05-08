50c3ac807770e1eaec51ea3bd04df18566710d54
Minh Le <minh-anh.le@cdtm.de>
2017-05-08 13:01:15 +0200

    to calculate the response variable out of the mse.reconstruction error, we need to set an threshold. The threshold depends on how many anomaly one has in the data, for now we assume 5% is anomalous, therefore set threshold at 95% quantile

--- R/Prediction.R
+++ R/Prediction.R
@@ -194,9 +194,12 @@ makePrediction.OneClassTaskDesc = function(task.desc, row.names, id, truth, pred
 
     # HACK need to create colnames with prob.<positive class> for one class
     # otherwise getPredictionProbabilities() will throw an error
+    # otherwise getPredictionProbabilities() will throw an error:
     # "Trying to get probabilities for nonexistant classes: %s", collapse(cl) (line 56)
     indices = stri_detect_fixed(names(data), colnames(y))
     if (sum(indices) > 0) #?
+      # can't be named "Reconstruction.MSE", it needs be named like follow,
+      # otherwise getPredictionProbabilitie() can't get the prob
       names(data)[indices] = stri_paste("prob.", colnames(y))
   }
 
@@ -209,9 +212,9 @@ makePrediction.OneClassTaskDesc = function(task.desc, row.names, id, truth, pred
     error = error
   )
   if (predict.type == "prob") {
-    # set default threshold to the 50% quantile of the mse reconstruction error
+    # set default threshold to the 95% quantile of the mse reconstruction error
     if (is.null(predict.threshold)) {
-      indices.threshold = order(y)[round(length(y) / 2)]  #mse reconstruction error in [0,inf[
+        indices.threshold = order(y)[round(length(y)*0.95)]  #mse reconstruction error in [0,inf[
 
       predict.threshold = y[indices.threshold]
       names(predict.threshold) = task.desc$positive
