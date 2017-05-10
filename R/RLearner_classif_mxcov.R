566de3b781c8fa9012dcaf5f5964ae0524f09867
smilesun <smilesun.east@gmail.com>
2017-05-10 11:31:18 +0200

    convmx

--- R/RLearner_classif_mxcov.R
+++ R/RLearner_classif_mxcov.R
@@ -4,64 +4,7 @@ makeRLearner.classif.mxconv = function() {
     cl = "classif.mxff",
     package = "mxnet",
     par.set = makeParamSet(
-      makeIntegerLearnerParam(id = "nodes1", lower = 1L, default = 1L),
-      makeIntegerLearnerParam(id = "nodes2", lower = 0L,
-        requires = quote(testIntegerish(nodes1, lower = 1))),
-      makeIntegerLearnerParam(id = "nodes3", lower = 0L,
-        requires = quote(testIntegerish(nodes2, lower = 1))),
-      makeIntegerLearnerParam(id = "nodes_out", lower = 1L, default = 2),
-      makeDiscreteLearnerParam(id = "act1", default = "tanh",
-        values = c("tanh", "relu", "sigmoid", "softrelu")),
-      makeDiscreteLearnerParam(id = "act2", default = "tanh",
-        values = c("tanh", "relu", "sigmoid", "softrelu"),
-        requires = quote(testIntegerish(nodes1, lower = 1) && testIntegerish(nodes2, lower = 1))),
-      makeDiscreteLearnerParam(id = "act3", default = "tanh",
-        values = c("tanh", "relu", "sigmoid", "softrelu"),
-        requires = quote(testIntegerish(nodes2, lower = 1) && testIntegerish(nodes3, lower = 1))),
-      makeDiscreteLearnerParam(id = "act_out", default = "softmax",
-        values = c("rmse", "softmax", "logistic")),
-      makeNumericLearnerParam(id = "dropout", lower = 0, upper = 1),
-      makeUntypedLearnerParam(id = "ctx", default = mx.ctx.default(), tunable = FALSE),
-      makeIntegerLearnerParam(id = "begin.round", default = 1),
-      makeIntegerLearnerParam(id = "num.round", default = 10),
-      makeDiscreteLearnerParam(id = "optimizer", default = "sgd",
-        values = c("sgd", "rmsprop", "adam", "adagrad", "adadelta")),
-      makeUntypedLearnerParam(id = "initializer", default = mx.init.uniform(0.01)),
-      makeUntypedLearnerParam(id = "eval.data", default = NULL, tunable = FALSE),
-      makeUntypedLearnerParam(id = "eval.metric", default = NULL, tunable = FALSE),
-      makeUntypedLearnerParam(id = "epoch.end.callback", default = NULL, tunable = FALSE),
-      makeUntypedLearnerParam(id = "batch.end.callback", default = NULL, tunable = FALSE),
-      makeIntegerLearnerParam(id = "array.batch.size", default = 128),
-      makeDiscreteLearnerParam(id = "array.layout", default = "rowmajor",
-        values = c("auto", "colmajor", "rowmajor"), tunable = FALSE),
-      makeUntypedLearnerParam(id = "kvstore", default = "local", tunable = FALSE),
-      makeLogicalLearnerParam(id = "verbose", default = FALSE, tunable = FALSE),
-      makeUntypedLearnerParam(id = "arg.params", tunable = FALSE),
-      makeUntypedLearnerParam(id = "aux.params", tunable = FALSE),
-      makeNumericLearnerParam(id = "rho", default = 0.9, requires = quote(optimizer %in%
-          c("adadelta"))),
-      makeNumericLearnerParam(id = "epsilon",
-        requires = quote(optimizer %in% c("adadelta", "adagrad", "adam"))),
-      makeNumericLearnerParam(id = "wd", default = 0,
-        requires = quote(optimizer %in% c("adadelta", "adagrad", "adam", "rmsprop", "sgd"))),
-      makeNumericLearnerParam(id = "rescale.grad", default = 1,
-        requires = quote(optimizer %in% c("adadelta", "adagrad", "adam", "rmsprop", "sgd"))),
-      makeNumericLearnerParam(id = "clip_gradient",
-        requires = quote(optimizer %in% c("adadelta", "adagrad", "adam", "rmsprop", "sgd"))),
-      makeFunctionLearnerParam(id = "lr_scheduler",
-        requires = quote(optimizer %in% c("adagrad", "adam", "rmsprop", "sgd"))),
-      makeNumericLearnerParam(id = "learning.rate",
-        requires = quote(optimizer %in% c("adagrad", "adam", "rmsprop", "sgd"))),
-      makeNumericLearnerParam(id = "beta1", default = 0.9,
-        requires = quote(optimizer %in% c("adam"))),
-      makeNumericLearnerParam(id = "beta2", default = 0.999,
-        requires = quote(optimizer %in% c("adam"))),
-      makeNumericLearnerParam(id = "gamma1", default = 0.95,
-        requires = quote(optimizer %in% c("rmsprop"))),
-      makeNumericLearnerParam(id = "gamma2", default = 0.9,
-        requires = quote(optimizer %in% c("rmsprop"))),
-      makeNumericLearnerParam(id = "momentum", default = 0,
-        requires = quote(optimizer %in% c("sgd")))
+      makeIntegerLearnerParam(id = "nodes1", lower = 1L, default = 1L)
     ),
     properties = c("twoclass", "multiclass", "numerics"),
     par.vals = list(learning.rate = 0.1, array.layout = "rowmajor", verbose = FALSE),
@@ -72,38 +15,43 @@ makeRLearner.classif.mxconv = function() {
 }
 
 #' @export
-trainLearner.classif.mxconv = function(.learner, .task, .subset, .weights = NULL,
-  nodes1 = 1, nodes2 = NULL, nodes3 = NULL, nodes_out = NULL,
-  act1 = "tanh", act2 = NULL, act3 = NULL, act_out = "softmax", dropout = NULL, ...) {
+trainLearner.classif.mxconv = function(.learner, .task, .subset, .weights = NULL) {
   # transform data into correct format
   d = getTaskData(.task, subset = .subset, target.extra = TRUE)
   y = as.numeric(d$target) - 1
   X = data.matrix(d$data)
 
+  train.array <- train.x
+  dim(train.array) <- c(201, 8, 1, ncol(train.x))
 
-
+  device.cpu <- mx.cpu()
   data <- mx.symbol.Variable('data')
   # first conv
-  conv1 <- mx.symbol.Convolution(data=data, kernel=c(5,5), num_filter=20)
+  conv1 <- mx.symbol.Convolution(data=data, kernel=c(5,2), num_filter=2)
   tanh1 <- mx.symbol.Activation(data=conv1, act_type="tanh")
   pool1 <- mx.symbol.Pooling(data=tanh1, pool_type="max",
-    kernel=c(2,2), stride=c(2,2))
+    kernel=c(2,1), stride=c(2,1))
   # second conv
-  conv2 <- mx.symbol.Convolution(data=pool1, kernel=c(5,5), num_filter=50)
+  conv2 <- mx.symbol.Convolution(data=pool1, kernel=c(5,2), num_filter=2)
   tanh2 <- mx.symbol.Activation(data=conv2, act_type="tanh")
   pool2 <- mx.symbol.Pooling(data=tanh2, pool_type="max",
-    kernel=c(2,2), stride=c(2,2))
+    kernel=c(2,2), stride=c(2,1))
   # first fullc
   flatten <- mx.symbol.Flatten(data=pool2)
   fc1 <- mx.symbol.FullyConnected(data=flatten, num_hidden=500)
   tanh3 <- mx.symbol.Activation(data=fc1, act_type="tanh")
   # second fullc
-  fc2 <- mx.symbol.FullyConnected(data=tanh3, num_hidden=10)
+  fc2 <- mx.symbol.FullyConnected(data=tanh3, num_hidden=2)  # two class classification
   # loss
   lenet <- mx.symbol.SoftmaxOutput(data=fc2)
 
- # create model
-  model = mx.model.FeedForward.create(out, X = X, y = y, ...)
+  # create model
+
+  model = mx.model.FeedForward.create(lenet, X= train.array, y=train.y,
+    ctx=device.cpu, num.round=3, array.batch.size=100,
+    learning.rate=0.05, momentum=0.9, wd=0.00001,
+    eval.metric=mx.metric.accuracy,
+    epoch.end.callback=mx.callback.log.train.metric(100))
 
   return(model)
 }
