50f0cef1ff81f02bc310e7a7cad13c9ab49839a2
Minh Le <minh-anh.le@cdtm.de>
2017-05-22 11:31:29 +0200

    no message

--- R/RLearner_oneclass_svm.R
+++ R/RLearner_oneclass_svm.R
@@ -40,7 +40,6 @@ predictLearner.oneclass.svm = function(.learner, .model, .newdata, ...) {
      p = as.factor(p)
      levels(p) = union(levels(p), .model$task.desc$class.levels)
    }
-
   return(p)
 }
 
