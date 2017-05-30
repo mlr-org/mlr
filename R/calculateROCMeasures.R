c7c114fdf6a0bf60413e0e24c91475c2702d6824
Minh Le <minh-anh.le@cdtm.de>
2017-05-30 13:07:01 +0200

    simplify check prediction for oneclass&classif

--- R/calculateROCMeasures.R
+++ R/calculateROCMeasures.R
@@ -40,11 +40,8 @@
 #' calculateROCMeasures(pred)
 #'
 calculateROCMeasures = function(pred) {
-  if (pred$task.desc$type == "classif") {
-  checkPrediction(pred, task.type = "classif", check.truth = TRUE, no.na = TRUE, binary = TRUE)
-  } else if (pred$task.desc$type == "oneclass") {
-    checkPrediction(pred, task.type = "oneclass", check.truth = TRUE, no.na = TRUE, binary = TRUE)
-  }
+  checkPrediction(pred, task.type = c("oneclass", "classif", check.truth = TRUE, no.na = TRUE, binary = TRUE)
+
   tab = calculateConfusionMatrix(pred, relative = TRUE)$relative.row[1:2, 1:2]
   response = getPredictionResponse(pred)
   truth = getPredictionTruth(pred)
