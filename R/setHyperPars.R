dc8c342dd2901aee1a0db2f3ac824c6e165cc290
mb706 <ma.binder@campus.lmu.de>
2017-06-05 14:59:32 +0200

    setHyperPars: assert uniquely named parameters

--- R/setHyperPars.R
+++ R/setHyperPars.R
@@ -21,8 +21,8 @@
 #' print(cl2)
 setHyperPars = function(learner, ..., par.vals = list()) {
   args = list(...)
-  assertList(args, names = "named", .var.name = "parameter settings")
-  assertList(par.vals, names = "named", .var.name = "parameter settings")
+  assertList(args, names = "unique", .var.name = "parameter settings")
+  assertList(par.vals, names = "unique", .var.name = "parameter settings")
   setHyperPars2(learner, insert(par.vals, args))
 }
 
