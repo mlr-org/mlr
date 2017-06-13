cc42feda2a09ffe85880bd19f81efa2c92d58d43
Minh Le <minh-anh.le@cdtm.de>
2017-06-13 16:03:21 +0200

    rename file

--- R/Make_measure_AMV.R
+++ R/Make_measure_AMV.R
@@ -1,45 +1,54 @@
-#' @title Compute Area under Mass-Volume Curve (AMV)
+#' @title Creates the measure Area under Mass-Volume Curve (AMV) for Anomaly detection (oneclass)
+#'
 #' @description
-#' Computes the Area under the Mass-Volume Curve via Monte-Carlo approximation
-#' of the diagonal. Uses the trapezoidal rule as implemented in
+#' Creates a measure for oneclass classification. The measure computes the
+#' Area under the Mass-Volume Curve via Monte-Carlo approximation
+#' of the diagonal. It uses the trapezoidal rule as implemented in
 #' package \code{caTools} for integration. The implementation is based on the
 #' python implementation: \link{https://github.com/albertcthomas/anomaly_tuning}.
 #' Differences are the type of quantile used, as the python default is not
 #' available in R.
 #'
-#' @param model [\code{list}]  \cr
-#'   Model used for scoring data and mcmc samples.
-#' @param data [\code{data.frame | matrix}]  \cr
-#'   Data to compute the AMV for.
-#' @param predictions [\code{numeric}] \cr
-#'   Model scores for data from model. If \code{NULL}, the data is scored using
-#'   predict(model, newdata = data, predicttype = "distance").
+#' @param id [\code{character(1)}]\cr
+#'   Name of measure.
+#'   Default is \dQuote{costs}.
 #' @param alphas [\code{numeric}] \cr
 #'   Numeric vector of alphas from 0 to 1, representing the computed quantiles.
 #'   Default: 0.05, 0.1, 0.15, ... , 1.
-#' @param scorefun [\code{function}] \cr
-#'   Scoring function, such as the prediction function from a trained model.
 #' @param nsim [\code{numeric(1)}] \cr
 #'   Number of Monte-Carlo Samples, Default: 10^4.
 #' @return [\code{numeric(1)}]
 #'   Area under Mass-Volume Curve (AMV).
 #' @references Thomas, A. et al. Learning Hyperparameters for Unsupervised Anomaly Detection,
 #' ICML Anomaly Detection Workshop 2016
+#' @inheritParams makeMeasure
+#' @template ret_measure
 #' @export
-calculateAMVMeasures = function(task, model, pred, alphas = seq(from = 0.05, to = 1, by = 0.05), nsim = 10e3) {
+#' @family performance.
+#'
+makeAMVMeasure = function(id = "amv", minimize = FALSE, alphas = seq(from = 0.05, to = 1, by = 0.05), nsim = 10e3, feat.subsampling = FALSE, nfeat.subsampling = NULL, best = 1, worst = 0, name = id, note = "") {
 
+  assertString(id)
+  assertFlag(minimize)
   assertNumeric(alphas, lower = 0, upper = 1)
   assertCount(nsim)
+  assertFlag(feat.subsampling)
+  # Curse of dimensionality for more than 5 features
+  assertNumeric(nfeat.subsampling, lower = 0, upper = 5, null.ok = TRUE)
+  assertString(name)
+  assertString(note)
 
-  n.feat = sum(task$task.desc$n.feat)
-  data = task$env$data[,1:n.feat]
+  makeMeasure(id = id, minimize = minimize, extra.args = list(alphas, nsim, feat.subsampling, nfeat.subsampling),
+    properties = c("oneclass", "req.task", "req.model", "req.pred", "predtype.prob"),
+    best = best, worst = worst,
+    fun = function(task, model, pred, feats, extra.args) {
 
-  if (model$learner$predict.type != "prob") {
-    stopf("For calculation of the Mass Volume Curve of a scoring function predict.type =\"prob\" is required.")
-  }
-  if (n.feat >=5) {
-    warningf("Dimension might be too high for volume estimation")
-  }
+    n.feat = sum(task$task.desc$n.feat)
+    data = task$env$data[,1:n.feat]
+
+    if (n.feat >=5 && feat.subsampling == FALSE) {
+      warningf("Dimension might be too high for volume estimation. Apply feature resampling")
+    }
 
   # vector of offsets for different alphas
   # type = 8: The resulting quantile estimates are approximately median-unbiased
@@ -72,6 +81,10 @@ calculateAMVMeasures = function(task, model, pred, alphas = seq(from = 0.05, to 
     amv = ((diff(alphas)) %*% (sum.vol) / 2) / volume
 
     # Return area under mass-volume curve
-    return(as.numeric(amv))
+    as.numeric(amv)
+    },
+    name = name,
+    note = note
+  )
 }
 
