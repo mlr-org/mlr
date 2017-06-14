3e3f299594403eaeb4513e2e2e02cd0bb4402011
mb706 <ma.binder@campus.lmu.de>
2017-06-14 12:03:48 +0200

    Multiplexer, Applicator

--- R/CPO_concrete.R
+++ R/CPO_concrete.R
@@ -50,3 +50,101 @@ cpoScale = makeCPO("scale", center = TRUE: logical, scale = TRUE: logical, .data
 }, cpo.retrafo = {
   as.data.frame(scale(as.matrix(data), center = control$center, scale = control$scale))
 })
+
+#' @title CPO Multiplexer
+#'
+#' @template cpo_description
+#'
+#' @param cpos [\code{list} of (\code{CPO} | \code{CPOConstructor})]\cr
+#'   The CPOs to multiplex. If this is a named list, the
+#'   names must be unique and represent the IDs that will
+#'   be given to the CPOs upon construction.
+#' @param selected.cpo [\code{character(1)}]\cr
+#'   Selected CPO. Will default to the first item of \code{cpos}
+#'   if \code{NULL}. Default is \code{NULL}.
+#' @template arg_cpo_id
+#' @family CPO
+#' @export
+cpoMultiplex = function(cpos, selected.cpo = NULL, id = NULL) {
+  assertList(cpos, c("CPO", "CPOConstructor"), min.len = 1)
+  has.names = !is.null(names(cpos))
+  if (!has.names) {
+    names(cpos) = sapply(cpos, function(c) {
+      if ("CPOConstructor" %in% class(c)) {
+        getCPOName(c)
+      } else if ("CPOPrimitive" %in% class(c)) {
+        coalesce(getCPOId(c), getCPOName(c))
+      } else {
+        c = as.list(c)[[1]]
+        coalesce(getCPOId(c), getCPOName(c))
+      }
+    })
+  }
+  dupnames = unique(names(cpos)[duplicated(names(cpos))])
+  if (length(dupnames)) {
+    stopf("%s must be unique, but duplicates found: %s",
+          ifelse(has.names, "names of parameter 'cpos'", "CPO types given"), collapse(dupnames, sep = ", "))
+  }
+  assertList(cpos, names = "unique")
+
+  if (is.null(selected.cpo)) {
+    selected.cpo = names(cpos)[1]
+  }
+  assertChoice(selected.cpo, names(cpos))
+
+  constructed = lapply(names(cpos), function(n) {
+    if ("CPOConstructor" %in% class(cpos[[n]])) {
+      cpos[[n]](id = n)
+    } else {
+      cpos[[n]]
+    }
+  })
+  names(constructed) = names(cpos)
+
+  paramset.list = do.call(base::c, lapply(names(constructed), function(n) {
+    ps = getParamSet(constructed[[n]])
+    ps$pars = lapply(ps$pars, function(p) {
+      if (is.null(p$requires)) {
+        p$requires = substitute(selected.cpo == n, list(n = n))
+      } else {
+        p$requires = substitute((selected.cpo == n) && (otherreq), list(n = n, otherreq = p$requires))
+      }
+      p
+    })
+    ps
+  }))
+
+  paramset = c(paramSetSugar(selected.cpo = selected.cpo: discrete[names(cpos)]), paramset.list)
+
+  allprops = lapply(constructed, getCPOProperties, only.data = TRUE)
+  properties = Reduce(intersect, extractSubList(allprops, "properties", simplify = FALSE))
+  properties.needed = Reduce(intersect, extractSubList(allprops, "properties.needed", simplify = FALSE))
+  properties.adding = Reduce(union, extractSubList(allprops, "properties.adding", simplify = FALSE))
+
+  makeCPO("multiplex", .par.set = paramset, .datasplit = "task", .properties = properties, .properties.adding = properties.adding,
+          .properties.needed = properties.needed, cpo.trafo = function(data, target, selected.cpo, ...) {
+            cpo = constructed[[selected.cpo]]
+            cpo = setHyperPars(cpo, par.vals = list(...)[names(getParamSet(cpo)$pars)])
+            res = data %>>% cpo
+            control = retrafo(res)
+            res
+          }, cpo.retrafo = function(data, control, ...) { data %>>% control })(id = id)
+}
+
+
+#' @title CPO Applicator
+#'
+#' @template cpo_description
+#'
+#' @param cpos [\code{list} of (\code{CPO} | \code{CPOConstructor})]\cr
+#'   The CPOs to multiplex. If this is a named list, the
+#'   names must be unique and represent the IDs that will
+#'   be given to the CPOs upon construction.
+#' @param selected.cpo [\code{character(1)}]\cr
+#'   Selected CPO. Will default to the first item of \code{cpos}
+#'   if \code{NULL}. Default is \code{NULL}.
+#' @template arg_cpo_id
+#' @family CPO
+#' @export
+cpoApply = makeCPO("apply", .par.set = makeParamSet(makeUntypedLearnerParam("cpo")), .datasplit = "task",  # nolint
+                   cpo.trafo = { control = retrafo({res = data %>>% cpo}) ; res }, cpo.retrafo = { data %>>% control })
