3a70b92ae9cbd27140b4d1e21b394d0ad9f70c99
mb706 <ma.binder@campus.lmu.de>
2017-06-20 11:34:06 +0200

    Impute CPO + tests

--- R/CPO_concrete.R
+++ R/CPO_concrete.R
@@ -127,6 +127,9 @@ registerCPO(cpoDummyEncode, "data", "feature conversion", "Convert factorial col
 #' @param invert [\code{logical(1)}]\cr
 #'   Invert column selection: Drop the named columns and return the rest, instead of keeping the selected
 #'   columns only. Default is \code{FALSE}.
+#'
+#' @template arg_cpo_id
+#' @family CPO
 #' @export
 cpoSelect = makeCPO("select",  # nolint
   .par.set = c(
@@ -152,3 +155,129 @@ cpoSelect = makeCPO("select",  # nolint
   }, cpo.retrafo = NULL)
 registerCPO(cpoSelect, "data", "feature selection ", "Select features from a data set by type, column name, or column index.")
 
+#' @title Impute and re-impute data
+#'
+#' @description
+#' Allows imputation of missing feature values through various techniques.
+#' Note that you have the possibility to re-impute a data set
+#' in the same way as the imputation was performed during training.
+#' This especially comes in handy during resampling when one wants to perform the
+#' same imputation on the test set as on the training set.
+#'
+#' The function \code{impute} performs the imputation on a data set and returns,
+#' alongside with the imputed data set, an \dQuote{ImputationDesc} object
+#' which can contain \dQuote{learned} coefficients and helpful data.
+#' It can then be passed together with a new data set to \code{\link{reimpute}}.
+#'
+#' The imputation techniques can be specified for certain features or for feature classes,
+#' see function arguments.
+#'
+#' You can either provide an arbitrary object, use a built-in imputation method listed
+#' under \code{\link{imputations}} or create one yourself using \code{\link{makeImputeMethod}}.
+#'
+#' \code{cpoImpute} will impute some columns. \code{cpoImputeAll} behaves just like \code{cpoImpute},
+#' except that it will throw an error if there are any missings remaining in its output. \code{cpoImputeAll}
+#' should be used if one wants to prepend an imputer to a learner.
+#'
+#' @details
+#' The description object contains these slots
+#' \describe{
+#'   \item{target [\code{character}]}{See argument.}
+#'   \item{features [\code{character}]}{Feature names (column names of \code{data}).},
+#'   \item{classes [\code{character}]}{Feature classes (storage type of \code{data}).}
+#'   \item{lvls [\code{named list}]}{Mapping of column names of factor features to their levels,
+#'     including newly created ones during imputation.}
+#'   \item{impute [\code{named list}]}{Mapping of column names to imputation functions.}
+#'   \item{dummies [\code{named list}]}{Mapping of column names to imputation functions.}
+#'   \item{impute.new.levels [\code{logical(1)}]}{See argument.}
+#'   \item{recode.factor.levels [\code{logical(1)}]}{See argument.}
+#' }
+#'
+#' @template cpo_description
+#'
+#' @template arg_taskdf
+#' @param target.cols [\code{character}]\cr
+#'   Name of the column(s) specifying the response.
+#'   Default is \code{character(0)}.
+#' @param classes [\code{named list}]\cr
+#'   Named list containing imputation techniques for classes of columns.
+#'   E.g. \code{list(numeric = imputeMedian())}.
+#' @param cols [\code{named list}]\cr
+#'   Named list containing names of imputation methods to impute missing values
+#'   in the data column referenced by the list element's name. Overrules imputation set via
+#'   \code{classes}.
+#' @param dummy.classes [\code{character}]\cr
+#'   Classes of columns to create dummy columns for.
+#'   Default is \code{character(0)}.
+#' @param dummy.cols [\code{character}]\cr
+#'   Column names to create dummy columns (containing binary missing indicator) for.
+#'   Default is \code{character(0)}.
+#' @param dummy.type [\code{character(1)}]\cr
+#'   How dummy columns are encoded. Either as 0/1 with type \dQuote{numeric}
+#'   or as \dQuote{factor}.
+#'   Default is \dQuote{factor}.
+#' @param force.dummies [\code{logical(1)}]\cr
+#'   Force dummy creation even if the respective data column does not
+#'   contain any NAs. Note that (a) most learners will complain about
+#'   constant columns created this way but (b) your feature set might
+#'   be stochastic if you turn this off.
+#'   Default is \code{FALSE}.
+#' @param impute.new.levels [\code{logical(1)}]\cr
+#'   If new, unencountered factor level occur during reimputation,
+#'   should these be handled as NAs and then be imputed the same way?
+#'   Default is \code{TRUE}.
+#' @param recode.factor.levels [\code{logical(1)}]\cr
+#'   Recode factor levels after reimputation, so they match the respective element of
+#'   \code{lvls} (in the description object) and therefore match the levels of the
+#'   feature factor in the training data after imputation?.
+#'   Default is \code{TRUE}.
+#' @template arg_cpo_id
+#' @return [\code{list}]
+#'   \item{data [\code{data.frame}]}{Imputed data.}
+#'   \item{desc [\code{ImputationDesc}]}{Description object.}
+#' @export
+#' @family impute
+#' @family CPO
+cpoImpute = makeCPO("impute", # nolint
+  .par.set = c(
+      makeParamSet(makeUntypedLearnerParam("target.cols", default = character(0)),
+        makeUntypedLearnerParam("classes", default = list()),
+        makeUntypedLearnerParam("cols", default = list()),
+        makeUntypedLearnerParam("dummy.classes", default = character(0)),
+        makeUntypedLearnerParam("dummy.cols", default = character(0))),
+      paramSetSugar(dummy.type = "factor": discrete[numeric, factor],
+        force.dummies = FALSE: logical,
+        impute.new.levels = TRUE: logical,
+        recode.factor.levels = TRUE: logical)),
+  .datasplit = "target", # no properties.adding 'missings', since we don't impute all cols
+  .properties.needed = "factors",
+  cpo.trafo = function(data, target, target.cols, ...) {
+    impresult = impute(data, target.cols, ...)
+    control = impresult[[2]]
+    impresult[[1]]
+  }, cpo.retrafo = function(data, control, ...) {
+    reimpute(data, control)
+  })
+
+#' @rdname cpoImpute
+#' @export
+cpoImputeAll = makeCPO("impute", # nolint
+  .par.set = c(
+      makeParamSet(makeUntypedLearnerParam("target.cols", default = character(0)),
+        makeUntypedLearnerParam("classes", default = list()),
+        makeUntypedLearnerParam("cols", default = list()),
+        makeUntypedLearnerParam("dummy.classes", default = character(0)),
+        makeUntypedLearnerParam("dummy.cols", default = character(0))),
+      paramSetSugar(dummy.type = "factor": discrete[numeric, factor],
+        force.dummies = FALSE: logical,
+        impute.new.levels = TRUE: logical,
+        recode.factor.levels = TRUE: logical)),
+  .datasplit = "target", .properties.adding = "missings",
+  .properties.needed = "factors",
+  cpo.trafo = function(data, target, target.cols, ...) {
+    impresult = impute(data, target.cols, ...)
+    control = impresult[[2]]
+    impresult[[1]]
+  }, cpo.retrafo = function(data, control, ...) {
+    reimpute(data, control)
+  })
