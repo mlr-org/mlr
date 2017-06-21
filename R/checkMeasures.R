2ea448e907503c8b0fbff0c534494cf9bfe38283
mb706 <ma.binder@campus.lmu.de>
2017-06-21 21:08:20 +0200

    checkMeasures: instead of missing(), use NULL

--- R/checkMeasures.R
+++ R/checkMeasures.R
@@ -1,5 +1,5 @@
-checkMeasures = function(measures, obj, aggr = NULL) {
-  if (missing(measures)) {
+checkMeasures = function(measures = NULL, obj, aggr = NULL) {
+  if (is.null(measures)) {
     measures = list(getDefaultMeasure(obj))
   } else {
     measures = ensureVector(measures, n = 1L, cl = "Measure")
