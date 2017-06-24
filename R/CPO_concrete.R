82a72ffb1c7660d652849b6b59285a29fdfddc05
mb706 <ma.binder@campus.lmu.de>
2017-06-25 00:02:05 +0200

    MissingIndicators CPO

--- R/CPO_concrete.R
+++ R/CPO_concrete.R
@@ -253,3 +253,37 @@ cpoFixFactors = makeCPO("fixfactors", drop.unused.levels = TRUE: logical, fix.fa
   })
 registerCPO(cpoFixFactors, "data", "cleanup", "Clean up Factorial Features.")
 
+
+#' @title Convert Data into Factors indicating Missing Data
+#'
+#' @description
+#' Convert a data.frame into a data.frame with the same column names,
+#' but with columns of factors indicating whether data was missing or not.
+#'
+#' This is most useful in combination with \code{\link{cpoCbind}}.
+#'
+#' @param force.dummies [\code{logical(1)}]\cr
+#'   Whether to create dummy columns even for data that is not missing.
+#'   This can be useful if missing data is expected during test in columns
+#'   where it did not occur during training.
+#'
+#' @template arg_cpo_id
+#' @family CPO
+#' @export
+cpoMissingIndicators = makeCPO("missingindicators", force.dummies = FALSE: logical,
+  .datasplit = "target",
+  .properties.needed = "factors",
+  .properties.adding = c("numerics", "ordered", "missings"),
+  cpo.trafo = {
+    dummycols = sapply(data, function(x) any(is.na(x)))
+    cpo.retrafo = function(data) {
+      data = data[force.dummies | dummycols]
+      for (d in names(data)) {
+        data[[d]] = as.factor(is.na(data[[d]]))
+      }
+      data
+    }
+    cpo.retrafo(data)
+  }, cpo.retrafo = NULL)
+registerCPO(cpoMissingIndicators, "tools", "imputation", "Generate factorial columns indicating whether data was NA.")
+
