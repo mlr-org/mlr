d429aba48c32ecc9a916f3391f3a803c0847368a
pfistfl <pfistererf@googlemail.com>
2017-06-29 20:38:15 +0200

    Suggestions and FIXME's added to makeFDAData.R

--- R/makeFDAData.R
+++ R/makeFDAData.R
@@ -5,3 +5,85 @@ makeFDAData = function(df, fd.features, fd.grids) {
   addClasses(df, "fda.data.frame")
 }
 
+#' Create a functional feature from a data.frame
+#' @param m [\code{matrix}] \cr Numeric matrix.
+#' @param fname [\code{character}] \cr Name of the functional feature
+#' @param fd.grids [\code{numeric}] \cr fd.grids
+makeFunctionalFeature = function(m, fname = NULL, fd.grids = NULL) {
+  assertMatrix(m, mode = "numeric", any.missing = FALSE)
+  if(is.null(fd.grids))
+    fd.grids = seq_len(ncol(m))
+  assertNumeric(fd.grids, len = ncol(m))
+  if(is.null(fname))
+    # FIXME: Maybe reconstruct call and take "name" of m
+    fname = "fd1"
+  assertCharacter(fname, len = 1L)
+  attr(m, "fd.grids") = fd.grids
+  attr(m, "fname") = fname
+  addClasses(m, "functional")
+}
+
+if (FALSE) {
+  # Case1: Outside of make...Task()
+  # Functional Feature m
+  nrw = 40L
+  ncl = 50L
+  m = matrix(rnorm(nrw * ncl), ncol = ncl)
+  # Create a functional feature from m
+  fd1 = makeFunctionalFeature(m)
+
+  # The data.frame we want to create a task for.
+  df = data.frame(x1 = sin(1:40), x2 = as.factor(sample(letters[1:2], size = 40L, replace = TRUE)))
+  tsk = makeClassifTask(data = df, target = "x2")
+
+}
+# Way 2: And add a constructor that adds it to a task / data.frame
+addFunctional = function(obj, fname = NULL, fd) {
+  assertClass(fd, "functional")
+  if (!is.null(fname))
+    attr(fd, "fname") = fname
+  if("Task" %in% class(obj)) {
+    df = getTaskData(obj)
+    df[, attr(fd, "fname")] = fd
+    obj = mlr:::changeData(obj, df)
+  } else if (class(obj) == "data.frame") {
+    obj[, attr(fd, "fname")] = fd
+  }
+  return(obj)
+}
+
+if(FALSE) {
+  # Add functional to a Task
+  tsk1 = addFunctional(tsk, fd1)
+
+  # Make a Task from a data.frame containing a functional
+  df2 = df
+  df2$fd1 = fd1
+  tsk2 = makeClassifTask(data = df2, target = "x2")
+
+  # Add a functional to a data.frame
+  df3 = addFunctional(df, fd1)
+  tsk3 = makeClassifTask(data = df3, target = "x2")
+
+  # This aswell alread works for regression tasks and other tasks.
+  tsk4 = makeRegrTask(data = df3, target = "x1")
+}
+
+
+# FIXME_1: Do we want this to work? Some learners only allow numerics, how is this done there?
+# Suggestion: Allow it to be treated as numeric, but add a warning?
+lrn = makeLearner("classif.rpart")
+mod = train(lrn, tsk1)
+
+# FIXME_2: Specify for each learner if it can deal with functional, much like we have "ordered" and so on.
+# This has to be done for the former "fdalearners" only.
+
+# FIXME_3: Get rid of FDATask and FDALearners. Maybe keep the "fda" in the ID, e.g. classif.fdaknn
+
+# FIXME_4: [DO?] Allow the user to construct tasks using the original api, i.e by specifying fd.features and fd.grids.
+#          In this case, we simply lapply over the fd.features, extract the columns, create a functional feature and
+#          add them back into the data.frame, while erasing the old. This is semi-intelligent, as we introduce I/O
+#          when creating the task.
+
+
+
