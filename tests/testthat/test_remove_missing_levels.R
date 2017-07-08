4baab604d2cca3abdbda8a71979aafbca511c0d9
pat-s <patrick.schratz@gmail.com>
2017-07-08 11:23:29 +0200

    add test

---
+++ tests/testthat/test_remove_missing_levels.R
@@ -0,0 +1,18 @@
+context("remove_missing_levels.R")
+
+test_that("remove_missing_levels.R", {
+  data("basque")
+
+  task <- makeClassifTask(id = "pathogen_data", data = basque,
+                          target = "diplo01", positive = "1")
+
+  rdesc <- makeResampleDesc("RepCV", folds = 10, reps = 2)
+
+  lrn <- makeLearner("classif.binomial",
+                     link = "logit",
+                     predict.type = "prob")
+
+  resa <- resample(lrn, task, rdesc, measures = list(auc))
+
+  expect_length(resa, 12)
+})
