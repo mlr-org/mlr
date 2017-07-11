7391655c0b29594b7783319da16c7806432c0c7c
pat-s <patrick.schratz@gmail.com>
2017-07-11 23:35:18 +0200

    add RLearner_classif_gam

---
+++ tests/testthat/test_classif_gam.R
@@ -0,0 +1,14 @@
+context("classif_gam")
+
+test_that("classif_gam", {
+  requirePackagesOrSkip("mgcv", default.method = "load")
+  # train("classif.gam", sonar.task)
+
+  m = mgcv::gam(formula = getTaskFormula(binaryclass.task, explicit.features = TRUE), data = binaryclass.train, control = learnerArgsToControl(mgcv::gam.control), family = binomial)
+  p = predict(m, newdata = binaryclass.test, type = "response")
+  p.prob = 1-p
+  p.class = as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
+
+  testSimple("classif.gam", binaryclass.df, binaryclass.target, binaryclass.train.inds, p.class)
+
+})
