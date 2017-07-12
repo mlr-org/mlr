90db8b61641f49415a1044704c1b87574fb370b8
pfistfl <pfistererf@googlemail.com>
2017-07-12 15:47:09 +0200

    Add functionals to task.desc.feat

--- R/TaskDesc.R
+++ R/TaskDesc.R
@@ -45,7 +45,8 @@ makeTaskDescInternal = function(type, id, data, target, weights, blocking) {
   n.feat = c(
     numerics = sum(cl[c("integer", "numeric")], na.rm = TRUE),
     factors = sum(cl["factor"], na.rm = TRUE),
-    ordered = sum(cl["ordered"], na.rm = TRUE)
+    ordered = sum(cl["ordered"], na.rm = TRUE),
+    functionals = sum(cl["functional"], na.rm = TRUE)
   )
 
   makeS3Obj("TaskDesc",
--- tests/testthat/test_base_fda_makeFunctionalData.R
+++ tests/testthat/test_base_fda_makeFunctionalData.R
@@ -82,6 +82,71 @@ test_that("makeFunctionalData works for different inputs", {
 })
 
 
+test_that("makeFunctionalData works for different inputs", {
+
+  df = data.frame(matrix(rnorm(50), nrow = 5))
+  # for 1-D matricies
+  fdf = makeFunctionalData(df, fd.features = list("fd1" = 1, "fd2" = 2:10))
+  expect_equal(lapply(fdf, class)[[1]], c("functional", "matrix"))
+  expect_equal(lapply(fdf, class)[[2]], c("functional", "matrix"))
+  expect_equal(dim(fdf), c(5, 2))
+  expect_class(fdf, "data.frame")
+
+  # for column name inputs
+  fdf = makeFunctionalData(df, fd.features = list("fd1" = "X1", "fd2" = paste0("X", 2:10)))
+  expect_equal(lapply(fdf, class)[[1]], c("functional", "matrix"))
+  expect_equal(lapply(fdf, class)[[2]], c("functional", "matrix"))
+  expect_equal(dim(fdf), c(5, 2))
+  expect_class(fdf, "data.frame")
+
+  # for empty lists
+  fdf = makeFunctionalData(df, fd.features = list())
+  expect_equal(lapply(fdf, class)[[1]], c("functional", "matrix"))
+  expect_equal(dim(fdf), c(5, 1))
+  expect_class(fdf, "data.frame")
+
+  # default
+  fdf = makeFunctionalData(df)
+  expect_equal(lapply(fdf, class)[[1]], c("functional", "matrix"))
+  expect_equal(dim(fdf), c(5, 1))
+  expect_class(fdf, "data.frame")
+
+  # data.frame already has matrix
+  # FIXME: The colnames in prints are ugly.
+  df2 = df[, 1, drop = FALSE]
+  df2$fd1 = as.matrix(df[, 2:10])
+  fdf = makeFunctionalData(df2)
+  expect_equal(lapply(fdf, class)[[1]], c("numeric"))
+  expect_equal(lapply(fdf, class)[[2]], c("functional", "matrix"))
+  expect_equal(dim(fdf), c(5, 2))
+  expect_class(fdf, "data.frame")
+})
+test_that("makeFunctionalData works Tasks work", {
+
+  df = data.frame(matrix(rnorm(50), nrow = 5))
+  df$tcl = as.factor(letters[1:5])
+  df$treg = 1:5
+  fdf = makeFunctionalData(df, fd.features = list("fd1" = 1, "fd2" = 5:10, "fd3" = c("X2", "X3", "X4")))
+
+  clt = makeClassifTask(data = fdf, target = "tcl")
+  expect_class(clt, c("ClassifTask", "SupervisedTask", "Task"))
+  expect_equal(clt$task.desc$n.feat["functionals"], c("functionals" = 3L))
+  expect_equal(clt$task.desc$n.feat["numerics"], c("numerics" = 1L))
+
+  regt = makeRegrTask(data = fdf, target = "treg")
+  expect_class(regt, c("RegrTask", "SupervisedTask", "Task"))
+  expect_equal(regt$task.desc$n.feat["functionals"], c("functionals" = 3L))
+  expect_equal(regt$task.desc$n.feat["factors"], c("factors" = 1L))
+
+  clustt = makeClusterTask(data = fdf)
+  expect_class(clustt, c("ClusterTask", "UnsupervisedTask", "Task"))
+  expect_equal(clustt$task.desc$n.feat["functionals"], c("functionals" = 3L))
+  expect_equal(clustt$task.desc$n.feat["factors"], c("factors" = 1L))
+  expect_equal(clustt$task.desc$n.feat["numerics"], c("numerics" = 1L))
+})
+
+
+
 # test_that("Code from Bernd", {
   # d = list(x1 = matrix(123, 3, 2), x2 = matrix(1:6, 3, 2), x3 = 1:3)
   # class(d) = "data.frame"
