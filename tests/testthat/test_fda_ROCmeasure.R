bc4eb86171f3449b2b614f05063a9c7150e66f0a
lbeggel <laura.beggel@googlemail.com>
2017-07-13 15:36:10 +0200

    ROC and tunethreshold tests

---
+++ tests/testthat/test_fda_ROCmeasure.R
@@ -0,0 +1,26 @@
+context("fda_ROCmeasure")
+
+test_that("ROC measure works for fda learners", {
+  rdesc = makeResampleDesc("CV", iters = 3)
+  pred = resample(makeLearner("classif.fdaknn"), fda.binary.gp.task.small, rdesc)
+  r = calculateROCMeasures(pred$pred)
+
+  expect_list(r$measures, types = "double", any.missing = FALSE, len = 12)
+  expect_matrix(r$confusion.matrix, mode = "double", any.missing = FALSE, nrows = 2, ncols = 2)
+  expect_true(all(rowSums(r$confusion.matrix) == 1))
+
+  response = getPredictionResponse(pred$pred)
+  truth = getPredictionTruth(pred$pred)
+  positive = pred$pred$task.desc$positive
+  negative = pred$pred$task.desc$negative
+
+  expect_equal(r$measures$tpr, measureTPR(truth, response, positive))
+  expect_equal(r$measures$fnr, measureFNR(truth, response, negative, positive))
+  expect_equal(r$measures$fdr, measureFDR(truth, response, positive))
+  expect_equal(r$measures$tnr, measureTNR(truth, response, negative))
+  expect_equal(r$measures$ppv, measurePPV(truth, response, positive))
+  expect_equal(r$measures$fdr, 1 - measurePPV(truth, response, positive))
+  expect_equal(r$measures$npv, measureNPV(truth, response, negative))
+  expect_equal(r$measures$fomr, 1 - measureNPV(truth, response, negative))
+  expect_equal(r$measures$acc, measureACC(truth, response))
+})
