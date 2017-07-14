bd79a9cddc4349c3e1513a906287b7bfb41e9f88
pat-s <patrick.schratz@gmail.com>
2017-07-14 21:42:25 +0200

    lintr

--- tests/testthat/test_remove_missing_levels_lm.R
+++ tests/testthat/test_remove_missing_levels_lm.R
@@ -25,7 +25,7 @@ test_that("remove.missing.levels.lm.R", {
   n = getTaskSize(task)
   train.set = seq(1, n, by = 2)
   test.set = seq(2, n, by = 2)
-  mod <- train(lrn, task, subset = train.set)
+  mod = train(lrn, task, subset = train.set)
 
   task.pred = predict(mod, task = task, subset = test.set)
 
