b557bfd3fbdc4da6381c0e7da423da65a0e97cf7
pat-s <patrick.schratz@gmail.com>
2017-07-14 18:07:48 +0200

    add test for train+predict

--- tests/testthat/test_remove_missing_levels_lm.R
+++ tests/testthat/test_remove_missing_levels_lm.R
@@ -16,3 +16,19 @@ test_that("remove.missing.levels.lm.R", {
 
   expect_length(resa, 12)
 })
+
+test_that("remove.missing.levels.lm.R", {
+
+  data("bc.task.spatial")
+  task = bc.task.spatial
+
+  n = getTaskSize(task)
+  train.set = seq(1, n, by = 2)
+  test.set = seq(2, n, by = 2)
+  mod <- train(lrn, task, subset = train.set)
+
+  task.pred = predict(mod, task = task, subset = test.set)
+
+  expect_length(task.pred, 7)
+})
+
