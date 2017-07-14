9d26f906f1a74c5e448d0a2555dc9ccd1d7ee844
pat-s <patrick.schratz@gmail.com>
2017-07-14 17:24:47 +0200

    condition on fix.factors.prediction flag

--- tests/testthat/test_remove_missing_levels_lm.R
+++ tests/testthat/test_remove_missing_levels_lm.R
@@ -1,15 +1,16 @@
 context("remove.missing.levels.lm.R")
 
 test_that("remove.missing.levels.lm.R", {
-  data("basque")
 
   data("bc.task.spatial")
+  task = bc.task.spatial
 
   rdesc = makeResampleDesc("RepCV", folds = 10, reps = 2)
 
   lrn = makeLearner("classif.binomial",
                     link = "logit",
-                    predict.type = "prob")
+                    predict.type = "prob",
+                    fix.factors.prediction = TRUE)
 
   resa = resample(lrn, task, rdesc, measures = list(auc))
 
