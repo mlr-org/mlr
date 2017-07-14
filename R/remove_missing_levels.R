1eb1856cbf89e6036cade867df4032bf25911a4a
pat-s <patrick.schratz@gmail.com>
2017-07-14 15:52:30 +0200

    - style guide (gsub) - update info message

--- R/remove_missing_levels.R
+++ R/remove_missing_levels.R
@@ -2,7 +2,7 @@
 #' @description Accounts for missing factor levels only present in test data
 #' but not in train data by setting levels to NA
 #'
-#' @importFrom stringi stri_split
+#' @importFrom stringi stri_split stri_replace_all
 #' @importFrom BBmisc warningf
 #'
 #' @param fit fitted model on training data
@@ -25,8 +25,8 @@ remove_missing_levels = function(fit, test_data) {
   # account for it
   if (any(class(fit) == "glmmPQL")) {
     # Obtain factor predictors in the model and their levels
-    factors = (gsub("[-^0-9]|as.factor|\\(|\\)", "",
-                     names(unlist(fit$contrasts))))
+    factors = stri_replace_all_regex(names(unlist(fit$learner.model$contrasts)),
+                                     "[-^0-9]|as.factor|\\(|\\)", "")
     # do nothing if no factors are present
     if (length(factors) == 0) {
       return(test_data)
@@ -39,13 +39,13 @@ remove_missing_levels = function(fit, test_data) {
     model_factors = as.data.frame(cbind(factors, factor_levels))
   } else {
     # Obtain factor predictors in the model and their levels
-    factors = (gsub("[-^0-9]|as.factor|\\(|\\)", "",
-                     names(unlist(fit$xlevels))))
+    factors = stri_replace_all_regex(names(unlist(fit$learner.model$xlevels)),
+                                     "[-^0-9]|as.factor|\\(|\\)", "")
     # do nothing if no factors are present
     if (length(factors) == 0) {
       return(test_data)
     }
-    factor_levels = unname(unlist(fit$xlevels))
+    factor_levels = unname(unlist(fit$learner.model$xlevels))
     model_factors = as.data.frame(cbind(factors, factor_levels))
   }
 
@@ -67,8 +67,10 @@ remove_missing_levels = function(fit, test_data) {
       # drop empty factor levels in test data
       test_data = droplevels(test_data)
       # issue warning to console
-      warningf("\nSetting missing levels in '%s', only present in test data but missing in train data, to 'NA'.",
+      catf("\nSetting missing levels in '%s', only present in test data but missing in train data, to 'NA'.",
                       var)
+      catf("This affected %s of %s total observations in test data.",
+           table(is.na(test_data))[[2]], nrow(test_data))
     }
   }
   return(test_data)
