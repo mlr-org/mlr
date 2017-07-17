c8d9b1b1a1bdc117c8551ae419cc2f594e621d8e
pat-s <patrick.schratz@gmail.com>
2017-07-17 17:00:34 +0200

    specify family dependent link parameters

--- R/RLearner_classif_gam.R
+++ R/RLearner_classif_gam.R
@@ -4,31 +4,34 @@ makeRLearner.classif.gam = function() {
     cl = "classif.gam",
     package = "mgcv",
     par.set = makeParamSet(
-      # method, optimizer, select
+      makeUntypedLearnerParam(id = "formula", default = NULL, tunable = FALSE),
       makeDiscreteLearnerParam(id = "method", default = "GCV.Cp",
                                values = c("GACV.Cp", "GCV.Cp", "REML", "P-REML", "ML", "P-ML")),
       # wrong! check how to supply vectors as parameters
       makeDiscreteLearnerParam(id = "optimizer", default = c("outer", "newton"),
-                               values = list(newton = c("outer", "newton"), perf = "perf", efs = "efs",
+                               values = list(newton = c("outer", "newton"), perf = c("perf"), efs = c("efs"),
                                              bfgs = c("outer", "bfgs"), optim = c("outer", "optim"),
                                              nlm = c("outer", "nlm"), nlm.fd = c("outer", "nlm.fd"))),
-      makeDiscreteLearnerParam(id = "family", default = "gaussian",
-                               values = c("binomial", "gaussian", "binomial", "poisson", "Gamma",
-                                          "inverse.gaussian", "quasi", "quasibinomial", "quasipoisson",
-                                          "Tweedie", "negbin", "ocat", "tw", "nb", "betar", "scat", "ziP",
-                                          "cox.ph", "gaulss", "gevlss", "ziplss", "mvn",
+      makeDiscreteLearnerParam(id = "family", default = "binomial",
+                               values = c("binomial","quasibinomial",
+                                          "Tweedie", "negbin",
                                           "multinom")),
-      makeDiscreteLearnerParam(id = "link", default = "logit",
-                               values = c("logit", "probit", "identity", "inverse", "1/mu^2", "log",
-                                          "power(0)", "list = 'identity', 'logb')", "list('identity','identity','logit'')")),
+      makeDiscreteLearnerParam(id = "binomial.link", default = "logit",
+                               values = c("logit", "probit", "cauchit", "log", "cloglog"), requires = quote(family == "binomial")),
+      makeDiscreteLearnerParam(id = "quasibinomial.link", default = "logit",
+                               values = c("logit", "probit", "identity", "inverse", "log", "1/mu^2", "sqrt"), requires = quote(family == "quasibinomial")),
+      makeDiscreteLearnerParam(id = "Tweedie.link", default = "logit",
+                               values = c("log", "identity", "inverse", "sqrt"), requires = quote(family == "Tweedie")),
+      makeDiscreteLearnerParam(id = "negbin.link", default = "log",
+                               values = c("log", "identity", "sqrt"), requires = quote(family == "negbin")),
       makeNumericLearnerParam(id = "gamma", default = 1, lower = 0),
       makeNumericLearnerParam(id = "scale", default = 0),
       makeLogicalLearnerParam(id = "select", default = FALSE),
-      makeNumericVectorLearnerParam(id = "knots"),
-      makeNumericVectorLearnerParam(id = "sp"),
-      makeNumericLearnerParam(id = "min.sp"),
-      makeNumericLearnerParam(id = "H"),
-      makeNumericLearnerParam(id = "in.out"),
+      #makeNumericVectorLearnerParam(id = "knots"),
+      #makeNumericVectorLearnerParam(id = "sp"),
+      #makeNumericLearnerParam(id = "min.sp"),
+      #makeNumericLearnerParam(id = "H"),
+      #makeNumericLearnerParam(id = "in.out"),
       makeLogicalLearnerParam(id = "fit", default = TRUE),
       # "optional list specifying any penalties to be applied to parametric model terms. gam.models explains more."
       # how to specify such a Param?
@@ -49,16 +52,22 @@ makeRLearner.classif.gam = function() {
 
 #' @export
 trainLearner.classif.gam = function(.learner, .task, .subset, .weights = NULL, family = "gaussian", link = "logit", K = 1,
-                                    theta = stop("'theta' must be specified"), p = 1, optimizer = optimizer,
-                                    method = method, ...) {
+                                    theta = stop("'theta' must be specified"), p = 1, optimizer = c("outer", "newton"),
+                                    method = "GCV.Cp", scale = 0, select = FALSE, drop.unused.levels = TRUE,
+                                    drop.intercept = FALSE, formula, ...) {
 
   ctrl = learnerArgsToControl(mgcv::gam.control)
-  f = getTaskFormula(.task, explicit.features = TRUE)
+  if (is.null(formula)) {
+    f = getTaskFormula(.task, explicit.features = TRUE)
+  } else {
+    f = BBmisc::asQuoted(formula)
+  }
   family = switch(family,
-                  binomial = stats::bionomial(link = link),
+                  binomial = stats::binomial(link = make.link(binomial.link)),
+                  quasibinomial = stats::quasibinomial(link = make.link(quasibinomial.link)),
                   multinom = mgcv::multinom(K = K),
-                  negbin = mgcv::negbin(theta = theta, link = link),
-                  Tweedie = mgcv::Tweedie(p = p, link = link)
+                  negbin = mgcv::negbin(theta = theta, link = make.link(negbin.link)),
+                  Tweedie = mgcv::Tweedie(p = p, link = make.link(Tweedie.link))
   )
   mgcv::gam(f, data = getTaskData(.task, .subset), control = ctrl, family = family,
             optimizer = optimizer, method = method, ...)
