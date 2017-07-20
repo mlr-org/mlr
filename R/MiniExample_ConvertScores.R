8028778ef9da36dfe2ff97e33a48d9374b798b70
Minh Le <minh-anh.le@cdtm.de>
2017-07-20 14:07:08 +0200

    no message

--- R/MiniExample_ConvertScores.R
+++ R/MiniExample_ConvertScores.R
@@ -1,30 +1,31 @@
 data = readRDS("R/anomaly.data.5percent.rds")
 
-data = readRDS("banana.rds")
-speach = readRDS("speach.rds")
-shuttle = readRDS("shuttle.rds")
-satellite = readRDS("satellite.rds")
-pen.local = readRDS("pen.local.rds")
-pen.global = readRDS("pen.global.rds")
-letter = readRDS("letter.rds")
-kdd99  = readRDS("kdd99.rds")
-breast.cancer = readRDS("breast.cancer.rds")
-annthyroid = readRDS("annthyroid.rds")
-aloi = readRDS("aloi.rds")
-artificial.unsup = readRDS("artificial.unsup.rds")
-
-cover = readRDS("cover.rds")
-http = readRDS("http.rds")
-mammography = readRDS("mammography.rds")
-mulcross = readRDS("mulcross.rds")
-smtp = readRDS("smtp.rds")
+# data = readRDS("banana.rds")
+# speach = readRDS("speach.rds")
+# shuttle = readRDS("shuttle.rds")
+# satellite = readRDS("satellite.rds")
+# pen.local = readRDS("pen.local.rds")
+# pen.global = readRDS("pen.global.rds")
+# letter = readRDS("letter.rds")
+# kdd99  = readRDS("kdd99.rds")
+# breast.cancer = readRDS("breast.cancer.rds")
+# annthyroid = readRDS("annthyroid.rds")
+# aloi = readRDS("aloi.rds")
+# artificial.unsup = readRDS("artificial.unsup.rds")
+#
+# cover = readRDS("cover.rds")
+# http = readRDS("http.rds")
+# mammography = readRDS("mammography.rds")
+# mulcross = readRDS("mulcross.rds")
+# smtp = readRDS("smtp.rds")
 
 
 col = factor(data$Target, levels = c("Normal", "Anomaly"), labels =  c("black", "red"))
+data$Target = NULL
 
 # nu 0.5 per default
-svm.model0.5 <- svm(data[, 1:4], y = NULL, type='one-classification', nu = 0.5, kernel="radial", scale=TRUE)
-svm.pred0.5 <- predict(svm.model0.5, data[,1:4], decision.values = TRUE)
+svm.model0.5 <- svm(data, y = NULL, type='one-classification', nu = 0.5, kernel="radial", scale=TRUE)
+svm.pred0.5 <- predict(svm.model0.5, data, decision.values = TRUE)
 scores0.5 = attr(svm.pred0.5, "decision.values")
 convert0.5 = convertingScoresToProbability(scores0.5, parainit = c(0,1), optim.method = "glm")
 prob0.5 = convert0.5$probability
@@ -32,8 +33,8 @@ convert0.5$p
 o0.5 = order(scores0.5)
 
 # nu 0.09
-svm.model0.09 <- svm(data[, 1:4], y = NULL, type='one-classification', nu = 0.09, kernel="radial", scale=TRUE)
-svm.pred0.09 <- predict(svm.model0.09, data[,1:4], decision.values = TRUE)
+svm.model0.09 <- svm(data, y = NULL, type='one-classification', nu = 0.09, kernel="radial", scale=TRUE)
+svm.pred0.09 <- predict(svm.model0.09, data, decision.values = TRUE)
 scores0.09 = attr(svm.pred0.09, "decision.values")
 convert0.09 = convertingScoresToProbability(scores0.09, parainit = c(0,1), optim.method = "glm")
 prob0.09 = convert0.09$probability
@@ -41,8 +42,8 @@ convert0.09$p
 o0.09 = order(scores0.09)
 
 # 0.05 (erwartet 5% anomaly)
-svm.model0.05 <- svm(data[, 1:4], y = NULL, type='one-classification', nu = 0.05, kernel="radial", scale=TRUE)
-svm.pred0.05 <- predict(svm.model0.05, data[,1:4], decision.values = TRUE)
+svm.model0.05 <- svm(data, y = NULL, type='one-classification', nu = 0.05, kernel="radial", scale=TRUE)
+svm.pred0.05 <- predict(svm.model0.05, data, decision.values = TRUE)
 scores0.05 = attr(svm.pred0.05, "decision.values")
 convert0.05 = convertingScoresToProbability(scores0.05, parainit = c(0,1), optim.method = "glm")
 prob0.05 = convert0.05$probability
