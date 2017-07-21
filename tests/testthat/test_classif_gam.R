96f930f214f62be5b37b5d378dd7f7e100877719
pat-s <patrick.schratz@gmail.com>
2017-07-21 17:04:28 +0200

    add 'sp' arg, fix .weights arg

--- tests/testthat/test_classif_gam.R
+++ tests/testthat/test_classif_gam.R
@@ -9,14 +9,12 @@ test_that("classif_gam_binomial", {
                 data = binaryclass.train[, 57:61], control = learnerArgsToControl(mgcv::gam.control),
                 family = binomial)
   p = predict(m, newdata = binaryclass.test, type = "response")
-  p.prob = 1-p
+  p.prob = 1 - p
   p.class = as.factor(binaryclass.class.levs[ifelse(p > 0.5, 2, 1)])
 
   testSimple("classif.gam", binaryclass.df, binaryclass.target, binaryclass.train.inds, p.class, parset = list(formula = fo))
 
 })
-
-context("classif_gam")
 
 test_that("classif_gam_binomial", {
   requirePackagesOrSkip("mgcv", default.method = "load")
@@ -31,41 +29,29 @@ test_that("classif_gam_binomial", {
     list(family = quasibinomial(link = "logit")),
     list(family = quasibinomial(link = "probit")),
     list(family = quasibinomial(link = "identity")),
-    #list(family = quasibinomial(link = "inverse")),
-    #list(family = quasibinomial(link = "sqrt")),
+    list(family = quasibinomial(link = "inverse")),
+    #list(family = quasibinomial(link = "sqrt"))
     list(family = quasibinomial(link = "1/mu^2"))
-    #list(family = Tweedie(link = "log")),
-    #list(family = Tweedie(link = "inverse")),
-    #list(family = Tweedie(link = "sqrt")),
-    #list(family = Tweedie(link = "log")),
-    #list(family = Tweedie(link = "identity")),
-    #list(family = Tweedie(link = "sqrt")),
-    #list(family = negbin(link = "log")),
-    #list(family = negbin(link = "identity")),
-    #list(family = negbin(link = "sqrt"))
+    #list(family = nb(link = "log"))
+    #list(family = nb(link = "identity"))
+    #list(family = nb(link = "sqrt"))
   )
 
-  parset.list2 = list(
-    list(family = "binomial", binomial.link = "logit", formula = fo),
-    list(family = "binomial", binomial.link = "cloglog", formula = fo),
-    #list(family = "binomial", binomial.link = "probit", formula = fo),
-    list(family = "binomial", binomial.link = "cauchit", formula = fo),
-    list(family = "quasibinomial", quasibinomial.link = "logit", formula = fo),
-    list(family = "quasibinomial", quasibinomial.link = "identity", formula = fo),
-    list(family = "quasibinomial.link", quasibinomial.link = "probit", formula = fo),
-    #list(family = "quasibinomial.link", quasibinomial.link = "inverse", formula = fo),
-    #list(family = "quasibinomial.link", quasibinomial.link = "sqrt", formula = fo),
-    list(family = "quasibinomial", quasibinomial.link = "1/mu^2", formula = fo)
-
-    ### here we need to set 'p' accordingly. see help pages
-    # list(family = "Tweedie.link", quasibinomial.link = "log", formula = fo),
-    # list(family = "Tweedie.link", quasibinomial.link = "identity", formula = fo),
-    # list(family = "Tweedie.link", quasibinomial.link = "inverse", formula = fo),
-    # list(family = "Tweedie.link", quasibinomial.link = "sqrt", formula = fo),
-    # list(family = "negbin.link", quasibinomial.link = "log", formula = fo),
-    # list(family = "negbin.link", quasibinomial.link = "identity", formula = fo),
-    # list(family = "negbin.link", quasibinomial.link = "sqrt", formula = fo)
-  )
+  # parset.list2 = list(
+  #   list(family = "binomial", binomial.link = "logit", formula = fo),
+  #   list(family = "binomial", binomial.link = "cloglog", formula = fo),
+  #   #list(family = "binomial", binomial.link = "probit", formula = fo),
+  #   list(family = "binomial", binomial.link = "cauchit", formula = fo),
+  #   list(family = "quasibinomial", quasibinomial.link = "logit", formula = fo),
+  #   list(family = "quasibinomial", quasibinomial.link = "identity", formula = fo),
+  #   list(family = "quasibinomial.link", quasibinomial.link = "probit", formula = fo),
+  #   #list(family = "quasibinomial.link", quasibinomial.link = "inverse", formula = fo),
+  #   #list(family = "quasibinomial.link", quasibinomial.link = "sqrt", formula = fo),
+  #   list(family = "quasibinomial", quasibinomial.link = "1/mu^2", formula = fo),
+  #   list(family = "negbin", nb.link = "log", formula = fo),
+  #   list(family = "negbin", nb.link = "identity", formula = fo),
+  #   list(family = "negbin", nb.link = "sqrt", formula = fo)
+  # )
 
   old.predicts.list = list()
   old.probs.list = list()
@@ -77,6 +63,7 @@ test_that("classif_gam_binomial", {
   # important here is that parset combinations work -> they do if no error occurs
   expect_warning(for (i in seq_along(parset.list1)) {
     parset = parset.list1[[i]]
+    # print(paste0(parset$family$family, " ", parset$family$link))
     set.seed(getOption("mlr.debug.seed"))
     m = mgcv::gam(data = binaryclass.train[, 57:61],
                   family = parset[[1]], formula = BBmisc::asQuoted(fo))
