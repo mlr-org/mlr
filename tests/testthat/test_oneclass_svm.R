49e9095ec6ae30c0d429775ededf5f50faf8ad00
Minh Le <minh-anh.le@cdtm.de>
2017-08-07 17:58:58 +0200

    no lint

--- tests/testthat/test_oneclass_svm.R
+++ tests/testthat/test_oneclass_svm.R
@@ -3,7 +3,7 @@ context("oneclass_svm")
 # we cannot do a prob test, as set.seed sems not to work on e1071 svm for the prob parameters!
 #requirePackagesOrSkip("e1071", default.method = "load")
 
-test_that ("oneclass_svm", {
+test_that("oneclass_svm", {
   requirePackagesOrSkip("e1071", default.method = "load")
 
   parset.list = list(
@@ -15,7 +15,7 @@ test_that ("oneclass_svm", {
 
   old.predicts.list = list()
 
-  for (i in seq_along (parset.list)) {
+  for (i in seq_along(parset.list)) {
     parset = parset.list[[i]]
     pars = list(x = oneclass.train[, -oneclass.col])
     pars = c(pars, list(type = "one-classification"))
