903f33d6f61171e7d4cf93ebcc57fef919680344
Minh Le <minh-anh.le@cdtm.de>
2017-08-08 17:50:29 +0200

    no lint

--- R/Make_measure_precision.R
+++ R/Make_measure_precision.R
@@ -79,7 +79,7 @@ makePrecisionMeasure = function(id = "Precision", minimize = FALSE, best = 1, wo
 
       # proportion of correct results in the top n ranks
       n.anomaly = sum(pred$data$truth == pred$task.desc$positive)
-      scores = pred$data[,3]
+      scores = pred$data[, 3]
       rank = order(scores, decreasing = TRUE)
       ind.true = which(pred$data$truth == pred$task.desc$positive)
       if (n.anomaly == 0) {
@@ -97,7 +97,7 @@ makePrecisionMeasure = function(id = "Precision", minimize = FALSE, best = 1, wo
       }
 
       if (adjusted == TRUE) {
-        expected.index = n.anomaly/nrow(pred$data)
+        expected.index = n.anomaly / nrow(pred$data)
         precision = max(0, (precision - expected.index) / (1 - expected.index))
       }
       precision
