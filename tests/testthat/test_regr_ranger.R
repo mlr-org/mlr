e4faacf7b910f4bbe6775268ac00ada75d764e38
Damian Gola <gola@imbs.uni-luebeck.de>
2017-08-21 17:08:51 +0200

    add test for ranger se prediction

--- tests/testthat/test_regr_ranger.R
+++ tests/testthat/test_regr_ranger.R
@@ -22,3 +22,27 @@ test_that("regr_ranger", {
 
   testSimpleParsets("regr.ranger", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list)
 })
+
+test_that("regr_ranger se", {
+  requirePackagesOrSkip("ranger", default.method = "load")
+
+  parset.list = list(
+    list(),
+    list(num.trees = 100),
+    list(num.trees = 250, mtry = 4),
+    list(num.trees = 500, min.node.size = 2)
+  )
+  old.predicts.list = list()
+
+  for (i in seq_along(parset.list)) {
+    parset = parset.list[[i]]
+    parset.list[[i]] = c(parset, predict.type = "se")
+    parset = c(parset, list(data = regr.train, formula = regr.formula, write.forest = TRUE, respect.unordered.factors = TRUE, keep.inbag = TRUE))
+    set.seed(getOption("mlr.debug.seed"))
+    m = do.call(ranger::ranger, parset)
+    p  = predict(m, data = regr.test, type = "se")
+    old.predicts.list[[i]] = cbind(p$predictions, p$se)
+  }
+
+  testSimpleParsets("regr.ranger", regr.df, regr.target, regr.train.inds, old.predicts.list, parset.list)
+})
