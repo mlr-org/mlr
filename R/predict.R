15b14a4e01e2123ade735935c8843ce56af0aa16
pat-s <patrick.schratz@gmail.com>
2017-09-14 21:29:46 +0200

    move fix.factors.prediction part to predictLearner.regr.lm

--- R/predict.R
+++ R/predict.R
@@ -72,26 +72,6 @@ predict.WrappedModel = function(object, task, newdata, subset = NULL, ...) {
     newdata = newdata[subset, , drop = FALSE]
   }
 
-
-  # set factor levels, present in test but missing in train, to NA
-  if (model$learner$fix.factors.prediction == TRUE &&
-      any(vlapply(newdata, function(x) is.factor(x))) &&
-      any(class(model$learner.model) == "lm")) {
-    # sometimes we have no task here, e.g. in test_tune_tuneMBO@34
-    if (!missing(task)) {
-      newdata = task$env$data[subset, ]
-    } else {
-      newdata = newdata
-    }
-
-    # cheap error catching here
-    # in @test_base_generateFilterValuesData.R#93 data is not stored in m$learner.model ??
-    if (is.null(subset)) {
-      newdata = model$learner.model$data[subset, ]
-    }
-    newdata = missingLevelsTrain(model, newdata)
-  }
-
   # if we saved a model and loaded it later just for prediction this is necessary
   requireLearnerPackages(learner)
   t.col = match(td$target, colnames(newdata))
