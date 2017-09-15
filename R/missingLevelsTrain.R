61e6829946afdab17f38816f409e58efdd30b73c
pat-s <patrick.schratz@gmail.com>
2017-09-15 23:30:08 +0200

    update documentation

--- R/missingLevelsTrain.R
+++ R/missingLevelsTrain.R
@@ -1,11 +1,10 @@
 #' @title Set missing factor levels in test data to NA
 #' @description
 #' Sets missing factor levels in test data to NA to avoid prediction error for lm models
-#' @param model \cr
-#'   A model fitted on training data.
-#' @param test.data [`data.frame`] \cr
+#' @param model A lm* model fitted on training data.
+#' @param test.data [\code{data.frame}] \cr
 #'   Data set for prediction.
-#' @return A `data.frame` with matching factor levels of test data and fitted model
+#' @return A \code{data.frame} with matching factor levels of test data and fitted model
 missingLevelsTrain = function(model, test.data) {
   lmod = getLearnerModel(model)
   UseMethod("missingLevelsTrain", lmod)
