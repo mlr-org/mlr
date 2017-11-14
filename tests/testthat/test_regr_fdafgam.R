921b59d3c5ecac5db289cde7c5c4f3cb44f0d2e9
smilesun <smilesun.east@gmail.com>
2017-11-14 18:03:15 +0100

    fgam

--- tests/testthat/test_regr_fdafgam.R
+++ tests/testthat/test_regr_fdafgam.R
@@ -3,17 +3,15 @@ context("FDA_regr_fgam")
 test_that("FDA_regr_fgam", {
   requirePackagesOrSkip("refund")
   data(DTI)
-  DTI1 = DTI[DTI$visit == 1 & complete.cases(DTI),]  # subtract a portion of the data
+  DTI1 = DTI[DTI$visit == 1 & complete.cases(DTI),]  # subtract a portion of the data(complete.cases select non missing value rows)
   # DTI1 is already a matrix dataframe, Fit model with additive functional term for functional feature cca, using tensor product basis
   fit.af = refund::pfr(formula = pasat ~ af(cca, Qtransform=TRUE, k=c(7,7)), data = DTI1)
-  predict(fit.af, newdata = DTI1, type = 'response')
-  df = data.frame(as.list(DTI[, c("pasat", "cca")]))
-  df = df[!is.na(df$pasat),]
-  df = impute(df, classes = list(numeric = imputeMedian()))$data
-  # impute those na values
-  fdf = makeFunctionalData(df, fd.features = list("cca" = 2:94))
+  prd_refund = predict(fit.af, newdata = DTI1, type = 'response')
+  df = data.frame(as.list(DTI1[, c("pasat", "cca")]))  # makeFunctionalData require plain dataframe
+  fdf = makeFunctionalData(df, fd.features = list("cca" = 2:94))  # dim(DTI1$cca) = (66,93)
   lrn = makeLearner("regr.fdafgam", mgcv.s.k = -1L )
   task = makeRegrTask(data = fdf, target = "pasat")
   mod1f = train(learner = lrn, task = task)
-  predict(object = mod1f, newdata = fdf)
+  prd_mlr = predict(object = mod1f, newdata = fdf)
+  all.equal(as.numeric(prd_refund), prd_mlr$data$response)
 })
