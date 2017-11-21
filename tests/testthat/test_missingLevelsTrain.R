55269be6c693edbd5fafd6fa3e54ecd6e3660908
pat-s <patrick.schratz@gmail.com>
2017-11-21 15:26:08 +0100

    rename tests, check for error with default behaviour

--- tests/testthat/test_missingLevelsTrain.R
+++ tests/testthat/test_missingLevelsTrain.R
@@ -1,10 +1,11 @@
 context("missingLevelsTrain.R")
 
-test_that("missingLevelsTrain.R", {
+test_that("additional factor levels in test data are removed and resample() does not error", {
 
   task = sonar.task
   # introduce factor level with only one observation
-  task$env$data$test_fac = factor(c(rep("1", 1), c(rep("2", 3), rep("3", length(task$env$data$V1) - 4))))
+  task$env$data$test_fac = factor(c(rep("1", 1), c(rep("2", 3),
+    rep("3", length(task$env$data$V1) - 4))))
 
   rdesc = makeResampleDesc("RepCV", folds = 5, reps = 2)
 
@@ -18,25 +19,20 @@ test_that("missingLevelsTrain.R", {
   expect_length(resa, 12)
 })
 
-test_that("missingLevelsTrain.R", {
+test_that("resample() DOES error if additional factor levels in test data are not removed (default behaviour)", {
 
   task = sonar.task
   # introduce factor level with only one observation
-  task$env$data$test_fac = factor(c(rep("1", 1), c(rep("2", 3), rep("3", length(task$env$data$V1) - 4))))
+  task$env$data$test_fac = factor(c(rep("1", 1), c(rep("2", 3),
+    rep("3", length(task$env$data$V1) - 4))))
 
-  n = getTaskSize(task)
-  train.set = seq(1, n, by = 2)
-  test.set = seq(2, n, by = 2)
+  rdesc = makeResampleDesc("RepCV", folds = 5, reps = 2)
 
   lrn = makeLearner("classif.binomial",
                     link = "logit",
                     predict.type = "prob",
-                    fix.factors.prediction = TRUE)
+                    fix.factors.prediction = FALSE)
 
-  mod = train(lrn, task, subset = train.set)
-
-  task.pred = predict(mod, task = task, subset = test.set)
-
-  expect_length(task.pred, 7)
+  expect_error(resample(lrn, task, rdesc, measures = list(auc)),
+     "factor test_fac has new levels 1")
 })
-
