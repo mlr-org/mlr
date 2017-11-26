41f13f7e84bda45f36fc97b77f8e3a6e11871ac0
Collin Leiber <c.leiber@campus.lmu.de>
2017-11-26 21:11:58 +0100

    Use getLearnerId, getLearnerType and getLearnerPackages methods. Use stri_paste instead of paste

--- tests/testthat/test_base_multilabelWrapperIds.R
+++ tests/testthat/test_base_multilabelWrapperIds.R
@@ -1,31 +1,28 @@
 context("multiLabelWrapperIds")
 
+binary.learner = makeLearner("classif.rpart")
+
 test_that("multiLabelWrapperIds DBR", {
-  binary.learner = makeLearner("classif.rpart")
   mlw = makeMultilabelDBRWrapper(binary.learner)
-  expect_equal(getLearnerId(mlw), "multilabelDBR.classif.rpart")
+  expect_equal(getLearnerId(mlw), "multilabel.DBR.classif.rpart")
 })
 
 test_that("multiLabelWrapperIds NestedStacking", {
-  binary.learner = makeLearner("classif.rpart")
   mlw = makeMultilabelNestedStackingWrapper(binary.learner)
-  expect_equal(getLearnerId(mlw), "multilabelNestedStacking.classif.rpart")
+  expect_equal(getLearnerId(mlw), "multilabel.nestedStacking.classif.rpart")
 })
 
 test_that("multiLabelWrapperIds Stacking", {
-  binary.learner = makeLearner("classif.rpart")
   mlw = makeMultilabelStackingWrapper(binary.learner)
-  expect_equal(getLearnerId(mlw), "multilabelStacking.classif.rpart")
+  expect_equal(getLearnerId(mlw), "multilabel.stacking.classif.rpart")
 })
 
 test_that("multiLabelWrapperIds BinaryRelevance", {
-  binary.learner = makeLearner("classif.rpart")
   mlw = makeMultilabelBinaryRelevanceWrapper(binary.learner)
-  expect_equal(getLearnerId(mlw), "multilabelBinaryRelevance.classif.rpart")
+  expect_equal(getLearnerId(mlw), "multilabel.binaryRelevance.classif.rpart")
 })
 
 test_that("multiLabelWrapperIds ClassifierChains", {
-  binary.learner = makeLearner("classif.rpart")
   mlw = makeMultilabelClassifierChainsWrapper(binary.learner)
-  expect_equal(getLearnerId(mlw), "multilabelClassifierChains.classif.rpart")
+  expect_equal(getLearnerId(mlw), "multilabel.classifierChains.classif.rpart")
 })
