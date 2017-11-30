d75c2223f9c67c8d68188fd850f0a576225b015b
haijunXue <xuehaijun793g@gmail.com>
2017-11-30 22:43:35 +0100

    commit

--- R/RLearner_classif_adaboostM1.R
+++ R/RLearner_classif_adaboostM1.R
@@ -1,32 +1,31 @@
 # checked props
 #' @export
-makeRLearner_classif_adaboostM1 = function() {
+makeRLearner.classif.adaboostM1 = function() {
   makeRLearnerClassif(
     cl = "classif.adaboostM1",
-    package = "RWeka",
+    package = c("RWeka", "rpart"),
     par.set = makeParamSet(
-      makeLogicalLearnerParam(id = "P", default = 100L,lower =90),
+      makeIntegerLearnerParam(id = "P", default = 100L, lower = 90L),
       makeLogicalLearnerParam(id = "Q", default = FALSE),
       makeNumericLearnerParam(id = "S", default = 1L),
       makeIntegerLearnerParam(id = "I", default = 10L, lower = 1L),
       makeLogicalLearnerParam(id = "D", default = FALSE, requires = quote(!U)),
-      makeIntegerLearnerParam(id = "W", default = weka.classifiers.trees.DecisionStump),
-      makeLogicalLearnerParam(id = "D", default = FALSE),
+      makeUntypedLearnerParam(id = "W", default = RWeka::DecisionStump),
       makeLogicalLearnerParam(id = "output-debug-info", default = FALSE, tunable = FALSE)
     ),
     properties = c("twoclass", "multiclass", "missings", "numerics", "factors", "prob"),
-    name = "adaboostM1 Decision Trees",
+    name = "adaboostM1 boosting Trees",
     short.name = "adaboostM1",
-    note = "NAs are directly passed to WEKA with `na.action = na.pass`.",
+    note = "",
     callees = c("adaboostM1", "Weka_control")
   )
 }
 
 #' @export
 trainLearner.classif.adaboostM1 = function(.learner, .task, .subset, .weights = NULL,  ...) {
-  ctrl = RWeka::Weka_control(..., Q = as.integer(runif(1, min = -.Machine$integer.max, max = .Machine$integer.max)))
   f = getTaskFormula(.task)
-  RWeka::adaboostM1(f, data = getTaskData(.task, .subset), control = ctrl, na.action = na.pass)
+  ctrl = RWeka::Weka_control(...)
+  RWeka::AdaBoostM1(f, data = getTaskData(.task, .subset), control = ctrl, na.action = na.pass)
 }
 
 #' @export
---
+++ tests/testthat/test_base_adaboostM1.R
@@ -0,0 +1,38 @@
+context("classif_adaboostM1")
+
+test_that("classif_adaboostM1", {
+  requirePackagesOrSkip("RWeka", default.method = "load")
+
+  parset.list = list(
+    list(),
+    list(P = 100),
+    list(I = 10, S = 1),
+    list(I = 5, Q= FALSE)
+  )
+
+  old.predicts.list = list()
+  old.probs.list = list()
+
+  for (i in seq_along(parset.list)) {
+    parset = parset.list[[i]]
+    set.seed(getOption("mlr.debug.seed"))
+    ctrl = do.call(RWeka::Weka_control, parset)
+    m = RWeka::AdaBoostM1(formula = multiclass.formula, data = multiclass.train, control = ctrl)
+    p  = predict(m, newdata = multiclass.test, type = "class")
+    p2 = predict(m, newdata = multiclass.test, type = "prob")
+    old.predicts.list[[i]] = p
+    old.probs.list[[i]] = p2
+  }
+
+  testSimpleParsets("classif.adaboostM1", multiclass.df, multiclass.target, multiclass.train.inds, old.predicts.list, parset.list)
+  testProbParsets("classif.adaboostM1", multiclass.df, multiclass.target, multiclass.train.inds, old.probs.list, parset.list)
+
+  tt = function(formula, data, subset, ...) {
+    RWeka::AdaBoostM1(formula, data = data[subset, ], control = RWeka::Weka_control(...))
+  }
+
+  tp = function(model, newdata) predict(model, newdata, type = "class")
+
+  testCVParsets("classif.adaboostM1", multiclass.df, multiclass.target, tune.train = tt, tune.predict = tp, parset.list = parset.list)
+
+})
