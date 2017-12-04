a4fab788007ef7f968473a90c338c3518ef8ad3a
likunlan <li.kunlan@campus.lmu.de>
2017-12-04 23:24:30 +0100

    improvement of the variable name

--- tests/testthat/test_base_subsettask.R
+++ tests/testthat/test_base_subsettask.R
@@ -4,7 +4,7 @@ test_that("test select response labels for subsettask", {
   yeast = getTaskData(yeast.task)
   labels = colnames(yeast)[1:14]
   yeast.task = makeMultilabelTask(id = "multi", data = yeast, target = labels)
-  new_labels = colnames(yeast)[1:4]
-  subtask = subsetTask(yeast.task, labels = new_labels)
-  expect_equal(subtask$task.desc$target, new_labels)
+  new.labels = colnames(yeast)[1:4]
+  subtask = subsetTask(yeast.task, labels = new.labels)
+  expect_equal(subtask$task.desc$target, new.labels)
 })
