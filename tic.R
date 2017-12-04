b5e49eb24368d35083f29010bfbd4522e8e54886
pat-s <patrick.schratz@gmail.com>
2017-12-04 09:04:01 +0100

    add tic.R

---
+++ tic.R
@@ -0,0 +1,15 @@
+add_package_checks()
+
+if (Sys.getenv("id_rsa") != "") {
+  # pkgdown documentation can be built optionally. Other example criteria:
+  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+  # - `ci()$is_tag()`: Only for tags, not for branches
+  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown()) %>%
+    add_step(step_push_deploy())
+}
