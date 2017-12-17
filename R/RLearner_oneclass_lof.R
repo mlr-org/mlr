e1edd2e083ae31e8ada9364ae4c2f3accffec77e
Minh Le <minh-anh.le@cdtm.de>
2017-12-16 21:50:51 -0500

    add learner lof for the lof version from the newer dbscan package

--- R/RLearner_oneclass_lof.R
+++ R/RLearner_oneclass_lof.R
@@ -1,32 +1,32 @@
 #' @export
-makeRLearner.oneclass.lofactor = function() {
+makeRLearner.oneclass.lof = function() {
   makeRLearnerOneClass(
-    cl = "oneclass.lofactor",
-    package = "DMwR",
+    cl = "oneclass.lof",
+    package = "dbscan",
     par.set = makeParamSet(
       makeIntegerLearnerParam(id = "k", default = 20L, lower = 1L, tunable = TRUE)
     ),
     properties =  c("oneclass", "numerics", "factors", "prob"),
     note = "Rule of thumb for number of neighbours k = 20,
     see http://scikit-learn.org/stable/modules/outlier_detection.html.",
-    name = "LOF DMwR",
-    short.name = "lofactor",
-    callees = "lofactor"
+    name = "LOF dbscan",
+    short.name = "lof",
+    callees = "lof"
   )
 }
 
 #' @export
-trainLearner.oneclass.lofactor = function(.learner, .task, .subset, .weights = NULL, ...) {
+trainLearner.oneclass.lof = function(.learner, .task, .subset, .weights = NULL, ...) {
   z = getTaskData(.task, .subset, target.extra = TRUE)
-  DMwR::lofactor(z$data, ...)
+  dbscan::lof(z$data, ...)
 }
 
 #' @export
-predictLearner.oneclass.lofactor = function(.learner, .model, .newdata, ...) {
+predictLearner.oneclass.lof = function(.learner, .model, .newdata, ...) {
   # calculate lof, no trained model is needed
   # the lower the local density of a point -> the point is in a sparser region than its neighbors, which suggests that the point is an outlier.
   k = ifelse(!is.null(.learner$par.vals$k), .learner$par.vals$k, .model$learner$par.vals$k)
-  p.df = DMwR::lofactor(.newdata, k = k, ...)
+  p.df = dbscan::lof(.newdata, k = k, ...)
   message("lof method is unsupervised, therefore predict()-fct trains and predicts on the same data")
   #p.df.old = .model$learner.model
   #threshold = min(p.df.old)
