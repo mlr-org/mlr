999f46ced2196687ff4723c34d2ea969b8687afe
pat-s <patrick.schratz@gmail.com>
2018-01-18 20:41:31 +0100

    move spcv tests into own file

--- tests/testthat/test_base_resample_cv.R
+++ tests/testthat/test_base_resample_cv.R
@@ -18,49 +18,6 @@ test_that("cv instance works", {
   }
 })
 
-test_that("SpCV instance works", {
-
-  coords = bc.task.spatial$env$data[, 1:2]
-  df <- bc.task.spatial$env$data
-  makeClassifTask(target = "diplo01", data = df, coordinates = coords)
-
-  rin = makeResampleInstance(makeResampleDesc("SpCV", iters = 3), coords = coords)
-
-  folds = rin$desc$iters
-  expect_equal(folds, 3)
-
-  for (i in 1:folds) {
-    i1 = rin$train.inds[[i]]
-    i2 = rin$test.inds[[i]]
-    expect_true(min(i1) >= 1)
-    expect_true(max(i1) <= 944)
-    expect_true(min(i2) >= 1)
-    expect_true(max(i2) <= 944)
-    expect_equal(sort(c(unique(i1), i2)), 1:944)
-  }
-})
-
-test_that("Nested SpRepCV works without errors", {
-  bc.task.spatial
-
-  lrn.ksvm = makeLearner("classif.ksvm",
-    predict.type = "prob", kernel = "rbfdot")
-
-  ps = makeParamSet(makeNumericParam("C", lower = 1, upper = 1),
-    makeNumericParam("sigma", lower = 1, upper = 1))
-
-  ctrl = makeTuneControlRandom(maxit = 1)
-  inner = makeResampleDesc("SpCV", iters = 2)
-
-  wrapper.ksvm = makeTuneWrapper(lrn.ksvm, resampling = inner, par.set = ps,
-    control = ctrl, show.info = FALSE, measures = list(auc))
-
-  outer = makeResampleDesc("SpRepCV", folds = 2, reps = 2)
-
-  resa.svm.spatial = resample(wrapper.ksvm, bc.task.spatial,
-    resampling = outer, show.info = FALSE, measures = list(auc))
-})
-
 test_that("cv resampling works", {
   data = multiclass.df
   formula = multiclass.formula
--- tests/testthat/test_base_spcv.R
+++ tests/testthat/test_base_spcv.R
@@ -18,12 +18,11 @@ test_that("Nested SpRepCV works without errors", {
 
   outer = makeResampleDesc("SpRepCV", folds = 2, reps = 2)
 
-  library(parallelMap)
-  parallelStart(mode = "multicore", level = "mlr.tuneParams", cpus = 2)
-
-  resa.svm.spatial = mlr::resample(wrapper.ksvm, bc.task.spatial,
+  out = resample(wrapper.ksvm, bc.task.spatial,
     resampling = outer, show.info = TRUE, measures = list(auc))
-  parallelStop()
+
+  expect_vector(out$measures.test$auc, any.missing = FALSE, len = 4)
+
 })
 
 test_that("SpRepCV works without errors", {
@@ -37,5 +36,30 @@ test_that("SpRepCV works without errors", {
   out = resample(learner = learner, task = bc.task.spatial,
     resampling = resampling, measures = list(auc))
 
+  expect_vector(out$measures.test$auc, any.missing = FALSE, len = 4)
+
 })
 
+test_that("SpCV instance works", {
+
+  data(bc.task.spatial, package = "mlr", envir = environment())
+
+  coords = bc.task.spatial$env$data[, 1:2]
+  df = bc.task.spatial$env$data
+  makeClassifTask(target = "diplo01", data = df, coordinates = coords)
+
+  rin = makeResampleInstance(makeResampleDesc("SpCV", iters = 3), coords = coords)
+
+  folds = rin$desc$iters
+  expect_equal(folds, 3)
+
+  for (i in 1:folds) {
+    i1 = rin$train.inds[[i]]
+    i2 = rin$test.inds[[i]]
+    expect_true(min(i1) >= 1)
+    expect_true(max(i1) <= 944)
+    expect_true(min(i2) >= 1)
+    expect_true(max(i2) <= 944)
+    expect_equal(sort(c(unique(i1), i2)), 1:944)
+  }
+})
