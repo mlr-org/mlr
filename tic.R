0cf3ff340433848f09f2d6c60249dd325946b82c
pat-s <patrick.schratz@gmail.com>
2018-03-18 16:30:08 +0100

    force pkgdown install from pat-s

--- tic.R
+++ tic.R
@@ -10,7 +10,7 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
 
   get_stage("install") %>%
     add_code_step(if (length(find.package("magick", quiet = TRUE)) == 0) install.packages("magick")) %>%
-    add_code_step(if (length(find.package("pkgdown", quiet = TRUE)) == 0) devtools::install_github("pat-s/pkgdown@cc1579abcf00cb11bc856e48f3b9d3c91432c2c2")) %>%
+    add_code_step(devtools::install_github("pat-s/pkgdown@cc1579abcf00cb11bc856e48f3b9d3c91432c2c2")) %>%
     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
 
   #get_stage("script") %>%
