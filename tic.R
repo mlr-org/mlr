2611436528d89dda24a584d17130ea60e5ff736d
pat-s <patrick.schratz@gmail.com>
2018-03-23 17:48:19 +0100

    use cache from tutorial?

--- tic.R
+++ tic.R
@@ -12,47 +12,48 @@
 #
 # }
 
+# if (Sys.getenv("TUTORIAL") == "HTML") {
+#
+#   get_stage("install") %>%
+#     add_code_step(if (length(find.package("magick", quiet = TRUE)) == 0) install.packages("magick")) %>% # favicon creation
+#     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+#
+#   get_stage("before_deploy") %>%
+#     add_step(step_setup_ssh()) %>%
+#     add_code_step(system2("sed", c("-i","-e", '/^##/ s/#/', "-e", "'/^###/ s/#/'", "'/^####/ s/#/'", "vignettes/tutorial/devel/*.Rmd")))
+#     # this pkgdown fork includes the tweaked navbar for mlr
+#     #add_code_step(devtools::install_github("pat-s/pkgdown"))
+#
+#
+#
+#   get_stage("deploy") %>%
+#     add_step(step_build_pkgdown(lazy = TRUE)) %>%
+#     add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
+#
+# } else if (Sys.getenv("TUTORIAL") == "PDF") {
+#
+#   get_stage("install") %>%
+#     add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
+#     add_code_step(if (length(find.package("fs", quiet = TRUE)) == 0) install.packages("fs")) %>%
+#     add_code_step(if (length(find.package("rmarkdown", quiet = TRUE)) == 0) install.packages("rmarkdown")) %>%
+#     add_code_step(if (length(find.package("bookdown", quiet = TRUE)) == 0) install.packages("bookdown")) %>%
+#     add_code_step(if (length(find.package("roxygen2", quiet = TRUE)) == 0) devtools::install_github("klutometis/roxygen")) %>%
+#     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+#
+#   # this ensures that the NAMESPACE is correct. R CMD Build is not enough for the PDF build.
+#   get_stage("script") %>%
+#     add_code_step(devtools::document())
+#
+#   get_stage("before_deploy") %>%
+#     add_step(step_setup_ssh())
+#
+#   get_stage("deploy") %>%
+#     add_code_step(rmarkdown::render("vignettes/tutorial/devel/pdf/_pdf_wrapper.Rmd")) %>%
+#     add_code_step(fs::file_move("vignettes/tutorial/devel/pdf/_pdf_wrapper.pdf", "vignettes/tutorial/devel/pdf/mlr-tutorial.pdf")) %>%
+#     add_step(step_push_deploy(orphan = FALSE, path = "vignettes/tutorial/devel/pdf", branch = "master",
+#                               remote_url = "git@github.com:pat-s/mlr.git"))
+# }
 if (Sys.getenv("TUTORIAL") == "HTML") {
-
-  get_stage("install") %>%
-    add_code_step(if (length(find.package("magick", quiet = TRUE)) == 0) install.packages("magick")) %>% # favicon creation
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
-
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_code_step(system2("sed", c("-i","-e", '/^##/ s/#/', "-e", "'/^###/ s/#/'", "'/^####/ s/#/'", "vignettes/tutorial/devel/*.Rmd")))
-    # this pkgdown fork includes the tweaked navbar for mlr
-    #add_code_step(devtools::install_github("pat-s/pkgdown"))
-
-
-
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown(lazy = TRUE)) %>%
-    add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
-
-} else if (Sys.getenv("TUTORIAL") == "PDF") {
-
-  get_stage("install") %>%
-    add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
-    add_code_step(if (length(find.package("fs", quiet = TRUE)) == 0) install.packages("fs")) %>%
-    add_code_step(if (length(find.package("rmarkdown", quiet = TRUE)) == 0) install.packages("rmarkdown")) %>%
-    add_code_step(if (length(find.package("bookdown", quiet = TRUE)) == 0) install.packages("bookdown")) %>%
-    add_code_step(if (length(find.package("roxygen2", quiet = TRUE)) == 0) devtools::install_github("klutometis/roxygen")) %>%
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
-
-  # this ensures that the NAMESPACE is correct. R CMD Build is not enough for the PDF build.
-  get_stage("script") %>%
-    add_code_step(devtools::document())
-
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
-  get_stage("deploy") %>%
-    add_code_step(rmarkdown::render("vignettes/tutorial/devel/pdf/_pdf_wrapper.Rmd")) %>%
-    add_code_step(fs::file_move("vignettes/tutorial/devel/pdf/_pdf_wrapper.pdf", "vignettes/tutorial/devel/pdf/mlr-tutorial.pdf")) %>%
-    add_step(step_push_deploy(orphan = FALSE, path = "vignettes/tutorial/devel/pdf", branch = "master",
-                              remote_url = "git@github.com:pat-s/mlr.git"))
-} else if (Sys.getenv("stage") == "covr") {
   get_stage("after_success") %>%
     step_run_code(covr::codecov(quiet = FALSE))
 }
