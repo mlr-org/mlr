c39c6aad0e9020410b93f4fd3664e1df221302db
pat-s <patrick.schratz@gmail.com>
2018-03-25 23:46:29 +0200

    standard envs again

--- tic.R
+++ tic.R
@@ -1,18 +1,13 @@
 # condition on env variable
 
-if (Sys.getenv("WARMUP") == "TRUE") {
+if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("install") %>%
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
-
-  get_stage("before_script") %>%
+                                           "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
-}
-
-if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("script") %>%
     add_code_step(devtools::document()) %>% # build namespace
