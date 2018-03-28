159c957c9ac0e4d9f2923cf59b0e5aff6f20cb15
Michel Lang <michellang@gmail.com>
2018-03-28 10:51:38 +0200

    added test for stratification

--- R/ResampleInstance.R
+++ R/ResampleInstance.R
@@ -92,7 +92,7 @@ makeResampleInstance = function(desc, task, size, ...) {
       stopf("Columns specified for stratification, but not present in task: %s", collapse(stratify.cols[i]))
     index = getTaskData(task, features = stratify.cols, target.extra = FALSE)[stratify.cols]
     if (any(vlapply(index, is.double)))
-      stop("Stratification on numeric variables not possible")
+      stop("Stratification on numeric double-precision variables not possible")
     grp = tapply(seq_row(index), index, simplify = FALSE)
     grp = unname(split(seq_row(index), grp))
 
