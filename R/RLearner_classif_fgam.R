4cd17e2283d698ed4b35b424ce0117ad55fa2887
smilesun <smilesun.east@gmail.com>
2018-04-17 17:44:39 +0200

    lintr fgam

--- R/RLearner_classif_fgam.R
+++ R/RLearner_classif_fgam.R
@@ -1,9 +1,3 @@
-# @title classification of functional data by Functional Generalized Additive Models.
-#
-# @description
-# FGAM estimate a smooth function for scalar on functional regression. Where the target is a scalar and the covarite is functional covariate, and the regression is an integration with respect to a link function upon the conditional expectation. $g\{E(Y_i|X_i)\} = \theta_0 +\int_{\tau} F\{X_i(\tau), \tau \}d{\tau}$, where the smooth function $F\{X(t), t\}$(estimation surface which is fitted againt all X(t), t pair) is not binded to be linear with the functional predictor $X(t)$ and takes an additive form which could quantify how important each functional point is to the response. The basic form for the smooth function is penalized splines. Typical application for FGAM is Diffusion tensor imaging(DTI) parallel diffusivity on Corpus Callosum where signal is higher for voxels where diffusion is hindered for multiple sclerosis patient PASAT score( A cognitive measure). For each pixel, there could be a tensor of 3*3 symmetric matrix which results from applying gradient from several non-collinear directions. In FGAM, X(t) is transformed to be in the interval of [0,1] by cdf tranform to let the limited observation data to fill all space of the surface and invariant to tranformation of functional predictors. FGAM support multiple functional covariate. Currently, only splines are used as base learner.
-#
-
 #' @export
 makeRLearner.classif.fgam = function() {
   makeRLearnerClassif(
