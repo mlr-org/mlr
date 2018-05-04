e003c2f0dcc536c891cd450795411cfb1314618a
pat-s <patrick.schratz@gmail.com>
2018-05-04 17:24:06 +0200

    save state

--- R/ResampleInstances.R
+++ R/ResampleInstances.R
@@ -78,3 +78,38 @@ instantiateResampleInstance.GrowingWindowCVDesc = function(desc, size, task = NU
   makeResamplingWindow(desc, size, task, coords, "GrowingWindowCV")
 }
 
+instantiateResampleInstance.BlockingDesc = function(desc, size, task = NULL) {
+  if (!is.na(desc$iters) && desc$iters != size) {
+    warningf("iters (%i) is not equal to blocking levels (%i)! Setting iters to length of unique blocking levels.", desc$iters, size)
+    desc$iters = 1
+  } else {
+    desc$iters = 1
+  }
+  desc2 = desc
+  attr(desc2, "class") = c("CVDesc", "ResampleDesc")
+  levs = levels(task$blocking)
+  size2 = length(levs)
+  desc2$id = "cross-validation"
+  desc2$iters = length(levs)
+  # create instance for blocks
+  inst = instantiateResampleInstance(desc2, size2, task)
+  # now exchange block indices with indices of elements of this block and shuffle
+
+  #### BIS HIERHIN OK
+  # test.inds werden korrekt berechnet
+
+  test.inds = lapply(inst$test.inds, function(i) sample(which(task$blocking %in% levs[i])))
+
+  # in a nested resampling call one fold will be length(0) because we are missing one factor level
+  # we check for this and need to remove this fold
+  length.test.inds = unlist(lapply(test.inds, function(x) length(x)))
+  if (0 %in% length.test.inds) {
+    index = match(0, length.test.inds)
+    test.inds[[index]] = NULL
+  }
+
+  #test.inds = lapply(inst$train.inds, function(x) setdiff(ti, x))
+  #inst$size = size
+  #test.inds = chunk(seq_len(size), shuffle = TRUE, n.chunks = desc$iters)
+  makeResampleInstanceInternal(desc, size, test.inds = test.inds)
+}
