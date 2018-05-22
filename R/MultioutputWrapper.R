a1803ecd4872a143e6a88a2b7b375a9707c0efe1
Jiew-Quay Au <auj@STAT.UNI-MUENCHEN.DE>
2018-05-22 16:15:39 +0200

    add multioutputwrapper (work in progress!)

---
+++ R/MultioutputWrapper.R
@@ -0,0 +1,100 @@
+#' @title ADD TITLE #FIXME
+#'
+#' @description
+#' ADD DESCRIPTION #FIXME
+#'
+#' Models can easily be accessed via [getLearnerModel].
+#'
+#' @template arg_learner
+#' @template arg_multilabel_cvfolds
+#' @template ret_learner
+#' @references
+#' ADD REFRENCES #FIXME
+#' @family wrapper
+#' @family multilabel
+#' @family multiregr
+#' @family mixedoutput
+#' @export
+#' @example # add example FIXME#inst/examples/MultilabelWrapper.R
+makeMultioutputWrapper = function(learner, type = "mixedoutput", learner.lvl1, learner.meta, true.label = TRUE,
+  cv.folds = 2, chaining = FALSE, order = NULL) {
+  if (!type %in% c("mixedoutput", "multilabel", "multiregr")) stop("type must be 'mixedoutput', 'multilabel', or 'multiregr'")
+  if (!xor(missing(learner), missing(learner.lvl1) & missing(learner.meta)))
+    stop("Pass either a learner alone or learner.lvl1 together with learner.meta to predict, but not both!")
+
+  if (missing(learner)) {
+    if (xor(missing(learner.lvl1), missing(learner.meta)))
+    stop("both learner.lvl1 and learner.meta must be specified")
+  }
+
+  assertLogical(true.label)
+  if (true.label) extra.label = "trueLabel" else extra.label = "predLabel"
+
+  assertLogical(chaining)
+  if (chaining) conditioning = "chaining" else conditioning = "full"
+
+  if (type == "multilabel") {
+    learner = checkLearner(learner, type = "classif", props = "twoclass")
+    id = stri_paste("multilabel", extra.label, conditioning, getLearnerId(learner), sep = ".")
+    packs = getLearnerPackages(learner)
+    type = getLearnerType(learner)
+    x = makeHomogeneousEnsemble(id, type, learner, packs, learner.subclass = stri_paste("multilabel", extra.label, conditioning, "wrapper"),
+      model.subclass = stri_paste("multilabel", extra.label, conditioning, "model"))
+    x$type = type
+    x$cv.folds = cv.folds
+    return(x)
+  }
+}
+
+#' @export
+trainLearner.MultilabelStackingWrapper = function(.learner, .task, .subset = NULL, .weights = NULL, ...) {
+  targets = getTaskTargetNames(.task)
+  .task = subsetTask(.task, subset = .subset)
+  data = getTaskData(.task)
+  # train level 1 learners
+  models.lvl1 = getLearnerModel(train(makeMultilabelBinaryRelevanceWrapper(.learner$next.learner), .task, weights = .weights))
+  # predict labels
+  f = function(tn) {
+    data2 = dropNamed(data, setdiff(targets, tn))
+    ctask = makeClassifTask(id = tn, data = data2, target = tn)
+    rdesc = makeResampleDesc("CV", iters = .learner$cv.folds)
+    r = resample(.learner$next.learner, ctask, rdesc, weights = .weights, show.info = FALSE)
+    as.numeric(as.logical(r$pred$data[order(r$pred$data$id), ]$response)) #did not use getPredictionResponse, because of ordering
+  }
+  pred.labels = sapply(targets, f)
+  # train meta level learners
+  g = function(tn) {
+    data.meta = dropNamed(data.frame(data, pred.labels), setdiff(targets, tn))
+    ctask = makeClassifTask(id = tn, data = data.meta, target = tn)
+    train(.learner$next.learner, ctask, weights = .weights)
+  }
+  models.meta = lapply(targets, g)
+  makeHomChainModel(.learner, c(models.lvl1, models.meta))
+}
+
+#' @export
+predictLearner.MultilabelStackingWrapper = function(.learner, .model, .newdata, .subset = NULL, ...) {
+  models = getLearnerModel(.model, more.unwrap = FALSE)
+  # Level 1 prediction (binary relevance)
+  models.lvl1 = models[seq_along(.model$task.desc$target)]
+  f = if (.learner$predict.type == "response") {
+    function(m) as.logical(getPredictionResponse(predict(m, newdata = .newdata, subset = .subset, ...)))
+  } else {
+    function(m) getPredictionProbabilities(predict(m, newdata = .newdata, subset = .subset, ...), cl = "TRUE")
+  }
+  if (.learner$predict.type == "response") {
+    pred.lvl1 = sapply(data.frame(asMatrixCols(lapply(models.lvl1, f))), as.numeric)
+  } else {
+    pred.lvl1 = data.frame(asMatrixCols(lapply(models.lvl1, f)))
+  }
+  colnames(pred.lvl1) = paste(.model$task.desc$target, ".1", sep = "")
+  # Meta level prediction
+  models.meta = models[(length(.model$task.desc$target) + 1):(2 * length(.model$task.desc$target))]
+  nd = data.frame(.newdata, pred.lvl1)
+  g = if (.learner$predict.type == "response") {
+    function(m) as.logical(getPredictionResponse(predict(m, newdata = nd, subset = .subset, ...)))
+  } else {
+    function(m) getPredictionProbabilities(predict(m, newdata = nd, subset = .subset, ...), cl = "TRUE")
+  }
+  asMatrixCols(lapply(models.meta, g), col.names = .model$task.desc$target)
+}
