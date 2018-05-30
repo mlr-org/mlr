61757509b680da5150e7db1c53bbb699c4571f0c
pat-s <patrick.schratz@gmail.com>
2018-05-30 10:38:58 +0200

    also support getFeatSelResult

--- R/getResamplingIndices.R
+++ R/getResamplingIndices.R
@@ -2,8 +2,8 @@
 #'
 #' @description
 #' After you resampled a tuning wrapper (see [makeTuneWrapper])
-#' with `resample(..., extract = getTuneResult)` this helper returns a `list` with
-#' the resampling indices used for tuning..
+#' with `resample(..., extract = getTuneResult)` or `resample(..., extract = getFeatSelResult)` this helper returns a `list` with
+#' the resampling indices used for the respective method.
 #'
 #' @param object ([ResampleResult]) \cr
 #'   The result of resampling of a tuning wrapper.
@@ -18,8 +18,9 @@ getResamplingIndices = function(object, inner = FALSE) {
   assertClass(object, "ResampleResult")
   assertList(object$extract)
   if (inner == TRUE) {
-    if (class(object$extract[[1]])[1] != "TuneResult") {
-      stopf("No object of class 'TuneResult' found in slot 'extract'. Did you run 'resample()' with 'extract = getTuneResult'?")
+    if (!inherits(object$extract[[1]], c("TuneResult", "FeatSelResult"))) {
+      stopf("No object of class 'TuneResult' or 'FeatuSelResult' found in slot 'extract'.
+             Did you run 'resample()' with 'extract = getTuneResult' or 'extract = getFeatSelResult'?")
     }
     lapply(object$extract, function(x) x$resampling[c("train.inds", "test.inds")])
   } else {
