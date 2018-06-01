6145f36ea30895c86f26532aeb07bda7a9256e88
pat-s <patrick.schratz@gmail.com>
2018-06-01 17:49:17 +0200

    typo

--- tests/testthat/test_base_resample_getResamplingIndices.R
+++ tests/testthat/test_base_resample_getResamplingIndices.R
@@ -1,6 +1,6 @@
 context("resample_cv")
 
-test_that("getResamplingIndices works correctly", {
+test_that("getResamplingIndices works with getTuneResult", {
 
   task = makeClassifTask(data = iris, target = "Species")
   lrn = makeLearner("classif.rpart")
@@ -18,6 +18,26 @@ test_that("getResamplingIndices works correctly", {
 
   # nested resampling for evaluation
   r = resample(lrn, task, outer, extract = getTuneResult)
+
+  # check outer indices
+  expect_equal(length(getResamplingIndices(r)$train.inds[[1]]), 75)
+
+  # check if inner test.inds are retrieved correctly
+  expect_length(unique(unlist(getResamplingIndices(r, inner = TRUE)[[1]]$test.inds)), 25)
+})
+
+test_that("getResamplingIndices works with getFeatSelResult", {
+
+  outer = makeResampleDesc("CV", iters = 2L)
+  inner = makeResampleDesc("Holdout")
+
+  lrn1 = makeLearner("classif.rpart")
+  ctrl = makeFeatSelControlRandom(maxit = 3)
+  lrn2 = makeFeatSelWrapper(lrn1, resampling = inner, control = ctrl)
+
+  r = resample(lrn2, multiclass.task, outer, extract = function(model) {
+    getFeatSelResult(model)
+  })
 
   # check outer indices
   expect_equal(length(getResamplingIndices(r)$train.inds[[1]]), 75)
