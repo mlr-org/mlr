4ff9b5914c102a36ba0c9677aad3720759cd6a59
pat-s <patrick.schratz@gmail.com>
2018-07-06 17:31:53 +0200

    devtools::update_packages

--- tic.R
+++ tic.R
@@ -7,6 +7,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
                                            "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
+    add_code_step(devtools::update_packages(TRUE)) %>%
     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
 
   get_stage("before_script") %>%
