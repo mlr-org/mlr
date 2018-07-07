3591f3cee837a0603c3acf009f41e71c160d2d2f
pat-s <patrick.schratz@gmail.com>
2018-07-07 10:53:44 +0200

    add createSpatialResamplingPlots.R

---
+++ tests/testthat/test_base_createSpatialResamplingPlots.R
@@ -0,0 +1,17 @@
+test_that("test createSpatialResamplingPlots() creates 10 ggplot objects", {
+
+  data(spatial.task, package = "mlr", envir = environment())
+
+  # take more reps to see if the restriction on two reps works
+  rdesc = makeResampleDesc("SpRepCV", folds = 5, reps = 3)
+  r = resample(makeLearner("classif.qda"), spatial.task, rdesc)
+
+  plots = createSpatialResamplingPlots(spatial.task, r, crs = 32717,
+    repetitions = 2, x.axis.breaks = c(-79.065, -79.085),
+    y.axis.breaks = c(-3.970, -4))
+
+  expect_class(plots[[1]][[1]], "gg")
+  expect_length(plots[[1]], 10)
+
+})
+
