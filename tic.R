b7858d8a84b95c2a6eb09887f613c432345e73bb
pat-s <patrick.schratz@gmail.com>
2018-07-10 17:51:13 +0200

    trying to fix check stage

--- tic.R
+++ tic.R
@@ -19,7 +19,6 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
     add_step(step_setup_ssh())
 
 get_stage("script") %>%
-    add_code_step(devtools::install_github("r-lib/rcmdcheck")) %>%
     add_code_step(devtools::document()) %>%
     add_step(step_rcmdcheck(notes_are_errors = FALSE))
 
