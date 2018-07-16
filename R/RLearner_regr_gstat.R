ee35d438ff4d879d073309163b92341de32f6f2e
pokyah <thomas.goossens@protonmail.com>
2018-07-16 08:39:22 +0000

    cleaning code indentations, typo, mlr style guidelines and removing old code comments

--- R/RLearner_regr_gstat.R
+++ R/RLearner_regr_gstat.R
@@ -1,18 +1,18 @@
 #' @export
-makeRLearner.regr.gstat = function(){#https://www.rdocumentation.org/packages/gstat/versions/1.1-6/topics/fit.variogram
+makeRLearner.regr.gstat = function() {
   makeRLearnerRegr(
     cl = "regr.gstat",
     package = "gstat",
-    par.set = makeParamSet(#https://www.rdocumentation.org/packages/ParamHelpers/versions/1.8/topics/LearnerParam
+    par.set = makeParamSet(
       makeFunctionLearnerParam(id = "g"),
-      makeUntypedLearnerParam(id = "id"),#FIXME
-      makeUntypedLearnerParam(id = "locations"),#FIXME
-      makeUntypedLearnerParam(id = "model", default = NULL),#FIXME what should be the type
+      makeUntypedLearnerParam(id = "id"), # FIXME what should be the type ?
+      makeUntypedLearnerParam(id = "locations"), # FIXME what should be the type ?
+      makeUntypedLearnerParam(id = "model", default = NULL), # FIXME what should be the type ?
       makeIntegerVectorLearnerParam(id = "beta"),
       makeIntegerLearnerParam(id = "nmax", default = 0),
       makeIntegerLearnerParam(id = "nmin", default = 0),
       makeIntegerLearnerParam(id = "omax", default = 0),
-      makeNumericLearnerParam(id = "maxdist", default = 100000000000),#FIXME should be Inf
+      makeNumericLearnerParam(id = "maxdist", default = Inf),
       makeLogicalLearnerParam(id = "force", default = FALSE),
       makeLogicalLearnerParam(id = "dummy", default = FALSE),
       makeUntypedLearnerParam(id = "set"),
@@ -20,40 +20,38 @@ makeRLearner.regr.gstat = function(){#https://www.rdocumentation.org/packages/gs
       makeLogicalLearnerParam(id = "fill.all", default = FALSE),
       makeLogicalLearnerParam(id = "fill.cross", default = TRUE),
       makeDiscreteLearnerParam(id = "variance", values = c("identity", "mu", "mu(1-mu)"), default = "identity"),
-      #makeNumericVectorLearnerParam(id = "weights", default = NULL),#FIXME
       makeUntypedLearnerParam(id = "merge"),
       makeIntegerLearnerParam(id = "degree", default = 0),
       makeLogicalLearnerParam(id = "vdist", default = FALSE),
       makeUntypedLearnerParam(id = "lambda")
     ),
-    par.vals = list(model = NULL, nmax = 0, nmin=0, omax=0, maxdist=Inf, force=FALSE, dummy=FALSE, fill.all=FALSE, fill.cross=TRUE, variance="identity", degree=0, vdist=FALSE),
+    par.vals = list(model = NULL, nmax = 0, nmin = 0, omax = 0, maxdist = Inf, force = FALSE,
+      dummy = FALSE, fill.all = FALSE, fill.cross = TRUE, variance = "identity", degree = 0, vdist = FALSE),
     properties = c("numerics", "factors" , "se", "weights", "missings"),
     name = "Multivariable Geostatistical Prediction And Simulation",
     short.name = "gstat",
     note = "To make the learner work, you cannot use the standard krige interface from gstat.\n
-    You must fisrt create a gstat object. The train function handles it for you.\n
-    More here : https://stackoverflow.com/questions/13920342/how-to-make-ordinary-kriging-by-using-gstat-predict.\n
+    You must first create a gstat object. The train function handles it for you.\n
+    More information here : https://stackoverflow.com/questions/13920342/how-to-make-ordinary-kriging-by-using-gstat-predict.\n
     The learner handles gstat variogram autofitting functionnality presented in this post https://www.r-spatial.org/r/2016/02/14/gstat-variogram-fitting.html.\n
-    Manual fitting : lrn.man = makeLearner(cl = 'regr.gstat', id= 'manual', model = list(psill=1, model='Sph', range=900, nugget=1), locations = ~x+y).\n
-    Automatic fitting : lrn.auto = makeLearner(cl = 'regr.gstat', id= 'auto', model = list(psill=c('Sph','Exp','Gau', 'Mat')), locations = ~x+y)"
+    Manual fitting example : lrn.man = makeLearner(cl = 'regr.gstat', id = 'manual', model = list(psill = 1, model = 'Sph', range = 900, nugget = 1), locations = ~x+y).\n
+    Automatic fitting example : lrn.auto = makeLearner(cl = 'regr.gstat', id = 'auto', model = list(psill = c('Sph','Exp','Gau', 'Mat')), locations = ~x+y)"
   )
 }
 
 #' @export
 trainLearner.regr.gstat = function(.learner, .task, .subset, .weights = NULL, model = NULL, ...) {
-  #browser()
-  # data
   d = getTaskData(.task, .subset)
-  #sp::coordinates(d) = ~x+y
-  # formula
   f <- getTaskFormula(.task, explicit.features = TRUE)
-  # remove location vars as handled by gstat - https://stackoverflow.com/questions/40308944/removing-offset-terms-from-a-formula
+  # remove location vars as they are handled by gstat - https://stackoverflow.com/questions/40308944/removing-offset-terms-from-a-formula
   f <- update(f, .~.-y-x)
-  if(!is.null(model)){
-    # variogram
-    v = variogram(object = f, locations = ~x+y, data =d)
-    fit <- gstat::fit.variogram(v, gstat::vgm(psill=model$psill, model=model$model, range=model$range, nugget = model$nugget))
-    # plot(v, fit)
+  # check if a variogram model is passed
+  if (!is.null(model)) {
+    # build the samples variogram
+    v = variogram(object = f, locations = ~x+y, data = d)
+    # fit the variogram model
+    fit <- gstat::fit.variogram(v, gstat::vgm(psill = model$psill, model = model$model,
+      range = model$range, nugget = model$nugget))
     # create the gstat object
     g <- gstat::gstat(
       formula = f,
@@ -61,7 +59,7 @@ trainLearner.regr.gstat = function(.learner, .task, .subset, .weights = NULL, mo
       model = fit,
       ...
     )
-  }else{
+  } else {
     g <- gstat::gstat(
       formula = f,
       data = d,
@@ -70,10 +68,9 @@ trainLearner.regr.gstat = function(.learner, .task, .subset, .weights = NULL, mo
   }
 }
 
-#' @export #multivariate prediction https://www.rdocumentation.org/packages/gstat/versions/1.0-2/topics/predict.gstat
+#' @export
 predictLearner.regr.gstat = function(.learner, .model, .newdata, ...) {
-  #browser()
-  p = predict( # https://www.rdocumentation.org/packages/gstat/versions/1.0-2/topics/predict
+  p = predict(
     object = .model$learner.model,
     newdata = .newdata
   )
