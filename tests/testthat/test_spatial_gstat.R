859b2e601b3406cfc2494e704d7b71537911e543
pokyah <thomas.goossens@protonmail.com>
2018-07-17 11:53:51 +0000

    removing list(id = "inverse_distance_weighted") as is this the same as the default behaviour without any extra param

--- tests/testthat/test_spatial_gstat.R
+++ tests/testthat/test_spatial_gstat.R
@@ -2,11 +2,10 @@ test_that("regr_gstat", {
   requirePackagesOrSkip("gstat", default.method = "load")
 
   parset.list = list(
-    list(),
+    list(), # = list(id = "inverse_distance_weighted"),
     list(id = "trend_surfaces_degree_1", degree = 1),
     list(id = "trend_surfaces_degree_2", degree = 2),
     list(id = "trend_surfaces_degree_3", degree = 3),
-    list(id = "inverse_distance_weighted"),
     list(id = "ordinary_kriging",
       model = list(psill = 1, model = "Sph", range = 900, nugget = 1))
   )
