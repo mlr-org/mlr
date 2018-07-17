849642f5f5c7d54899fe65cd49cdb7ece190d64c
pokyah <thomas.goossens@protonmail.com>
2018-07-17 11:44:34 +0000

    predictions working for old.predicts.list in test_spatial_gstat.R. Still bugging in testSimpleParsets

--- tests/testthat/test_spatial_gstat.R
+++ tests/testthat/test_spatial_gstat.R
@@ -2,30 +2,41 @@ test_that("regr_gstat", {
   requirePackagesOrSkip("gstat", default.method = "load")
 
   parset.list = list(
-    list(),
+    #list(),
     list(id = "trend_surfaces_degree_1", degree = 1, locations = ~x+y),
     list(id = "trend_surfaces_degree_2", degree = 2, locations = ~x+y),
     list(id = "trend_surfaces_degree_3", degree = 3, locations = ~x+y),
     list(id = "inverse_distance_weighted", locations = ~x+y),
-    list(id = "ordinary_kriging", predict.type = "response",
-      model = list(psill = 1, model = "Sph", range = 900, nugget = 1), locations = ~x+y),
-    list(id = "universal_kriging", predict.type = "response",
-      model = list(psill = 1, model = "Exp", range = 300, nugget = 1), locations = ~x+y)
+    list(id = "ordinary_kriging",
+      model = list(psill = 1, model = "Sph", range = 900, nugget = 1), locations = ~x+y)
   )
 
   old.predicts.list = list()
+
+  # https://stackoverflow.com/questions/13920342/how-to-make-ordinary-kriging-by-using-gstat-predict
 
   for (i in 1:length(parset.list)) {
     parset = parset.list[[i]]
     pars = list(formula = meuse.formula, data = meuse.train)
     pars = c(pars, parset)
     set.seed(getOption("mlr.debug.seed"))
+
+    if (!is.null(pars$model)) {
+      # build the samples variogram
+      v = gstat::variogram(object = pars$formula, locations = pars$locations, data = pars$data)
+      # fit the variogram model
+      fit = gstat::fit.variogram(object = v, gstat::vgm(psill = pars$model$psill, model = pars$model$model,
+        range = pars$model$range, nugget = pars$model$nugget))
+      pars = list(formula = pars$formula, data = pars$data, locations = pars$locations, model = fit)
+    }
     m = do.call(gstat::gstat, pars)
     set.seed(getOption("mlr.debug.seed"))
-    p = predict(m, task = meuse.train, type = "response")
+    p = predict(m, newdata = meuse.grid)
     old.predicts.list[[i]] = p
+    browser()
   }
 
-  testSimpleParsets(t.name = "spatial.gstat", df = meuse.df, target = meuse.target,
+
+  testSimpleParsets(t.name = "regr.gstat", df = meuse.df, target = meuse.target,
     train.inds = meuse.train.inds, old.predicts.list, parset.list)
 })
