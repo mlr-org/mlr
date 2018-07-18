e6b3275c3ed28ad3e667fdba4e69aed902edc98b
pokyah <thomas.goossens@protonmail.com>
2018-07-18 08:56:16 +0000

    renaming meuse related vars in helper_objects.R

--- tests/testthat/helper_objects.R
+++ tests/testthat/helper_objects.R
@@ -27,12 +27,12 @@ binaryclass.spatial.class.col = 3
 binaryclass.spatial.class.levs = levels(binaryclass.spatial.df[, binaryclass.spatial.class.col])
 binaryclass.spatial.task = makeClassifTask("binary", data = binaryclass.spatial.df, target = binaryclass.spatial.target, coordinates = coordinates)
 
-meuse.df = meuse.task$env$data
+meuse = meuse.task$env$data
 meuse.target = "cadmium"
-meuse.train.inds = (1:(nrow(meuse.df)/2))
-meuse.train = meuse.df[meuse.train.inds, ]
-meuse.test.inds = (length(meuse.train.inds) + 1):nrow(meuse.df)
-meuse.test = meuse.df[meuse.test.inds, ]
+meuse.train.inds = (1:(nrow(meuse)/2))
+meuse.train = meuse[meuse.train.inds, ]
+meuse.test.inds = (length(meuse.train.inds) + 1):nrow(meuse)
+meuse.test = meuse[meuse.test.inds, ]
 # meuse.trendsurfaces.task = dropFeatures(task = meuse.task, features = getTaskFeatureNames(meuse.task)[-c(1,2)])
 # meuse.idw.task = trendsurfaces.meuse.task
 # meuse.ordinarykriging.task = trendsurfaces.meuse.task
