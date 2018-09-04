62b90c7fa40d1819c8225afd49692d8e9f024adf
pokyah <thomas.goossens@protonmail.com>
2018-09-04 13:39:11 +0000

    writing the test to work with forced location

--- tests/testthat/test_spatial_regr_gstat.R
+++ tests/testthat/test_spatial_regr_gstat.R
@@ -2,6 +2,7 @@
 # devtools::test(filter = "gstat")
 # devtools::document()
 # devtools::load_all()
+# devtools::test(filter = "spatial_regr_gstat")
 # https://stackoverflow.com/questions/31548796/debugging-testthat-tests-in-rstudio
 # sink(NULL)
 # https://github.com/r-lib/devtools/issues/1675 - Error in x[[method]](...) : attempt to apply non-function
@@ -13,14 +14,14 @@ test_that("regr_gstat", {
   requirePackagesOrSkip("gstat", default.method = "load")
 
   parset.list = list(
-    list(id = "ordinary_kriging_manual",
-     psill = 1, model.manual = "Sph", range = 900, nugget = 2),
-    list(id = "ordinary_kriging_auto",
-     model.auto = c('Sph','Exp','Gau', 'Mat')) # was psill
-    # list(id = "inverse_distance_weighted"),
-    # list(id = "trend_surfaces_degree_1", degree = 1),
-    # list(id = "trend_surfaces_degree_2", degree = 2),
-    # list(id = "trend_surfaces_degree_3", degree = 3)
+     list(id = "ordinary_kriging_manual",
+      psill = 1, model.manual = "Sph", range = 900, nugget = 2),
+     list(id = "ordinary_kriging_auto",
+      model.auto = c('Sph','Exp','Gau', 'Mat')) ,# was psill
+     list(id = "inverse_distance_weighted"),
+    list(id = "trend_surfaces_degree_1", degree = 1),
+    list(id = "trend_surfaces_degree_2", degree = 2),
+    list(id = "trend_surfaces_degree_3", degree = 3)
   )
 
   old.predicts.list = list()
@@ -28,37 +29,51 @@ test_that("regr_gstat", {
   # https://stackoverflow.com/questions/13920342/how-to-make-ordinary-kriging-by-using-gstat-predict
 
   for (i in 1:length(parset.list)) {
-    #browser()
     parset = parset.list[[i]]
-    pars = list(formula = meuse.formula, data = meuse.train)
+    pars = list(formula = meuse.formula, data = meuse.train, locations = ~x+y)
     pars = c(pars, parset)
-    pars$locations = ~x+y
-
-    # if (is.null(parset$degree) && is.null(parset$psill)) {
-    #
-    #
-    # }
 
     set.seed(getOption("mlr.debug.seed"))
 
     if (!is.null(pars$model.manual) || !is.null(pars$model.auto)) {
-      # build the samples variogram
-      v = gstat::variogram(object = pars$formula, data = pars$data, locations = pars$locations)
-      # if auto-fitting
+      pars$formula = update(pars$formula, .~.-y-x)
+      #browser()
+      # calculate sample variogram
+      v = gstat::variogram(
+        object = pars$formula,
+        data = pars$data,
+        locations = pars$locations
+      )
+      # Check for auto-fitting
       if (!is.null(pars$model.auto)) {
         pars$psill = pars$model.auto
       }
+      # generate the variogram model (https://www.rdocumentation.org/packages/gstat/versions/1.1-6/topics/vgm)
+      model = gstat::vgm(
+        psill = pars$psill,
+        model = pars$model.manual,
+        range = pars$range,
+        nugget = pars$nugget
+      )
       # fit the variogram model
-      fit = gstat::fit.variogram(object = v, gstat::vgm(psill = pars$psill, model = pars$model.manual,
-        range = pars$range, nugget = pars$nugget))
+      fit = gstat::fit.variogram(
+        object = v,
+        model = model
+      )
       pars = list(formula = pars$formula, data = pars$data, locations = pars$locations, model = fit)
+    } else {
+        pars$formula = update(pars$formula, .~1) # https://stackoverflow.com/questions/18070131/update-formula-in-r
+        pars$model = NULL
     }
-    m = do.call(gstat::gstat, pars)
-    set.seed(getOption("mlr.debug.seed"))
-    p = predict(m, newdata = meuse.test)
+    browser()
+    g = do.call(
+      gstat::gstat,
+      pars
+    )
+    p = predict(g, newdata = meuse.test)
     old.predicts.list[[i]] = p[,3]
   }
-  #browser()
+  browser()
   #testSimpleParsets(t.name = "regr.gstat", df = meuse.task$env$data, target = meuse.target,
   testSimpleParsets(t.name = "regr.gstat", df = meuse.df, target = meuse.target,
     train.inds = meuse.train.inds, old.predicts.list = old.predicts.list, parset.list = parset.list)
