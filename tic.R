5ece21b2faa6a2de404d1c145be7934dd459af5a
pat-s <patrick.schratz@gmail.com>
2018-09-09 15:22:04 +0200

    use latest tic version

--- tic.R
+++ tic.R
@@ -7,9 +7,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(devtools::update_packages(TRUE)) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
-    add_code_step(devtools::install_github("pat-s/rcmdcheck@build-args")) %>% # FIXME: If this is solved in r-lib/rcmdcheck
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
   get_stage("before_script") %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
@@ -18,10 +16,10 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-get_stage("script") %>%
-    add_code_step(devtools::document()) %>%
-    add_step(step_rcmdcheck(notes_are_errors = FALSE, build_args = "--no-build-vignettes",
-                            check_args = "--ignore-vignettes --no-manual --as-cran"))
+# get_stage("script") %>%
+#     add_code_step(devtools::document()) %>%
+#     add_step(step_rcmdcheck(notes_are_errors = FALSE, build_args = "--no-build-vignettes",
+#                             check_args = "--ignore-vignettes --no-manual --as-cran"))
 }
 
 if (Sys.getenv("TUTORIAL") == "HTML") {
@@ -33,9 +31,9 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
                                            "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
   get_stage("install") %>%
-    add_code_step(if (length(find.package("magick", quiet = TRUE)) == 0) install.packages("magick")) %>% # favicon creation
-    add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+    add_step(step_install_cran("magick")) %>% # favicon creation
+    add_step(step_install_cran("pander")) #%>%
+    #add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
 
   get_stage("after_script") %>%
     add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
