a2dc7e8258f01ac331b9e0eda842588ff7f04eea
pat-s <patrick.schratz@gmail.com>
2018-09-11 11:42:23 +0200

    add *

--- tic.R
+++ tic.R
@@ -26,7 +26,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
       add_step(step_setup_ssh())
 
     get_stage("deploy") %>%
-      add_step(step_push_deploy(commit_paths = c("NEWS", "man/", "NAMESPACE")))
+      add_step(step_push_deploy(commit_paths = c("NEWS", "man/*", "NAMESPACE")))
   }
 }
 
@@ -52,7 +52,7 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
       add_step(step_setup_ssh())
 
     get_stage("deploy") %>%
-      add_step(step_push_deploy(commit_paths = "docs/"))
+      add_step(step_push_deploy(commit_paths = "docs/*"))
 
   }
 }
