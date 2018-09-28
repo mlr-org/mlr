31900ffb72bec1038fafc5c236adad4f3ce6448c
Patrick Schratz <patrick.schratz@gmail.com>
2018-09-28 17:43:15 +0200

    CI: Fix appveyor, test PDF manual (#2438)
    * bernd is the cre ;)
    
    * require latex
    
    * Deploy from Travis build 12570 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/431379030
    Commit: 37690368ca35f7db89dfdd36f4e387458f13c6b7
    
    * check pdf
    
    * use rtools on appveyor
    
    * Deploy from Travis build 12576 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/431876346
    Commit: 5bdaad2a6ccab0b8900ab9efdca85c4e6df70189
    
    * add latex to appveyor
    
    * update pandoc cache
    
    * remove empty directory
    
    * add src files to .Rbuildignore
    
    * Deploy from Travis build 12578 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432185967
    Commit: 541d71879d38c361aaca3eec72e6aabaef2e2fe6
    
    * Deploy from Travis build 12579 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432191561
    Commit: c1d7c352333f371a3bcec37b2ec88f235f1cb361
    
    * Deploy from Travis build 12581 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432204745
    Commit: 572f1b734ce2f02dc7ecfca43a12cbf830a21f4c
    
    * use 2017 image
    
    * set c++ flag
    
    * create makevars dir
    
    * escape
    
    * create file
    
    * fix file.create
    
    * next fix
    
    * dont build vignettes
    
    * Deploy from Travis build 12582 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432227385
    Commit: 8cbe430f39d61e53ad20a05ee0117a35b143e064
    
    * set makevars
    
    * Deploy from Travis build 12583 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432229044
    Commit: 086f8ef5869d42599197e22b18521982be351ca9
    
    * Deploy from Travis build 12584 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432232330
    Commit: 00a955cd150d66e8395afa53c65349f71fdf0d7e
    
    * no rtools
    
    * Deploy from Travis build 12585 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432235399
    Commit: fd44c6738de842b28da154e1b479ca7a25e19fe6
    
    * Deploy from Travis build 12586 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432239094
    Commit: fb898615faae9f91c0db892a472f9741caec06e8
    
    * Deploy from Travis build 12589 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432244196
    Commit: 19c1aa199cfadfb8336775eae254857236b6bb3e
    
    * next makevars attempt
    
    * try makevars.win
    
    * Deploy from Travis build 12606 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432685495
    Commit: 4850153887fe620d939dc3241986989e8e0a768d
    
    * Deploy from Travis build 12607 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432695490
    Commit: e22a09b7a51d3de1cb4b0259633f42bb9889a71f
    
    * next cxx_std attempt
    
    * next makevars attempt
    
    * Deploy from Travis build 12608 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432808781
    Commit: caa300a044e779ca145b69bbee823d34f85128db
    
    * Deploy from Travis build 12609 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432813323
    Commit: 7540dd1cf50df4ab192fbcb436b513cf907d1cea
    
    * build pkg manually
    
    * manual approach
    
    * fix arg name
    
    * find path
    
    * Deploy from Travis build 12616 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/432939767
    Commit: 75f0df9b64460e7d4a5ff5a16e1c3e31b111ceda
    
    * add comment
    
    * update docs
    
    * logo_navbar
    
    * Deploy from Travis build 12618 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/433010681
    Commit: c319147fe662ed00c5e3365eda6ff786ac29f118
    
    * Deploy from Travis build 12617 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/433007650
    Commit: 42ecb33d6aec0d83def0916d86855322e2c89950
    
    * merge docs
    
    * Deploy from Travis build 12643 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/434329962
    Commit: 57eacd2eaf0e266ac0d46a02115a8659589fd330

--- tic.R
+++ tic.R
@@ -18,7 +18,10 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("script") %>%
     add_code_step(devtools::document()) %>%
-    add_step(step_rcmdcheck(notes_are_errors = FALSE))
+    # manual approch until https://github.com/r-lib/rcmdcheck/issues/83#issuecomment-424314978 is solved
+    add_code_step(devtools::build(manual = TRUE)) %>%
+    add_code_step(rcmdcheck::rcmdcheck(path = "../mlr_2.13.9000.tar.gz", args = "--as-cran",
+                                       error_on = "error"))
 
   if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
 
