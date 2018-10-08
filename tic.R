ddca7e7197974e3e7cfa22f48b76808578a53d45
Patrick Schratz <patrick.schratz@gmail.com>
2018-10-08 21:00:22 +0200

    CI: Fix appveyor, XMeans only on Travis, smarten remotes/devtools (#2447)
    * clean appveyor.yml
    
    * requireNamespace
    
    * Xmeans only on travis
    
    * fix (
    
    * Deploy from Travis build 12696 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438083633
    Commit: fefbbeda1da2e3b1c391a4737b832c35ec026f53
    
    * Deploy from Travis build 12700 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438097740
    Commit: 4fbefaeaf369f3468a8caa7f13bb6ecbc9dfd2c6
    
    * merge master
    
    * Deploy from Travis build 12708 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438125115
    Commit: fafb183d6917987e8d9aac31ff269b5e2221ca3b
    
    * update github PAT
    
    * docs
    
    * verbose warmuppkgs
    
    * install testthat
    
    * try installing stringi as binary
    
    * fix
    
    * next binary installation try
    
    * next try preventing source install
    
    * next try
    
    * use fork
    
    * Deploy from Travis build 12718 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438221320
    Commit: 1a36859330555707f2bc37711e0643e32185dfcb
    
    * Deploy from Travis build 12720 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438225376
    Commit: 20843a5343208fcd4b17f8b3e37a2933d95bbda4
    
    * Deploy from Travis build 12722 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438230871
    Commit: 675562b7871d8c1ab0b81fcfcc7320adefd7ab47
    
    * use fork in all stages
    
    * merge docs
    
    * testthat should be automatically installed again
    
    * Deploy from Travis build 12730 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438706860
    Commit: 269df878b6a319358d60e9ccde2b0cc95ba03c40
    
    * Deploy from Travis build 12735 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/438739764
    Commit: 3ef4ce7503dddc030bc7b4690b9d37c444bafbce

--- tic.R
+++ tic.R
@@ -8,20 +8,26 @@ get_stage("after_script") %>%
 if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("install") %>%
-    add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
-      install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
+    add_step(step_install_cran("stringi", type = "both")) %>%
+    add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0) {
+      paste0("Installing WARMUPPKGS", trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
+      install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
+    }
+    ) %>%
     add_code_step(devtools::update_packages(TRUE))
 
-  get_stage("before_script") %>%
-    add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
+  if (inherits(ci(), "TravisCI")) {
+    get_stage("before_script") %>%
+      add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
+                                             "-install-package", "thirdparty/XMeans1.0.4.zip")))
+  }
 
   get_stage("script") %>%
     add_code_step(devtools::document()) %>%
     # manual approch until https://github.com/r-lib/rcmdcheck/issues/83#issuecomment-424314978 is solved
     add_code_step(devtools::build(manual = TRUE)) %>%
-    add_code_step(rcmdcheck::rcmdcheck(path = "../mlr_2.13.9000.tar.gz", args = "--as-cran",
-                                       error_on = "error"))
+    add_step(step_rcmdcheck(path = "../mlr_2.13.9000.tar.gz", args = "--as-cran",
+                            error_on = "error"))
 
   if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
 
