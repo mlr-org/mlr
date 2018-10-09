33e383ea6a30738fe664d73b9a54bf1164b2a048
PhilippPro <philipp_probst@gmx.de>
2018-10-09 15:05:02 +0200

    delete one s for kaplanmeier

--- R/RLearner_surv_kaplanmeier.R
+++ R/RLearner_surv_kaplanmeier.R
@@ -29,7 +29,7 @@ predictLearner.surv.kaplanmeier = function(.learner, .model, .newdata, ...) {
     # fill something arbitrary for preds
     preds = rep(median(.model$learner.model$times), nrow(.newdata))
     train.times = sort(unique(c(0, .model$learner.model$times)))
-    survest = stepfun(.model$learner.model$times, c(1, .model$learner.model$surv))
+    survest = stepfun(.model$learner.model$time, c(1, .model$learner.model$surv))
     prob = survest(train.times)
     probs = matrix(rep(prob, nrow(.newdata)), byrow = T, nrow = nrow(.newdata))
     list(preds = preds, probs = probs, train.times = train.times)
