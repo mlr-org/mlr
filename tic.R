56eda371520a2ebc485fd2af909c8329e6cf368b
pat-s <patrick.schratz@gmail.com>
2018-10-22 00:41:01 +0200

    typo

--- tic.R
+++ tic.R
@@ -24,7 +24,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("script") %>%
     add_code_step(devtools::document()) %>%
-    add_step(step_rcmdcheck("--as-cran", warning_are_errors = FALSE, notes_are_errors = FALSE))
+    add_step(step_rcmdcheck("--as-cran", warnings_are_errors = FALSE, notes_are_errors = FALSE))
 
   if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
 
