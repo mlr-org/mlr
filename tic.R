a54fd8dd98d6b471e501685bf2d643066307c965
pat-s <patrick.schratz@gmail.com>
2018-10-23 15:01:28 +0200

    require digest

--- tic.R
+++ tic.R
@@ -9,6 +9,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("install") %>%
     add_step(step_install_cran("stringi", type = "both")) %>%
+    add_step(step_install_cran("digest", type = "both")) %>%
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0) {
       paste0("Installing WARMUPPKGS", trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
