3901658b70ad268007402b2ecb5197d1c75729e3
pat-s <patrick.schratz@gmail.com>
2018-10-25 17:07:22 +0200

    tests for generateFilterValuesData() and makefilterWrapper()

--- tests/testthat/test_featsel_FilterWrapper.R
+++ tests/testthat/test_featsel_FilterWrapper.R
@@ -37,20 +37,22 @@ test_that("Filterwrapper permutation.importance (issue #814)", {
 })
 
 
-test_that("FilterWrapper with ensemble function", {
-  lrn1 = makeLearner("classif.lda")
-  lrn2 = makeFilterWrapper(lrn1, fw.method = c("chi.squared", "information.gain"), fw.perc = 1)
+test_that("FilterWrapper with ensemble function in a train call", {
+  lrn = makeLearner("classif.lda")
+
+  # these combinations should fail
+  expect_error(makeFilterWrapper(lrn, fw.method = "gain.ratio",
+                 fw.basal.methods = c("gain.ratio", "information.gain")))
+
+  expect_error(makeFilterWrapper(lrn, fw.method = "E-min",
+                 fw.basal.methods = c("E-min", "information.gain")))
+
+  expect_error(makeFilterWrapper(lrn, fw.method = c("chi.squared", "information.gain")))
+
+  lrn2 = makeFilterWrapper(lrn, fw.method = "E-min",
+    fw.basal.methods = c("chi.squared", "information.gain"), fw.perc = 0.43)
 
   m = train(lrn2, binaryclass.task)
-  # expect_true(!inherits(m, "FailureModel"))
-  # expect_equal(m$features, getTaskFeatureNames(binaryclass.task))
-  # lrn2 = makeFilterWrapper(lrn1, fw.method = "chi.squared", fw.abs = 0L)
-  # m = train(lrn2, binaryclass.task)
-  # expect_equal(getLeafModel(m)$features, character(0))
-  # expect_true(inherits(getLeafModel(m)$learner.model, "NoFeaturesModel"))
-  # lrn2 = makeFilterWrapper(lrn1, fw.method = "chi.squared", fw.perc = 0.1)
-  # res = makeResampleDesc("CV", iters = 2)
-  # r = resample(lrn2, binaryclass.task, res)
-  # expect_true(!any(is.na(r$aggr)))
-  # expect_subset(r$extract[[1]][[1]], getTaskFeatureNames(binaryclass.task))
+
+  expect_class(m, c("FilterModel","BaseWrapperModel", "WrappedModel"))
 })
