045e42f8b5dbad282b4e3b3cc3dee8b282a27614
pat-s <patrick.schratz@gmail.com>
2018-11-08 17:20:18 +0100

    try pkgbuild::compile_dll()

--- tic.R
+++ tic.R
@@ -24,7 +24,8 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
   }
 
   get_stage("script") %>%
-    add_code_step(devtools::document(".")) %>%
+    add_code_step(pkgbuild::compile_dll()) %>%
+    add_code_step(devtools::document()) %>%
     add_step(step_rcmdcheck("--as-cran", warnings_are_errors = FALSE, notes_are_errors = FALSE))
 
   if (!Sys.getenv("TRAVIS_EVENT_TYPE") == "cron") {
@@ -33,7 +34,8 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
       add_step(step_setup_ssh())
 
     get_stage("deploy") %>%
-      add_code_step(devtools::document(".")) %>%
+      add_code_step(pkgbuild::compile_dll()) %>%
+      add_code_step(devtools::document()) %>%
       add_step(step_push_deploy(commit_paths = "man/"))
   }
 }
