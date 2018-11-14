cd81bbdaab3b46683fb4564f34f65ed564ff6d02
pat-s <patrick.schratz@gmail.com>
2018-11-14 17:05:06 +0100

    solve NS clash

--- tic.R
+++ tic.R
@@ -58,7 +58,8 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
 
     get_stage("deploy") %>%
       add_code_step(pkgbuild::compile_dll()) %>%
-      add_step(step_build_pkgdown())
+      add_code_step(devtools::document()) %>%
+      add_step(step_build_pkgdown(document = FALSE))
 
     # only deploy in master branch
     if (ci()$get_branch() == "master") {
