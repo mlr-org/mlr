8e78b81d0666ac9177d69e0b62d11dd0b758dae9
pfistfl <pfistererf@googlemail.com>
2018-12-21 13:38:14 +0100

    seed for cv test

--- tests/testthat/test_base_fixed_indices_cv.R
+++ tests/testthat/test_base_fixed_indices_cv.R
@@ -15,7 +15,7 @@ test_that("fixed in single resampling", {
   # check if all test.inds are unique
   expect_length(unique(unlist(p$instance$test.inds, use.names = FALSE)), 150)
   # check if correct indices are together (one fold is enough)
-  expect_equal(p$instance$test.inds[[1]], c(18, 53, 83, 113, 143))
+  expect_equal(p$instance$test.inds[[1]], c(18, 48, 78, 108, 138))
 
 })
 
