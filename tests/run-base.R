7b3b6587ab35742cf178a2962b25cce2e1de743c
Patrick Schratz <patrick.schratz@gmail.com>
2019-02-03 14:54:53 +0100

    Run checks on Appveyor (#2538)
    * appveyor tests
    
    * move GITHUB_PAT to web interface
    
    * Deploy from Travis build 13334 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/486870787
    Commit: 3ead3905d3a9b4c743b7aae1b234e111c0ca95ab
    
    * test
    
    * fix
    
    * small c
    
    * test
    
    * test
    
    * Deploy from Travis build 13352 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/487049835
    Commit: a332cdb639314c9de1dc1fd1e26785cbf04e0bb9
    
    * add pandoc cache
    
    * query sys.getenv()
    
    * run tests?
    
    * test
    
    * test
    
    * test
    
    * compile dll
    
    * enable tests on appveyor
    
    * fix test
    
    * hm
    
    * no multiarch
    
    * almost...
    
    * no cluster tests
    
    * no appveyor for cluster learners
    
    * set seed
    
    * no parallel checks again
    
    * fix
    
    * Deploy from Travis build 13396 [ci skip]
    
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/488091441
    Commit: cd4f961e65afaeb720ef5b42012fc6516e308137

--- tests/run-base.R
+++ tests/run-base.R
@@ -1,5 +1,5 @@
 library(testthat)
 # NOT_CRAN = true is set by devtools and is the default way introduced by testthat to use skip_on_cran().
-if (identical(Sys.getenv("TRAVIS"), "true") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true") || identical(Sys.getenv("NOT_CRAN"), "true")) {
+if (identical(Sys.getenv("TRAVIS"), "True") || identical(Sys.getenv("APPVEYOR"), "True") || identical(Sys.getenv("R_EXPENSIVE_TEST_OK"), "true") || identical(Sys.getenv("NOT_CRAN"), "true")) {
   test_check("mlr", filter = "base_")
 }
