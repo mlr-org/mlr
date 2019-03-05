1c601a2607bc34ceda5a46da535a912c627d99a8
pat-s <patrick.schratz@gmail.com>
2019-03-05 23:16:41 +0100

    document

--- man-roxygen/arg_keep_pred.R
+++ man-roxygen/arg_keep_pred.R
@@ -4,4 +4,10 @@
 #'   object size / mem usage, if you do not really need them.
 #'   In this case you can set this argument to `FALSE`.
 #'   Default is `TRUE`.
+#' @param keep.extract (`logical(1)`)\cr
+#'   Keep the `extract` slot of the result object.
+#'   When creating a lot of benchmark results with extensive tuning, the
+#'   resulting objects can become vary large in size.
+#'   Note that when you remove this slot, you will not be able to make
+#'   post analysis on the tuning effects.
 #' @md
