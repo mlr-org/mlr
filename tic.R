6330708a1bb0d5f8c3d0659e09d269c576697f65
pat-s <patrick.schratz@gmail.com>
2019-04-09 20:58:18 +0200

    .

--- tic.R
+++ tic.R
@@ -1,8 +1,8 @@
 if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
   get_stage("install") %>%
-    add_step(step_install_cran("stringi", type = "both")) %>%
-    add_step(step_install_cran("digest", type = "both")) %>%
+    add_step(step_install_cran("stringi")) %>%
+    add_step(step_install_cran("digest")) %>%
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0) {
       paste0("Installing WARMUPPKGS", trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])
@@ -13,7 +13,7 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
                                              "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
     get_stage("script") %>%
-      add_step(step_install_deps()) %>%
+      add_step(step_install_deps(repos = c(getOption("repos"), remotes::bioc_install_repos()))) %>%
       add_step(step_rcmdcheck("--as-cran", error_on = "error"))
 
   # only deploy in master branch
