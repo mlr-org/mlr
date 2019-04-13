fe2d3fbdbe347d92d503b723f02b94a64d88f0dc
pat-s <patrick.schratz@gmail.com>
2019-04-13 18:16:40 +0200

    reduce even more

--- tic.R
+++ tic.R
@@ -1,26 +1,20 @@
 if (ci_has_env("Check")) {
 
   get_stage("install") %>%
-    #add_step(step_install_cran("stringi")) %>%
     add_step(step_install_cran("digest")) %>%
     add_step(step_install_cran("pander")) %>% # for tutorial
-    add_step(step_install_cran("ggpubr")) %>% # for tutorial
-    add_step(step_install_deps(repos = c(getOption("repos"), remotes::bioc_install_repos())))
+    add_step(step_install_cran("ggpubr"))     # for tutorial
 
     get_stage("before_script") %>%
       add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
                                              "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
-    get_stage("script") %>%
-      add_step(step_install_deps(repos = c(getOption("repos"), remotes::bioc_install_repos()))) %>%
-      add_step(step_rcmdcheck("--as-cran", error_on = "error"))
+    # R CMD Check
+    do_package_checks(args = "--as-cran", error_on = "error",
+      repos = c(getOption("repos"), remotes::bioc_install_repos()))
 
-    get_stage("before_deploy") %>%
-      add_step(step_setup_ssh())
-
-    get_stage("deploy") %>%
-      add_step(step_build_pkgdown()) %>%
-      add_step(step_push_deploy(commit_paths = "docs/*"))
+    # pkgdown
+    do_pkgdown_site(commit_paths = "docs/*")
 
     # only deploy man files in in master branch
     if (ci_get_branch() == "master") {
