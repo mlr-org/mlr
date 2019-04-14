c70a5768d32171650ae9530b6b4ba906f219c805
pat-s <patrick.schratz@gmail.com>
2019-04-14 15:35:26 +0200

    no need for env =check anymore

--- tic.R
+++ tic.R
@@ -1,21 +1,18 @@
-if (ci_has_env("Check")) {
+get_stage("before_script") %>%
+  add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
+    "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
-    get_stage("before_script") %>%
-      add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                             "-install-package", "thirdparty/XMeans1.0.4.zip")))
+# R CMD Check
+do_package_checks(args = "--as-cran", error_on = "error",
+  repos = c(getOption("repos"), remotes::bioc_install_repos()))
 
-    # R CMD Check
-    do_package_checks(args = "--as-cran", error_on = "error",
-      repos = c(getOption("repos"), remotes::bioc_install_repos()))
+# pkgdown
+do_pkgdown(commit_paths = "docs/*")
 
-    # pkgdown
-    do_pkgdown(commit_paths = "docs/*")
+# only deploy man files in in master branch
+if (ci_get_branch() == "master") {
 
-    # only deploy man files in in master branch
-    if (ci_get_branch() == "master") {
-
-      get_stage("deploy") %>%
-        add_code_step(devtools::document()) %>%
-        add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
-    }
+  get_stage("deploy") %>%
+    add_code_step(devtools::document()) %>%
+    add_step(step_push_deploy(commit_paths = c("man/", "DESCRIPTION", "NAMESPACE")))
 }
