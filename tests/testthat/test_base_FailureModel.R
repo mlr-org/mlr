b8944ab64c1caaac0c73a81441bcf06893f1021a
pat-s <patrick.schratz@gmail.com>
2019-05-07 10:55:20 +0200

    fix test_base_failureModel.R

--- tests/testthat/test_base_FailureModel.R
+++ tests/testthat/test_base_FailureModel.R
@@ -21,7 +21,7 @@ test_that("FailureModel", {
   p = predict(m, newdata = iris)
   expect_true(all(is.na(p$data$response)))
   prob = getPredictionProbabilities(p)
-  expect_true(dim(prob) == c(150, 3))
+  expect_true(all(dim(prob) == c(150, 3)))
   expect_true(all(is.na(prob)))
 
   task = dropFeatures(regr.task, "chas")
