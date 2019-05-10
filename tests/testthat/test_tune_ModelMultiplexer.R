422ce898935e368dd127015ed34299587e914738
pat-s <patrick.schratz@gmail.com>
2019-05-10 13:54:38 +0200

    fix coercion error

--- tests/testthat/test_tune_ModelMultiplexer.R
+++ tests/testthat/test_tune_ModelMultiplexer.R
@@ -109,7 +109,7 @@ test_that("ModelMultiplexer tuning", {
   res = tuneParams(lrn, task, rdesc, par.set = tune.ps, control = ctrl)
   expect_true(setequal(class(res), c("TuneResult", "OptResult")))
   y = getOptPathY(res$opt.path)
-  expect_true(!is.na(y) && is.finite(y))
+  expect_true(all(!is.na(y) && is.finite(y)))
 })
 
 # we had bug here, see issue #609
