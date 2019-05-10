09c0662656e84e44605235a6810065455e383e99
pat-s <patrick.schratz@gmail.com>
2019-05-10 16:31:29 +0200

    run only tests in devel and oldrel

--- tic.R
+++ tic.R
@@ -9,10 +9,12 @@ do_package_checks(args = "--as-cran", error_on = "error",
   repos = c(getOption("repos"), remotes::bioc_install_repos()))
 
 # pkgdown
-do_pkgdown(commit_paths = "docs/*", document = FALSE)
+if (ci_is_env("FULL", "true")) {
+  do_pkgdown(commit_paths = "docs/*", document = FALSE)
+}
 
 # only deploy man files in in master branch
-if (ci_get_branch() == "master") {
+if (ci_get_branch() == "master" && ci_is_env("FULL", "true")) {
 
   get_stage("deploy") %>%
     add_code_step(pkgbuild::compile_dll()) %>%
