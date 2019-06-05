0d24ac62f818fc1e12fcc5565db209c171b9d7b5
pat-s <patrick.schratz@gmail.com>
2019-06-05 15:24:27 +0200

    more keep.pred = TRUE reverts

--- man-roxygen/arg_keep_pred.R
+++ man-roxygen/arg_keep_pred.R
@@ -2,7 +2,7 @@
 #'   Keep the prediction data in the `pred` slot of the result object.
 #'   If you do many experiments (on larger data sets) these objects might unnecessarily increase
 #'   object size / mem usage, if you do not really need them.
-#'   This is why the default is set to `FALSE`.
+#'   The default is set to `TRUE`.
 #' @param keep.extract (`logical(1)`)\cr
 #'   Keep the `extract` slot of the result object. When creating a lot of
 #'   benchmark results with extensive tuning, the resulting R objects can become
