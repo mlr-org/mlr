78c61a5dbd9d6e7d1cb9ab77b011c4b4189d79e7
Jakob Richter <code@jakob-r.de>
2019-06-11 13:32:34 +0200

    add comment for ctree

--- R/RLearner_regr_cforest.R
+++ R/RLearner_regr_cforest.R
@@ -40,6 +40,8 @@ trainLearner.regr.cforest = function(.learner, .task, .subset, .weights = NULL,
 
   f = getTaskFormula(.task)
   d = getTaskData(.task, .subset)
+
+  # default handling necessary because the default of controls is `cforest_unbiased()` which does not allow all parameters (e.g. replace)
   defaults = getDefaults(getParamSet(.learner))
   if (missing(teststat)) teststat = defaults$teststat
   if (missing(testtype)) testtype = defaults$testtype
@@ -50,6 +52,7 @@ trainLearner.regr.cforest = function(.learner, .task, .subset, .weights = NULL,
     trace, teststat, testtype, mincriterion,
     minsplit, minbucket, stump,
     nresample, maxsurrogate, maxdepth, savesplitstats)
+
   party::cforest(f, data = d, controls = ctrl, weights = .weights, ...)
 }
 
