9198a246ff0a1df245f4bdf7da18af6e358d4e59
pat-s <patrick.schratz@gmail.com>
2019-10-07 17:52:34 +0200

    try setting path arg explicitly

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -4,7 +4,7 @@ if (Sys.getenv("TRAVIS") == "true") {
     library("lintr")
     library("rex")
     # linters are defined in help_lint.R
-    expect_lint_free(linters = linters)
+    expect_lint_free(path = ".", linters = linters)
   })
 } else {
   warning(paste("lintr test was disabled because of missing lintr.",
