1b4000f3db684d1dbab85c5fe779ff89a0f7fc4d
pat-s <patrick.schratz@gmail.com>
2019-10-09 11:09:10 +0200

    restrict lintr exec

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -1,5 +1,9 @@
 context("lint")
-if (Sys.getenv("TRAVIS") == "true") {
+# run only on
+# - PR
+# - R-release
+# - Travis
+if (Sys.getenv("TRAVIS") == "true" && Sys.getenv("TRAVIS_PULL_REQUEST") != "false" && Sys.getenv("TRAVIS_R_VERSION_STRING") == "release") {
   test_that("lint check", {
     library("lintr")
     library("rex")
