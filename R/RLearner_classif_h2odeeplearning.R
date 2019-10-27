2d3215a88e2985cb261f21bc85d2dd63a4adc9c1
Alberson Miranda <45690517+albersonmiranda@users.noreply.github.com>
2019-10-27 14:00:43 -0300

    Update RLearner_classif_h2odeeplearning.R
    Added params.

--- R/RLearner_classif_h2odeeplearning.R
+++ R/RLearner_classif_h2odeeplearning.R
@@ -217,8 +217,15 @@ makeRLearner.classif.h2o.deeplearning = function() {
       makeLogicalLearnerParam("average_activation", tunable = FALSE),
       # makeLogicalLearnerParam("sparsity_beta", tunable = FALSE),
       makeLogicalLearnerParam("reproducible", default = FALSE, tunable = FALSE),
-      makeLogicalLearnerParam("export_weights_and_biases", default = FALSE, tunable = FALSE)
-    ),
+      makeLogicalLearnerParam("export_weights_and_biases", default = FALSE, tunable = FALSE),
+      makeNumericLearnerParam("max_runtime_secs", default = 0, lower = 0),
+      makeNumericLearnerParam("stopping_rounds", default = 5, lower = 0),
+      makeDiscreteLearnerParam("stopping_metric", values=c("AUTO", "deviance", "logloss", "MSE", "RMSE", "MAE", "RMSLE", "AUC",
+                                                           "lift_top_group", "misclassification", "AUCPR", "mean_per_class_error",
+                                                           "custom", "custom_increasing"),
+                               default = "AUTO"),
+      makeNumericLearnerParam("stopping_tolerance", default = 0, lower = 0)
+      ),
     properties = c("twoclass", "multiclass", "numerics", "factors", "prob", "weights", "missings", "featimp"),
     name = "h2o.deeplearning",
     short.name = "h2o.dl",
