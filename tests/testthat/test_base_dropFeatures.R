3e2af6a098cc0ed544d10df85e6d992a076df119
pat-s <patrick.schratz@gmail.com>
2019-11-02 00:04:36 +0100

    test parallel

--- tests/testthat/test_base_dropFeatures.R
+++
@@ -1,7 +0,0 @@
-context("dropFeatures")
-
-test_that("dropFeatures", {
-  fns = getTaskFeatureNames(multiclass.task)
-  task2 = dropFeatures(multiclass.task, fns[1])
-  expect_equal(length(getTaskFeatureNames(task2)), 3L)
-})
--- tests/testthat/test_base_resample_weights.R
+++
@@ -1,7 +0,0 @@
-context("resample_weights")
-
-test_that("resample works with weights", {
-  task = makeClassifTask(data = iris, target = "Species", weights = as.integer(iris$Species))
-  res = resample(task = task, learner = "classif.rpart", resampling = makeResampleDesc("CV", iters = 2))
-  expect_is(res$pred, "ResamplePrediction")
-})
