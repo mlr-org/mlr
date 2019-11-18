a8ff38bf470004e9346c8ce06bdda25fd4133c1e
pfistfl <pfistererf@googlemail.com>
2019-11-18 17:41:01 +0100

    Update FDboost and classifunc

--- R/RLearner_classif_FDboost.R
+++ R/RLearner_classif_FDboost.R
@@ -8,11 +8,9 @@ makeRLearner.classif.FDboost = function() {
       makeIntegerLearnerParam(id = "mstop", default = 100L, lower = 1L),
       makeNumericLearnerParam(id = "nu", default = 0.1, lower = 0, upper = 1), # the learning rate
       makeUntypedLearnerParam(id = "custom.family.definition", requires = quote(family == "custom.family")), # list of parameters for the custom family
-      # makeDiscreteLearnerParam(id = "risk", values = c("inbag", "oobag", "none")), we don't need this in FDboost
       makeDiscreteLearnerParam(id = "Binomial.link", default = "logit",
         values = c("logit", "probit"), requires = quote(family == "Binomial")),
       makeNumericLearnerParam(id = "df", default = 4, lower = 0.5), # effective degrees of freedom, depend on the regularization parameter of the penality matrix and number of splines, must be the same for all base learners(covariates), the maximum value is the rank of the design matrix
-      # makeDiscreteLearnerParam(id = "baselearner", values = c("bbs", "bols")),  # we don't use "btree" in FDboost
       makeIntegerLearnerParam(id = "knots", default = 10L, lower = 1L), # determine the number of knots of splines, does not matter once there is sufficient number of knots, 30,40, 50 for example
       makeIntegerLearnerParam(id = "degree", default = 3L, lower = 1L), # degree of the b-spline
       makeIntegerLearnerParam(id = "differences", default = 1L, lower = 1L), # degree of the penalty
@@ -23,21 +21,32 @@ makeRLearner.classif.FDboost = function() {
     properties = c("functionals", "numerics", "twoclass", "prob"),
     name = "Functional linear array classification boosting",
     short.name = "FDboost",
-    note = "Only allow one base learner for functional covariate and one base learner for scalar covariate, the parameters for these base learners are the same. Also we currently do not support interaction between scalar covariates"
+    note = "Uses only one base learner per functional or scalar covariate. 
+      Uses the same hyperparameters for every baselearner.
+      Currently does not support interaction between scalar covariates.
+      Default for family has been set to 'Binomlial', as 'Gaussian' is not applicable."
   )
 }
 
 #' @export
-trainLearner.classif.FDboost = function(.learner, .task, .subset, .weights = NULL, mstop = 100L, knots = 10L, df = 4L, bsignal.check.ident = FALSE, degree = 3L, differences = 1L, Binomial.link = "logit", nu = 0.1, family = "Binomial", custom.family.definition = NULL, nuirange = c(0, 100), d = NULL, ...) {
+trainLearner.classif.FDboost = function(.learner, .task, .subset, .weights = NULL,
+  mstop = 100L, knots = 10L, df = 4L, bsignal.check.ident = FALSE, degree = 3L, differences = 1L, 
+  Binomial.link = "logit", nu = 0.1, family = "Binomial", custom.family.definition = NULL,
+  d = NULL, ...) {
+
   family = switch(family,
     Binomial = mboost::Binomial(link = Binomial.link),
     AdaExp = mboost::AdaExp(),
     AUC = mboost::AUC(),
-    # PropOdds = mboost::PropOdds(nuirange = nuirange, offrange = offrange),
     custom.family = custom.family.definition)
+
+  # Note: Family PropOdds applicable only for ordinal classification:
+  # PropOdds = mboost::PropOdds(nuirange = nuirange, offrange = offrange)
+  
   ctrl = learnerArgsToControl(mboost::boost_control, mstop, nu)
 
-  hh = getFDboostFormulaMat(.task, knots = knots, df = df, bsignal.check.ident = bsignal.check.ident, degree = degree, differences = differences)
+  hh = getFDboostFormulaMat(.task, knots = knots, df = df, bsignal.check.ident = bsignal.check.ident,
+    degree = degree, differences = differences)
   if (.learner$predict.type == "prob") {
     td = getTaskDesc(.task)
     levs = c(td$negative, td$positive)
@@ -50,7 +59,7 @@ trainLearner.classif.FDboost = function(.learner, .task, .subset, .weights = NUL
 #' @export
 predictLearner.classif.FDboost = function(.learner, .model, .newdata, ...) {
   type = ifelse(.learner$predict.type == "response", "class", "response") # additional parameters passed to mboost::predict()
-  # in mboost, "response" returns probabilities and "class" returns the predicted class
+
   p = predict(.model$learner.model, newdata = as.list(.newdata), type = type, ...)
 
   if (.learner$predict.type == "prob") {
@@ -58,10 +67,7 @@ predictLearner.classif.FDboost = function(.learner, .model, .newdata, ...) {
       stopf("The selected family %s does not support probabilities", getHyperPars(.learner)$family)
     } else {
       td = .model$task.desc
-      # one observation prediction + family PropOdds returns a numeric vector instead of matrix
-      # FIXME: add/change the outcommented line below to enable predicting one obs
-      # (caution: check whether the right class is assigned)
-      # if (nrow(.newdata) == 1 && is.vector(p)) dim(p) = c(1,2)
+      if (nrow(.newdata) == 1 && is.vector(p)) browser(); dim(p) = c(1,2)
       if (!is.null(dim(p))) {
         p = p[, 1L]
       }
--- R/RLearner_classif_classiFunc.knn.R
+++ R/RLearner_classif_classiFunc.knn.R
@@ -8,11 +8,11 @@ makeRLearner.classif.classiFunc.knn = function() {
         lower = 1L,
         # upper = expression(0.5 * nrow(getTaskData(task))),
         default = 1L),
-      # FIXME: is it ok to hand over the values with this function
-      # from the original package?
+      # Using metricChoices since there are so many options and this 
+      # keeps stuff flexible.
       makeDiscreteLearnerParam(id = "metric", default = "Euclidean",
         values = classiFunc::metricChoices()),
-      makeIntegerLearnerParam(id = "nderiv", default = 0L, lower = 0L),
+      makeIntegerLearnerParam(id = "q?", default = 0L, lower = 0L),
       makeLogicalLearnerParam(id = "derived", default = FALSE, tunable = FALSE),
       makeDiscreteLearnerParam(id = "deriv.method", default = "base.diff",
         values = c("base.diff", "fda.deriv.fd")),
@@ -22,7 +22,6 @@ makeRLearner.classif.classiFunc.knn = function() {
         },
         requires = quote(metric == "custom.metric"),
         tunable = FALSE),
-      # additional arguments to computeDistMat
       makeNumericLearnerParam(id = "dmin", default = 0,
         lower = 0, upper = 1),
       makeNumericLearnerParam(id = "dmax", default = 1,
@@ -41,11 +40,13 @@ makeRLearner.classif.classiFunc.knn = function() {
         lower = 0, upper = 1),
       makeNumericVectorLearnerParam(id = ".poi", # default = expression(seq(0, 1, ncol(task))),
         lower = 0, upper = 1),
-      # additional arguments to metrics in computeDistMat
       makeNumericLearnerParam(id = "p", default = 2),
-      makeNumericLearnerParam(id = "dtwwindow", lower = 0, upper = 1, requires = quote(metric == "rucrdtw")),
-      # TODO additional arguments to Data2fd
-      # TODO additional arguments to custom metric
+      makeNumericLearnerParam(id = "dtwwindow", lower = 0, upper = 1, requires = quote(metric == "rucrdtw"),
+      makeNumericLearnerParam(id = "lambda", lower = 0, default = 0),
+      makeDiscreteLearnerParam(id = "method", default = "chol", values = c("chol", "qr")),
+      makeNumericLearnerParam(id = "dfscale", lower = 0, upper = Inf, default = 1),
+      makeUntypedLearnerParam(id = "custom.metric.args", default = list())
+
       keys = "task",
       forbidden = expression(dmin >= dmax,
         dmin1 >= dmax1 |
@@ -53,11 +54,10 @@ makeRLearner.classif.classiFunc.knn = function() {
           dmin2 >= dmax2,
         knn %% 2 == 0)
     ),
-    # par.vals = list(metric = "Euclidean", knn = 1L),
     properties = c("twoclass", "multiclass", "prob", "single.functional"),
     name = "classiFunc.knn",
     short.name = "classiFunc.knn",
-    note = ""
+    note = "Default for lambda (Data2fd parameter) is 3e-8/diff(as.numeric(range(argvals)))."
   )
 }
 
@@ -68,6 +68,10 @@ trainLearner.classif.classiFunc.knn = function(.learner, .task, .subset, ...) {
   fd = getFunctionalFeatures(d$data)
 
   args = learnerArgsToControl("list", ...)
+  if (length(args$custom.metric.args) > 0) {
+    args = c(args, args$custom.metric.args)
+    args$custom.metric.args = NULL
+  }
   do.call(classiFunc::classiKnn, c(list(classes = d$target, fdata = fd), args))
 }
 
