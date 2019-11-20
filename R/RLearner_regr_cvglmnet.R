b04b0530a6a18d311cb22b123262e3404ca37a43
pat-s <patrick.schratz@gmail.com>
2019-11-20 14:56:06 +0100

    upd glmnet learners

--- R/RLearner_regr_cvglmnet.R
+++ R/RLearner_regr_cvglmnet.R
@@ -35,7 +35,7 @@ makeRLearner.regr.cvglmnet = function() {
       makeUntypedLearnerParam(id = "offset", default = NULL),
       makeDiscreteLearnerParam(id = "type.gaussian", values = c("covariance", "naive"), requires = quote(family == "gaussian")),
       makeNumericVectorLearnerParam(id = "gamma", lower = 0, upper = 1, requires = quote(relax == TRUE)),
-      makeLogicalLearnerParam("relax", default = FALSE)
+      makeLogicalLearnerParam("relax", default = FALSE)a
     ),
     properties = c("numerics", "factors", "weights"),
     name = "GLM with Lasso or Elasticnet Regularization (Cross Validated Lambda)",
