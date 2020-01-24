7687cfcb78366fa5f9816c737e2429b483b86e2f
pat-s <patrick.schratz@gmail.com>
2020-01-24 18:38:58 +0100

    only 64 bit windows checks

--- tic.R
+++ tic.R
@@ -4,10 +4,10 @@ get_stage("install") %>%
 
 get_stage("script") %>%
   add_code_step(RWeka::WPM("refresh-cache")) %>%
-  add_code_step(RWeka::WPM('install-package', 'XMeans'))
+  add_code_step(RWeka::WPM("install-package", "XMeans"))
 
 # R CMD Check
-do_package_checks(args = "--as-cran", error_on = "error", codecov = FALSE)
+do_package_checks(args = "--as-cran --no-multiarch", error_on = "error", codecov = FALSE)
 
 # pkgdown
 if (ci_is_env("FULL", "true")) {
