67ebe31e3608ded3c4cbdc970b11f150a32f56ff
pat-s <patrick.schratz@gmail.com>
2020-01-26 00:56:01 +0100

    tests

--- tic.R
+++ tic.R
@@ -7,7 +7,7 @@ get_stage("script") %>%
   add_code_step(RWeka::WPM("install-package", "XMeans"))
 
 # R CMD Check
-do_package_checks(args = c("--as-cran", "--no-multiarch"),
+do_package_checks(args = c("--as-cran", "--no-multiarch", "--no-manual"),
   error_on = "error", codecov = FALSE)
 
 # pkgdown
