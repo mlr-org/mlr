c950f12f0bd8b3b81c93bcab9be124297c052556
pat-s <patrick.schratz@gmail.com>
2020-02-18 09:25:07 +0100

    skip test which hangs on cran

--- tests/testthat/test_featsel_generateFilterValuesData.R
+++ tests/testthat/test_featsel_generateFilterValuesData.R
@@ -154,6 +154,8 @@ test_that("filter method 'variance' works with missing values", {
 })
 
 test_that("ensemble methods work", {
+  skip_on_cran()
+
   fi = generateFilterValuesData(multiclass.task,
     method = list("E-min", c("FSelectorRcpp_gain.ratio",
       "FSelectorRcpp_information.gain")))
