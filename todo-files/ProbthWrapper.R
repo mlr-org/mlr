b5d6e3cac9e8488dbeb58673a417082960a5a29b
pat-s <patrick.schratz@gmail.com>
2021-12-22 08:31:13 +0100

    style

--- todo-files/ProbthWrapper.R
+++ todo-files/ProbthWrapper.R
@@ -5,35 +5,38 @@ setClass(
 
 
 #' Fuses a classifier with thresholding. Creates a learner object, which can be
-#' used like any other learner object, but which produces discrete class labels 
+#' used like any other learner object, but which produces discrete class labels
 #' from probailities of teh base learner according to thresholds.
-#' These thresholds are additional hyperparameters of the new learner and 
-#' can therefore be tuned. 
-#' 
-#' See \code{\link{setThreshold}} for details of thresholding.  
+#' These thresholds are additional hyperparameters of the new learner and
+#' can therefore be tuned.
 #'
-#' @param learner [\code{\linkS4class{Learner}}]\cr 
-#'   Learning algorithm. See \code{\link{learners}}.  
+#' See \code{\link{setThreshold}} for details of thresholding.
+#'
+#' @param learner [\code{\linkS4class{Learner}}]\cr
+#'   Learning algorithm. See \code{\link{learners}}.
 #' @param classes [character] \cr
 #'   Classes of future classification task.
-#' 
+#'
 #' @return \code{\linkS4class{Learner}}.
-#' 
+#'
 #' @title Fuse learner with probability thresholding.
 #' @export
 
 makeProbthWrapper = function(learner, classes) {
-  if (is.character(learner))
+  if (is.character(learner)) {
     learner = makeLearner(learner)
-  if (learner@properties[["type"]] != "classif")
+  }
+  if (learner@properties[["type"]] != "classif") {
     stop("Only classifiers can be used as base learners!")
-  if (learner@predict.type != "prob")
+  }
+  if (learner@predict.type != "prob") {
     stop("The predict.type of the base learner must be 'prob'!")
+  }
   a = as.list(rep(0.5, length(classes)))
-  names(a) = paste("probth", classes, sep=".")
-  ps = do.call(makeParamSet, 
-    lapply(names(a), function(x) makeNumericLearnerParam(id=x, lower=0, upper=1)))
-  w = new("ProbthWrapper", learner=learner, par.set=ps, par.vals=a)
+  names(a) = paste("probth", classes, sep = ".")
+  ps = do.call(makeParamSet,
+    lapply(names(a), function(x) makeNumericLearnerParam(id = x, lower = 0, upper = 1)))
+  w = new("ProbthWrapper", learner = learner, par.set = ps, par.vals = a)
   w@properties["prob"] = FALSE
   setPredictType(w, "response")
 }
@@ -42,16 +45,16 @@ makeProbthWrapper = function(learner, classes) {
 setMethod(
   f = "predictLearner",
   signature = signature(
-    .learner = "ProbthWrapper", 
-    .model = "WrappedModel", 
+    .learner = "ProbthWrapper",
+    .model = "WrappedModel",
     .newdata = "data.frame"
   ),
-  
+
   def = function(.learner, .model, .newdata, ...) {
     p = predictLearner(.learner@learner, .model, .newdata)
     ths = unlist(.learner@par.vals)
-    # remove "probth"    
+    # remove "probth"
     names(ths) = sapply(strsplit(names(ths), "\\."), function(x) x[2])
-    setThreshold(p, threshold=ths)
+    setThreshold(p, threshold = ths)
   }
-) 
+)
